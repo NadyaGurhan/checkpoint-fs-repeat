4366a5ef7c1a61d9f0ccfee2462027a9dfa8c5b9
Stanislav Grishin <stanislav.v.grishin@gmail.com>
2020-11-05 09:59:23 +0300

    Almost (without the last

--- padawans.js
+++ padawans.js
@@ -1,7 +1,33 @@
-
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+
+const fs = require('fs');
+
+function getPadawanNames() {
+  let padawansNames = fs
+    .readFileSync('./data/padawans.txt', 'utf-8')
+    .split('\r\n');
+  return padawansNames.filter((element) => element.length > 0);
+}
+// console.log(getPadawanNames());
+
+function getLightsaberScores(){
+  let padawansScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n')
+  return padawansScores.map((element)=>+element)
+}
+// console.log(getLightsaberScores())
+function getStats(){
+  let padawansNames = getPadawanNames()
+  let padawansScores = getLightsaberScores()
+  let rezStat = []
+  for (let i = 0; i < padawansNames.length; i++) {
+    rezStat.push([padawansNames[i],padawansScores[i]])
+  }
+  fs.writeFileSync('./data/stats.txt', rezStat.join(','))
+  return rezStat
+}
+// console.log(getStat());
