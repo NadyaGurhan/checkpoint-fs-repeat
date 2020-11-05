38addf94f98ab414a2c8c1fba5fdb4a729fa0026
Victor Reznik <vict0r.frost@yandex.ru>
2020-11-05 08:59:06 +0200

    2/4

--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  return names;
+}
+function getLightsaberScores() {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n');
+  scores = scores.map( (el) => el * 1);
+  return scores;
+}
+function getStats() {
+  
+}
+function writeStats() {
+  for (let i = 0; i < arr.length; i++) {
+    fs.writeFileSync('data/stats.txt', arr[i]);
+  }
+}
+console.log(getLightsaberScores())
+
 
 module.exports = {
   getPadawanNames,
