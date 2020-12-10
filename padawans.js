90367c48ce5914a1bf430eb2e4e18915a148a5a7
Kuzzarvi <Kellthuzard@gmail.com>
2020-12-10 13:03:58 +0300

    Release0: done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,31 @@
+const fs = require('fs');
+function getPadawanNames() {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  names.pop();
+  return names;
+}
+
+function getLightsaberScores() {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  scores = scores.split('\n');
+  return scores.map(el => Number(el));
+}
+
+function getStats() {
+  let names = getPadawanNames();
+  let scores = getLightsaberScores();
+  let stats = [];
+  for (let i = 0; i < names.length; i++) {
+    stats.push([names[i], scores[i]]);
+  }
+  return stats;
+}
+
+function writeStats() {
+  let stats = getStats();
+  stats = stats.map(el => el.join(' '));
+  fs.appendFileSync('./data/stats.txt', stats.join('\n'));
+}
 
 module.exports = {
   getPadawanNames,
