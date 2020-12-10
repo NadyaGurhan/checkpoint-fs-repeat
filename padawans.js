51326d6095283772457eb503b3daaa5ae01d4cbb
Ivan Paponin <eldkirk@gmail.com>
2020-12-10 17:39:46 +0300

    check point fix 2

--- padawans.js
+++ padawans.js
@@ -1,44 +1,52 @@
 const fs = require('fs');
 
-  function getPadawanNames() {
-    const names = fs.readFileSync('data/padawans.txt', 'utf-8').trim().split("\n");
-    return names;
-  }
+function getPadawanNames() {
+  return fs.readFileSync('data/padawans.txt', 'utf-8').trim().split("\n");
+}
 
-  function getLightsaberScores() {
-    const scores = fs.readFileSync('data/scores.txt', 'utf-8').split("\n").map((el) => +el);
-    return scores;
-  }
+function getLightsaberScores() {
+  return fs.readFileSync('data/scores.txt', 'utf-8').split("\n").map((el) => Number(el));
+}
 
-  function getStats() {
-    const padawans = getPadawanNames();
-    const padawansScores = getLightsaberScores();
+function getStats() {
+  const padawans = getPadawanNames();
+  const padawansScores = getLightsaberScores();
 
-    data = [];
-    for (let i = 0; i < padawans.length; i++) {
-      data.push([padawans[i], padawansScores[i]]);
-    }
+  // data = [];
+  // for (let i = 0; i < padawans.length; i++) {
+  //   data.push([padawans[i], padawansScores[i]]);
+  // }
 
-    return data;
-  }
+  return padawans
+    .reduce((acc, item, index) => [...acc, [item, padawansScores[index]]], [])
+}
 
-  function writeStats() {
-    const dataToSend = getStats();
-    let str = '';
-    dataToSend.forEach((a) => (str += `${a[0]} ${a[1]}\n`));
-    fs.writeFileSync('data/stats.txt', str.trim());
-    // return dataToSend;
-  }
+function writeStats() {
+  const dataToSend = getStats();
+  // let str = '';
+  // dataToSend.forEach((a) => (str += `${a[0]} ${a[1]}\n`));
+  // fs.writeFileSync('data/stats.txt', str.trim());
+  // return dataToSend;
+  let convertedData = dataToSend.reduce((acc, item) => `${acc}${item[0]} ${item[1]}\n`, '')
 
-
-
-
-
-const a = writeStats();
-// console.log(a);
+  fs.writeFileSync('data/stats.txt', convertedData.trim());
+}
 
 
 
+
+
+writeStats()
+// const test = [1, [2], [3,4,5,6], 4, 5, 6, 7, 8, 9];
+
+// console.log(test);
+// console.log([22, ...test, 300, 500, 77]);
+
+// return padawans.reduce((acc, item, index) => {
+//   console.log(acc, item);
+
+//   return [...acc, [item, padawansScores[index]]]
+// }, [])
 
 
 
