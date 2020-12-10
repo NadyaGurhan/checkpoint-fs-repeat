08d174dfc0abd2983be72a5a935b25099cb4a48b
Anastasia <anastas1a982@icloud.com>
2020-12-10 10:04:35 +0300

    Releases

--- padawans.js
+++ padawans.js
@@ -1,3 +1,47 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8').trim().split('\n');
+  return data;
+}
+//console.log(getPadawanNames());
+
+
+
+
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf-8').split('\n').map((el) => +el);
+  return data;
+}
+//console.log(getLightsaberScores());
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const score = getLightsaberScores();
+
+  const data = [];
+  for (let i = 0; i < padawans.length; i++) {
+    data.push([padawans[i], score[i]]);
+  }
+
+  return data;
+}
+
+//console.log(getStats());
+
+
+function writeStats() {
+  const data = getStats();
+  let str = '';
+  data.forEach((a) => (str += `${a[0]} ${a[1]}\n`));
+  fs.writeFileSync('data/stats.txt', str.slice(0, str.length - 1));
+  
+}
+
+
+
+
 
 module.exports = {
   getPadawanNames,
