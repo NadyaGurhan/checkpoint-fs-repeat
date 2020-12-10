04829a3ae302651894bead569f78f281eba99448
Alexander Blinkov <tingol88@yandex.ru>
2020-12-10 10:02:17 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,36 @@
+const fs = require('fs');
+function getPadawanNames() {
+  let dataFromFile = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  dataFromFile = dataFromFile.split('\n');
+  dataFromFile.pop();
+  return dataFromFile;
+
+}
+
+function getLightsaberScores() {
+  let scoreFromFile = fs.readFileSync('./data/scores.txt', 'utf-8');
+  scoreFromFile = scoreFromFile.split('\n').map((el) => +el);
+  return scoreFromFile;
+}
+
+function getStats() {
+  let padawansString = getPadawanNames();
+  let scoresString = getLightsaberScores();
+  let res = padawansString.map((el, i) => el = [el, scoresString[i]])
+  return res;
+}
+
+function writeStats (){
+  let res = getStats();
+
+  fs.writeFileSync('./data/stats.txt', res.join('\n').replace(/,/g, ' '))
+}
+
+// getPadawanNames();
+// getLightsaberScores();
+
+
+// console.log(getPadawanNames(), getLightsaberScores(), getStats());
 
 module.exports = {
   getPadawanNames,
