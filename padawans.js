c6cb4a8166cdae6860ab40430ac196a0daa75f26
Alexandrtsukanov <alex217tsukanov@mail.ru>
2020-12-10 09:38:01 +0300

    All donegit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -A

--- padawans.js
+++ padawans.js
@@ -1,3 +1,4 @@
+const fs = require ('fs')
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +6,33 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  names.pop();
+  return names
+}
+
+function getLightsaberScores() {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map(el => Number(el));
+  return scores
+}
+
+function getStats() {
+  let stats = [];
+  let padawans = getPadawanNames();
+  let scores = getLightsaberScores();
+  for (let i = 0; i < padawans.length; i += 1) {
+    stats.push([padawans[i], scores[i]])
+  }
+  return stats
+}
+
+function writeStats() {
+  let stats = getStats();
+  let statsNew = stats.map(el => el.join(' ')).join('\n');
+  fs.writeFileSync('./data/stats.txt', statsNew);
+  return statsNew
+}
+
+console.log(writeStats())
