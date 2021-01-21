769c6f4125278e40d134440330ad21578e9bb273
PAV <popov.pav.aleksei@gmail.com>
2021-01-21 10:01:54 +0300

    test 1, 2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+function getPadawanNames() {
+    let padawansInfo = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').slice(0, -1);
+    return padawansInfo;
+};
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf8').split('\n').filter((el) => el.length).map((el) => Number(el));
+}
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
