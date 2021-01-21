96a4cd2f429194f9671a75769d938fdc2465a5ed
Арман Гаспарян <buscus111@mail.ru>
2021-01-21 09:57:09 +0300

    release 0: all tests passed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,42 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs
+    .readFileSync('./data/padawans.txt', 'utf-8')
+    .split('\n')
+    .filter((el) => el.length > 0);
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync('./data/scores.txt', 'utf-8')
+    .split('\n')
+    .filter((el) => el.length > 0)
+    .map((el) => parseFloat(el));
+}
+function getStats() {
+  const padawans = fs
+    .readFileSync('./data/padawans.txt', 'utf-8')
+    .split('\n')
+    .filter((el) => el.length > 0);
+
+  const lightSaber = fs
+    .readFileSync('./data/scores.txt', 'utf-8')
+    .split('\n')
+    .filter((el) => el.length > 0)
+    .map((el) => parseFloat(el));
+
+  const hash = {};
+  for (let i = 0; i < padawans.length; i++) {
+    hash[padawans[i]] = lightSaber[i];
+  }
+  // console.log(Object.entries(hash).join('\n').replace(/\,/g, ' '));
+  return Object.entries(hash);
+}
+function writeStats(getStatistic) {
+  const str = getStatistic.join('\n').replace(/\,/g, ' ');
+  return fs.writeFileSync('./data/stats.txt', str, 'utf-8');
+}
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +44,5 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+console.log(getStats());
