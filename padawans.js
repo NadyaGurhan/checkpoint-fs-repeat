3ef77109638cf087af3630c70eb2801717f3ee69
Alex <hafbek@gmail.com>
2021-02-25 10:10:26 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,32 @@
+const fs = require('fs');
+
+const listOfPadawans = './data/padawans.txt';
+const scoresOfPadawans = './data/scores.txt';
+
+
+function getPadawanNames () {
+  return fs.readFileSync(listOfPadawans, "utf8")
+  .trim()
+  .split('/n')
+  }
+
+function getLightsaberScores () {
+  return fs.readFileSync(scoresOfPadawans, "utf8")
+  .trim()
+  .split('/n')
+  .map(element => Number(element));
+  }
+
+function getStats () {
+
+}
+
+function writeStats () {
+
+}
+
+
+
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +34,33 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+// const padawan = new Padawans();
+// console.log(padawan.getPadawanNames(padawans.txt));
+// const people = parser.parse(fileName);
+// parser.write("text.csv", people);
+
+// class PerfectParser {
+//   parse(fileName) {
+//     const dataFromFile = fs.readFileSync(fileName, "utf8");
+//     const arrayFromFile = dataFromFile
+//       .split("\n")
+//       .map((element) => element.split(","))
+//       .slice(1, -1)
+//       .map((element) => new Person(...element));
+//     return arrayFromFile;
+//     // console.log('-----> 14', arrayFromFile)
+//   }
+//   write(filename, people) {
+//     let objectToString = JSON.stringify(people);
+//     fs.writeFileSync(filename, objectToString);
+
+//     // Saves the data for each person (new Person(...))
+//     // to the specified file.
+//   }
+// }
+
+// const parser = new PerfectParser();
+// // console.log(parser.parse(fileName));
+// const people = parser.parse(fileName);
+// parser.write("text.csv", people);
