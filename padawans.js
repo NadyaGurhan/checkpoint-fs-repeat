9d3a457058d5226abd9a6e83d87ac3a4e9719449
Eugene <ondrinevg@gmail.com>
2021-02-25 09:36:46 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
+
+function getPadawanNames() {  
+  return fs.readFileSync('./data/padawans.txt', 'utf8')
+  .trim()
+  .split('\n');
+}
+
+function getLightsaberScores() {  
+  return fs.readFileSync('./data/scores.txt', 'utf8')
+  .trim()  
+  .split('\n')
+  .map(padavanScore => +padavanScore);
+}
+
+function getStats() {
+  const padavans = getPadawanNames();
+  const padavansScores = getLightsaberScores();
+  return padavans.map((padavan, index) => [padavan, padavansScores[index]]);
+}
+
+function writeStats() {
+  const dataBase = getStats();  
+  fs.writeFileSync('data/stats.txt', dataBase
+  .map(padavan => padavan.join(' '))
+  .join('\n'));
+}
 
 module.exports = {
   getPadawanNames,
