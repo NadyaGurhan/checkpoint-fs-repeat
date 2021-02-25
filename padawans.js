bb8ec69b6fdde17d4e16a76bc69a654e0f6f9100
Ilya Azarov <trortheruined@yandex.ru>
2021-02-25 10:09:03 +0300

    Not done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,35 @@
+const fs = require("fs");
+
+function getPadawanNames() {
+  let names = fs
+    .readFileSync("./data/padawans.txt", "utf-8")
+    .split("\n")
+    .slice(0, -1);
+  return names;
+}
+
+function getLightsaberScores() {
+  let scores = fs
+    .readFileSync("./data/scores.txt", "utf-8")
+    .split("\n")
+    .map((a) => +a);
+  return scores;
+}
+
+function getStats() {
+  let names = fs
+    .readFileSync("./data/padawans.txt", "utf-8")
+    .split("\n")
+    .slice(0, -1);
+  let scores = fs.readFileSync("./data/scores.txt", "utf-8").split("\n");
+  const stats = [];
+}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+// console.log(getLightsaberScores("./data/scores.txt"));
+console.log(getStats("./data/scores.txt"));
