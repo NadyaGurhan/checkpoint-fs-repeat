c65a382c61281056de5da1f440a24e327c1f61f7
Andrey.Azarov <azarov.andrey1995@yandex.ru>
2021-02-25 10:14:26 +0300

    Padavans

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let padavansList = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  return padavansList;
+
+}
+
+function getLightsaberScores() {
+  const scoreList = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n').map(el => Number(el));
+  return scoreList;
+}
+
+function getStats() {
+
+}
+
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+console.log(getPadawanNames(`/data/padawans.txt`));
