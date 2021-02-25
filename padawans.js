9684c90616a4122baeaa06b236371ab563097b01
Artem Chebotarev <artp956@gmail.com>
2021-02-25 10:01:30 +0300

    checkpoint-fs: done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,39 @@
+let fs = require('fs');
+let firstFile = './data/padawans.txt';
+let firstData = fs.readFileSync(firstFile, 'utf8');
+
+let secondFile = './data/scores.txt';
+let secondData = fs.readFileSync(secondFile, 'utf8');
+
+function getPadawanNames() {
+  return firstData.split('\n').filter(Boolean)
+}
+
+let names = getPadawanNames();
+
+function getLightsaberScores() {
+  return secondData.split('\n').map(a => +a);
+}
+let scores = getLightsaberScores();
+
+function getStats() {
+  let result = [];
+  for (let i = 0; i < names.length; i++) {
+    result[i] = [names[i], scores[i]];
+  }
+  let str = JSON.stringify(result);
+  fs.appendFileSync('./data/stats.txt', str);
+  return result;
+}
+
+function writeStats() {
+  let result = []
+  for (let i = 0; i < names.length; i++) {
+    result.push(`${names[i]} ${scores[i]}`);
+  }
+  let str = result.join('\n');
+  fs.writeFileSync('./data/stats.txt', str);
+}
 
 module.exports = {
   getPadawanNames,
