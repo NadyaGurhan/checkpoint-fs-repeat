67063727a4d6a58a3784266271d2b3361de6609c
IluhaByrm81 <iluhaburm@yandex.ru>
2021-02-25 10:14:29 +0300

    50%

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
 
+const fs = require('fs');
+
+function getPadawanNames() {
+   return fs.readFileSync('data/padawans.txt', 'utf8').split('\n').slice(0, 4);
+};
+
+function getLightsaberScores() {
+   return fs.readFileSync('data/scores.txt', 'utf8').split('\n').slice(0, 4).map(elem => +elem);
+
+};
+
+function getStats() {
+   const dataNames = [];
+   const names = getPadawanNames();
+   const score = getLightsaberScores();
+
+   for (let i = 0; i < dataNames.length; i++){
+      for
+      dataNames.push(names[i] + score[i]);
+   }
+   return dataNames;
+}
+
+function writeStats(stats) {
+   return fs.readFileSync('data/stats.txt', 'utf8', stats.map(data => data.join('').join('\n')));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
