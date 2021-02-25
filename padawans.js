cc602179c26be1671882930febcdb2adef305a94
IvanShchadov <shchadov.ivan@gmail.com>
2021-02-25 08:46:51 +0300

    All Done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,42 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const text = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  .split('\n')
+  .filter(el => el.length > 0);
+  return text; 
+}
+
+// console.log(getPadawanNames())
+
+function getLightsaberScores() {
+  const text = fs.readFileSync('./data/scores.txt', 'utf-8')
+  .split('\n')
+  .map(Number);
+  return text; 
+}
+
+function getStats() {
+  array = [];
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  for (let i=0; i<padawans.length; i++) {
+    array.push([padawans[i], scores[i]]);
+  }
+  return array;
+}
+
+function writeStats(arr) {
+  fs.writeFileSync(
+    "./data/stats.txt",
+    arr.map((el) => el.join(" ")).join("\n"),
+    "utf-8"
+  );
+}
+
+writeStats(getStats())
+// console.log(getStats())
+
 
 module.exports = {
   getPadawanNames,
