e8a0884b7b9670abfbff1806deff13560ad3437f
Ilya Azarov <trortheruined@yandex.ru>
2021-02-25 13:52:21 +0300

    Now it's done

--- padawans.js
+++ padawans.js
@@ -12,24 +12,31 @@ function getLightsaberScores() {
   let scores = fs
     .readFileSync("./data/scores.txt", "utf-8")
     .split("\n")
-    .map((a) => +a);
+    .map((el) => +el);
   return scores;
 }
 
 function getStats() {
-  let names = fs
-    .readFileSync("./data/padawans.txt", "utf-8")
-    .split("\n")
-    .slice(0, -1);
-  let scores = fs.readFileSync("./data/scores.txt", "utf-8").split("\n");
-  const stats = [];
+  let stats = [];
+  let names = getPadawanNames();
+  let score = getLightsaberScores();
+  // for (let i = 0; i < names.length; i++) {
+  //   stats[i] = `${names[i]} ${score[i]}`;
+  // result = stats[i].split(" ");
+  // console.log(result);
+  newPeopleArray = names.map((el, i) => [el, score[i]]);
+  return newPeopleArray;
 }
 
+function writeStats(result) {
+  const data = getStats()
+    .map((el) => el.join(" "))
+    .join("\n");
+  fs.writeFileSync("./data/stats.txt", data);
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  // writeStats,
+  writeStats,
 };
-// console.log(getLightsaberScores("./data/scores.txt"));
-console.log(getStats("./data/scores.txt"));
