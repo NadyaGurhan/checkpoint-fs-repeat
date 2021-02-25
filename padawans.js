e8f70ef1c133a4c4f98ea8d50cfe6f6987b012ae
Igor <korol_all@mail.ru>
2021-02-25 10:01:39 +0300

    checkpoint-fs

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
+const fs = require('fs');
+
+const listPadawans = './data/padawans.txt' ;
+const scorePadawans = './data/scores.txt';
+
+  
+function getPadawanNames (listPadawans) {
+  
+    return fs.readFileSync(listPadawans, 'utf8').trim().split('\n');
+    
+  }
+  
+function getLightsaberScores (scorePadawans) {
+  return fs.readFileSync(listPadawans, 'utf8').trim().split('\n').map((el) => Number(el));
+}
+
+
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
