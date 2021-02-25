f50da19820220e8dd2ebb697c9d99446092032e2
Sergey Kudryashov <kudray@yandex.ru>
2021-02-25 10:01:24 +0300

    new file:   package-lock.json 	modified:   package.json 	modified:   padawans.js

--- padawans.js
+++ padawans.js
@@ -1,7 +1,29 @@
+const fs = require("fs");
+const padawanNamesFile = "./data/padawans.txt";
+const padawanSctoresFile = "./data/scores.txt";
+
+function getPadawanNames() {
+  return fs.readFileSync(padawanNamesFile, "utf8").trim().split("\n");
+}
+function getLightsaberScores() {
+  return fs
+    .readFileSync(padawanSctoresFile, "utf8")
+    .split("\n")
+    .map((x) => Number(x));
+}
+
+function getStats() {
+  
+  // return getPadawanNames().map ( x => x,...getLightsaberScores() )
+  getPadawanNames().forEach ( (el) => el + [...getLightsaberScores()])
+
+}
+
+console.log(getStats())
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
