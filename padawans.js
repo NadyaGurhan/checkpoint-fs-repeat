1ac288dd4626dedd4a5f43ec8285ccfa02665d86
Danya <danya.culakov@yandex.ru>
2021-02-25 10:01:35 +0300

    not good

--- padawans.js
+++ padawans.js
@@ -1,3 +1,22 @@
+const fs = require("fs");
+const fileName = "./data/padawans.txt";
+const ftleScore = "/data/scores.txt";
+const fileToSave = "./data/stats.txt";
+
+class getPadawanNames {
+  names = () => {
+    return fs.readFileSync("data/padawans.txt", "utf8").split("\n");
+  };
+}
+class getLightsaberScores {}
+
+class getStats {}
+
+class writeStats {}
+
+const padaw = new getPadawanNames();\
+const 
+console.log();
 
 module.exports = {
   getPadawanNames,
