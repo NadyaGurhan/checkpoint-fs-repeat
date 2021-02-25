bb58072abe04815436f2399109de05f1d1ba33dc
Elya <esultanova96@gmail.com>
2021-02-25 10:08:26 +0300

    3\4 tests

--- padawans.js
+++ padawans.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
+ function getPadawanNames() {
+   let str = fs.readFileSync(`data/padawans.txt`, 'utf-8');
+   return str.replace(/\n/g, ',').split(',').splice(0,4);
+ }
+
+ function getLightsaberScores() {
+   let str = fs.readFileSync(`data/scores.txt`, 'utf-8');
+   return str.replace(/\n/g, ',').split(',').splice(0,4).map(el => +el);
+ }
+
+ function getStats() {
+   const names = getPadawanNames();
+   const scores =  getLightsaberScores();
+   let newArr = names.map((elem, index) => (elem + ','+ scores[index]).split(','));
+   for (let i = 0; i<newArr.length; i++) {
+     for(let j = 0; j < newArr[i].length; j++) {
+       if (j = 1){newArr[i][j]= +newArr[i][j]}
+     }
+   }
+
+return newArr
+ }
+
+ function writeStats() {
+  fs.appendFileSync(`data/stats.txt`, `${getStats()}`);
+ }
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +32,6 @@ module.exports = {
   getStats,
   writeStats,
 };
+console.log(getPadawanNames())
+console.log(getLightsaberScores())
+console.log(getStats())
