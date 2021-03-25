72baf0262eeb7de69fd670e64b8dda9811315a93
Elbrus <elbrus@pashaz.localdomain>
2021-03-25 10:09:05 +0300

    Релиз

--- padawans.js
+++ padawans.js
@@ -1,3 +1,45 @@
+const fs = require('fs');
+let data = fs.readFileSync('./data/padawans.txt', 'utf-8');
+let dataScore = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+function getPadawanNames () {
+  let dataPadawans = data.split('\n')
+  dataPadawans.pop()
+  return dataPadawans
+}
+
+function getLightsaberScores () {
+  let saberScore = dataScore.split('\n')
+  for (let i = 0; i < saberScore.length; i++) {
+    saberScore[i] = +saberScore[i]
+  }
+  return saberScore
+}
+
+function getStats () {
+  let dataPadawans = data.split('\n')
+  dataPadawans.pop()
+  
+  let saberScore = dataScore.split('\n')
+  for (let i = 0; i < saberScore.length; i++) {
+    saberScore[i] = +saberScore[i]
+  }
+  let arrScore = [];
+  for (let j = 0; j < dataPadawans.length; j++) {
+    let personInfo = [dataPadawans[j],saberScore[j]]
+    arrScore.push(personInfo)
+  }
+return arrScore
+}
+
+
+function writeStats (file) {
+  for (let i = 0; i < file.length; i++) {
+    file[i] = file[i].join(' ')
+  }
+  file.join('\n')
+  fs.writeFileSync('/data/stats.txt', file)
+}
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +47,5 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+
