91f818b6d588f6521d29b107501995e76acaefaf
amanatik <amanatik10@gmail.com>
2021-03-25 10:06:53 +0300

    ne done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,32 @@
+const fs = require('fs')
+
+
+  function getPadawanNames() {
+    const namesPadawan = fs.readFileSync('./data/padawans.txt', 'utf-8')
+    let resArr = namesPadawan.split('\n')
+    let popped = resArr.pop()
+    return resArr
+  }
+  
+  function getLightsaberScores() {
+    const scores = fs.readFileSync('./data/scores.txt', 'utf-8')
+    let arr = scores.split('\n')
+    // let popped = arr.pop()
+    return arr
+    
+  }
+
+  function getStats() {
+
+  }
+
+  function writeStats(){
+
+  }
+
+console.log(getLightsaberScores())
+
+
 
 module.exports = {
   getPadawanNames,
