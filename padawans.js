9b5fec14c3a55d49b75691b2b47704d5d2ef963f
Akimaaly <akimaaly.amankulov@gmail.com>
2021-03-25 10:05:55 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
+const fs = require("fs");
+
+let padNamesArr = [];
+let padScoresArr = [];
+function getPadawanNames () {
+  padNamesArr = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  padNamesArr.pop();
+  return padNamesArr;
+}
+function getLightsaberScores() {
+  padScoresArr = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  return padScoresArr;
+}
+function getStats() {
+  let stats = [];
+  for (let j of padNamesArr) {
+      stats.push(j);
+    }
+    console.log(stats);
+  }
+  
+getStats();
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
