c4d4455b322503a8cfc564417d3753bb671bae71
liana <lianova1209@bk.ru>
2021-03-25 10:34:05 +0300

    Done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,38 @@
+const fs = require('fs');
+
+
+getPadawanNames = () => {
+    const data = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').slice(0, -1)
+    return data
+}
+getLightsaberScores = () => {
+        const dateSecond = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map(Number)
+        return dateSecond
+    }
+    // console.log(getLightsaberScores())
+
+getStats = () => {
+    let names = getPadawanNames()
+    let scores = getLightsaberScores()
+
+    return names.map((el, i) => [el, scores[i]]);
+}
+console.log(getStats())
+
+writeStats = () => {
+    const fourDate = getStats()
+    return fs.writeFileSync('./data/stats.txt', fourDate.map((el) => el.join(' ')).join('\n'));
+
+}
+console.log(writeStats())
+
+
+
+
 
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+    getPadawanNames,
+    getLightsaberScores,
+    getStats,
+    writeStats,
 };
