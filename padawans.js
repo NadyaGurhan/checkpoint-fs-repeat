5377c6e9c770c03d3faa3ec6833885fd73707514
Danya <danya.culakov@yandex.ru>
2021-03-25 10:08:22 +0300

    2of 4

--- padawans.js
+++ padawans.js
@@ -1,3 +1,21 @@
+const fs = require("fs");
+const fileName = "./data/padawans.txt";
+const fileScore = "./data/scores.txt";
+const fileSave = ".data/stats.txt";
+
+function getPadawanNames() {
+  let name = [];
+  return (name = fs.readFileSync(fileName, "utf-8").trim().split("\n"));
+}
+
+function getLightsaberScores() {
+  let score = [];
+  return (score = fs.readFileSync(fileScore, "utf-8").split("\n").map((el) => Number(el)));
+}
+function getStats() {
+  return 
+}
+function writeStats() {}
 
 module.exports = {
   getPadawanNames,
