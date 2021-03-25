86322d02d402e48d1cf4490a121fe946471626e5
Shamil <indastr1es@gmail.com>
2021-03-25 10:05:54 +0300

    release-1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,34 @@
+const fs = require('fs')
 
+
+let pars1 = []
+function getPadawanNames () {
+let pars = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n').filter(el => el !== '')
+pars1.push(pars)
+return pars
+}
+let pars2  = []
+function getLightsaberScores () {
+  let pars = fs.readFileSync('data/scores.txt', 'utf-8').split('\n').map(el => el = Number(el))
+  pars2.push(pars)
+  return pars
+  
+}
+getPadawanNames()
+getLightsaberScores()
+
+function getStats () {
+  let res = []
+res = pars1.concat(pars2)
+
+return res
+
+}
+
+console.log(getStats());
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
