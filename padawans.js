aa6016959d0cde333a524785cef0880112830d42
DenBotvinovskii <denis.botvinovsky@gmail.com>
2021-03-25 10:20:14 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,22 @@
+const fs = require("fs");
+function getPadawanNames() {
+  let getNames = fs.readFileSync("./data/padawans.txt", "utf-8");
+  let namesArr = getNames.split("\n");
+  console.log(namesArr);
+}
+console.log(getPadawanNames());
+
+// const padawans = new getPadawanNames();
+// padawans.parse();
+// class getLightsaberScores {
+//   parse(scores) {
+//     let getScores = fs.readFileSync("./data/scores.txt", "utf-8");
+//   }
+// }
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  //getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
