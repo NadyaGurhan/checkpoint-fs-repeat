5ae0c6f9e04d73c305ea4740352f717202d48925
Chubak <musaev4t@gmail.com>
2021-03-25 10:07:54 +0300

    chekpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,22 @@
+const fs = require('fs');
+let data = fs.readFileSync('data/padawans.txt', "utf8");
+let data = fs.readFileSync('data/scores.txt', "utf8");
+
+
+function getPadawanNames(){
+  let arr = data.split('\n');
+  arr.pop();
+  return arr;
+}
+
+function getLightsaberScores(){
+  let arr = data.split('\n')
+  return arr
+}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
