2b4c904db152947366e94b1ec36059d33803d76a
Margo <margosha.novikova@gmail.com>
2021-03-25 09:49:58 +0300

    Release wohoo

--- padawans.js
+++ padawans.js
@@ -1,3 +1,32 @@
+const fs = require('fs');
+// let data = fs.readFileSync('padawans.txt', 'utf-8');
+
+function getPadawanNames() {
+  let names = fs.readFileSync('data/padawans.txt', 'utf-8').split("\n");
+  names.pop();
+  return names;
+}
+
+function getLightsaberScores() {
+  let scores = fs.readFileSync('data/scores.txt', 'utf-8').split("\n").map((el) => +el);
+  return scores;
+}
+
+function getStats() {
+  let stats = [];
+  let names = getPadawanNames();
+  let scores = getLightsaberScores();
+  for (let i = 0; i < names.length; i++) {
+    stats.push([names[i],scores[i]]);
+  }
+  return stats;
+}
+
+function writeStats() {
+  let statStr = getStats().join("\n").replace(/,/gm, " ");
+  fs.writeFileSync('data/stats.txt', statStr)
+
+}
 
 module.exports = {
   getPadawanNames,
