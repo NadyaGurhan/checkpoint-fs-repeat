af782584553a0e08855ad4c0c3de1d6d64aa4517
Ivan <schastlivitch@gmail.com>
2021-03-25 09:39:17 +0300

    Release checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,46 @@
+const fs = require('fs');
+let padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+let scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+function getPadawanNames() {
+  const namesArr = padawans.split('\n');
+  namesArr.pop();
+  return namesArr;
+}
+
+function getLightsaberScores() {
+  const scoresArr = scores.split('\n');
+  for (let i = 0; i < scoresArr.length; i++) {
+    scoresArr[i] = +scoresArr[i];
+  }
+  return scoresArr;
+}
+
+function getStats() {
+  const namesArr = padawans.split('\n');
+  namesArr.pop();
+  
+  const scoresArr = scores.split('\n');
+  for (let i = 0; i < scoresArr.length; i++) {
+    scoresArr[i] = +scoresArr[i];
+  }
+
+  const statsArr = [];
+  for (let i = 0; i < namesArr.length; i++) {
+    const person = [namesArr[i], scoresArr[i]];
+    statsArr.push(person);
+  }
+  
+  return statsArr;
+}
+
+function writeStats(stats) {
+  for (let i = 0; i < stats.length; i++) {
+    stats[i] = stats[i].join(' ');
+  }
+  fs.appendFileSync('./data/stats.txt', stats.join('\n'));
+}
+
 
 module.exports = {
   getPadawanNames,
