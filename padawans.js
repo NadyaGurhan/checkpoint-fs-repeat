372b0cafac247caaecf059705130ce8de49a35fd
Nick <belov_nikolai@mail.ru>
2021-03-25 10:06:26 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,33 @@
+const fs = require('fs')
 
+function getPadawanNames () {
+  let a = fs.readFileSync('data/padawans.txt', 'utf-8')
+  let b = a.split('\n')
+  return b.filter(el => el !== '')
+}
+// console.log(getPadawanNames())
+
+function getLightsaberScores () {
+  let a = fs.readFileSync('data/scores.txt', 'utf-8')
+  let b = a.split('\n')
+  return b.map(el => Number(el))
+}
+// console.log(getLightsaberScores())
+
+
+class getStats {
+  constructor(getPadawanNames, getLightsaberScores) {
+
+  }
+}
+console.log(getStats())
+
+// function writeStats {
+  
+// }
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
