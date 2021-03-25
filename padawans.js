6b2c9f3122767b0581fd2bf67b667c5f5d5bf2ac
Alex Frankenstein <aleksej@MacBook-Pro-Alex.local>
2021-03-25 09:32:26 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,4 +1,27 @@
+const fs = require("fs");
+function getPadawanNames() {
+  return fs.readFileSync("./data/padawans.txt", "utf8").trim().split("\n");
+}
 
+function getLightsaberScores() {
+  return fs
+    .readFileSync("./data/scores.txt", "utf8")
+    .trim()
+    .split("\n")
+    .map((el) => +el);
+}
+function getStats() {
+  let getPadawan = getPadawanNames();
+  let getLightsaber = getLightsaberScores();
+  return getPadawan.map((newPadawan, i) => [newPadawan, getLightsaber[i]]);
+}
+const writeStats = () => {
+  const data = getStats();
+  fs.writeFileSync(
+    "./data/stats.txt",
+    data.map((i) => i.join(" ")).join("\n")
+  );
+};
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
