29ec90033a35112805c483b43a2f42440c415af7
Andrey <trifonova86@mail.ru>
2021-03-25 10:01:13 +0300

    checpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,34 @@
+const fs = require('fs');
+
+function getPadawanNames(){
+  let list = fs.readFileSync('./data/padawans.txt','utf-8').split("\n")
+  return list.filter(el => el.length != 0)
+}
+
+function getLightsaberScores(){
+  let list = fs.readFileSync('./data/scores.txt','utf-8').split("\n")
+  list.filter(el => el.length != 0)
+  return list.map(el=>Number(el))
+}
+function getStats(){
+let names=getPadawanNames()
+let scores=getLightsaberScores()
+arr=[]
+for (let i = 0; i < names.length; i++) {
+  arr.push([]);
+  arr[i].push(names[i], scores[i]);
+}
+
+return arr
+}
+function writeStats(){
+  let string=people.map(el=>`${el}\n`).join('')
+ 
+  fs.writeFileSync('.data/stats.txt',string,{flag:"a"})
+}
+
+console.log(getPadawanNames('./data/padawans.txt'));
+
 
 module.exports = {
   getPadawanNames,
