a23ce6c8f954c028ed7e28c8bc901b107a89749c
Dmitrii Penkin <jesone94@gmail.com>
2021-03-30 10:45:54 +0400

    Checkpoint done!

--- padawans.js
+++ padawans.js
@@ -1,3 +1,41 @@
+const fs = require('fs')
+
+
+function getPadawanNames() {
+  let list = fs.readFileSync('./data/padawans.txt','utf-8') 
+  return list.split('\n').filter(el => el !== '')
+}
+
+function getLightsaberScores() {
+
+  let list = fs.readFileSync('./data/scores.txt','utf-8') 
+  return list.split('\n').filter(el => el !== '').map(el => Number(el))
+
+}
+
+function getStats() {
+  const list = getPadawanNames()
+  const score = getLightsaberScores()
+  const result = []
+  for (let i=0;i<list.length;i++){
+    result.push([list[i],score[i]])
+  }
+  return result
+
+
+}
+
+function writeStats() {
+  let data = getStats()
+  for (i=0;i<data.length;i++){
+    let str = data[i].join(' ') + '\n'
+    fs.appendFileSync('data/stats.txt',str)
+  }
+
+}
+
+
+
 
 module.exports = {
   getPadawanNames,
