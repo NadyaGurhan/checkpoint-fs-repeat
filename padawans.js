1ab1a4ff1451073bea9dc058aa721101584de94a
Oksana Klymenok <kapris15@yahoo.com>
2021-03-29 20:42:08 -1000

    Done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs=require('fs');
+const getPadawanNames =()=>{
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+}
+const getLightsaberScores=()=>{
+  return fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n').map(el=>Number(el));
+}
+const getStats=()=>{
+  const names=getPadawanNames();
+  const scores=getLightsaberScores();
+  const stats=[];
+  for (let i=0; i<names.length; i++){
+    stats.push([names[i], scores[i]])
+  }
+  return stats;
+}
+
+const writeStats=()=>{
+  const arr=getStats();
+  fs.writeFileSync('./data/stats.txt', arr.join('\n').replace(/\,/mgi, ' '));
+}
 
 module.exports = {
   getPadawanNames,
