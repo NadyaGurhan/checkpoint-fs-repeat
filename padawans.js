364487170ded57e574a3a29bfee406d4d2ccdd31
SergeyTsybezov <demidgrey@gmail.com>
2021-03-30 10:57:24 +0400

    checkpoint fs

--- padawans.js
+++ padawans.js
@@ -1,3 +1,37 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const contents = fs.readFileSync('data/padawans.txt', 'utf-8');
+  const array = contents.split('\n');
+  array.pop();
+  return array;
+}
+
+function getLightsaberScores() {
+  const contents = fs.readFileSync('data/scores.txt', 'utf-8');
+  const array = contents.split('\n');
+  return array.map((el) => Number(el));
+}
+
+function getStats() {
+  const person = getPadawanNames();
+  const scores = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i <= person.length; i += 1) {
+    result.push([person[i], scores[i]]);
+  }
+  result.pop();
+  return result;
+}
+
+function writeStats() {
+  // let data = `${getPadawanNames()} + ${getLightsaberScores()} + \n`
+  const stats = getStats();
+  for (let elem of stats) {
+    let string = `${elem.join(' ')}\n`;
+    fs.appendFileSync('data/stats.txt', string);
+  }
+}
 
 module.exports = {
   getPadawanNames,
