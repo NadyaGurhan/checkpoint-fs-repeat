ca14c7c123849480bcf7a5304df404071a4ca824
Vadim Sh <V.Shuynov@yandex.ru>
2021-04-15 09:55:55 +0300

    Release 0 Done Refactioring

--- padawans.js
+++ padawans.js
@@ -1,35 +1,31 @@
 const fs = require('fs');
 
-
-function getPadawanNames(fileName){
-  let text = fs.readFileSync(fileName = './data/padawans.txt','utf8').split('\n');
+function getPadawanNames(fileName) {
+  const text = fs.readFileSync(fileName = './data/padawans.txt', 'utf8').split('\n');
   text.pop();
-  return text
+  return text;
 }
 
-
-function getLightsaberScores(fileName){
-  let text = fs.readFileSync(fileName = './data/scores.txt','utf8').split('\n');
-  text = text.map(el => +el)
-  return text
+function getLightsaberScores(fileName) {
+  let text = fs.readFileSync(fileName = './data/scores.txt', 'utf8').split('\n');
+  text = text.map((el) => +el);
+  return text;
 }
 
-function getStats ( ){
-  let score = getLightsaberScores('./data/scores.txt','utf8');
-  let padavans = getPadawanNames('./data/padawans.txt','utf8');
+function getStats() {
+  const score = getLightsaberScores('./data/scores.txt', 'utf8');
+  const padavans = getPadawanNames('./data/padawans.txt', 'utf8');
 
-  let end = []
-  padavans.forEach((el,index)=> end.push([el,score[index]]));
-  return end
+  const end = [];
+  padavans.forEach((el, index) => end.push([el, score[index]]));
+  return end;
 }
-function writeStats(array =[]){
+function writeStats(array = []) {
   array = array.join('\n').split(',').join(' ');
   fs.writeFileSync('data/stats.txt', array);
 }
 
 console.log(writeStats(getStats()));
-
-
 
 module.exports = {
   getPadawanNames,
