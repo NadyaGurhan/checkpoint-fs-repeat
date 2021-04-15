c838e7502d8321d5912c5cb42a3b43c16302f178
Akimaaly <akimaaly.amankulov@gmail.com>
2021-04-15 10:06:57 +0300

    Checkpoint FS

--- padawans.js
+++ padawans.js
@@ -1,3 +1,29 @@
+const fs = require('fs');
+
+const getPadawanNames = () =>{
+  let data = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n')
+  return data
+}
+const getLightsaberScores = () =>{
+  let data = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n').map(el => +el)
+  return data
+}
+const getStats = () =>{
+  let tableOfScores =  [];
+  for (let i =0; i < getPadawanNames().length; i++) {
+    tableOfScores.push([getPadawanNames()[i], getLightsaberScores()[i]])
+  }
+  return tableOfScores
+}
+const writeStats = (arr) =>{
+  let data = fs.writeFileSync(`data/stats.txt`, arr.join(''))
+  return data
+}
+console.log(getPadawanNames()); 
+console.log(getLightsaberScores()); 
+console.log(getStats()); 
+writeStats(getStats())
+
 
 module.exports = {
   getPadawanNames,
