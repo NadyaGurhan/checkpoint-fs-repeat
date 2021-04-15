c88efdd6c9d9937b7be7af4484d616bd3f37e2dd
Aleksey Maslov <amaslov@sharevault.com>
2021-04-15 09:55:43 +0300

    full release 0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let data = fs.readFileSync("./data/padawans.txt", "utf8").split("\n");
+  data.pop();
+  return data;
+}
+
+function getLightsaberScores() {
+  fs.readFileSync("./data/scores.txt", "utf8").split("\n").map(x => parseFloat(x));
+  return fs.readFileSync("./data/scores.txt", "utf8").split("\n").map(x => parseFloat(x));
+}
+
+function getStats() {
+  let name = getPadawanNames();
+  let score = getLightsaberScores();
+  return name.map((x, i) => [x, score[i]]);
+}
+
+function writeStats(stats) {
+  stats = stats.map(x=> x.join(" ")).join("\n");
+  fs.writeFileSync("./data/stats.txt", stats)
+}
 
 module.exports = {
   getPadawanNames,
