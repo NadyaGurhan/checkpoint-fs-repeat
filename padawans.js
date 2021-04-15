7f35b836e9a6b4b8945057d49a245fe177e413f2
xMellox <gmellog117@gmail.com>
2021-04-15 09:37:55 +0300

    Completed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,32 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8');
+  return data.split('\n').filter((padawan) => padawan !== '');
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf-8');
+  return data.split('\n').filter((score) => score !== '').map((score) => Number(score));
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  return Array.from({ length: padawans.length }, (v, i) => [padawans[i], scores[i]]);
+}
+
+function writeStats(stats) {
+  let buffer = '';
+
+  for (const stat of stats) {
+    buffer += `${stat[0]} ${stat[1]}`;
+    if (stat != stats[stats.length - 1]) {
+      buffer += '\n';
+    }
+  }
+  fs.writeFileSync('data/stats.txt', buffer);
+}
 
 module.exports = {
   getPadawanNames,
