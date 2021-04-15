55b98e70471e73a360679eeaa0403e8590bc1b62
Astorshi <denpervyj@yandex.ru>
2021-04-15 19:53:36 +0300

    release oll

--- padawans.js
+++ padawans.js
@@ -1,3 +1,53 @@
+const fs = require('fs');
+// let filePath = './data/padawans.txt';
+// let data = fs.readFileSync(filePath, 'utf8');
+
+
+function getPadawanNames() {
+  
+  let filePath = './data/padawans.txt';
+  let data = fs.readFileSync(filePath, 'utf8');
+  //console.log(data);
+  let arr = data.split('\n');
+  arr.pop();
+  return arr;
+}
+
+
+function getLightsaberScores() {
+  let filePath = './data/scores.txt';
+  let data = fs.readFileSync(filePath, 'utf8').trim().split('\n');
+  //console.log(data);
+  let arr2 = data.map(el => Number(el));
+  return arr2;
+}
+
+function getStats(){
+  let names = getPadawanNames();
+  let score = getLightsaberScores();
+  result = []
+  for (i=0; i < names.length; i++){
+    result[i] = [];
+  }
+  for (let i=0; i < names.length; i++) {
+    result[i].push(names[i])
+  }
+  for (let i=0; i < score.length; i++) {
+    result[i].push(score[i])
+  }
+  return result
+  }
+  
+function writeStats(){
+  const data = getStats();
+  data2 = data.join('\n');
+  let data3 = data2.replace(/[,]/g , ' ')
+  //console.log(data3)
+  fs.writeFileSync('./data/stats.txt', data3 );
+}
+
+
+
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +55,6 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+
+
