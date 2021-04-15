18cb00a3ed8e3d5f0c0bfd0fea579408964413fc
Zaur <Zulpikarov.Z@gmail.com>
2021-04-15 10:08:22 +0300

    Cheack

--- padawans.js
+++ padawans.js
@@ -1,3 +1,29 @@
+const fs = require('fs');
+
+function getPadawanNames(filename) {  
+  let names = fs.readFileSync(filename = `data/padawans.txt`, 'utf-8').split('\n');
+  names.pop();
+  return names;
+}
+
+function getLightsaberScores(filename) {
+  let names = fs.readFileSync(filename = `data/scores.txt`, 'utf-8').split('\n');
+  return names.map(el => +el);
+}
+
+function getStats() {
+  let names = getPadawanNames();
+  let scores = getLightsaberScores();
+  let stats = [];
+  return names.map((el, i) => ([el, scores[i]]));
+}
+
+function writeStats() {
+
+}
+
+
+
 
 module.exports = {
   getPadawanNames,
