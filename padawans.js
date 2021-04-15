e5d55aa93ad4c6661ed18104b7a5414f46b053aa
olehandros <olehandros@rambler.ru>
2021-04-15 10:09:44 +0300

    Final checkPoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,49 @@
 
+
+
+
+const fs = require ('fs');
+
+
+
+function getPadawanNames(filename )
+{
+  
+  
+let padArr= fs.readFileSync(filename= './data/padawans.txt', 'utf8').trim().split('\n');
+ return padArr;
+
+  
+}
+
+
+
+
+function getLightsaberScores(filename )
+{
+ 
+  getLight = fs.readFileSync(filename= './data/scores.txt' , 'utf-8').split('\n');
+  return getLight.map((elem) => +elem); 
+
+
+}
+console.log(getLightsaberScores());
+
+function getStats()
+  {
+  let names = getPadawanNames();
+  let scores = getLightsaberScores();
+  return names.map((elem,i) => ([elem, scores[i]]));
+
+}
+function writeStats(array){
+ array = array.join('\n').split(',').join(' ');
+
+ fs.writeFileSync('data/stats.txt', array);
+ 
+}
+
+console.log(writeStats(getStats()));
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
