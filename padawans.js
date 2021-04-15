d96980e53e20a0a5bbae82d88d09cf576720d1a8
Nurbek <nurbek.itu@gmail.com>
2021-04-15 09:46:39 +0300

    Release DOne

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs = require("fs");
+
+const getName = fs.readFileSync("./data/padawans.txt", "utf-8");
+const getScores = fs.readFileSync("./data/scores.txt", "utf-8");
+
+function getPadawanNames() {
+  return getName.trim().split("\n");
+}
+
+function getLightsaberScores() {
+  return getScores.split("\n").map(Number);
+}
+
+function getStats() {
+  return getPadawanNames().map((elem, i) => [elem, getLightsaberScores()[i]]);
+}
+
+function writeStats() {
+  return fs.writeFileSync(
+    "./data/stats.txt",
+    getStats()
+      .map((el) => el.join(" "))
+      .join("\n")
+  );
+}
 
 module.exports = {
   getPadawanNames,
