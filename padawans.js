460405192850307e27b1c27132e5f3f54c30ae66
TimurVS <vos-tim@yandex.ru>
2021-04-15 10:06:53 +0300

    3 of 4

--- padawans.js
+++ padawans.js
@@ -1,7 +1,44 @@
 
+const fs = require('fs');
+const { get } = require('http');
+
+
+function getPadawanNames(filename = 'data/padawans.txt') {
+
+  let names = fs.readFileSync(filename, 'utf8').split('\n')
+  names.pop()
+  return names
+
+}
+
+function getLightsaberScores(filename = 'data/scores.txt') {
+
+  let names = fs.readFileSync(filename, 'utf8').split('\n')
+  return names.map((el => +el))
+
+
+}
+
+function getStats() {
+  let names = getPadawanNames()
+  let scores = getLightsaberScores()
+
+  return names.map((el, i) => ([el, scores[i]]));
+
+
+
+}
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+
+
+
