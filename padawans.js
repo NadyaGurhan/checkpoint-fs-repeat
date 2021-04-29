b2e32ebf0b2ffc94050cb85a835081fb57a4ca03
Alexei189 <kadylev189@gmail.com>
2021-04-29 10:03:40 +0300

    0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,22 @@
+const fs = require("fs");
+
+function getPadawanNames() {
+  return fs
+    .readFileSync("./data/padawans.txt", "utf-8")
+    .split("\n")
+    .slice(0, -1);
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync("./data/scores.txt", "utf-8")
+    .split("\n")
+    .map((el) => +el);
+}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  //writeStats,
 };
