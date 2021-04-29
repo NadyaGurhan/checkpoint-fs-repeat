806dafc412d27ffb887332a473439fad2876cf20
Sergey Prilepko <mjcomand@mail.ru>
2021-04-29 09:30:03 +0300

    All done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,25 @@
+const fs = require('fs')
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').slice(0, -1)
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map(el => +el)
+}
+
+function getStats() {
+  const names = getPadawanNames()
+  const scores = getLightsaberScores()
+  let stats = names.map((name, index) => [name, scores[index]])
+  return stats
+}
+
+function writeStats(stats) {
+  const string = stats.map(arr => arr.join(' ')).join('\n')
+  fs.writeFileSync('data/stats.txt', string)
+}
+
 
 module.exports = {
   getPadawanNames,
