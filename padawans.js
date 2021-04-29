4a959bab20cc671c7edbb8aa40ebb0a7e6159350
Aleksey <mobb.steam@gmail.com>
2021-04-29 10:01:25 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs = require('fs')
+
+function getPadawanNames(){
+  const names = fs.readFileSync('data/padawans.txt', 'utf8').split('\n')
+  names.pop()
+  return names
+}
+console.log(getPadawanNames())
+function getLightsaberScores(){
+  const stat = fs.readFileSync('data/scores.txt', 'utf8').split('\n')
+  let result = stat.map(e => Number(e))
+  return result
+}
+
+function getStats(){
+  const name = getPadawanNames()
+  const stat = getLightsaberScores()
+  let result = name.map((e, i) => [e, stat[i]])
+  return result
+}
+
+function writeStats(){
+  const stat = getStats().join('\n').split(',').join(' ')
+  fs.writeFileSync('data/stats.txt', stat)
+}
 
 module.exports = {
   getPadawanNames,
