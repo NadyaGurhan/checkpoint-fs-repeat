2f72f3a5e5a5ba9f98e3babd9222207d24209406
Treister Aleksei <45953646+TipTipple@users.noreply.github.com>
2021-04-29 09:42:34 +0300

    Check

--- padawans.js
+++ padawans.js
@@ -1,3 +1,39 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const name = fs
+    .readFileSync('./data/padawans.txt', 'utf-8')
+    .trim()
+    .split('\n');
+  return name;
+}
+
+function getLightsaberScores() {
+  const score = fs
+    .readFileSync('./data/scores.txt', 'utf-8')
+    .trim()
+    .split('\n')
+    .map((el) => +el);
+  return score;
+}
+
+function getStats() {
+  let padawan = getPadawanNames();
+  let score = getLightsaberScores();
+  let stats = [];
+  for (let i = 0; i < padawan.length; i++) {
+    stats.push([padawan[i],score[i]]);
+    // stats.push(score[i]);
+  }
+  return stats;
+}
+
+function writeStats() {
+  let stats = getStats();
+  const write = stats.map(el => el.join(' ')).join('\n')
+  fs.writeFileSync('./data/stats.txt', write);
+
+}
 
 module.exports = {
   getPadawanNames,
