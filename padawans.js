19365150345077dd275b9d9d58311d55ab1204b1
NNikonov <n.nikonov008@gmail.com>
2021-05-13 10:01:46 +0300

    Problems

--- padawans.js
+++ padawans.js
@@ -1,3 +1,44 @@
+const fs = require('fs');
+// const padawans = fs.readFileSync('../padawans.txt', 'utf8');
+
+// class Person {
+  
+//   constructor(name1, name2, name) {
+//       this.first_name = first_name;
+//       this.last_name = last_name;
+//       this.email = email;
+//       this.phone = phone;
+//       this.born_at = born_at; //посатвить Date
+//     }
+
+//   }
+
+  function getPadawanNames () {
+      // const data = fs.readFileSync('../padawans.txt', 'utf8');
+      const names = fs.readFileSync('./data/padawans.txt', 'utf8');
+      const arr = names.split("\n");
+      return arr.slice(0,-1)
+      
+      // return arr.slice(0,-1);
+  }
+  // console.log(getPadawanNames());
+  
+  function getLightsaberScores() {
+    const scores = fs.readFileSync('./data/scores.txt', 'utf8');
+    const scoreArr = scores.split("\n");
+    return scoreArr.slice(0, -1);
+  }
+  
+  function getStats() {
+  
+  }
+  
+  function writeStats () {
+  
+  }
+
+
+
 
 module.exports = {
   getPadawanNames,
