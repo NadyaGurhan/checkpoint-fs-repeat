f9ad1d0c950893cce84406d1c21d789edc81cf1b
Maria <mt9686@inbox.ru>
2021-05-13 10:04:37 +0300

    nothing

--- padawans.js
+++ padawans.js
@@ -1,3 +1,25 @@
+const fs = require('fs');
+// class Padawan{
+//   constructor(name, stats){
+//     this.name = name
+//     this.stats = stats
+//   }
+// }
+function getPadawanNames(){
+  return fs.readFileSync("./data/padawans.txt", "utf8").trim().split('\n');
+}
+console.log(getPadawanNames())
+
+function getLightsaberScores(){
+  let d = fs.readFileSync("./data/scores.txt", "utf8").trim().split();
+  return d.map()
+}
+function getStats(){
+
+}
+function writeStats(){
+
+}
 
 module.exports = {
   getPadawanNames,
