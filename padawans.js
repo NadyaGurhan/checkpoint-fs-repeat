f1ec3ddcc2dcb128db086c293a70abe07492a91b
Anastasia <nastia_kim_96@mail.ru>
2021-05-13 17:05:02 +1000

    Check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
+  const fs = require('fs')
+ let data = fs.readFileSync('./data/padawans.txt', 'utf8')
+//console.log(data);
+ let score = fs.readFileSync('./data/scores.txt', 'utf8')
+//console.log(score.split('\n'));
 
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+function getPadawanNames(data) {
+  let arr = data.split('\n')
+  arr.pop()
+  return arr
+}
+function getLightsaberScores(score) {
+ return score.split('\n').map((el) => +el)
+  
+}
+
+function getStats() {
+  return getPadawanNames().map((el, i) => el + getLightsaberScores()[i])
+}
+
+function writeStats() {
+
+}
+console.log(getPadawanNames(data));
+//module.exports = {
+//   getPadawanNames
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
