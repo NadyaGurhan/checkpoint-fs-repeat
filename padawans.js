f5ae03ad63e5f9176cb64bec0df4a38e30020e49
Sasha <sashashvetsov3@gmail.com>
2021-05-13 10:02:29 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +6,30 @@ module.exports = {
   getStats,
   writeStats,
 };
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').slice(0, -1)
+}
+
+//console.log(getPadawanNames())
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf8').split('\n').map((el) => (+el))
+}
+
+//console.log(getLightsaberScores())
+
+function getStats() {
+  let a = getPadawanNames()
+  let b = getLightsaberScores()
+  let arr = []
+  for (let i = 0; i < a.length; i++) {
+    console.log(a[i])
+  }
+  // for (let j = 0; l < b.length; j++) {
+  //   return b[j]
+  arr.push()
+}
+
+function writeStats() {
+
+}
