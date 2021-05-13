13186d73b4f505fbae29919f7cacb84f3b068c5c
VoronovY <voronov.yuri87@gmail.com>
2021-05-13 10:02:17 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,42 @@
+const fs = require('fs');
+
+// const padawansTxt = fs.readFileSync('data/padawans.txt', 'utf-8');
+// console.log(padawansTxt);
+const padawansTxt = fs.readFileSync('data/padawans.txt', 'utf-8');
+const scoreTxt = fs.readFileSync('data/scores.txt', 'utf-8');
+
+function getPadawanNames() {
+
+  return padawansTxt.split('\n').slice(0, -1);
+
+}
+
+function getLightsaberScores() {
+
+  return scoreTxt.split('\n').map((el) => +el);
+}
+
+function getStats() {
+
+  const arrPadawans = getPadawanNames();
+  const arrScores = getLightsaberScores();
+  arrPadawans.map((el, index) => el.split().push(arrScores[index]));
+  let res = ''
+  const writeTxt = arrPadawans.forEach((el) => res += `${el[0]} ${el[1]}\n`)
+
+  fs.writeFileSync('data/stats.txt', res)
+
+  return arrPadawans
+
+
+
+
+
+}
+function writeStats() {
+
+}
+
 
 module.exports = {
   getPadawanNames,
