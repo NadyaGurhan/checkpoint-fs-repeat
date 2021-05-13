5e21d9a6c4603da06ed00dd90e81983e21f871be
MariaR <8insouciance@gmail.com>
2021-05-13 09:59:53 +0300

    Release Test 1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,36 @@
+const fs = require('fs')
+let data = fs.readFileSync("./data/padawans.txt", "utf8");
+//console.log(data)
+
+//class Padawan {
+function getPadawanNames() {
+    let dataArr = data.split('\n')
+    let padArr = []
+    
+    for (let i = 0; i < dataArr.length; i++) {
+      padArr.push(dataArr[i])
+    }
+    //console.log(padArr)
+
+  let finalArr;
+  finalArr = padArr.slice(0, -1)
+  //console.log(finalArr)
+  return finalArr
+  }
+  console.log(getPadawanNames())
+  
+  
+  //getLightsaberScores()
+  //getStats()
+  //writeStats()
+  
+//}
+
+
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  //getLightsaberScores,
+  //getStats,
+  //writeStats,
 };
