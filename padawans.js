210f9a95de50af303503a90647936092b5c63330
Влад <vlad@MacBook-Air-Vlad.local>
2021-05-13 10:06:32 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +6,33 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const padawan = fs.readFileSync("data/padawans.txt", "utf-8");
+  return padawan.trim().split("\n");
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync("data/scores.txt", "utf-8");
+  return scores.split("\n");
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  let newArr = [];
+  names.forEach((item, index, arr) => {
+    newArr.push([names[index], +scores[index]]);
+  });
+  return newArr;
+}
+
+function writeStats(stats) {
+  stats = getStats();
+  stats.map((item) => {
+    return item;
+  });
+  fs.appendFileSync("data/stats.txt", stats, "utf-8");
+}
+
+writeStats(getStats());
