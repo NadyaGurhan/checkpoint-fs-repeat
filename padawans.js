3d076f4d0a6f55cd6252a4c359ce9059c5eabfce
Victor Yanochkin <v65988@gmail.com>
2021-05-13 10:01:31 +0300

    Done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,25 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let newArr = fs.readFileSync(`./data/padawans.txt`, 'utf8').split('\n').map((el) => el);
+  newArr.pop();
+  return newArr;
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync(`./data/scores.txt`, 'utf8').split('\n').map((el) => +el);
+}
+
+function getStats() {
+  return getPadawanNames().map((el, i) => ([el, getLightsaberScores()[i]]));
+}
+
+function writeStats() {
+  return fs.writeFileSync(`./data/stats.txt`, getStats().map((el) => el.join(' ')).join('\n'));
+
+}
+
+console.log(writeStats());
 
 module.exports = {
   getPadawanNames,
