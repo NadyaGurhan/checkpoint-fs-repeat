af3085020a2f72d6f06e0158a4ace73f8ab44ee1
Insaev <insaevstudio@gmail.com>
2021-05-25 09:55:20 +0300

    All Releases Done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,33 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n');
+  const score = scores.map((el) => +el);
+  return score;
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const stats = padawans.map((el, i) => {
+    const arr = []
+    arr.push(el, scores[i]);
+    return arr;
+  });
+  return stats;
+}
+
+function writeStats() {
+  let data = getStats();
+  data = data.map((el) => el.join(',')).join('\n').replace(/,/gm, ' ');
+  console.log(data);
+  fs.writeFileSync('./data/stats.txt', data);
+}
 
 module.exports = {
   getPadawanNames,
