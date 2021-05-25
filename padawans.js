8b17829d098e81d5ee4c4bd064ec0e4d37bd8fd9
Andrew Bochka <a.g.bochka@gmail.com>
2021-05-25 10:19:56 +0300

    Release1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,50 @@
+const fs = require('fs');
+let padawanNames;
+let lightsaberScores;
+let tableScores = [];
+let strtableScores = '';
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf8');
+  padawanNames = data.split('\n');
+  for (let i = 0; i < padawanNames.length; i++) {
+    if (padawanNames[i] === '') padawanNames.splice(i, 1);
+  }
+  return padawanNames;
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf8');
+  const dataArr = data.split('\n');
+  for (let i = 0; i < dataArr.length; i++) {
+    if (dataArr[i] === '') dataArr.splice(i, 1);
+  }
+  lightsaberScores = dataArr.map((item) => (item = Number(item)));
+  return lightsaberScores;
+}
+
+function getStats() {
+  for (let i = 0; i < padawanNames.length; i++) {
+    let nameAndScore = [];
+    nameAndScore.push(padawanNames[i]);
+    nameAndScore.push(lightsaberScores[i]);
+    tableScores.push(nameAndScore);
+  }
+  for (let item of tableScores) {
+    strtableScores = strtableScores.concat(item.toString(), '\n');
+  }
+  strtableScores = strtableScores.replaceAll(',', ' ');
+  fs.writeFileSync('data/stats.txt', strtableScores);
+  return tableScores;
+}
+
+function writeStats() {
+  for (let item of tableScores) {
+    strtableScores = strtableScores.concat(item.toString(), '\n');
+  }
+  strtableScores = strtableScores.replaceAll(',', ' ');
+  fs.writeFileSync('data/stats.txt', strtableScores);
+}
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +52,8 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+// console.log(getPadawanNames());
+// console.log(getLightsaberScores());
+// // console.log(getStats());
+// console.log(writeStats());
