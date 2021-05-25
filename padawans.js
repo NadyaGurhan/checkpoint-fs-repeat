ba3c7066e4f367b5a12d15d360c8d2d55050cdcb
Anastasia <kovalchuk0687@gmail.com>
2021-05-25 10:03:04 +0300

    Passed 2 test

--- padawans.js
+++ padawans.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('data/padawans.txt', 'utf-8');
+  return names.trim().split('\n');
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('data/scores.txt', 'utf-8');
+  return scores.split('\n').map((elem) => Number(elem));
+}
+
+function getStats() {
+  const stats = getLightsaberScores();
+  const gettingStats = fs.writeFileSync('data/stats.txt', stats);
+  return gettingStats;
+
+}
+
+function writeStats(stats) {
+  const statsWrite = fs.writeFileSync('data/padawans.txt', stats);
+  return statsWrite;
+}
 
 module.exports = {
   getPadawanNames,
