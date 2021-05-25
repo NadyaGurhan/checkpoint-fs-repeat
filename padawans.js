2db7d5b645e363db192ab459106bbec2b11c737e
ElinaKhisamutdinova <khisamutdinova.elina@gmail.com>
2021-05-25 08:38:12 +0200

    Done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,27 @@
+const fs = require('fs')
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf8').trimEnd().split('\n');
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf8').trimEnd().split('\n').map((elem) => Number(elem));
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  let x = [];
+  for (let i = 0; i < 4; i++) {
+    x.push([names[i], scores[i]])
+  }
+  return x;
+}
+
+function writeStats() {
+  stats = getStats().map((elem) => elem.join(',').replace(',', ' ')).join('\n')
+  fs.appendFileSync('data/stats.txt', stats);
+}
 
 module.exports = {
   getPadawanNames,
