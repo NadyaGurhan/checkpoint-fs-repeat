428397f9364db54c02d4db62a0b1583908e4285d
Anya <makarobaanya@yandex.ru>
2021-06-03 10:10:36 +0300

    1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
 
+const fs = require("fs");
+
+
+
+function getPadawanNames(){
+  const data = fs.readFileSync("data/padawans.txt", "utf8").split('\n')
+  data.pop()
+  return data
+}
+function getLightsaberScores(){
+  const data1 = fs.readFileSync("data/scores.txt", "utf8").split('\n').map(element => Number(element))
+ 
+  return data1
+
+}
+
+
+
+
+
+class getStats{}
+class writeStats{}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+getPadawanNames()
