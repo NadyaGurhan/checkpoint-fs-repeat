599cdb56bc657425190d056b3792df911955a4a5
Yury <Yrecink@mail.ru>
2021-06-03 10:09:24 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,29 @@
+const fs = require('fs')
+
+function getPadawanNames(){
+  let data = fs.readFileSync('data/padawans.txt', 'utf-8')
+  return data.split('\n').slice(0,4)
+}
+
+function getLightsaberScores() {
+  let data = fs.readFileSync('data/scores.txt', 'utf-8')
+  return data.split('\n').map((el) => Number(el))
+}
+
+function getStats () {
+  let stats = []
+  let key = getPadawanNames()
+  let value = getLightsaberScores()
+  for (let i = 0; i < key.length; i++){
+    stats.push([key[i],value[i]])
+  }
+  return stats
+}
+
+function writeStats(stats){
+  let newStats = stats.map((el) => el.join(' ').split(' '))
+  fs.writeFileSync('data/stats.txt', newStats)
+}
 
 module.exports = {
   getPadawanNames,
