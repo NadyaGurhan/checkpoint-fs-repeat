9b76e5ca53668e2f54da28bd871047d995904c0a
Alex <Karunas353@gmail.com>
2021-06-03 14:13:33 +0300

    Check update

--- padawans.js
+++ padawans.js
@@ -1,28 +1,27 @@
 const fs = require("fs");
 
-function getPadawanNames() {
-  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
-}
+const getPadawanNames = () =>
+  fs.readFileSync("data/padawans.txt", "utf-8").trim().split("\n");
 
-function getLightsaberScores() {
-  return fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n').map(Number);
-}
+const getLightsaberScores = () =>
+  fs
+    .readFileSync("data/scores.txt", "utf-8")
+    .trim()
+    .split("\n")
+    .map((el) => Number(el));
 
-function getStats() {
-  let names = getPadawanNames();
-  let stats = getLightsaberScores();
-  let statTable = [];
-  for (let i = 0; i < names.length; i++) {
-    statTable.push(names[i], stats[i]);
-  }
-  return statTable;
-}
+const getStats = () => {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
 
-function writeStats() {
-  return fs.appendFileSync('data/stats.txt', statTable, 'utf-8');
-}
+  return names.map((name, index) => [name, scores[index]]);
+};
 
-// console.log(getLightsaberScores());
+const writeStats = (stats) => {
+  const data = stats.map((el) => el.join(" ")).join("\n");
+  fs.writeFileSync("data/stats.txt", data);
+};
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
