d8fd6a402818229f30fcace8b841e5639e4510b8
Sensaur <zhigarev@bk.ru>
2021-06-03 10:03:22 +0300

    Rel1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
+const fs = require('fs');
+const fileName = "./data/padawans.txt"
+const fileScore = "./data/scores.txt"
+const dataName = fs.readFileSync(fileName, "utf8")
+const dataScore = fs.readFileSync(fileScore, "utf8")
+
+
+function getPadawanNames(filename) {
+  let arr = dataName.split("\n").slice(0);
+  arr.pop()
+  arrNames= arr
+  return arrNames
+}
+
+function getLightsaberScores(fileScore) {
+  let arr = dataScore.split("\n").slice(0);
+  arrScores = arr.map((el) => +el)
+  return arrScores
+}
+
+function getStats(arrNames, arrScores) {
+arrNames.map((el) => el.push(arrScores))
+}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,git
 };
