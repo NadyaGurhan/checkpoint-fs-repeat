e30e16a9c767797d96a4475ce1bba06dd1e47899
nail <derzkiyachot@gmail.com>
2021-06-03 10:10:58 +0300

    release0-10

--- padawans.js
+++ padawans.js
@@ -1,3 +1,4 @@
+const fs=require('fs')
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +6,26 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames(){
+  const padawans=fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  padawans.pop();
+  return padawans;
+}
+
+function getLightsaberScores(){
+  const scores=fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return scores.map((el)=>parseFloat(el));
+  //  return scores.map((el)=>Number(el));
+  //  return scores.map((el)=>+el);
+}
+function getStats(){
+  const fullName=getPadawanNames();
+  const fullScore= getLightsaberScores();
+  return fullName.map((el, index)=>[el,fullScore[index]]);
+}
+
+function writeStats(){
+  const stats=getStats().map((el)=>el.join(',').replace(/,/g, ' ')).join('\n');
+  fs.writeFileSync('./data/stats.txt',stats);
+}
