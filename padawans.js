9debd1d1b4c3618b3dfad61b013f61fbcfd96db4
Makapenko <makap88@gmail.com>
2021-06-03 10:09:44 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,34 @@
 
+
+
+// .split('\n');
+// allPadawans.shift();
+
+
+const fs = require('fs');
+
+function getPadawanNames() {
+  const filePath = './data/padawans.txt';
+  const names = fs.readFileSync(filePath, 'utf8').split('\n');
+  names.pop();
+  return names;
+}
+
+function getLightsaberScores() {
+  const filePath = './data/scores.txt';
+  const scores = fs.readFileSync(filePath, 'utf8').split('\n');
+  return scores.map(item => Number(item));
+}
+
+function getStats() {
+  let result = []
+  result.map()
+  console.log(names, scores)
+}
+
+let writeStats = 1;
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
