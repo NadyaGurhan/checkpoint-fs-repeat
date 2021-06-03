b28e5ee34818466d98401e211394e5d67e8a4e69
Daniil <daniilnardin13@gmail.com>
2021-06-03 10:00:20 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs = require("fs");
+
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf8');
+  const charArr = data.split('\n');
+  return charArr.slice(0, charArr.length - 1)
+} 
+// console.log(getPadawanNames())
+
+function getLightsaberScores() {
+  const dataScore = fs.readFileSync('data/scores.txt', 'utf8');
+  const scoreArr = dataScore.split('\n');
+  return scoreArr.map((e) => Number(e))
+} 
+getLightsaberScores()
+
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
