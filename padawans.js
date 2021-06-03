d8ff540ae08d0e83ad42df1d561112c08343c024
Fedor <fedrdrdr@yandex.ru>
2021-06-03 10:10:08 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,32 @@
+const fs = require("fs");
+
+function getPadawanNames(){
+  const filePath='data/padawans.txt';
+  const names = fs.readFileSync(filePath, "utf8").split("\n");
+  names.pop();
+  return names;
+} 
+
+function getLightsaberScores(){
+  const filePath='data/scores.txt';
+  const names = fs.readFileSync(filePath, "utf8").split("\n");
+  return names.map(item => Number(item))
+}
+function getStats(){
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const result=[]
+    for(let i=0; i<names.length; i++){
+      result[i]=names[i]+' ' + scores[i]
+    }
+    return result
+}
+
+
+
+const writeStats=1;
+
+
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +34,5 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+console.log(getStats())
