6bdab17d8f5264d2f15f2862d01c0ccdcfba42be
Nikita <timandyrak1488@yandex.ru>
2021-06-24 15:01:38 +0300

    release1

--- padawans.js
+++ padawans.js
@@ -1,40 +1,45 @@
-const fs = require('fs');
+const fs = require("fs");
 
+const getPadawanNames = () =>
+  fs.readFileSync(`./data/padawans.txt`, "utf-8").split("\n").slice(0, 4);
 
-function getPadawanNames(err,data){
-   const name = fs.readFile(`./data/padawans.txt`, 'utf-8',(err,data) => {
-    if (err) console.error(err)
-    console.log(data)
-    if(data){
-    let padaArr = name;
-    return padaArr.split(" ")
-    }
-  })
+function getLightsaberScores() {
+  return fs
+    .readFileSync(`./data/scores.txt`, "utf-8")
+    .split("\n")
+    .map((el) => Number(el));
 }
-getPadawanNames();
 
-function getLightsaberScores(){
-  // let padaScores = fs.readFile(`./data/scores.txt`, 'utf-8',() => {
-  // let scoresArr = padaScores.split("\n ");
-  // return scoresArr 
-// })
+function getStats() {
+  let nameList = getPadawanNames();
+  let padaScores = getLightsaberScores();
+  // let arr = [];
+  // for (let i = 0; i < name.length; i++) {
+  //   // console.log(name[i], padaScores[i])
+  //   arr.push([name[i], +padaScores[i]]);
+  //   arr[i].map((x) => +x);
+  // }
+  return nameList.map((name, index) => [name, padaScores[index]]);
 }
- function getStats(){
 
- }
- function  writeStats(){
+function writeStats(stats = []) {
+  // console.log(stats);
+  for (let i = 0; i < stats.length; i++) {
+    //stats.replace(',',' ')
+    fs.appendFileSync(`./data/stats.txt`, `${stats[i][0]} ${stats[i][1]}\n`);
+  }
+  //OR BELOW>>>>>>>>>>>>>>>>>>>>>>>>.
+  // const resultString = stats.reduce(
+  //   (acc, el) => acc + `${el[0]} ${el[1]}\n`,
+  //   ""
+  // );
+  // fs.writeFileSync(`./data/stats.txt`, `${resultString}`);
+  // return resultString;
+}
 
- }
+const res = writeStats(getStats());
 
-
-
-
-
-
-
-
-
-
+console.log(res);
 
 module.exports = {
   getPadawanNames,
