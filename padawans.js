ae110701e5003cd25b512243642d40f3c15b9027
Artem <gabidov05@gmail.com>
2021-06-24 10:03:43 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,41 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let result;
+  fs.readFile('./data/padawans.txt', 'utf8', (err, content) => {
+    let names = [];
+    let counter = 0;
+    let arr = content.split('\n');
+    for (let i = 0; i < arr.length - 1; i++, counter++) {
+      names.push(arr[i]);
+      if (counter === arr.length - 1) {
+        result = names;
+      }
+    }
+  });
+  return result;
+}
+
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  let names = [];
+  fs.readFile('./data/scores.txt', 'utf8', (err, content) => {
+    let arr = content.split('\n');
+    for (let elem of arr) {
+      names.push();
+      }
+      return names;
+  });
+}
+
+function getStats() {
+  
+}
+
+function writeStats() {
+  
+}
 
 module.exports = {
   getPadawanNames,
