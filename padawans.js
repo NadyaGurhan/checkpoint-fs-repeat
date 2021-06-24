083fbef52c68b6dac8cb24d30b49248b94824ebe
Dieloffta <rombow3@gmail.com>
2021-06-24 10:02:35 +0300

    0 tests apssed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,36 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  // let names;
+  let files = fs.readFile('./data/padawans.txt', 'utf-8', (errInfo, fileInfo) => {
+    if (errInfo) return console.log(errInfo);
+    return fileInfo.split('\n');
+  });
+  return files;
+}
+
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
+
+
+
+
+
+
+
+
+
 
 module.exports = {
   getPadawanNames,
