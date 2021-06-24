8cd0c90a311dea839c294b9ac980e0fe6fab4504
Papakhin <papakhin.egor@gmail.com>
2021-06-24 10:05:59 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,25 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const result = fs.readFileSync('data/padawans.txt', 'utf8')
+    .trim()
+    .split('\n');
+  return result;
+}
+
+function getLightsaberScores() {
+  const result = fs.readFileSync('data/scores.txt', 'utf8')
+    .trim()
+    .split('\n');
+  return result;
+
+}
+function getStats() {
+
+}
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
