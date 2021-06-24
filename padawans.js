af4319a5768acd9e7d820c5f8e2e15da1eb0bc00
k0sten <rjcnty@gmail.com>
2021-06-24 10:04:28 +0300

    end 2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
+const fs = require("fs");
 
+function getPadawanNames() {
+  let fileContent = fs.readFileSync("./data/padawans.txt", "utf8");
+  
+  let result = fileContent.split("\n").join(',').split(',')
+  let array = result.slice(0, 4)
+  return array;
+};
+function getLightsaberScores() {
+  let scores = fs.readFileSync("./data/scores.txt", 'utf8')
+  .trim()
+  return scores.split("\n")
+}
+function getStats() {
+
+}
+function writeStats() {
+
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
