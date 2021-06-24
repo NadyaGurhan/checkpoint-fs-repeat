4ec511f9d24ca33ecced0485ce1716c51bc06422
Artur <ites777@gmail.com>
2021-06-24 10:09:55 +0300

    2 test passed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,22 @@
+const fs = require("fs");
+
+function getPadawanNames() {
+ names = fs.readFileSync("./data/padawans.txt", "utf-8")
+ names = names.split('\n')
+ names = names.slice(0, names.length - 1)
+  return names
+}
+
+function getLightsaberScores() {
+ scores = fs.readFileSync("./data/scores.txt", "utf-8")
+ scores = scores.split('\n')
+ scores = scores.map(element => +element)
+  return scores
+}
+
+function getStats() {}
+
+function writeStats() {}
 
 module.exports = {
   getPadawanNames,
