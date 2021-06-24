48f024b38c44b70007c84197815b4ab261b945ed
Alexey <alexeymarkin92@gmail.com>
2021-06-24 10:01:48 +0300

    Release1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,30 @@
+/* eslint-disable */
+
+const fs = require("fs");
+function getPadawanNames() {
+  const getName = fs
+    .readFileSync("./data/padawans.txt", "utf-8")
+    .trim()
+    .split("\n");
+  return getName;
+}
+
+function getLightsaberScores() {
+  const getScore = fs.readFileSync("data/scores.txt", "utf-8");
+  const scores = getScore.split("\n").map((item) => Number(item));
+  return scores;
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  stats = names.map((padawanName, i) => [padawanName, scores[i]]);
+  return stats;
+}
+function writeStats() {
+  const stats = getStats().join("\n");
+  fs.writeFileSync("data/stats.txt", stats);
+}
 
 module.exports = {
   getPadawanNames,
