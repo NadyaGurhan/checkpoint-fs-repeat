2ce9c1875ed3101c2a1cb15c66683e35bd3979e5
007eroxin <007eroxin00@gmail.com>
2021-06-24 15:26:32 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,28 +1,40 @@
-const fs = require('fs');
+const fs = require("fs");
 
+const getPadawanNames = () => {
+  let wars = fs
+    .readFileSync("./data/padawans.txt", "utf8")
+    .split("\n")
+    .slice(0, 4);
+  return wars;
+};
 
-bestCrew = fs.readFileSync("./data/padawans.txt", 'utf8').split('\n');
-function getPadawanNames() {
-let pop = bestCrew.pop()
-return bestCrew; 
-}
-// return console.log(getPadawanNames());
+const getLightsaberScores = () => {
+  let numbs = fs
+    .readFileSync("./data/scores.txt", "utf8")
+    .split("\n")
+    .map((el) => +el);
+  return numbs;
+};
 
+const getStats = () => {
+  let wars = getPadawanNames();
+  let numbs = getLightsaberScores();
+  let elem = [];
+  for (let i = 0; i < wars.length; i++) {
+    elem.push([wars[i], numbs[i]]);
+  }
+  return elem;
+};
 
-
-function getLightsaberScores() {
-
-}
-
-function getStats() {
-
-}
-
-function writeStats() {
-
-}
-
-
+const writeStats = (stars) => {
+  // let stars = getStats()
+  // let elem = [];
+  // let arr;
+  for (let i = 0; i < stars.length; i++) {
+    fs.fs.writeFileSync(`./data/stats.txt`, `${stars[i][0]} ${stars[i][1]}\n`)
+    }
+  
+  }
 
 
 module.exports = {
