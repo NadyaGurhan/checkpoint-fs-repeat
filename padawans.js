86fdc41bd1e8d105071f74d9f98e5d2425468559
Anton <aylarionov@yandex.ru>
2021-06-24 14:32:02 +0300

    checkout

--- padawans.js
+++ padawans.js
@@ -1,3 +1,32 @@
+const fs = require("fs");
+
+function getPadawanNames() {
+  return fs
+    .readFileSync("./data/padawans.txt", "utf-8")
+    .slice(0, -1)
+    .split("\n");
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync("./data/scores.txt", "utf-8")
+    .split("\n")
+    .map((item) => (item = +item));
+}
+
+function getStats() {
+  let arr = [];
+  for (let i = 0; i < getPadawanNames().length; i++) {
+    arr.push([getPadawanNames()[i], getLightsaberScores()[i]]);
+  }
+  return arr;
+}
+
+function writeStats() {
+  let str = getStats().join("\n");
+
+  return fs.writeFileSync("./data/stats.txt", str.replace(/,/g, " "));
+}
 
 module.exports = {
   getPadawanNames,
