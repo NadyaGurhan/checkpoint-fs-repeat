23e86dde684b946c18990ea537e516b81780b151
007eroxin <007eroxin00@gmail.com>
2021-06-24 10:06:08 +0300

    result nikakoi

--- padawans.js
+++ padawans.js
@@ -1,3 +1,29 @@
+const fs = require('fs');
+
+
+bestCrew = fs.readFileSync("./data/padawans.txt", 'utf8').split('\n');
+function getPadawanNames() {
+let pop = bestCrew.pop()
+return bestCrew; 
+}
+// return console.log(getPadawanNames());
+
+
+
+function getLightsaberScores() {
+
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
+
+
+
 
 module.exports = {
   getPadawanNames,
