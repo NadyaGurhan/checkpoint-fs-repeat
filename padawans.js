bb9a795da34e83276b143a9e365e6749bad42279
Nikita <timandyrak1488@yandex.ru>
2021-06-24 10:06:07 +0300

    release0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,40 @@
+const fs = require('fs');
+
+
+function getPadawanNames(err,data){
+   const name = fs.readFile(`./data/padawans.txt`, 'utf-8',(err,data) => {
+    if (err) console.error(err)
+    console.log(data)
+    if(data){
+    let padaArr = name;
+    return padaArr.split(" ")
+    }
+  })
+}
+getPadawanNames();
+
+function getLightsaberScores(){
+  // let padaScores = fs.readFile(`./data/scores.txt`, 'utf-8',() => {
+  // let scoresArr = padaScores.split("\n ");
+  // return scoresArr 
+// })
+}
+ function getStats(){
+
+ }
+ function  writeStats(){
+
+ }
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
   getPadawanNames,
