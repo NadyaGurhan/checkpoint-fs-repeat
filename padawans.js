f457248c6913fc270b9d12d2daade36fb2b6f73c
Roman <mr.baskovr@gmail.com>
2021-06-24 10:04:52 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,40 @@
 
+const fs = require('fs')
+
+function getPadawanNames() {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  for (let i = 0; i < names.length; i++) {
+    if(names[i] === '') {
+      names.splice(i, 1)
+    };
+  }
+  return names;
+}
+
+function getLightsaberScores() {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  for (let i = 0; i < scores.length; i++) {
+    scores[i] = +scores[i];
+  }
+  return scores;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
