b58e0208d56aadf148ed0dba750019cb5cf0e235
shamil <shamil.hanbickov@yandex.ru>
2021-06-24 10:04:06 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,46 @@
+const fs = require("fs")
+
+function getPadawanNames(){
+ //let arr =[]
+  fs.readFile(".data/padawans.txt", "utf8", (err, text) => {
+    return text.split("\n");
+  });
+}
+
+
+
+function getLightsaberScores(){
+let myArr = []
+  fs.readFile(".data/scores.txt","utf8",(err, namber) => {
+    myArr.push(namber)
+});
+}
+
+
+function getStats(){
+  let hero = []
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < myArr.length; j++) {
+      hero.push(arr[i,myArr[j]])
+    
+  }
+  
+}
+return hero
+}
+
+
+function writeStats(){
+
+}
+
+//getPadawanNames()
+
+
+
+
+
+
 
 module.exports = {
   getPadawanNames,
