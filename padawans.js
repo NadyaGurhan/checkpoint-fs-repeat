fe002f6fb77b60cd777bd7e255960ed5c8a168e0
Zeus <maver176005@mail.ru>
2021-07-13 10:05:44 +0300

    release done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,30 @@
+const fs = require("fs");
+
+function getPadawanNames() {
+  const namesFromTxt = fs.readFileSync("./data/padawans.txt", "utf-8");
+  const x = namesFromTxt.split("\n");
+  x.pop();
+  return x;
+}
+
+function getLightsaberScores() {
+  const scoresTxt = fs.readFileSync("./data/scores.txt", "utf-8").split("\n");
+  const arrOfScores = scoresTxt.map((el) => +el);
+  return arrOfScores;
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  return names.map((el, i) => [el, scores[i]]);
+}
+
+function writeStats() {
+  const wholeArr = getStats().join("\n").replace(/,/g, " ");
+  return fs.writeFileSync("data/stats.txt", wholeArr, "utf-8");
+}
+
+console.log(writeStats());
 
 module.exports = {
   getPadawanNames,
