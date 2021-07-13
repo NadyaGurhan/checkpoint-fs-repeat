b4764320d229e0bec2019d07056ded184607657a
Andrey <andrey.ki47@gmail.com>
2021-07-13 11:05:56 +0400

    2/4

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs=require('fs')
+const padawans=fs.readFileSync('./data/padawans.txt','utf-8')
+const scores=fs.readFileSync('./data/scores.txt','utf-8')
+
+function getPadawanNames(){
+ return padawans.split('\n')
+}
+function getLightsaberScores(){
+  
+  return scores.split('\n').map(Number)
+}
+
+function getStats(){
+  const padawansArr= padawans.split('\n')
+  const scoresArr= scores.split('\n')
+  const padawansScore=[];
+  for (let i=0; i<padawansArr.length; i++){
+    padawansScore.push(padawansArr[i])
+    padawansScore.push(scoresArr[i])
+  }
+}
+
+function writeStats(){
+
+}
 
 module.exports = {
   getPadawanNames,
