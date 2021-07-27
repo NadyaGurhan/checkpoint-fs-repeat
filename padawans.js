7c10977238452504daa0afdf2ec1b99795f81d5c
Юлия Глухова <juliaglukhova666@gmail.com>
2021-07-27 11:00:24 +0400

    done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,61 @@
+const path = require('path');
+const fs = require('fs');
+
+function getArrFromTxt(pathToFile) {
+    const fileContent = fs.readFileSync(path.resolve(__dirname, pathToFile), 'utf8');
+    const arr = fileContent.split(/[\n]/);
+    return arr;
+}
+
+function getPadawanNames() {
+    const arr = getArrFromTxt('./data/padawans.txt');
+    return arr;
+}
+
+function getLightsaberScores() {
+    let arr = getArrFromTxt('./data/scores.txt');
+    arr = arr.map((el) => (el = +el));
+    return arr;
+}
+
+function getStats() {
+    const padawans = getArrFromTxt('./data/padawans.txt');
+    let scores = getArrFromTxt('./data/scores.txt');
+    scores = scores.map((el) => (el = +el));
+    const resultArr = [];
+    for (let i = 0; i < padawans.length; i += 1) {
+        resultArr.push([padawans[i], scores[i]]);
+    }
+    return resultArr;
+}
+
+const clearFolder = () => {
+    const files = fs.readdirSync('../task1');
+
+    files.forEach((file) => {
+        if (path.extname(file) === '.txt') {
+            fs.unlinkSync(file);
+        }
+    });
+};
+
+function writeStats() {
+    // fs.unlinkSync('./data/stats.txt');
+    const stats = getStats();
+    const resultArr = [];
+    for (let i = 0; i < stats.length; i += 1) {
+        const str = `${stats[i][0]} ${stats[i][1]}`;
+        resultArr.push(str);
+    }
+    const filePath = './data/stats.txt';
+    fs.appendFile(filePath, resultArr.join('\n'), (err) => {
+        if (err) throw err;
+    });
+}
 
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+    getPadawanNames,
+    getLightsaberScores,
+    getStats,
+    writeStats,
 };
