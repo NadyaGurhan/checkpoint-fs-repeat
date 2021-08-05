c23218bb83ff1751fa9c33486f23105ef5c631ac
Ksenia <ks.nikolaychuk@gmail.com>
2021-08-05 12:41:08 +0300

    Done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,35 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').slice(0, 4);
+  return names;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  const arrayOfScore = [];
+  for (let i = 0; i < scores.length; i += 1) {
+    arrayOfScore.push(Number(scores[i]));
+  }
+  return arrayOfScore;
+}
+
+function getStats() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').slice(0, 4);
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  const result = [];
+  for (let i = 0; i < names.length; i += 1) {
+    const tempArr = [];
+    tempArr.push(names[i], +scores[i]);
+    result.push(tempArr);
+  }
+  return result;
+}
+
+function writeStats() {
+  const wholeArr = getStats().join("\n").replace(/,/g, " ");
+  return fs.writeFileSync("data/stats.txt", wholeArr, "utf-8");
+}
 
 module.exports = {
   getPadawanNames,
