6d9c41fd6d8ae67ebad70e83a4b10bf1d0c63a92
Zhukov Saveliy <82917677+trixelpixel@users.noreply.github.com>
2021-08-26 10:03:05 +0300

    Tests 2/4 passed

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
+const fs = require('fs');
 
+function getPadawanNames() {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return names.split('\n').slice(0,-1)
+}
+
+function getLightsaberScores() {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  let result = scores.split('\n').join(' ').split(' ');
+  return result.map(el => +el);
+}
+
+function getStats() {
+
+}
+
+getLightsaberScores()
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
