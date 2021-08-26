16a377b29721a5e30cc3d30c0f2c31786ce6a334
Monika Mráziková <monika.monika.mrazikova@gmail.com>
2021-08-26 10:06:38 +0300

    Checkpoint: 1 test passed of total 4

--- padawans.js
+++ padawans.js
@@ -1,3 +1,21 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padNames = fs.readFileSync(
+    './data/padawans.txt',
+    'utf-8',
+  );
+  return padNames.split('\n').slice(0, -1);
+}
+
+function getLightsaberScores() {
+}
+
+function getStats() {
+}
+
+function writeStats() {
+}
 
 module.exports = {
   getPadawanNames,
