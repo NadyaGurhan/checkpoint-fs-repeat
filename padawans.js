21e8a740c745caf62d90d5af2c29640aae98f8b0
Oleg <vysockiy91@gmail.com>
2021-08-26 10:00:47 +0300

    плохо

--- padawans.js
+++ padawans.js
@@ -1,3 +1,29 @@
+// const { rejects } = require('assert')
+const fs = require('fs')
+// const { resolve } = require('path')
+
+const filesWay = '../padawans'
+function getPadawanNames(filesWay) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filesWay, 'utf8', (errRead, dataRead) => {
+      if (errRead) reject(errRead)
+      resolve(dataRead)
+    })
+  })
+}
+
+function getLightsaberScores() {
+
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
+
 
 module.exports = {
   getPadawanNames,
