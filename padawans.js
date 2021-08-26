1762dbd900a634aaded2b39f328c9d7138814192
SashaShman <sancho1741@mail.ru>
2021-08-26 10:10:03 +0300

    0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
+const fs = require('fs');
+
+
+function getPadawanNames(){
+  fs.readFile('./data/padawans.txt', 'utf8', (err, data) => {
+    if (err) throw new Error(err)
+    data.split('\n').slice(0,-1) // return data
+  });
+  return data
+}
+// console.log(getPadawanNames())
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
+// fs.writeFile('./hello.txt', newString, (err, data) => {
+//       if (err) console.log('ERRROOORRR!!!!11', err);
+//       // console.log(data);
+//       fs.readFile('./hello.txt', 'utf-8', (err, data) => {
+//         if (err) console.log('ERRROOORRR!!!!11', err);
+//         // console.log(data);
+//       })
+//     })
