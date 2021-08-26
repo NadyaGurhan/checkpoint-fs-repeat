fc746462d2168bac49723ca7c10a70324538949a
Алина <alina.12.98@mail.ru>
2021-08-26 10:06:40 +0300

    2 tests passed

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let result = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return result.split('\n').slice(0, -1);
+}
+function getLightsaberScores() {
+  let result = fs.readFileSync('./data/scores.txt', 'utf-8');
+  let split = result.split('\n');
+  return split.map(el => Number(el));
+}
+
+function getStats() {
+  //   let arr = [];
+  // let names = getPadawanNames();
+  // let scores = getLightsaberScores();
+
+}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
-};
+  //writeStats,
+}
