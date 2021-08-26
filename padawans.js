fa41217751c282f8ef78408d75349b1485493e91
Nikita <mister.keyn@mail.ru>
2021-08-26 10:10:40 +0300

    AAAtests

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
+const fs = require('fs')
 
+function getPadawanNames() {
+  const files = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  let result;
+  result = files.split('\n')
+  result.pop(1)
+  return result
+}
+
+function getLightsaberScores() {
+  const files = fs.readFileSync('./data/scores.txt', 'utf-8');
+  let result;
+  result = files.split('\n');
+  return result.map(el => Number(el));
+}
+
+function getStats() {
+  let result = []
+  const score = getLightsaberScores();
+  const name = getPadawanNames();
+  for (let i = 0; i < name.length; i++) {
+    const stats = [name[i], score[i]] 
+    result.push(stats)
+    
+  }
+  return result
+}
+
+function writeStats() {
+  const all = getStats();
+  const result = all.join('\n').replace(/,/g,' ')
+  fs.writeFileSync('./data/stats.txt', result)  
+}
+  
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
