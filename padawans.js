798d487dd8f40ce5d0d7c2c35a40b32cf2fe997c
larinaleksandr0305@gmail.com <larinaleksandr0305@gmail.com>
2021-08-26 10:07:54 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,33 @@
+const fs = require('fs');
+function getPadawanNames() {
+  fs.readFile("./data/padawans.txt", "utf8", function (error, data) {
+    // console.log(data.split('\n').slice(0,4))
+    if (error) throw error
+    return data.split('\n').slice(0, 4);
+  });
+};
+
+function getLightsaberScores() {
+  fs.readFile("./data/scores.txt", "utf8", function (error, data) {
+    // console.log(data.split('\n').slice(0,4))
+    if (error) throw error
+    return data.split('\n').slice(0, 4);
+  });
+}
+
+function getStats() {
+  fs.open('stats.txt', 'w', (err) => {
+
+  });
+  fs.writeFile("./data/stats.txt", function (error, data) {
+    if (error) throw error
+    return data;
+  });
+};
+
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
