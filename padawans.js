7d4e6f4046f652460ef2c093faea56df03f513f3
Hamzat Hasanov <hasanov9597@mail.ru>
2021-08-26 10:06:46 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
 
+function getPadawanNames() {
+  const path = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return path.split(' \n');
+
+}
+function getLightsaberScores() {
+  let path1 = fs.readFileSync('./data/scores.txt', 'utf8');
+  let number = Number(path1.split(' \n'))
+  return number;
+}
+function getStats() {
+  for (let i = 0; i < getPadawanNames; i += 1) {
+
+  }
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+    writeStats,
 };
