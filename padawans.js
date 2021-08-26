35f3074e53e169b12ded2b21bcc450d5b4c86b8b
Eduard <eduard.gurdjian@gmail.com>
2021-08-26 09:42:00 +0300

    Test 4/4

--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const data = fs.readFileSync("data/padawans.txt", 'utf-8');
+  const result = data.split('\n');
+  result.pop();
+  return result;
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync("data/scores.txt", 'utf-8');
+  const result = data.split('\n').map((elem) => +elem);
+  return result;
+}
+function getStats() {
+  const arrLightsaberScores = getLightsaberScores();
+  return getPadawanNames().map( (elem, index) => [elem, arrLightsaberScores[index]]);
+}
+function writeStats() {
+  fs.writeFileSync('data/stats.txt', getStats().map((elem) => elem.join(' ')).join('\n'));
+}
 
 module.exports = {
   getPadawanNames,
