9dab8f251eb191e8b7107e787b0847aeaeb6454f
Nikita <kaminskiynikita1991@gmail.com>
2021-09-07 09:55:04 +0300

    All tests is passed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,43 @@
+const fs = require('fs');
+
+function getFileDataArr(fileName) {
+  const fileDataStr = fs.readFileSync(`./data/${fileName}`, 'utf-8');
+  const fileDataArr = fileDataStr.split('\n').filter((padawan) => padawan !== '');
+  return fileDataArr;
+}
+
+const padawans = getFileDataArr('padawans.txt');
+const scores = getFileDataArr('scores.txt');
+
+function getPadawanNames() {
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scoresNum = scores.map((score) => parseFloat(score));
+  return scoresNum;
+}
+
+function getStats() {
+  const stats = [];
+  const scoresNum = getLightsaberScores();
+
+  for (let i = 0; i < padawans.length; i++) {
+    stats.push([padawans[i], scoresNum[i]]);
+  }
+  return stats;
+}
+
+function writeStats() {
+  let statsTxt = '';
+  const statsArr = getStats();
+  for (let i = 0; i < statsArr.length; i++) {
+    statsTxt += `${statsArr[i][0]} ${statsArr[i][1]}`;
+    if (i !== statsArr.length - 1) statsTxt += '\n';
+  }
+
+  fs.writeFileSync('./data/stats.txt', statsTxt);
+}
 
 module.exports = {
   getPadawanNames,
