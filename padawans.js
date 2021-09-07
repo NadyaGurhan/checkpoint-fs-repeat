4415de8e50fb8352deb6adea0e099654c0b64b5a
Kirill <belokopytovks2014@yandex.ru>
2021-09-07 09:57:34 +0300

    Checkpoint syncFs - done

--- padawans.js
+++ padawans.js
@@ -1,50 +1,36 @@
 const fs = require("fs");
 
-// function getPadawanNames() {
-//   let readFyle = fs.readdirSync('./data');
-//   let readFyle2 = fs.readFileSync('./data/padawans.txt');
-//   for (let i = 0; i < readFyle2.length; i++) {
-//     console.log(readFyle2[i]);
-//   }
-// }
-// function getPadawanNames() {
-//   fs.readdir('./data', (err, data) => {
-//     for (let i = 0; i < data.length; i++) {
-//       fs.readFile(`./data/${data[i]}`, (err, data) => {
-//         console.log(data[i]);
-//       })
-//       return data[i]
-//     }
-//   })
-// }
-// function getPadawanNames() {
-//   let arr = [];
-//   fs.readFile("data/padawans.txt", "utf8", (err, data) => {
-//     if (err) throw err;
-//     for (let i = 0; i < data.length; i++) {
-//     arr.push(data[i]);
-//     }
-//     return arr;
-//   });
-// }
 function getPadawanNames() {
-  const content = fs.readFileSync("data/padawans.txt", "utf8");
-  let arr = [];
-  let res = arr.push(content.split('\n'));
-  return arr.flat().splice(arr.length -1, 4);
+  const padawansNames = fs.readFileSync("data/padawans.txt", "utf8");
+  const padawansArrNames = padawansNames.split("\n");
+  padawansArrNames.pop();
+  return padawansArrNames;
+}
+function getLightsaberScores() {
+  const padawansScores = fs.readFileSync("data/scores.txt", "utf-8");
+  const padawansArrScores = padawansScores.split("\n");
+  return padawansArrScores.map((el) => +el);
 }
 
-function getLightsaberScores() {
-  const content2 = fs.readFileSync('data/scores.txt', 'utf-8');
-  let arr = [];
-  let res = arr.push(content2.split('\n'));
-  return arr.flat().splice(arr.length - 1, 4).map(el => +el);
+function getStats() {
+  const padawansArrNames = getPadawanNames();
+  const padawansArrScores = getLightsaberScores();
+  const padawansArrStats = new Array(padawansArrNames.length).fill([]);
+  for (let i = 0; i < padawansArrNames.length; i += 1) {
+    padawansArrStats[i] = [padawansArrNames[i], padawansArrScores[i]];
+  }
+  return padawansArrStats;
 }
-console.log(getLightsaberScores());
+
+function writeStats() {
+  const padawansArrStats = getStats();
+  const statsString = padawansArrStats.join("\n").replaceAll(",", " ");
+  fs.writeFileSync("data/stats.txt", statsString);
+}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  // getStats,
-  // writeStats,
+  getStats,
+  writeStats,
 };
