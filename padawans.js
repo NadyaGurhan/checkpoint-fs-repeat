69b4e4d9da206d780ee37c12c3c99701ec2aa0ff
Nikita <nik44226269@yandex.ru>
2021-09-07 10:10:48 +0300

    release1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,34 @@
+const fs = require("fs");
+let padawanNames = fs.readFileSync("./data/padawans.txt", "utf8").toString().split('\n').slice(0,-1);
+
+let lightsaberScores = fs.readFileSync("./data/scores.txt", "utf8").toString().split('\n').map(item => +item);
+
+
+let getPadawanNames = () => {
+  return padawanNames
+}
+
+let getLightsaberScores = () => {
+  return lightsaberScores
+}
+
+let getStats = () => {
+   statsArr = []
+   for (let i = 0; i < padawanNames.length; i++){
+     statsArr.push([`${padawanNames[i]}`, +`${lightsaberScores[i]}`])
+   }
+    return statsArr
+
+}
+console.log(getStats())
+
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+
+
