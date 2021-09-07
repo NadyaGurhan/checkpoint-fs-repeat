4c64f3ff13170b4ca2aa7772f273045a0877ab39
SemyonSergeev <semyonsergeev30@gmail.com>
2021-09-07 10:17:56 +0300

    3 теста из 4

--- padawans.js
+++ padawans.js
@@ -1,3 +1,39 @@
+const fs = require('fs');
+
+const padawanList = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+
+padawanList.pop();
+
+// console.log(padawanList);
+
+function getPadawanNames() {
+  return padawanList;
+}
+
+const scoresList = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+
+// console.log(scoresList);
+
+function getLightsaberScores() {
+  const scoresArr = [];
+  for (let i = 0; i < scoresList.length; i++) {
+    scoresArr.push(Number(scoresList[i]));
+  }
+  return scoresArr;
+}
+
+function getStats() {
+  const statsArr = [];
+  for (let i = 0; i < padawanList.length; i++) {
+    statsArr.push([padawanList[i], Number(scoresList[i])]);
+  }
+  return statsArr;
+}
+
+function writeStats() {
+  const createStatsFile = fs.writeFileSync('./data/stats.txt', `${getStats()}`);
+  return createStatsFile;
+}
 
 module.exports = {
   getPadawanNames,
