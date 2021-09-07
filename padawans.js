53f99c573d28917a5a2df0aa2af6c56106017f6e
qqbit <pavlik.merkulov@gmail.com>
2021-09-07 10:10:12 +0300

    1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,22 @@
+const fs = require('fs');
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+const getPadawanNames = () => { 
+  let padawansArr = padawans.split('\n') 
+  padawansArr.splice(-1, 1);
+  return padawansArr;
+};
+const  scores = fs.readFileSync('./data/scores.txt', 'utf8');
+const getLightsaberScores = () => { 
+  let scoresArr = scores.split('\n') 
+  return scoresArr;
+};
+let scores2 = getLightsaberScores();
+let padawans2 = getPadawanNames();
+
+// const data = fs.writeFileSync(`./stats.txt`, `${arr.join('\n')}`)
+console.log(padawans2)
+console.log(scores2)
+
 
 module.exports = {
   getPadawanNames,
