474fa524b8b7a64025468b2611c5028f5d364242
Kirill <k.pirinen.rus@gmail.com>
2021-09-07 10:13:02 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const read = (path) => fs.readFileSync(path, 'utf8').trim().split('\n');
+const getPadawanNames = (arr=read('./data/padawans.txt')) =>  arr;
+const getLightsaberScores = (arr=read('./data/scores.txt')) => arr.map(e => Number(e));
+const getStats = (arr=getLightsaberScores()) => getPadawanNames().map((e, i) => [e, arr[i]]);
+const result = getStats().map(e => e.join(' ') + '\n');
+const writeStats = arr=result => fs.writeFileSync('./data/stats.txt', result.toString());
 
 module.exports = {
   getPadawanNames,
