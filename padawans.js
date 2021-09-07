105e53f31c995c7084c389a4dc1e627e882bdf68
Veronika Tsvelodub <ver.tsvel@gmail.com>
2021-09-07 10:20:12 +0300

    ckeck

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
+const fs =require('fs')
+function getPadawanNames() {
+  let data = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  let dataArr = data.split('\n')
+  return dataArr
 
+  //console.log(dataArr)
+}
+
+function getLightsaberScores() {
+  let data = fs.readFileSync('data/scores.txt', 'utf-8')
+  let dataArr = data.split('\n').map(el => parseInt(el)) 
+   return dataArr
+   
+}
+function getStats(){
+ let names = getPadawanNames()
+ let stat = getLightsaberScores()
+}
+function writeStats(){
+
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
