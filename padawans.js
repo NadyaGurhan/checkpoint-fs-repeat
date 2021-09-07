160a34f9d54e0cfe728b0653e4a45e0db15254e1
Muxaun <1871mg@gmail.com>
2021-09-07 10:12:55 +0300

    Release 2

--- padawans.js
+++ padawans.js
@@ -1,5 +1,34 @@
+const fs = require('fs');
+let padawanstxt = fs.readFileSync('./data/padawans.txt', 'utf8');
+let scorestxt = fs.readFileSync('./data/scores.txt', 'utf8');
 
-module.exports = {
+padawanstxt = padawanstxt.replace(/\n/g, ', ');
+const padawans = padawanstxt.split(', ');
+padawans.splice(-1,1)
+
+function getPadawanNames() {
+	return padawans;
+}
+
+scorestxt = scorestxt.replace(/\n/g, ', ');
+const scores = scorestxt.split(', ');
+for (let i = 0; i<scores.length; i++) {
+	scores[i] = Number(scores[i]);
+}
+
+function getLightsaberScores() {
+	return scores;
+}
+
+function getStats() {
+	return 5;
+}
+
+function writeStats() {
+	return 5;
+}
+
+	module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
