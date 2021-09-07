86229adc8ced595ba199efddde3f90e6e81ae465
Christina <belyakovachristina@rambler.ruwd>
2021-09-07 10:12:20 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,53 @@
+const fs = require('fs');
+const path = require('path');
 
+function names() {
+  return fs
+    .readFileSync(
+      path.join(__dirname, 'data/padawans.txt'),
+      'utf8',
+    )
+    .split('\n');
+}
+function scores() {
+  return fs
+    .readFileSync(
+      path.join(__dirname, 'data/scores.txt'),
+      'utf8',
+    )
+    .split('\n');
+}
+function getPadawanNames() {
+  const newNames = names();
+  newNames.splice(-2);
+  return newNames;
+}
+
+function getLightsaberScores() {
+  const newScores = scores()
+    .map((num) => (Number(num)));
+  return newScores;
+}
+
+function getStats() {
+  const newNames = names();
+  newNames.splice(-2);
+  const newScores = scores()
+    .map((num) => (Number(num)));
+  const newArr = [
+    [newNames[0].concat(' ', Number(newScores[0]))],
+    [newNames[1].concat(' ', Number(newScores[1]))],
+    [newNames[2].concat(' ', Number(newScores[2]))],
+    [newNames[2].concat(' ', Number(newScores[2]))],
+  ];
+  return newArr;
+}
+console.log(getStats());
+function writeStats() {
+
+}
+
+console.log(getPadawanNames());
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
