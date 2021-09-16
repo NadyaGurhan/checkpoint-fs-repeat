a17e1df3b53e6af2cf65f0b66f8b36ca864c3c5b
Christina <belyakovachristina@rambler.ruwd>
2021-09-16 09:49:42 +0300

    done

--- padawans.js
+++ padawans.js
@@ -35,19 +35,30 @@ function getStats() {
   const newScores = scores()
     .map((num) => (Number(num)));
   const newArr = [
-    [newNames[0].concat(' ', Number(newScores[0]))],
-    [newNames[1].concat(' ', Number(newScores[1]))],
-    [newNames[2].concat(' ', Number(newScores[2]))],
-    [newNames[2].concat(' ', Number(newScores[2]))],
+    [newNames[0], Number(newScores[0])],
+    [newNames[1], Number(newScores[1])],
+    [newNames[2], Number(newScores[2])],
+    [newNames[3], Number(newScores[3])],
   ];
   return newArr;
 }
-console.log(getStats());
-function writeStats() {
+// console.log(getStats());
 
+function writeStats() {
+  const newNames = names();
+  newNames.splice(-2);
+  const newScores = scores()
+    .map((num) => (Number(num)));
+  const text = [
+    [newNames[0].concat(' ', Number(newScores[0]))],
+    [newNames[1].concat(' ', Number(newScores[1]))],
+    [newNames[2].concat(' ', Number(newScores[2]))],
+    [newNames[3].concat(' ', Number(newScores[3]))],
+  ];
+  fs.writeFileSync('data/stats.txt', text.join('\n'));
 }
 
-console.log(getPadawanNames());
+console.log(writeStats());
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
