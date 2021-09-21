0d2165f5925a06addded242f10c59f21258b0414
vladmilko <vladmilko27@gmail.com>
2021-09-21 13:49:06 +0600

    Правильные файлы

--- padawans.js
+++ padawans.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const padawans = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8').split('\n');
 const scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8').split('\n');
 
+for (let i = 0; i < scores.length; i += 1) {
+  scores[i] = +scores[i];
+}
+
 function getPadawanNames() {
   return padawans;
 }
@@ -14,7 +18,7 @@ function getLightsaberScores() {
 function getStats() {
   const newArr = [];
   for (let i = 0; i < padawans.length; i += 1) {
-    newArr.push([`${padawans[i]}, ${scores[i]}`]);
+    newArr.push([padawans[i], scores[i]]);
   }
 
   for (let i = 0; i < newArr.length; i += 1) {
@@ -25,12 +29,12 @@ function getStats() {
 }
 
 function writeStats(arr) {
-  const newArr = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    for (let j = 0; j < arr[i].length; j += 1) {
-      newArr.push(arr[i][j].replace(',', ''));
-    }
-  }
+  const newArr = arr.join('');
+  // for (let i = 0; i < arr.length; i += 1) {
+  //   for (let j = 0; j < arr[i].length; j += 1) {
+  //     newArr.push(arr[i][j].replace(',', ''));
+  //   }
+  // }
   for (let i = 0; i < arr.length; i += 1) {
     fs.writeFileSync(`${__dirname}/data/stats.txt`, `${newArr}\n`);
   }
