8e85455b3bdc5d3fca1b19d766db4689c240a270
DaryaGoloviznina <dal.goloviznina@gmail.com>
2021-09-21 09:57:18 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').trim();
+  return names.split('\n');
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').trim();
+  const arr = scores.split('\n');
+  return arr.map((el) => +el);
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push([names[i], scores[i]]);
+  }
+  return result;
+}
+
+function writeStats(data) {
+  data = (data.map((el) => `${el.join(' ')}\n`)).join('').slice(0, -1);
+  fs.writeFileSync('./data/stats.txt', data);
+}
 
 module.exports = {
   getPadawanNames,
