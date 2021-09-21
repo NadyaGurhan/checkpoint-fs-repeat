3b168ab5663e3293e042555adca78faf89f1d6f7
Anton Tselovalnikov <antselovalnikov@bk.ru>
2021-09-21 10:04:30 +0300

    1/4

--- padawans.js
+++ padawans.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+
+
+function getPadawanNames() {
+  const namesPadawans = fs.readFileSync("./data/padawans.txt", "utf8").split("\n").slice(0, 4);
+}
+getPadawanNames(); 
+
+function getLightsaberScores() {
+  const arrScoresPadawans = fs.readFileSync("./data/scores.txt", "utf8").split("\n");
+  
+
+}
+getLightsaberScores();
+
+
 
 module.exports = {
   getPadawanNames,
