10294ed8550b2d23d3515091df844ad38265bd30
Vlad <woodygarryson@gmail.com>
2021-09-21 10:04:36 +0300

    50/100

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
 
+function getPadawanNames() {
+  return padawans.split('\n').slice(0, -1);
+}
+
+ const arrScores = scores.split('\n');
+
+  function getLightsaberScores() {
+    const arr = [];
+    for (let i = 0; i < arrScores.length; i++) {
+      arr[i] = +arrScores[i];
+    }
+    return arr;
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
