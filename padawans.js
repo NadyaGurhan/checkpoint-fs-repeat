330677815bb1c423d43dc279b0064c8219304a5d
Ivan <lysenkov.orel@gmail.com>
2021-09-28 10:03:32 +0300

    Release 0-1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
+const fs = require('fs');
+const { get } = require('http');
 
+// let padavantxt = 'data/padawans.txt';
+// let scoretxt = 'data/scores.txt';
+// let dataPadavan = fs.readFileSync(padavantxt, 'utf8');
+// let dataScore = fs.readFileSync(scoretxt, 'utf8');
+// dataPadavan += 'text';
+
+
+function getPadawanNames() {
+let padavantxt = 'data/padawans.txt';
+//let scoretxt = 'data/scores.txt';
+let dataPadavan = fs.readFileSync(padavantxt, 'utf8');
+// let dataScore = fs.readFileSync(scoretxt, 'utf8');
+
+return  dataPadavan.trim().split('\n'); 
+}
+
+
+function getLightsaberScores() {
+  let scoretxt = 'data/scores.txt';
+  let dataScore = fs.readFileSync(scoretxt, 'utf8');
+  return dataScore.trim().split('\n').map(elem => Number(elem)) 
+}
+getPadawanNames()
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+getPadawanNames,
+getLightsaberScores,
+//   getStats,
+//   writeStats,
+}
