f8668f5e980dd87df2d0282e47d763bd5b785cd2
Andrew <isofthetruth@gmail.com>
2021-09-28 10:14:07 +0300

    Почти доделал

--- padawans.js
+++ padawans.js
@@ -1,7 +1,42 @@
+const fs = require('fs');
+
+const path = './data/';
+
+const getPadawanNames = () => {
+  const padawans = fs.readFileSync(`${path}padawans.txt`, 'utf-8').split('\n');
+  padawans.pop();
+
+  return padawans;
+};
+
+// getPadawanNames(padawans);
+
+const getLightsaberScores = () => {
+  const score = fs.readFileSync(`${path}scores.txt`, 'utf-8').split('\n');
+  return score.map((item) => Number(item));
+};
+// console.log()
+
+const getStats = () => {
+  const newArr = [];
+
+  const arr1= getPadawanNames()
+  const arr2= getLightsaberScores()
+
+ 
+  for (let i = 0; i < arr1.length; i += 1) {
+    newArr.push(`${arr1[i]}, ${Number(arr2[i])}`)
+  }
+  console.log(newArr)
+  return newArr
+   
+};
+
+// getStats(getPadawanNames(), getLightsaberScores())
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
