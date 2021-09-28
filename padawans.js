c62c673a9d00d59c1038509a8039556732021e22
Vitaliy Moskalevich <phobym@gmail.com>
2021-09-28 10:08:04 +0300

    3\4

--- padawans.js
+++ padawans.js
@@ -1,3 +1,41 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawansNames = fs.readFileSync('./data/padawans.txt', 'utf-8');
+
+  return padawansNames.trim().split('\n');
+}
+
+function getLightsaberScores() {
+  const lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const result = lightsaberScores.trim().split('\n')
+  const resultNum = []
+  for (let i = 0; i < result.length; i++) {
+    resultNum.push(Number(result[i]));
+    
+  }
+  return resultNum;
+}
+
+function getStats() {
+  const stats = [];
+  const padawansNames = getPadawanNames();
+  const lightsaberScores = getLightsaberScores();
+
+  for (let i = 0; i < padawansNames.length; i++) {
+    stats.push([padawansNames[i], lightsaberScores[i]]);
+  }
+  return stats;
+}
+
+function writeStats() {
+  const stats = getStats();
+  for (let i = 0; i < stats.length; i++) {
+    fs.appendFileSync('data/stats.txt', `${stats[i]}\n`, 'utf8');
+  }
+}
+
+writeStats();
 
 module.exports = {
   getPadawanNames,
