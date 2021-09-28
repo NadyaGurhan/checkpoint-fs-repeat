536a5482bdd49eddb1c8a6f53114b0d0f925e56d
Mikhail <novikovmm97@gmail.com>
2021-09-28 10:08:15 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,23 @@
+const fs = require('fs');
+
+const dataPadawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().replace(/^\s+/, ' ');
+const dataScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+function getPadawanNames() {
+  return dataPadawans.split();
+}
+
+function getLightsaberScores() {
+  return dataScores.split();
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
