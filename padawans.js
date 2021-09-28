a7f1a978e8284036e291129f4f547cdd6cd52a7a
UULKHAN M <uulkhan.mairambek@gmail.com>
2021-09-28 10:15:30 +0300

    finish

--- padawans.js
+++ padawans.js
@@ -1,4 +1,21 @@
 
+const fs = require('fs');
+
+function getPadawanNames(){
+  let filePath = "./data/padawans.txt";
+  let data = fs.readFileSync(filePath, "utf8");
+  let splittted = data.trim().split("\n")
+  return splittted
+}
+function getLightsaberScores() {
+  let filePath = "./data/scores.txt";
+  let data = fs.readFileSync(filePath, "utf8");
+  let num= data.split('\n').map((el)=> +el );
+  return num
+}
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
