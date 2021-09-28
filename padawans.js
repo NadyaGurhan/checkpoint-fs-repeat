42782dfc67962b73909eb1898db03e002b856689
talgat.aimbetov.b@gmail.com <84536810+Frataim@users.noreply.github.com>
2021-09-28 10:08:17 +0300

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
+const fs = require('fs')
+let padawans = fs.readFileSync('./data/padawans.txt', 'utf8')
+let scores = fs.readFileSync('./data/scores.txt', 'utf8')
+console.log(padawans)
+function getPadawanNames() {
+  let result = padawans.split('\n')
+  result.pop()
+  return result
+}
+
+function getLightsaberScores() {
+  let result = scores.split('\n')
+  result.pop()
+  return result
+}
+function getStats() {}
+function writeStats() {}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
-};
+}
