691cdce390c763c42fe34c32565e7f1fbe118d74
Dmitry Kim <kim.dima.93@gmail.com>
2021-09-28 09:52:03 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,34 @@
+const fs = require('fs');
+
+function getFileContent(fileName) {
+  return fs.readFileSync(`./data/${fileName}.txt`, 'utf-8');
+}
+
+function writeContentToFile(fileName, content) {
+  fs.writeFileSync(`./data/${fileName}.txt`, content);
+}
+
+function getPadawanNames() {
+  return getFileContent('padawans').trim().split('\n');
+}
+
+function getLightsaberScores() {
+  return getFileContent('scores').trim().split('\n').map((score) => Number(score));
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < padawans.length; i += 1) {
+    result.push([padawans[i], scores[i]]);
+  }
+  return result;
+}
+
+function writeStats() {
+  writeContentToFile('stats', getStats().map((el) => el.join(' ')).join('\n'));
+}
 
 module.exports = {
   getPadawanNames,
