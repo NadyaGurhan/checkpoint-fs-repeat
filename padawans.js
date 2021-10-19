4fd5b92bc4c80a3591cf606b79e478dc1f7756e1
Michael <alsm16.wm@gmail.com>
2021-10-19 10:06:47 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,51 @@
+const { profile } = require("console");
+const fs = require("fs");
 
+function getPadawanNames() {
+  const padawanNames = fs
+    .readFileSync("./data/padawans.txt", "utf-8")
+    .split("\n")
+    .filter((element) => element != "");
+  return padawanNames;
+
+  // List of names
+}
+function getLightsaberScores() {
+  // Light-sword  training scores
+  const scores = fs
+    .readFileSync("./data/scores.txt", "utf-8")
+    .split("\n")
+    .filter((element) => element)
+    .map((element) => Number(element));
+  return scores;
+}
+
+function getStats() {
+  // return list of list, each element is a list
+  // which includes name and score
+  const stats = [];
+  const [names, scores] = [getPadawanNames(), getLightsaberScores()];
+  let profile;
+
+  for (let i = 0; i < names.length; i += 1) {
+    profile = [names[i], scores[i]];
+    stats.push(profile);
+  }
+  return stats;
+}
+getStats();
+
+function writeStats() {
+  // create a new file and save smth inside
+  const statisticsRaw = getStats();
+  const statistics = [];
+  for (let i = 0; i < statisticsRaw.length; i += 1) {
+    let line = statisticsRaw[i].join(" ");
+    statistics.push(line);
+  }
+  fs.writeFileSync("./data/stats.txt", statistics.join("\n"));
+}
+writeStats();
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
