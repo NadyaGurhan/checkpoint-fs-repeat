8e01ca14d7b4d3a9f8b0b232457dc1c2029bd1bb
Sasha Terenteva <sane4ka29051990@gmail.com>
2021-10-19 00:14:54 -0700

    Релиз

--- padawans.js
+++ padawans.js
@@ -1,3 +1,27 @@
+const fs = require('fs');
+const { get } = require('http');
+
+function getPadawanNames() {
+  let allNames = [];
+  let names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  names.split('\n').forEach(item => {
+    if(item !== '') {
+      allNames.push(item);
+    }
+  });
+  return allNames;
+}
+function getLightsaberScores() {
+  let allScore = [];
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  scores.split('\n').forEach(item => {
+    if(item !== '') {
+      allScore.push(+item);
+    }
+  });
+  return allScore;
+}
+
 
 module.exports = {
   getPadawanNames,
