a45d608a7a35d2cd1c6e85ae1eb0ce78aa1c9726
Victoria <vi.park6@gmail.com>
2021-10-19 09:01:42 +0200

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,34 @@
+const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').trim();
+  return names.split('\n');
+}
+
+function getLightsaberScores() {
+  const file = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  let scores = [];
+  for (let i = 0; i < file.length; i++) {
+    scores.push(+file[i]);
+  }
+  return scores;
+}
+
+function getStats() {
+  let names = getPadawanNames();
+  let scores = getLightsaberScores();
+  let list = [];
+  for (let i = 0; i < names.length; i++) {
+    list.push([names[i], scores[i]]);
+  }
+  return list;
+}
+
+function writeStats() {
+  
+}
+
 
 module.exports = {
   getPadawanNames,
