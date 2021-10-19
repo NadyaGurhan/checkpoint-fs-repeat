ac51b9ce73902d6622373de0031ca411280adc26
Oleg Grachev <sk8cool@mail.ru>
2021-10-19 10:11:10 +0300

    only one

--- padawans.js
+++ padawans.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+let padawan = fs.readFileSync('./data/padawans.txt', 'utf-8');
+let stats = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+function getPadawanNames() {
+  padawan = padawan.split('\n')
+  padawan.pop()
+  return padawan
+}
+
+function getLightsaberScores() {
+  stats = stats.split('\n')
+  for (let i=0; i< stats.length;i++) {
+    stats[i] = String(stats[i])
+  }
+  return stats
+}
+
+
+
+// console.log(getLightsaberScores())
+getStats = 0
+writeStats = 0
 
 module.exports = {
   getPadawanNames,
