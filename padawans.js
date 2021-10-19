6207960d38124bd782cc7026cb79c3de0225546a
Egor17358 <egor17358@ya.ru>
2021-10-19 10:11:12 +0300

    2 ok

--- padawans.js
+++ padawans.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
+
+function getPadawanNames () {
+let padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8');
+let namesList = padawanNames.split('\n');
+namesList.pop()
+return namesList;
+}
+
+// console.log(getPadawanNames())
+
+function getLightsaberScores () {
+  let lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  let lightsaberScor = lightsaberScores.split('\n');
+  let result = lightsaberScor.map(i => Number(i));
+  return result;
+}
+// console.log(getLightsaberScores())
+
+function getStats () {
+  // console.log(getPadawanNames())
+}
+
+function writeStats () {
+  // console.log(getPadawanNames())
+}
+
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +32,4 @@ module.exports = {
   getStats,
   writeStats,
 };
+
