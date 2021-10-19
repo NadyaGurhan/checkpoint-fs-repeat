f2fcf05daf29f1b986cdb7d05f3f7533f211e01b
GarriGarrison <garrisonford@yandex.ru>
2021-10-19 10:10:45 +0300

    Test 1, 2, 3

--- padawans.js
+++ padawans.js
@@ -1,7 +1,79 @@
+/* eslint-disable */
+const fs = require("fs")
 
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+
+function getPadawanNames() {
+  const padawanFile = fs.readFileSync("./data/padawans.txt", "utf8")
+  //console.log(padawanFile)
+  let padawanList = padawanFile.split('\n')
+  padawanList.pop()
+  //console.log(padawanList)
+  return padawanList
+}
+
+function getLightsaberScores() {
+  const saberFile = fs.readFileSync("./data/scores.txt", "utf8")
+  //console.log(saberFile)
+  const saberScoreStr = saberFile.split('\n')
+  let saberScore = []
+
+  for (score of saberScoreStr) {
+    let num = +score
+    saberScore.push(num)
+  }
+  //console.log(saberScore)
+  return saberScore
+}
+
+function getStats() {
+  const padawanFile = fs.readFileSync("./data/padawans.txt", "utf8")
+  const saberFile = fs.readFileSync("./data/scores.txt", "utf8")
+
+  let padawanList = padawanFile.split('\n')
+  padawanList.pop()
+
+  const saberScoreStr = saberFile.split('\n')
+  let saberScore = []
+
+  for (score of saberScoreStr) {
+    let num = +score
+    saberScore.push(num)
+  }
+
+  let result = []
+  for (let i = 0; i < padawanList.length; i++) {
+    let temp = []
+    temp.push(padawanList[i])
+    temp.push(saberScore[i])
+    result.push(temp)
+  }
+
+  //console.log(result)
+  return result
+}
+
+function writeStats() {
+  const padawanFile = fs.readFileSync("./data/padawans.txt", "utf8")
+  const saberFile = fs.readFileSync("./data/scores.txt", "utf8")
+
+  let padawanList = padawanFile.split('\n')
+  padawanList.pop()
+
+  let saberScoreList= saberFile.split('\n')
+ 
+  for (let i = 0; i < padawanList.length; i++) {
+    //собираем строку
+  }
+}
+
+
+ module.exports = {
+   getPadawanNames,
+   getLightsaberScores,
+   getStats,
+//   writeStats,
+ };
+
+//getPadawanNames()
+//getLightsaberScores()
+//getStats()
