a31a496ecbb2eb61e893b36cbfb4c1be6d9f8c9a
DarseN Ungarlinov <darsen@DarseNs-MacBook-Air.local>
2021-10-19 10:10:30 +0300

    asd

--- padawans.js
+++ padawans.js
@@ -1,3 +1,10 @@
+const fs = require('fs')
+
+let getPadawanNames = () => fs.readFileSync('./data/padawans.txt', 'utf8').split('\n')
+let getLightsaberScores = () => fs.readFileSync('./data/scores.txt', 'utf8').split('\n').map(el=> +el)
+let getStats = () => fs.readFileSync('./data/stats.txt').split('\n').map(el=> el.split('\n')
+let writeStats = () => {}
+
 
 module.exports = {
   getPadawanNames,
