d2c7ec1fc7e1dc661ce4985580c0a8da8df29651
Nikita <1nikita7@mail.ru>
2021-10-19 10:15:47 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+/* eslint-disable */
+const fs = require('fs');
+
+function getPadawanNames() {
+const padawanws = fs.readFileSync("./data/padawans.txt", "utf-8").split('\n');
+return (padawanws)
+}
+
+function getLightsaberScores() {
+  const scoresSword = fs.readFileSync("./data/scores.txt", "utf-8").split('\n')
+  const mapScoresSword = scoresSword.map(Number)
+ 
+  return (scoresSword)
+}
+
+function getStats() {
+  const padawanws = fs.readFileSync("./data/padawans.txt", "utf-8").split('\n');
+  const scoresSword = fs.readFileSync("./data/scores.txt", "utf-8").split('\n')
+  const mapScoresSword = scoresSword.map(Number)
+ 
+}
+ getStats()
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +30,4 @@ module.exports = {
   getStats,
   writeStats,
 };
+
