38f112f127b7eacc5ab63531cd27638b11d336b7
Anatoly Karpov <karpovplace@gmail.com>
2021-10-19 10:12:27 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
+const fs = require('fs')
+
+function getPadawanNames () {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  names.split(', ');
+  names.length.pop()
+  return names
+}
+
+
+
+
+
+
+
+
+
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
 };
+
+
