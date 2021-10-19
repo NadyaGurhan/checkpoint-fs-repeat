7c7de509165c67ccc0d3461e14bb96570b0b090d
Yuri Briliov <yuribriliov@gmail.com>
2021-10-19 10:11:36 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,31 @@
+const fs = require('fs')
+
+function getPadawanNames(){
+  let padawanData = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  let semiRes =  padawanData.split('\n')
+  let result = semiRes.filter((item)=>{
+    return item.length > 1
+  })
+  return result
+}
+
+function getLightsaberScores(){
+  let swordScore = fs.readFileSync('./data/scores.txt', 'utf-8')
+  let semiRes = swordScore.split('\n')
+  let result = semiRes.map((item)=>{
+    return Number(item)
+  })
+  return result
+}
+
+function getStats(){
+  
+}
+
+function writeStats(){
+
+}
+
 
 module.exports = {
   getPadawanNames,
