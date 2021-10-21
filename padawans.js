a68c28929c88d0be7d5f9e500fe98e350e6a196f
Jony <flekpy@gmail.com>
2021-10-21 10:50:34 -0400

    Checkpoint-fs

--- padawans.js
+++ padawans.js
@@ -1,3 +1,41 @@
+const fs = require("fs");
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+const arrNum = scores.split('\n');
+
+
+function getPadawanNames() {
+  
+  let arr = padawans.split('\n');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '') {
+      let result = arr.splice([i], 1)
+    }
+}
+ return arr;
+} 
+
+function getLightsaberScores() {
+  let resArr = [];
+  for (let i = 0; i < arrNum.length; i++) {
+    resArr.push(Number(arrNum[i]));
+  }
+  return resArr;
+}
+
+function getStats() {
+  const arrResult = [];
+  const getResult = [];
+  for (let i = 0; i < getPadawanNames().length; i++) {
+    getResult.push(arrResult.concat(getPadawanNames()[i], getLightsaberScores()[i]));
+  }
+  return getResult;
+}
+
+function writeStats() {
+  const arr = getStats().map((element) => element.join(' ') ).join('\n');
+  fs.appendFileSync('data/stats.txt', `${arr}`, 'utf-8');
+}
 
 module.exports = {
   getPadawanNames,
