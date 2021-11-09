d8780a41556571ed4394730a31e05e5843e2cb54
Mikhail <mikssur@gmail.com>
2021-11-09 10:14:38 +0300

    1 test: done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,18 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const src = './data/padawans.txt';
+  const padawansList = fs.readFileSync(src, 'utf-8');
+  const list = padawansList.split('\n');
+  const remove = list.pop();
+  return list;
+}
+
+function getLightsaberScores() {}
+
+function getStats() {}
+
+function writeStats() {}
 
 module.exports = {
   getPadawanNames,
