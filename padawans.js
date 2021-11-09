98fcab35d17fa8e1a20a03b81c9d0b419672f85d
Poli <polina-chernogor@mail.ru>
2021-11-09 10:18:17 +0300

    chekPoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
+const fs = require("fs");
 
+function getPadawanNames() {
+  let padawan = fs.readFileSync('./data/padawans.txt', 'utf8').trim();
+  return padawan.split("\n");
+}
+
+
+function getLightsaberScores() {
+  let score = fs.readFileSync('./data/scores.txt', 'utf-8').trim();
+  let score1 = score.split('\n');
+  let arr = [];
+  let scr;
+  for (let i = 0; i < score1.length; i++) {
+    scr = score1[i];
+    scr = + scr;
+    arr.push(scr);
+  }
+  return arr;
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  //getStats,
+  //writeStats,
 };
