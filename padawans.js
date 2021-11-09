234f14fc58401afb1d8cbe7ceb09456ed052b3e4
egor <egornick98@gmail.com>
2021-11-09 10:13:32 +0300

    null)

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
+let ul = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n')
+console.log(ul)
+
+const result = [];
+
+function getPadawanNames() {
+  for (let i = 0; i < ul.length; i++) {
+     result.push(ul[i])
+}
+  return result
+}
+getPadawanNames()
+function getLightsaberScores() {
+
+}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  //getStats,
+  //writeStats,
 };
