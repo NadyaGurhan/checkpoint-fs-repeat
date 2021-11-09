668dfa0fe4e8372cac64c7149e1b9814358bc919
Pavel <t1.saifar@gmail.com>
2021-11-09 10:25:07 +0300

    release 1 2 3

--- padawans.js
+++ padawans.js
@@ -1,7 +1,42 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+  let padawanNames = [];
+  for (let i = 0; i < padawans.length; i++) {
+    padawanNames.push(padawans[i]);
+  }
+  padawanNames.pop()
+  return padawanNames;
+}
+// console.log(getPadawanNames());
+
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  let lightsaberScores = [];
+  for (let i = 0; i < scores.length; i++) {
+    lightsaberScores.push(+scores[i]);
+  }
+  return lightsaberScores;
+}
+// console.log(getLightsaberScores())
+
+function getStats() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  const stats = [];
+  for (let i = 0; i < padawans.length; i++) {
+    stats.push([padawans[i], +scores[i]])
+  }
+  stats.pop();
+  return stats
+}
+console.log(getStats())
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
