f97dc5fadad17543a23d04df7b22ef291e528fc5
IlyaRogovenko <hello@librory.org>
2021-11-09 10:14:52 +0300

    Release 0,5

--- padawans.js
+++ padawans.js
@@ -1,3 +1,29 @@
+const fs = require('fs');
+const { get } = require('https');
+let padawans = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8')
+let scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8')
+
+function getPadawanNames(){
+ let x = padawans.split('\n');
+ let y = x.pop()
+ return x;
+}
+
+function getLightsaberScores(){
+  let x = scores.split('\n')
+  for (let i = 0; i < x.length; i++){
+    x[i] = +x[i]
+  }
+  return x;
+}
+
+function getStats(){
+
+}
+
+function writeStats(){
+
+}
 
 module.exports = {
   getPadawanNames,
