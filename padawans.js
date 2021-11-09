f191abc005e2bdbb090db4537cc86ca868ee93f6
Natalya7772222 <71067949+Natalya7772222@users.noreply.github.com>
2021-11-09 10:09:37 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
+const fs = require('fs');
 
+// function readFile() {
+//   const data = fs.readFileSync('../padawans.txt', 'utf-8').trim().split('\n');
+//   return data;
+// }
+
+function getPadawanNames() {
+  const res = fs.readFileSync('data/padawans.txt', 'utf-8').trim().split('\n');
+  return res;
+}
+
+function getLightsaberScores() {
+  const res = fs.readFileSync('data/scores.txt', 'utf-8').trim().split('\n').map((score) => Number(score));
+  return res;
+}
+
+function getStats() {
+  const scores = getPadawanNames();
+  const padawans = getLightsaberScores();
+  const stats = scores.map((item,i) => [item,padawans[i]]);
+  return stats;
+}
+function writeStats() {
+  const res = getStats();
+  const changedData = res.map((padawan) => padawan.join(' ')).join('\n');
+  fs.writeFileSync('data/stats.txt', changedData);
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
