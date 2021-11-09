1b90956fa57b4d0551442dcc3abd1757282db460
Ekaterina Kalashnikova <mochalova_ekaterina@lenta.ru>
2021-11-09 10:00:04 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,26 @@
+const fs = require('fs')
+
+function getPadawanNames() {
+  let result = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n')
+  return result.filter(el => el !== '')
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map(el => +el)
+}
+
+function getStats() {
+  let padawans = getPadawanNames();
+  let scores = getLightsaberScores();
+  return padawans.map((el, i) => [el, scores[i]])
+}
+
+
+function writeStats(){
+  let list = getStats()
+  let result = list.map(el => el.join(' '))
+  result.forEach(el => fs.appendFileSync('data/stats.txt', `${el}\n`))
+}
 
 module.exports = {
   getPadawanNames,
