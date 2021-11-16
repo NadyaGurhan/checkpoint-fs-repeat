19c122d77b8942e7225b8f1f7dc1bdddafdfffe0
Artem <zakharovartem0000@gmail.com>
2021-11-16 08:23:34 +0100

    не успел сделать последнее

--- padawans.js
+++ padawans.js
@@ -1,3 +1,36 @@
+const fs = require("fs");
+const padawans = fs.readFileSync("./data/padawans.txt", "utf-8").split("\n");
+const score = fs.readFileSync("./data/scores.txt", "utf-8").split("\n");
+
+function getPadawanNames() {
+  return padawans.filter((el) => el.length > 1);
+}
+
+// console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  score.sort((a, b) => a + b);
+
+  return score.map((item) => +item);
+}
+
+// console.log(getLightsaberScores());
+
+function getStats() {
+  let result = [];
+  for (let i = 0; i < padawans.length - 1; i++) {
+    result.push([padawans[i], Number(score[i])]);
+  }
+
+  return result;
+}
+
+console.log(getStats());
+
+function writeStats() {
+  let filePath = `./data/stats.txt`;
+  return fs.writeFileSync(filePath, `${}}`);
+}
 
 module.exports = {
   getPadawanNames,
