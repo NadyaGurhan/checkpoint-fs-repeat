46b9633dd66e145af2fd33e893982018ad2cbda6
Elena <ele313054@yandex.ru>
2021-11-16 14:40:15 +0400

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,38 @@
 
+
+const fs = require('fs');
+
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').splice(0, 4);
+  return padawans;
+}
+
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+  return scores;
+}
+let array = [];
+function getStats() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+  for (let i = 0; i < 4; i++) {
+    let arr = [];
+    arr.push(padawans[i], scores[i]);
+    array.push(arr)
+  }
+  return array;
+}
+
+function writeStats() {
+
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
-};
+}
