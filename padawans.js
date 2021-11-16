ca4cb4e9d8b30770961c86f4f59527e084605949
Denis <adadenis1982@yandex.ru>
2021-11-16 09:44:20 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,25 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+}
+function getLightsaberScores() {
+  return fs
+    .readFileSync('./data/scores.txt', 'utf-8')
+    .split('\n')
+    .map((el) => Number(el));
+}
+function getStats() {
+  const name = getPadawanNames();
+  const scores = getLightsaberScores();
+  return name.map((el, i) => [name[i], scores[i]]);
+}
+function writeStats() {
+  return fs.writeFileSync(
+    './data/stats.txt',
+    `${getStats().join('\n').replaceAll(',', ' ')}`,
+  );
+}
 
 module.exports = {
   getPadawanNames,
