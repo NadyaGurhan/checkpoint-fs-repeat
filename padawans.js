89f220197e4832ee820ac95ae9a8cc450d1d282d
Ilya <westandsouth2@gmail.com>
2021-11-16 20:02:53 +0300

    SOS

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
+const fs = require('fs');
 
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+  return names;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  return scores.forEach((num) => +num);
+}
+function getStats() {
+  const arr = `${getPadawanNames}, ${getLightsaberScores}`;
+  return [arr];
+}
+function writeStats() {
+  // const data = getStats();
+  // return fs.writeFileSync('./data/stats.txt', data.join('\n'));
+}
+writeStats();
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
