5bb90f433ca943c8fa3b388dc546334f28480eae
VVonderfu11 <kabaka093@gmail.com>
2021-11-16 12:05:05 +0500

    Test 1-2

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
+const fs = require('fs');
 
+function getPadawanNames() {
+  const dataOfPadawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const padawanNames = dataOfPadawans.split('\n');
+  padawanNames.pop();
+  return padawanNames;
+}
+function getLightsaberScores() {
+  const dataofscores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const scores = dataofscores.split('\n');
+  for (let i = 0; i < scores.length; i += i) {
+    scores[i] = +scores[i];
+  }
+  return scores;
+}
+function getStats() {
+
+}
+function writeStats() {
+
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
