02a095de5c7caf39192b91de73a3316b0a13ef5d
Ali <ali-ozdemirov@mail.ru>
2021-11-26 17:39:16 +0300

    fs

--- padawans.js
+++ padawans.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
+
+function getPadawanNames () {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+}
+
+function getLightsaberScores () {
+  const arr = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return arr.map((el) => +el)
+}
+
+function getStats () {
+  let result = []
+  const padawan = getPadawanNames()
+  const scores = getLightsaberScores()
+  for (let i = 0; i < padawan.length; i++) {
+  result.push([padawan[i], scores[i]])
+  
+  }
+  return result
+}
+
+function writeStats (data) {
+  data = getStats()
+  return fs.writeFileSync('./data/stats.txt',data.join('\n').replace(/,/g, ' '));
+  
+}
 
 module.exports = {
   getPadawanNames,
