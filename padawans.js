700df567e7a59c18b9c204f00c71ebd003ddeeab
Murad <murad186992@mail.ru>
2021-11-26 18:44:26 +0300

    fs

--- padawans.js
+++ padawans.js
@@ -1,7 +1,51 @@
 
+const fs = require('fs')
+
+
+
+function getPadawanNames () {
+  return fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n')
+  
+}
+
+
+
+ function getLightsaberScores () {
+  const a =  fs.readFileSync('./data/scores.txt', 'utf8').split('\n')
+  return a.map(el => el*1)
+  
+ }
+
+  function getStats() {
+     const padawan = getPadawanNames()
+     const scores = getLightsaberScores()
+     let result = []
+     for(let i = 0; i < padawan.length; i++){
+     result.push([padawan[i], scores[i]])
+       
+     }
+     
+    return result
+  }
+
+
+
+function writeStats (data) {
+  data = getStats()
+  return fs.writeFileSync('./data/stats.txt',data.join('\n').replace(/,/g, ' '));
+
+}
+
+
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
+getStats,
   writeStats,
 };
+
