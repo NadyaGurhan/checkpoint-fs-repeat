16469db8d9fdc3323f40fccd69932054802c7c92
Evgeniy <JenyaUtkin90@yandex.ru>
2021-11-30 10:02:09 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
 
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n')
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n')
+function getPadawanNames() {
+  padawans.splice(4)
+  return padawans
+
+}
+getPadawanNames()
+
+function getLightsaberScores() {
+  scores.splice(4)
+  arrSxores = []
+  console.log(scores)
+  for(let i = 0; i < scores.length; i++) {
+    
+  }
+  return scores
+}
+getLightsaberScores()
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
