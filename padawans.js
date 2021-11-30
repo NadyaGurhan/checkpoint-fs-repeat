db829ba0e8d3578647233299375f2e44fe158210
Nick B <nikita.bukof@gmail.com>
2021-11-30 10:02:04 +0300

    Checkpoint_1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
 
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+const getPadawanNames = () => {
+  let nameArr = [];
+  let text =  fs.readFileSync('./data/padawans.txt', 'utf-8');
+
+  return nameArr.push(text);
+
+}
+getPadawanNames();
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
