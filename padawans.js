e4dd5f13b8267df300db349cc8703958f5f34603
Enumka <xzxmohctepxzx93@gmail.com>
2021-11-30 09:58:54 +0300

    Done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,25 @@
+const fs = require('fs');
+
+const padavans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+const getPadawanNames = () => padavans.trim().split('\n');
+
+const getLightsaberScores = () => scores.split('\n').map((el) => Number(el));
+
+const getStats = () => {
+  const padavan = getPadawanNames();
+  const score = getLightsaberScores();
+  return (result = padavan.map((el, i) => [el, score[i]]));
+  // return result;
+};
+const resultOne = getStats();
+
+const writeStats = () => {
+  const itog = resultOne.join('\n').split(',').join(' ');
+  fs.writeFileSync('data/stats.txt', itog);
+};
 
 module.exports = {
   getPadawanNames,
