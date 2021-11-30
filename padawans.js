b7fb640adc21ee02d5ca97e91f18a62d312e4302
Nagornaya <o.nagornaya7@gmail.com>
2021-11-30 10:02:17 +0300

    release -0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,19 @@
 
+
+function getPadawanNames(padawans) {
+  const fs = require('fs');
+  const names = fs.readFileSync('data/padawans.txt.txt', 'utf-8');
+  const padawans = names.split('\n');
+  // console.log(padawans);
+return padawans;
+}
+getPadawanNames(padawans);
+
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
