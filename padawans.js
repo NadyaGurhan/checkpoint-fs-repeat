5846fd19e25d58cc0ef1260e78f0540e3bccf484
Evgeniy <JenyaUtkin90@yandex.ru>
2021-12-02 20:39:03 +0300

    finish

--- padawans.js
+++ padawans.js
@@ -1,28 +1,52 @@
 
 const fs = require('fs');
 
-const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n')
-const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n')
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n')
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n')
+
+
 function getPadawanNames() {
-  padawans.splice(4)
+  //padawans.splice(4)
   return padawans
 
 }
-getPadawanNames()
 
 function getLightsaberScores() {
-  scores.splice(4)
-  arrSxores = []
-  console.log(scores)
-  for(let i = 0; i < scores.length; i++) {
-    
-  }
-  return scores
+  //arrSxores = []
+ // console.log(scores)
+  // for(let i = 0; i < scores.length; i++) {
+  //   arrSxores.push(Number(scores[i]))
+  // }
+  let newArr = scores.map((el) =>  Number(el))
+  return newArr
 }
-getLightsaberScores()
+function getStats () {
+  // let newArr = []
+  // for(let i = 0; i < padawans.length; i++) {
+  //   newArr.push([padawans[i], Number(scores[i])])
+  // }
+  // return newArr
+  let result = padawans.reduce((acc, el, index) => {
+    acc.push([el, Number(scores[index])])
+    return acc
+  } ,[] )
+  return result
+}
+
+function writeStats () {
+ // return newArr
+  let result = padawans.reduce((acc, el, index) => {
+  acc.push([el, scores[index]])
+  return acc
+} ,[] )
+  let data = result.join('\n').replace(/\,/g, ' ')
+  console.log(data)
+  fs.writeFileSync('./data/stats.txt', data, () => console.log('file saved') )
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  // getStats,
-  // writeStats,
+  getStats,
+  writeStats,
 };
