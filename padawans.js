3cb908712b6735dfa1b26c035b8b9a1cf4a3f785
Andrew <andrey.kitaev55@gmail.com>
2021-12-04 14:29:05 +0300

    Release 2.0

--- padawans.js
+++ padawans.js
@@ -1,37 +1,27 @@
 const fs = require('fs');
 
 function getPadawanNames() {
-  const mass = fs.readFileSync('./data/padawans.txt', 'utf-8').toString().split('\n');
-  mass.pop();
-  return mass;
+  return fs.readFileSync('data/padawans.txt', 'utf-8').split('\n').filter(a => a !== '');
 }
 
 function getLightsaberScores() {
-  const mass = fs.readFileSync('./data/scores.txt', 'utf-8').toString().split('\n');
-  return mass.map(a => +a);
+  return fs.readFileSync('data/scores.txt', 'utf-8').split('\n').filter(a => a !== '').map(a => +a);
 }
 
 function getStats() {
-  const mass = [];
-  const padawansNames = getPadawanNames();
-  const LightsaberScores = getLightsaberScores();
-  for (let i = 0; i < padawansNames.length; i++) {
-    const newMass = [];
-    newMass.push(padawansNames[i]);
-    newMass.push(LightsaberScores[i]);
-    mass.push(newMass);
-  }
-  for (let user of mass) {
-    fs.appendFileSync('./data/stats.txt', `${user}\n`);
-  }
-  return mass;
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const padawansScores = padawans.reduce((acc, item, index) => {
+    acc.push([item, scores[index]]);
+    return acc;
+  }, []);
+  return padawansScores;
 }
 
 function writeStats() {
   const stats = getStats();
-  const statsString = stats.map(a => a.toString());
-  for (let user of statsString) {
-    fs.appendFileSync('./data/stats.txt', `${user}\n`);
+  for(let item of stats) {
+    fs.appendFileSync('data/stats.txt', `${item.join(' ')}\n`)
   }
 }
 
