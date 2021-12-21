025a0b512ff91a02bf1f4757e6ed041177bc2134
AlexG <g_engineer@mail.ru>
2021-12-21 13:22:04 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,25 +1,33 @@
 const fs = require("fs");
 const { func } = require("prop-types");
-const padawans =fs.readFileSync(__dirname + "/data/padawans.txt", "utf8");
-const scores =fs.readFileSync(__dirname + "/data/scores.txt", "utf8");
-
+const padawans = fs.readFileSync(__dirname + "/data/padawans.txt", "utf8");
+const scores = fs.readFileSync(__dirname + "/data/scores.txt", "utf8");
 function getPadawanNames() {
-  let result = padawans.split('\n');
-  result.pop();
+  let result = padawans.trim().split('\n');
   return result;
 }
-getPadawanNames();
+
 
 function getLightsaberScores() {
 let scoreString = scores.split('\n');
-
-return scoreString.map(i => +i);
-} getLightsaberScores();
+return scoreString.map(i => i * 1);
+} 
 
 function getStats() {
+let padawan = getPadawanNames();
+let score = getLightsaberScores();
+let arr = [];
+for (let i = 0; i < padawan.length; i++) {
+arr.push([padawan[i], score[i]]);
 
 }
+return arr;
+}
+function writeStats() {
+  let expor = getStats();
+return fs.writeFileSync("data/stats.txt", expor.join('\n').split(',').join(' '));
 
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
