c0e08bcd7379f97640c0efb79bdd40682018d47a
Abu-Bakar <abu-bakar96@mail.ru>
2021-12-21 10:13:58 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,16 @@
+const fs = require("fs");
+const padawans = fs.readFileSync(__dirname+'/data/padawans.txt', 'utf8');
+const scores = fs.readFileSync(__dirname+'/data/scores.txt', 'utf8');
+
+ function getPadawanNames() {
+ let result = padawans.split('\n')
+ return result
+ }
+
+ function getLightsaberScores () {
+   let result = scores.split('\n')
+ return result
+ }
 
 module.exports = {
   getPadawanNames,
