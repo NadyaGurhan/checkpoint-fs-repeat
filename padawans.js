3bef3489fb90f51d7b2d91c8b445a8133bd15712
Almambet <alymkulov.almambet@gmail.com>
2021-12-21 10:14:45 +0300

    chekpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,20 @@
-
+const fs = require('fs');
+const { all } = require('micromatch');
+const allPadavan = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf8'); 
+let allScores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf8');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
 };
+function getPadawanNames() {
+return allPadavan.split('\n');
+}
+function getLightsaberScores() {
+  let arrScores = allScores.split('\n');
+  let numScores = arrScores.map(Number);
+return numScores;
+}
+function getStats() {
+  return [[getPadawanNames()[0], getLightsaberScores()[0], [getPadawanNames()[1], getLightsaberScores()[1]], [getPadawanNames()[2], getLightsaberScores()[2]], [getPadawanNames()[3], getLightsaberScores()[3]]]
+}
