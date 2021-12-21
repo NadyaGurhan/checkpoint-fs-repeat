4547a6d13829fcbd3c062404187a1eee655abdf6
Igor S <tombcold@gmail.com>
2021-12-21 10:12:03 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let text = fs.readFileSync(__dirname+'/data/padawans.txt','utf8');
+  let spl = text.split('\n');
+  let pads = spl.slice(0, -1);
+  return pads;
+}
+// console.log(getPadawanNames());
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
