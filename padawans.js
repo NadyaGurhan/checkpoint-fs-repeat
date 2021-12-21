a391caa0107e243047fad5f3099fa9d971112a78
Seregb <seregb68@gmail.com>
2021-12-21 10:11:15 +0300

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,39 @@
+const fs = require('fs');
+
+getStats();
+
+function getPadawanNames(){
+  let padawanList = fs.readFileSync('./data/padawans.txt','utf8');
+  padawanList = padawanList.split('\n');
+  padawanList.pop();
+  return padawanList;
+};
+
+function getLightsaberScores(){
+  let scoreList = fs.readFileSync('./data/scores.txt','utf8').split('\n');
+  for (let i = 0; i < scoreList.length; i++){
+    scoreList[i] = +scoreList[i];
+  }
+  return scoreList;
+};
+
+
+function getStats(){
+  let sidearr = [];
+  let arr = [];
+  let names = NaN;
+  let scores = NaN;
+  for (let i = 0; i < getPadawanNames().length; i++){
+    arr.push(getPadawanNames()[i]);
+    arr.push(getLightsaberScores()[i]);
+  }
+  console.log(arr);
+};
+
+function writeStats(){
+  
+};
+
 
 module.exports = {
   getPadawanNames,
