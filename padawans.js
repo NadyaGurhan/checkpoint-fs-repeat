e1b492d4732f7519bba86bb3f7d92a8e17f65020
Bulat Khisamov <bulatkhisamov25142@gmail.com>
2021-12-21 09:46:39 +0300

    made two task

--- padawans.js
+++ padawans.js
@@ -1,7 +1,22 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let a = fs.readFileSync('data/padawans.txt', 'utf-8')
+  let arr = a.split("\n")
+  arr.pop()
+  return arr
+}
+function getLightsaberScores() {
+  let a = fs.readFileSync('data/scores.txt', 'utf-8')
+  let arr = a.split("\n")
+  return arr.map(item => +item)
+
+}
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  /*getStats,
+  writeStats,*/
 };
