40272639013cfd92a4f5131b018100768e16dcfb
AlexG <g_engineer@mail.ru>
2021-12-21 10:15:25 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs = require("fs");
+const { func } = require("prop-types");
+const padawans =fs.readFileSync(__dirname + "/data/padawans.txt", "utf8");
+const scores =fs.readFileSync(__dirname + "/data/scores.txt", "utf8");
+
+function getPadawanNames() {
+  let result = padawans.split('\n');
+  result.pop();
+  return result;
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+let scoreString = scores.split('\n');
+
+return scoreString.map(i => +i);
+} getLightsaberScores();
+
+function getStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
