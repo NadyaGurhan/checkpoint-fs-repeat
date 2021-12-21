fcc715fec0ca24ff3b6cbfbae35736bcb644f84a
Bulat Khisamov <bulatkhisamov25142@gmail.com>
2021-12-21 10:04:09 +0300

    all done

--- padawans.js
+++ padawans.js
@@ -6,17 +6,46 @@ function getPadawanNames() {
   arr.pop()
   return arr
 }
+
 function getLightsaberScores() {
   let a = fs.readFileSync('data/scores.txt', 'utf-8')
   let arr = a.split("\n")
   return arr.map(item => +item)
-
 }
+
+function getStats() {
+  let name = fs.readFileSync('data/padawans.txt', 'utf-8').split("\n")
+  let number = fs.readFileSync('data/scores.txt', 'utf-8').split("\n")
+  name.pop()
+  let arr3 = []
+  for (i = 0; i < name.length; ++i) {
+    arr3[i] = [name[i], +number[i]]
+  }
+  fs.writeFileSync('data/stats.txt', arr3.reduce((accum, item) => accum += item + '\n', ""), (err) => {
+    if (err) console.log(err)
+  })
+  return arr3
+}
+
+function writeStats() {
+  let name = fs.readFileSync('data/padawans.txt', 'utf-8').split("\n")
+  let number = fs.readFileSync('data/scores.txt', 'utf-8').split("\n")
+  name.pop()
+  let arr3 = []
+  for (i = 0; i < name.length; ++i) {
+    arr3[i] = name[i] + " " + number[i]
+  }
+  fs.writeFileSync('data/stats.txt', arr3.reduce((accum, item) => accum += item + '\n', ""), (err) => {
+    if (err) console.log(err)
+  })
+}
+
+writeStats()
 
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  /*getStats,
-  writeStats,*/
+  getStats,
+  writeStats,
 };
