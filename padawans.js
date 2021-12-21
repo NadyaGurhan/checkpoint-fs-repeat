4b9492d42d721b890ed3c254f0f3a3675e4c597e
Alexxx <elbruswork1@gmail.com>
2021-12-21 10:22:59 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,40 @@
+const fs = require("fs");
+let padawan = fs.readFileSync("./data/padawans.txt", "utf8");
+let scorese = fs.readFileSync("./data/scores.txt", "utf8");
+
+function getPadawanNames () {
+return padawan.trim().split('\n')
+}
+getPadawanNames()
+
+
+function getLightsaberScores() {
+ let mass =  scorese.split('\n');
+ let xxx = mass.map(Number)
+ return xxx
+}
+getLightsaberScores()
+
+function getStats () {
+   let res = []
+   let newPad = padawan.trim().split('\n')
+   let newScore = scorese.split('\n');
+   for (let i = 0; i < newPad.length; i++) {
+      res.push(newPad[i] + ", " + newScore[i])
+      
+   }
+   console.log(res[0])
+   return res
+}
+getStats()
+
+
+
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+//   writeStats,
 };
