436c8df54fab2c2d45482fead287bcfa702ba444
Hadzhi95 <kreyg1992@bk.ru>
2021-12-21 10:24:09 +0300

    Check1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
 
+const fs = require('fs')
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8')
+const scores = fs.readFileSync('./data/scores.txt', 'utf8')
+
+function getPadawanNames() {
+  return padawans.trim().split('\n')
+}
+
+
+function getLightsaberScores() {
+return scores.split('\n')
+console.log(scores);
+}
+function getStats() {
+  const newScores = getLightsaberScores()
+  const newPadawans = getPadawanNames();
+  let result = []
+  for (let i = 0; i < newPadawans.length; i++) {
+    result.push([newPadawans[i], newScores[i]])
+  }
+  return result
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
