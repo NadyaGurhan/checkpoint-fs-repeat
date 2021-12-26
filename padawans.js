cc649869a20d149848c271599d97b40d70cee652
Bulat Khisamov <bulatkhisamov25142@gmail.com>
2021-12-26 18:12:54 +0300

    Refactoring

--- padawans.js
+++ padawans.js
@@ -1,47 +1,32 @@
 const fs = require('fs');
 
+const padawans = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+padawans.pop();
+
+const scores = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+
 function getPadawanNames() {
-  let a = fs.readFileSync('data/padawans.txt', 'utf-8')
-  let arr = a.split("\n")
-  arr.pop()
-  return arr
+  return padawans;
 }
 
 function getLightsaberScores() {
-  let a = fs.readFileSync('data/scores.txt', 'utf-8')
-  let arr = a.split("\n")
-  return arr.map(item => +item)
+  return scores.map((item) => +item);
 }
 
 function getStats() {
-  let name = fs.readFileSync('data/padawans.txt', 'utf-8').split("\n")
-  let number = fs.readFileSync('data/scores.txt', 'utf-8').split("\n")
-  name.pop()
-  let arr3 = []
-  for (i = 0; i < name.length; ++i) {
-    arr3[i] = [name[i], +number[i]]
+  const arr3 = [];
+  for (let i = 0; i < padawans.length; i += 1) {
+    arr3[i] = [padawans[i], +scores[i]];
   }
-  fs.writeFileSync('data/stats.txt', arr3.reduce((accum, item) => accum += item + '\n', ""), (err) => {
-    if (err) console.log(err)
-  })
-  return arr3
+  const content = arr3.reduce((accum, item) => `${accum + item.join(' ')}\n`, '');
+  fs.writeFileSync('data/stats.txt', content);
+  return arr3;
 }
 
 function writeStats() {
-  let name = fs.readFileSync('data/padawans.txt', 'utf-8').split("\n")
-  let number = fs.readFileSync('data/scores.txt', 'utf-8').split("\n")
-  name.pop()
-  let arr3 = []
-  for (i = 0; i < name.length; ++i) {
-    arr3[i] = name[i] + " " + number[i]
-  }
-  fs.writeFileSync('data/stats.txt', arr3.reduce((accum, item) => accum += item + '\n', ""), (err) => {
-    if (err) console.log(err)
-  })
+  const content = padawans.reduce((accum, item, index) => `${accum + item} ${scores[index]}\n`, '');
+  fs.writeFileSync('data/stats.txt', content);
 }
-
-writeStats()
-
 
 module.exports = {
   getPadawanNames,
