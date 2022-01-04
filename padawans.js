870cef811470e9c2feb74bd81c4b1079037ecc5f
Svetlana Shiianova <92839518+Shiianova-S@users.noreply.github.com>
2022-01-05 01:13:14 +0300

    done

--- padawans.js
+++ padawans.js
@@ -13,19 +13,15 @@ function getPadawanNames() {
 }
 
 function getLightsaberScores() {
-  const score = [];
-  const scoreStrArr = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').filter(Number);
-  for (let i = 0; i < scoreStrArr.length; i += 1) {
-    score.push(+scoreStrArr[i]);
-  }
-  return score;
+  const scoreStrArr = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').filter(String).map( key => +key);
+  return scoreStrArr;
 }
 
 function getStats() {
   const getStat = [];
   const padavans = getPadawanNames();
   const score = getLightsaberScores();
-  for (let i = 0; i < padavans.length; i += 1) {
+  for (let i = 0; i < padavans.length; i++) {
     const arr = [padavans[i], score[i]];
     getStat.push(arr);
   }
@@ -35,7 +31,7 @@ function getStats() {
 function writeStats() {
   const stat = getStats();
   const newStat = stat.join('\n').split(',').join(' ');
-  fs.writeFileSync('./data/stats.txt', newStat);
+  fs.writeFileSync('data/stats.txt', newStat);
 }
 
 
