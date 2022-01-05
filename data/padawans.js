70d9e761b23f1d32a46e15de5cfd199e19b7e87a
Rushan <rushan.bil@gmail.com>
2022-01-05 17:18:55 +0300

    Release 2

--- data/padawans.js
+++ data/padawans.js
@@ -2,7 +2,7 @@ function getPadawanNames() {
   const fs = require("fs");
   let text = fs.readFileSync("data/padawans.txt", "utf8");
   let temp = text.split('\n');
-  let result = temp.splice(0, 4);
+  let result = temp.splice(0, temp.length - 1);
   return result;
 }
 
@@ -11,31 +11,42 @@ function getLightsaberScores() {
   let text = fs.readFileSync("data/scores.txt", "utf8");
   let temp = text.split('\n');
   for (let i = 0; i < temp.length; i++) {
-    temp[i] = +temp[i];
+    temp[i] = Number(temp[i]);
   }
 
-  let result = temp.splice(0, 4);
+  let result = temp.splice(0, temp.length - 1);
   return result;
 }
 
-// function getStats() {
-//   const fs = require("fs");
-//   let text = fs.readFileSync("data/padawans.txt", "utf8");
-//   let temp = text.split('\n');
-//   let result = temp.splice(0, 4);
+function getStats() {
+  const fs = require("fs");
+  let text = fs.readFileSync("data/padawans.txt", "utf8");
+  let temp = text.split('\n');
+  let result = temp.splice(0, temp.length - 1);
 
-//   let text2 = fs.readFileSync("data/scores.txt", "utf8");
-//   let temp2 = text2.split('\n');
-//   for (let i = 0; i < temp2.length; i++) {
-//     temp2[i] = +temp2[i];
-//   }
+  let text2 = fs.readFileSync("data/scores.txt", "utf8");
+  let temp2 = text2.split('\n');
+  for (let i = 0; i < temp2.length; i++) {
+    temp2[i] = Number(temp2[i]);
+  }
 
-//   let arrNew = [];
-// }
+  let arrNew = [];
+  for (let i = 0; i < result.length; i++) {
+    arrNew.push([result[i], temp2[i]]);
+  }
+  return arrNew;
+}
+
+function writeStats() {
+  const fs = require("fs");
+  let stats = getStats();
+  let text = stats.map(item => item.join(' ')).join('\n');
+  fs.writeFileSync('data/stats.txt', String(text));
+}
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  // getStats,
-  // writeStats, 
+  getStats,
+  writeStats,
 }
