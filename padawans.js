4a58d7dc2954d9d5b0e17721232ed17dd7548037
Igor S <tombcold@gmail.com>
2022-01-08 20:27:42 +0300

    all tests done

--- padawans.js
+++ padawans.js
@@ -1,16 +1,36 @@
 const fs = require('fs');
 
 function getPadawanNames() {
-  let text = fs.readFileSync(__dirname+'/data/padawans.txt','utf8');
-  let spl = text.split('\n');
-  let pads = spl.slice(0, -1);
+  const text = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const spl = text.split('\n');
+  const pads = spl.slice(0, -1);
   return pads;
 }
-// console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  const textScore = fs.readFileSync('./data/scores.txt', 'utf8');
+  const spl = textScore.split('\n');
+  return spl.map(Number);
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  let arr = [];
+  for (let i = 0; i < names.length; i++) {
+    arr.push([names[i], scores[i]]);
+  }
+  return arr;
+}
+
+function writeStats() {
+  const stats = getStats();
+  return fs.writeFileSync('./data/stats.txt', stats.join('\n').replace(/,/g, ' '));
+}
 
 module.exports = {
   getPadawanNames,
-  // getLightsaberScores,
-  // getStats,
-  // writeStats,
+  getLightsaberScores,
+  getStats,
+  writeStats,
 };
