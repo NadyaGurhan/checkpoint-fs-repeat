33e3c6788eec72ad64a8fc61134c9542632a074b
Sergey Romanov <study-front@mail.ru>
2022-01-11 10:26:11 +0300

    realise-1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,37 @@
+const fs = require('fs');
+
+function getPadawanNames () {
+  let padawansName = fs.readFileSync('./data/padawans.txt', 'utf8');
+  padawansName = padawansName.split('\n');
+  padawansName.pop();
+  return padawansName;
+}
+
+function getLightsaberScores () {
+  let swordScore = fs.readFileSync('./data/scores.txt', 'utf8');
+  swordScore = swordScore.split('\n');
+  let result = swordScore.map(Number);
+  return result;
+}
+
+function getStats () {
+  let padawansName = fs.readFileSync('./data/padawans.txt', 'utf8');
+  padawansName = padawansName.split('\n');
+  padawansName.pop();
+  let swordScore = fs.readFileSync('./data/scores.txt', 'utf8');
+  swordScore = swordScore.split('\n');
+  let result = swordScore.map(Number);
+  let statsDjedi = [];
+  for (let i = 0; i < padawansName.length; i++){
+    statsDjedi.push([padawansName[i], result[i]]);
+  }
+  return statsDjedi;
+}
+
+ function writeStats () {
+  const stats = getStats().join('\n').replace(/[,]/gi, ' ');
+  fs.writeFileSync('./data/stats.txt', stats);
+ }
 
 module.exports = {
   getPadawanNames,
