5da1607c574f1e904959c2d159134bf7eb0492fc
Vlad <woodygarryson@gmail.com>
2022-01-11 10:21:52 +0300

    so-so done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,34 @@
+const fs = require('fs');
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
 
+function getPadawanNames() {
+  return padawans.split('\n').slice(0, -1);
+}
+
+ const arrPadawans = padawans.split('\n')
+ const arrScores = scores.split('\n');
+
+  function getLightsaberScores() {
+    const arr = [];
+    for (let i = 0; i < arrScores.length; i++) {
+      arr[i] = +arrScores[i];
+    }
+    return arr;
+}
+
+
+function getStats () {
+  const stats = []
+  for (let i = 0; i < arrScores.length; i++) {
+    stats[i] = [arrPadawans[i], arrScores[i]]
+  } return stats
+  // fs.writeFileSync(`data/stats.txt`, stats)
+}
+console.log(getStats());
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
