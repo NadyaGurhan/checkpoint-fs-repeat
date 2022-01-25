dff6a469d1d381636d7e1b79842645cc8513d00f
Artem Tarasov <tarasowar@gmail.com>
2022-01-25 10:01:38 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs = require('fs')
+
+function getPadawanNames(){
+const name = fs.readFileSync('./data/padawans.txt', 'utf-8');
+ return arrName.push(name); 
+}
+
+function getLightsaberScores() {
+  const score = fs.readFileSync('./data/scores.txt','utf-8');
+ return score;
+}
+getPadawanNames()
+// getLightsaberScores()
+
+function getStats() {
+  let arrName = [];
+  let arrScore = [];
+  let name = arrName.push(getPadawanNames());
+  let score = arrScore.push(getLightsaberScores());
+ for(let i = 0; i < getPadawanNames; i++){
+  name[i] = name[i]+score[i];
+ }
+  return name;
+  }
+getStats()
 
 module.exports = {
   getPadawanNames,
