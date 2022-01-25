575b0ee9c88a3e69eaf2ca0f54ca7b3240a13a95
Alex Flower <aazvetkov@gmail.com>
2022-01-25 09:51:40 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,78 @@
+const fs = require("fs");
+
+function getPadawanNames() {
+  return fs
+    .readFileSync("./data/padawans.txt", "utf8")
+    .split("\n")
+    .splice(0, 4);
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync("./data/scores.txt", "utf8")
+    .split("\n")
+    .map((el) => +el);
+}
+
+function getStats() {
+  let padScores = fs
+    .readFileSync("./data/scores.txt", "utf8")
+    .split("\n")
+    .map((el) => +el);
+
+  let padNames = fs
+    .readFileSync("./data/padawans.txt", "utf8")
+    .split("\n")
+    .splice(0, 4);
+
+  let result = [];
+
+  for (let i = 0; i < padNames.length; i++) {
+    result.push([padNames[i], padScores[i]]);
+  }
+
+  console.log(padNames);
+  console.log(padScores);
+  console.log(result);
+
+  fs.writeFileSync("./data/stats.txt", result.toString());
+
+  return result;
+}
+
+// getStats();
+
+function writeStats() {
+  let padScores = fs
+    .readFileSync("./data/scores.txt", "utf8")
+    .split("\n")
+    .map((el) => +el);
+
+  let padNames = fs
+    .readFileSync("./data/padawans.txt", "utf8")
+    .split("\n")
+    .splice(0, 4);
+
+  let result = [];
+
+  for (let i = 0; i < padNames.length; i++) {
+    result.push([padNames[i], padScores[i]]);
+  }
+
+  console.log(padNames);
+  console.log(padScores);
+  console.log(result);
+
+  result = result.map((el) => el.join(" "));
+
+  console.log(result);
+
+  fs.writeFileSync("./data/stats.txt", result.join("\n"));
+
+  return result;
+}
+
+writeStats();
 
 module.exports = {
   getPadawanNames,
