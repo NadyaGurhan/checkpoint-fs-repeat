869750a1d07891dd987612887b7c93373012a71b
Bond <sasha.bond1995@gmail.com>
2022-01-25 10:05:02 +0300

    padawans

--- padawans.js
+++ padawans.js
@@ -1,3 +1,29 @@
+const fs = require('fs');
+
+function getPadawanNames(){
+  const padawans = './data/padawans.txt'
+  const readFile = fs.readFileSync(padawans, 'utf8');
+  const sortA = readFile.split('\n');
+
+  sortA.pop();
+  
+  return sortA;
+}
+
+
+
+function getLightsaberScores(){
+
+}
+
+function getStats(){
+
+}
+
+function writeStats(){
+
+}
+
 
 module.exports = {
   getPadawanNames,
