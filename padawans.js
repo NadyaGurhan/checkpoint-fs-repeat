fb6ac3710c9b1b6395df2889fe0ad6f4613860a4
Dmitry <dmitrynedoboy@gmail.com>
2022-01-25 09:59:51 +0300

    release 0.5

--- padawans.js
+++ padawans.js
@@ -1,3 +1,33 @@
+const fs = require('fs');
+
+let teamList = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+let scoreList = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+const padawanNames = teamList.pop();
+const padawanScores = scoreList.map((elem) => +elem);
+
+function getPadawanNames() {
+  return teamList;
+}
+
+function getLightsaberScores() {
+  return padawanScores;
+}
+
+function getStats() {
+  let statsTable = [];
+  let singlePerson = [];
+  for (let i = 0; i < teamList.length; i++) {
+    statsTable.push(teamList[i], padawanScores[i]);
+  }
+
+  return statsTable;
+}
+
+function writeStats() {
+  let statsTable = getStats();
+  let writeFile = fs.appendFileSync('./data/stats.txt', statsTable);
+  return writeFile;
+}
 
 module.exports = {
   getPadawanNames,
