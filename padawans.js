06d6a1dc7d1095bad88018e8f25f8b0af7c18a3d
Anzhelika <mrstonem@mail.ru>
2022-01-25 10:00:07 +0300

    checkpiont

--- padawans.js
+++ padawans.js
@@ -1,3 +1,33 @@
+const fs = require('fs');
+
+
+function getPadawanNames() {
+  const padawanNames = fs.readFileSync('data/padawans.txt', 'utf8', (err) => {
+    if (err) throw err;
+  }).split('\n');
+  return padawanNames;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('data/scores.txt', 'utf8', (err) => {
+    if (err) throw err;
+  }).split('\n');
+  return scores.map((score) => Number(score));
+}
+
+function getStats() {
+  const scores = getLightsaberScores();
+  const padawanNames = getPadawanNames();
+  const stats = [];
+  
+}
+
+console.log(getStats());
+
+function writeStats() {
+
+}
+
 
 module.exports = {
   getPadawanNames,
