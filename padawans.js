d0de96387e1e687a603894ffa74238ac6cc2c70c
Maks <dyupriz89evms@mail.ru>
2022-01-25 10:02:34 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
 
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+function getPadawanNames(){
+  const text = fs.readFileSync('data/padawans.txt', 'utf-8');
+}
+
+function getLightsaberScores() {
+  const number = fs.readFileSync('data/scores.txt', 'utf-8');
+}
+
+function getStats() {
+  fs.writeFileSync('data/stats.txt', 'dell');
+  //fs.appendFileSync('data/stats.txt', 'newData in currentFile.txt');
+  }
+  
+
+  module.exports = {
+    getPadawanNames,
+    getLightsaberScores,
+    getStats,
+    writeStats,
+  };
+ 
