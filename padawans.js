bb1e2b8bbec2aefafb8d8148d4ab24e1614d998b
Artem <artemivlev@yandex.ru>
2022-01-25 09:49:22 +0300

    checkpoint p1w1d1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,27 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').slice(0, 4);
+  return names;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n').map((i) => Number(i));
+  return scores;
+}
+
+function getStats() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').slice(0, 4);
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n').map((i) => Number(i));
+  return names.map((el, i) => [el, scores[i]]);
+}
+
+function writeStats() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').slice(0, 4);
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n').map((i) => Number(i));
+  const stats = names.map((el, i) => [el, scores[i]]).join('\n').split(',').join(' ');
+  return fs.writeFileSync('./data/stats.txt', stats);
+}
 
 module.exports = {
   getPadawanNames,
