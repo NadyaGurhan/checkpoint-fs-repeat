dd804584ee8cf2048a0c5897f26d48ecd78faf73
Dmitrii <demyanoff1337@yandex.ru>
2022-01-25 09:35:56 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,36 @@
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+
+function getPadawanNames() {
+  let result = padawans;
+  result.pop();
+
+  return result;
+}
+
+function getLightsaberScores() {
+  return scores.map(score => +score);
+}
+
+function getStats() {
+  return scores.map((score, i) => [padawans[i], +score]);
+}
+
+function writeStats() {
+  const statsToString = () => {
+    let result = '';
+
+    for (let i = 0; i < scores.length; i++) {
+      result = `${result}${padawans[i]} ${scores[i]}${i === scores.length - 1 ? '' : '\n'}`;
+    }
+
+    return result;
+  }
+
+  fs.writeFileSync('data/stats.txt', statsToString());
+}
 
 module.exports = {
   getPadawanNames,
