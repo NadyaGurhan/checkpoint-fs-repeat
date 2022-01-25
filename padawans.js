a7f4aeee0a6968c637d9650832c34487a9fceb23
AlexG <g_engineer@mail.ru>
2022-01-25 10:06:03 +0300

    Check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,33 @@
+const fs = require('fs');
+const padawans = fs.readFileSync('/home/alexandr/elbrus/week1/02/checkpoint-fs/data/padawans.txt', 'utf8');
+const scores = fs.readFileSync('/home/alexandr/elbrus/week1/02/checkpoint-fs/data/scores.txt', 'utf8');
+const padLength = padawans.trim('').split('\n');
+function getPadawanNames() {
+return padawans.trim('').split('\n');
+}
 
+function getLightsaberScores() {
+return scores.split('\n').map((el) => el * 1);
+
+}
+function getStats() {
+  let arr = [];
+const pad = getPadawanNames();
+const scor = getLightsaberScores();
+  for (let i = 0; i < padLength.length; i++) {
+arr.push([pad[i], scor[i]]);
+}
+return arr;
+}
+getStats();
+
+function writeStats() {
+  let expor = getStats();
+return fs.writeFileSync("data/stats.txt", expor.join('\n').split(',').join(' '));
+}
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+getLightsaberScores,
+getStats,
+writeStats,
 };
