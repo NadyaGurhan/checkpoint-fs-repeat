be0ef718c6b8d5116b250ed6f341f344d18cb0eb
Darya Vyushkova <89069763121@mail.ru>
2022-01-25 09:28:18 +0300

    Checkpoint-FS

--- padawans.js
+++ padawans.js
@@ -1,3 +1,31 @@
+const fs = require("fs");
+const padawans = fs.readFileSync("./data/padawans.txt", "utf8").split('\n',4)
+
+const scores = fs.readFileSync("./data/scores.txt", "utf8").split('\n')
+
+
+function getPadawanNames(){
+  return padawans
+}
+
+function getLightsaberScores(){
+  return scores.map((el) => Number(el))
+}
+
+function getStats(){
+  let result = [];
+  for (let i = 0; i < padawans.length; i++){
+    let res = []
+    res.push(padawans[i],Number(scores[i]))
+    result.push(res)
+  }
+  return result
+}
+
+
+function writeStats(){
+  fs.writeFileSync("./data/stats.txt", `${getStats().join('\n').replaceAll(',', ' ')}`)
+}
 
 module.exports = {
   getPadawanNames,
