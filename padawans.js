8bd68dcabcf6bdff44f0bedb3daeb4e4c97b43c8
Dima <cska2004@gmail.com>
2022-01-25 09:50:42 +0300

    Checkpoint completed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,31 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8');
+  return data.split('\n').filter(s => s);
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf-8');
+  return data.split('\n').filter(s => s).map(Number);
+}
+
+function getStats() {
+  const arrNames = getPadawanNames();
+  const arrScores = getLightsaberScores();
+  let res = [];
+
+  for (let i = 0; i < arrNames.length; i++) {
+    res.push([arrNames[i], arrScores[i]]);
+  }
+  return res;
+}
+
+function writeStats() {
+  const stats = getStats();
+
+  fs.writeFileSync('data/stats.txt', stats.join('\n').replaceAll(',', ' '));
+}
 
 module.exports = {
   getPadawanNames,
