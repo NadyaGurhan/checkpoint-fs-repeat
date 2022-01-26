c8d8c46ae434402548f2858db806b1e9df8a889f
hadzhehsen <skiptomylou999@fmail.com>
2022-01-27 01:37:02 +0300

    release 2

--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+
+const getPadawanNames = () => fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').slice(0, -1);
+
+const getLightsaberScores = () => fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map(Number);
+
+function getStats() {
+  const a = getPadawanNames();
+  const b = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < a.length; i++) {
+    result.push([a[i], b[i]]);
+  }
+  return result;
+}
+
+function writeStats() {
+  const x = './data/stats.txt';
+  const y = getStats().join('\n').replaceAll(',', ' ');
+  return fs.writeFileSync(x, y);
+}
 
 module.exports = {
   getPadawanNames,
