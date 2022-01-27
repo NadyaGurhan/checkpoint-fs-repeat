df120524ead17279fedcebe7cca4faa119b851cd
Viktor-Vrazumitel <Viktor_bogatyrev@mail.ru>
2022-01-27 12:18:05 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,39 @@
+const fs = require('fs');
+
+const allPadawan = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+const swordScore = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+
+function getPadawanNames() {
+  return allPadawan.slice(0, -1);
+}
+
+function getLightsaberScores() {
+  return swordScore.map(el => Number(el));
+}
+
+function getStats() {
+let result = [];
+const arrPad = getPadawanNames();
+const arrScor = getLightsaberScores();
+for (let i = 0; i < arrPad.length; i++) {
+  result.push([arrPad[i],arrScor[i]])
+  
+}
+
+return result;
+};
+
+function writeStats() {
+  let result = '';
+let arr = getStats().join(',\n');
+for (let item of arr) {
+  if(item !== ','){
+    result += item
+  } 
+  
+  }
+fs.writeFileSync('./data/stats.txt', arr);
+}
 
 module.exports = {
   getPadawanNames,
