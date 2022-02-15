fd98812de970370983fd9e0fe03c9ce9e265f47e
ArtemKa <kashuta@GMAIL.gmail.com>
2022-02-15 10:38:03 +0300

    All tests pass

--- padawans.js
+++ padawans.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 const readPadavans = fs.readFileSync('./data/padawans.txt', 'utf8');
 const readScores = fs.readFileSync('./data/scores.txt', 'utf8');
-const writeStats = fs.writeFileSync('./data/stats.txt', 'utf8');
 
 function getPadawanNames() {
-  const result = readPadavans.split('\n');
-  result.pop();
-  return result;
+  const arr = readPadavans.split('\n');
+  arr.pop()
+  return arr;
 }
 function getLightsaberScores() {
   return readScores.split('\n').map((x) => parseFloat(x, 10));
@@ -20,11 +19,13 @@ function getStats() {
   }
   return arr;
 }
-function writeStats(){
-const nl = getStats().join('\n');
-const fileName = './data/stats.txt';
-return fs.writeFileSync(fileName, nl);
+
+function writeStats() {
+  const nl = getStats();
+  let str = nl.join('\n').replaceAll(',', ' ');
+  return fs.writeFileSync('./data/stats.txt', str);
 }
+
 
 module.exports = {
   getPadawanNames,
