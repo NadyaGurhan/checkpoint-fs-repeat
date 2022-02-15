e2165a58d02259facd2fecb11bb57c7247780728
Aleksandr <AK84Tomck@mail.ru>
2022-02-15 10:13:00 +0300

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -2,6 +2,32 @@
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+const fs = require('fs');
+const padawanList = fs.readFileSync('./data/padawans.txt', 'utf-8');
+function getPadawanNames() {
+return padawanList.split('\n')
+.slice(0, -1);
+}
+console.log(getPadawanNames());
+
+
+const lightScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+function getLightsaberScores() {
+return lightScores.split('\n')
+.map((el) => el * 1)
+}
+console.log(getLightsaberScores())
+
+
+
+
+
+// function table() {
+
+// }
+
+
