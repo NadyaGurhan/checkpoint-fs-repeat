12e69b19b4dd1be8f759c09625e1bbe3c3d80fd4
Anton <anton_st_01_02@mail.ru>
2022-02-15 12:11:38 +0500

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,13 @@
+function getPadawanNames() {
+ return padawans.split('/n');
+}
+const fs = require('fs');
+const padawans = fs.readFileSync('./data/padawans.txt');
+console.log(getPadawanNames)
+
+
+
+
 
 module.exports = {
   getPadawanNames,
