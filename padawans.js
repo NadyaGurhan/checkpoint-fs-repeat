10dc4167b5c7e5dbad5811e37734bced2df73c2f
sergfitil <fitil73@mail.ru>
2022-02-15 10:03:52 +0300

    Первый коммит : не получилось

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
+function getPadawanNames() {
+  let padawans;
+  return padawans = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
 
+}
+function getLightsaberScores() {
+  let rating = [];
+  return rating = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
