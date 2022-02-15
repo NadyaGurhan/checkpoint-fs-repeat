55441dfc092bc7748e68647698d2c0a24eedf8b6
Mihail <mr.derks1989@gmail.com>
2022-02-15 10:01:06 +0300

    Chek point

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
+const fs = require('fs');
 
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n')
+  .slice(0, 4);
+
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf-8').split('\n')
+  .map((el) => +el);
+}
+//console.log(getPadawanScores())
+
+function getStats() {
+  let padNames = getPadawanNames();
+  let padScores = getLightsaberScores();
+  let result = [];
+
+  for (let i = 0; i < padNames.length; i++) {
+    result.push([padNames[i], padScores[i]]);
+  }
+  result = result.map((el) => el.join(" "));
+
+  
+  return result;
+}
+
+function writeStats(){
+  fs.appendFileSync("./data/stats.txt", result.join("\n"));
+}
+
+
+
+//console.log(getStats())
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
