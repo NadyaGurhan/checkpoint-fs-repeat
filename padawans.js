f22fc8b6e9ac8742ee48785d450e149e854f9e49
maxim <maximmukhanov.job@gmail.com>
2022-02-15 10:13:43 +0300

    Все плохо c:

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
+const fs = require("fs");
+const readPadawans = fs.readFileSync("./data/padawans.txt", "utf8");
+const readScores = fs.readFileSync("./data/scores.txt", "utf8");
 
+function getPadawanNames() {
+  let names = [];
+  readPadawans.split("\n");
+  console.log(readPadawans);
+}
+getPadawanNames();
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getPadawanNames,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
