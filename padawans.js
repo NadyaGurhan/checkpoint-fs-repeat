7bc9ab14985bf492dcd21ac5eab907c512e1448e
Mark <smirnovsd19@mail.ru>
2022-02-15 10:02:55 +0300

    2 tests past

--- padawans.js
+++ padawans.js
@@ -1,7 +1,36 @@
+const fun = require('fs')
+// const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
+// console.log(padawans)
 
+
+function getPadawanNames() {
+  const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
+  return padawans
+}
+getPadawanNames()
+
+function getLightsaberScores() {
+  const scores = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/scores.txt', 'utf8').split('\n').map((el) => Number(el))
+  return scores
+}
+getLightsaberScores()
+
+// function getStats() {
+//   const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
+//   const scores = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/scores.txt', 'utf8').split('\n').map((el) => Number(el))
+// console.log(padawans)
+// }
+
+// const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
+// const scores = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/scores.txt', 'utf8').split('\n').map((el) => Number(el))
+
+// console.log(padawans, scores, sum)
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+
+
