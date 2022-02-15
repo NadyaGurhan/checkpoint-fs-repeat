a219cf37d4d2b011fcf3791992cc72c56b13943f
Timur <minzulin@bk.ru>
2022-02-15 10:08:06 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,17 @@
+const fs = require('fs')
+const padawan = fs.readFileSync('/Users/timurminzulin/elbrus/phase-1/week1/tuesday/checkpoint-fs/data/padawans.txt', 'utf-8')
+
+
+function getPadawanNames() {
+  let arr = padawan.split('\n')
+  let pad = arr.filter(a => a != '');
+  return pad;
+}
+console.log(getPadawanNames())
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
