9d4efff95300c0ed93b6e32d090a7ad4a4adba1b
Aleksandr Schepilov <gornostay4eg@mail.ru>
2022-02-15 10:11:50 +0300

    chek

--- padawans.js
+++ padawans.js
@@ -1,7 +1,14 @@
-
+const fs = require(`fs`)
+let namesPad = fs.readFileSync(`/Users/aleksandrsepilov/Elbrus/1 фаза/2 day/chek 1 day/checkpoint-fs/data/padawans.txt`,`utf-8`);
 module.exports = {
-  getPadawanNames,
+  getPadawanNames(),
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames () {
+  
+  let result = namesPad.split(`\n`).filter(a=>a !=``)
+  return result
+}
