b56450ee25343771b713734f5363bd79ecc0457c
Tokhirbek <karamaov.tohirbek@gmail.com>
2022-02-15 10:08:30 +0300

    релиз

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
+const fs = require('fs');
+const readNames = fs.readFileSync("./data/padawans.txt", "utf-8");
+
+function getPadawanNames()  {
+  let arr = [];
+
+
+// readNames.split('\n')
+ 
+ 
+  for(let i = 0; i < readNames; i++ ) {
+
+    // if( i === '') {
+    //   continue
+    // }
+    arr.push(readNames);
+  }
+  return arr;
+}
+
+console.log(getPadawanNames())
+
+
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
