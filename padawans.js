dee4b476490a501dc3b8ea9b7dc9bcf5dc85de28
Alex <alexshilikhin@yandex.ru>
2022-02-15 10:17:19 +0300

    Chek

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
+const padawanNames = fs.readFileSync('./data/padawans.txt', 'UTF-8');
+const saberScore = fs.readFileSync('./data/scores.txt', 'UTF-8');
 
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+function getPadawanNames() {
+  const colNames = padawanNames.split('\n');
+  colNames.pop()
+  return colNames
 };
+
+function getLightsaberScores() {
+  const colScore = saberScore.split(', ');
+  colScore.push()
+return colScore
+}
+
+console.log(getLightsaberScores());
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
