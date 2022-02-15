ae73e383470b7c74733f07faece7a987287ac22a
Mikhail21 <saveshit8@gmail.com>
2022-02-15 10:05:20 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,20 @@
+function getPadawanNames (){
+return padawans.split(" ")
+}
+
+function getLightsaberScores () {
+  let result = []
+  
+  stats.sort ()
+}
+}
+
+
+
+const fs = require('fs')
+const padawans = fs.readFileSync('./data/padawans.txt')
+const stats = fs.readFileSync('./data/scores.txt')
+
 
 module.exports = {
   getPadawanNames,
