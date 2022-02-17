7606326c1f6e09cd18afb931db1a05f2da538264
Mark <smirnovsd19@mail.ru>
2022-02-17 18:11:13 +0300

    Done

--- padawans.js
+++ padawans.js
@@ -1,35 +1,41 @@
 const fun = require('fs')
-// const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
-// console.log(padawans)
+
 
 
 function getPadawanNames() {
   const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
   return padawans
 }
-getPadawanNames()
+
 
 function getLightsaberScores() {
   const scores = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/scores.txt', 'utf8').split('\n').map((el) => Number(el))
   return scores
 }
-getLightsaberScores()
 
-// function getStats() {
-//   const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
-//   const scores = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/scores.txt', 'utf8').split('\n').map((el) => Number(el))
-// console.log(padawans)
-// }
 
-// const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
-// const scores = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/scores.txt', 'utf8').split('\n').map((el) => Number(el))
+function getStats() {
+  const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
+  const scores = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/scores.txt', 'utf8').split('\n').map((el) => Number(el))
+  const list = padawans.map((el, ind) => [el, scores[ind]])
+  return list
 
-// console.log(padawans, scores, sum)
+}
+// console.log(getStats())
+
+function writeStats() {
+  const padawans = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/padawans.txt', 'utf8').split('\n').slice(0, -1)
+  const scores = fun.readFileSync('/Users/a1/Desktop/ElbrusBootcamp/checkpoint-fs/data/scores.txt', 'utf8').split('\n').map((el) => Number(el))
+  const list = padawans.map((el, ind) => `${el} ${scores[ind]}`)
+  return fun.writeFileSync('data/stats.txt', list.join('\n'))
+}
+// writeStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  // getStats,
-  // writeStats,
+  getStats,
+  writeStats,
 };
 
 
