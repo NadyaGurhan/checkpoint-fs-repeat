92e5407c3906e95ef95b9e43ff3be97a71c1b813
rKefear <sl.malyutin@gmail.com>
2022-02-19 17:24:01 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,19 +1,35 @@
-function getPadawanNames() {
-return padawans.split('\n').filter(a=>a !='')
-}
-function getLightsaberScores() {
-return scores.split('\n').map(a=>+a)
-}
-function getStats() {
-
-}
-function writeStats() {
-
-}
 const fs = require('fs')
-const { join } = require('path')
+
 const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8')
 const scores = fs.readFileSync('./data/scores.txt', 'utf-8')
+
+const padawansEdit = padawans.split('\n')
+const scoresEdit = scores.split('\n')
+
+function getPadawanNames() {
+padawansEdit.pop()
+return padawansEdit
+}
+
+function getLightsaberScores() {
+return scoresEdit.map((a) => +a)
+}
+
+function getStats() {
+  let result = []
+  for(let i = 0; i < padawansEdit.length; i++){
+      result.push([padawansEdit[i], +scoresEdit[i]])
+    }
+  return result
+    }
+
+function writeStats() {
+    let result = getStats();
+    let resultFin = result.join('\n').replaceAll(',', ' ');
+    return fs.writeFileSync('./data/stats.txt', resultFin);
+  }
+
+
 
 
 module.exports = {
