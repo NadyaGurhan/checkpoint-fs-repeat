8f3014e328b49026be98bde579b5a62c6cdf4196
Dima <schtanckevitch.dima@yandex.ru>
2022-03-01 13:51:50 +0300

    done mission

--- padawans.js
+++ padawans.js
@@ -1,4 +1,40 @@
+let fs = require('fs');
+let call = fs.readFileSync('./data/padawans.txt', 'utf-8');
+function getPadawanNames() {
+  let res = '';
 
+  for (let i = 0; i < call.length - 1; i++) {
+    res += call[i];
+  }
+  return res.split('\n');
+};
+
+let nam = getPadawanNames();
+// console.log(getPadawanNames());
+
+let scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+function getLightsaberScores() {
+  let res = '';
+
+  for (let i = 0; i < scores.length; i++) {
+    res += scores[i];
+  }
+  return res.split('\n');
+}
+
+let scr = getLightsaberScores();
+
+// console.log(getLightsaberScores());
+
+function getStats() {
+  let res = [];
+  for (let i = 0; i < nam.length - 1; i++) {
+    let arr = [];
+    arr.push(nam[i], +scr[i]);
+    res.push(arr);
+  }
+  return res;
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
