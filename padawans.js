69a7728538d3127eaa2b230182efcdd6469cfba3
Anush <anushharutyunyan.99@mail.ru>
2022-03-01 10:38:07 +0400

    getPadawanNames

--- padawans.js
+++ padawans.js
@@ -1,7 +1,17 @@
+const fs = require('fs')
+
+function getPadawanNames() {
+	const padawansList = fs
+		.readFileSync('./data/padawans.txt', 'utf-8')
+		.split('\n')
+	padawansList.pop()
+	return padawansList
+}
+
 
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+	getPadawanNames,
+	getLightsaberScores,
+	getStats,
+	writeStats,
+}
