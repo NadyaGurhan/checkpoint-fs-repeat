1bcd9eab1b785207d36c0385ed2baa3071dab57e
Liza <lizakorneva1997@gmail.com>
2022-03-01 10:23:09 +0300

    test-1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,27 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const arrPadawans = [];
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  console.log(padawans);
+
+  for (let i = 0; i < padawans.length; i += 1) {
+    arrPadawans.push(padawans[i]);
+  }
+  return arrPadawans;
+}
+
+function getLightsaberScores() {
+
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
