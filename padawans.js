ecb64fe733924a1d0fa93d6d8a5f40217796ae6f
OskMurphy <oskarahm@ya.ru>
2022-03-01 10:16:00 +0300

    Checkpoint 2/4

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
+const fs = require('fs');
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
 
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+function getPadawanNames() {
+  const names = padawans.split('\n');
+  names.pop();
+ return names;
+}
+
+function getLightsaberScores() {
+ const saberScores = scores.split('\n'); 
+  return saberScores.map(string => +string);
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
+
+module.exports = { getPadawanNames, getLightsaberScores, getStats, writeStats };
