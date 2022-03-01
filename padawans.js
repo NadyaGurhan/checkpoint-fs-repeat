74491115a939d842396cf1fb8d1cf9e4649f2b25
OlegBovkush <obovkush@gmail.com>
2022-03-01 17:16:12 +1000

    3 test done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,35 @@
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+padawans.length -= 1;
+
+function getPadawanNames() {
+  return padawans;
+}
+
+let scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+
+function getLightsaberScores() {
+  scores = scores.map(Number);
+  return scores;
+}
+
+const stats = [];
+
+function getStats() {
+  for (let i = 0; i < scores.length; i += 1) {
+    stats[i] = [padawans[i], scores[i]];
+  }
+  return stats;
+}
+const str = stats.join('\n');
+const filePath = 'data/stats.txt';
+
+console.log(str);
+
+function writeStats() {
+  return fs.writeFileSync(filePath, str);
+}
 
 module.exports = {
   getPadawanNames,
