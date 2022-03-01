4effa81bc6ad5da483ce02b3735485ee9320ba7c
Anush <anushharutyunyan.99@mail.ru>
2022-03-01 10:39:59 +0400

    writeStats

--- padawans.js
+++ padawans.js
@@ -16,17 +16,22 @@ function getLightsaberScores() {
 	return scores
 }
 
-
 function getStats() {
 	const arr = []
 	const padavans = getPadawanNames()
 	const scores = getLightsaberScores()
 	for (let i = 0; i < padavans.length; i++) {
-		let arrMin = []
+		const arrMin = []
 		arrMin.push(padavans[i], scores[i])
 		arr.push(arrMin)
 	}
 	return arr
+}
+
+function writeStats() {
+	const arr = getStats()
+	const newArr = arr.map(el => el.join(' ')).join('\n')
+	fs.writeFileSync('./data/stats.txt', `${newArr}`)
 }
 
 module.exports = {
