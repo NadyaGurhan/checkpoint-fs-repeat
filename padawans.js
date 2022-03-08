f322b198c8355695bffe6100f36a47462a0e08d2
Alexaner Piontek <nekom113@mail.ru>
2022-03-08 10:11:45 +0300

    1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,17 @@
+const { readFileSync } = require('fs');
+
+function getPadawanNames() {
+  const padavanList = readFileSync('./data/padawans.txt', 'utf-8');
+  const newList = padavanList.split('\n');
+  return newList;
+}
+function getPadawanScores() {
+  const scoresList = readFileSync('./data/scores.txt', 'utf-8');
+  const newList = scoresList.split('\n');
+  return newList;
+}
+
+console.log(getPadawanScores(), getPadawanNames());
 
 module.exports = {
   getPadawanNames,
