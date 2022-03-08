bb35266809ae02129aeb45fdd330e50d9a268849
Aleksey <domnin.a.s.85@gmail.com>
2022-03-08 12:11:40 +0500

    CheckPoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,13 @@
+const fs = require('fs');
+const padawans = fs.readFileSync('data/padawans.txt', 'utf-8');
+// console.log(padawans);
+const saberScore = fs.readFileSync('data/scores.txt', 'utf-8');
+const stat = fs.readFileSync('data/stats.txt', 'utf-8');
 
+const getPadawanNames = () => {
+  let padawansArr = padawans.split();
+  return padawansArr;
+};
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
