637230f5dd639ebfc14a488c931a2d5de1fb5460
SkiPPy43 <skippy4343@yandex.ru>
2022-03-08 10:10:41 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf8');
+
+function getPadawanNames() {
+  return padawans.trim().split('\n');
+}
+// console.log(padawans);
+function getLightsaberScores() {
+// for (let i = 0; i < scores; i++) {
+  return scores.split('\n').map(el => Number(el));
+// }
+}
+
+function getStats() {
+  return 
+}
+function writeStats() {
+  return 
+}
+
+
+
 
 module.exports = {
   getPadawanNames,
