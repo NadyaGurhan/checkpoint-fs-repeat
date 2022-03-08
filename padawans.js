601dbdb0666f9b8ccdabc4f7c2c5cd87aad6ade3
Roman <dsdxxx2@rambler.ru>
2022-03-08 10:10:25 +0300

    Release1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,19 @@
+const fs = require('fs');
 
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf8');
+function getPadawanNames() {
+  return padawans.trim().split('\n');
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+  return scores.split('\n').map(el => Number(el));
+}
+getLightsaberScores();
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+   getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
