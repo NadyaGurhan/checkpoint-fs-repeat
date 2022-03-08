50fe8bdf1c8d5eb173ff4b0506dc8479387e05ea
Vlad <gosduma000@mail.ru>
2022-03-08 10:15:01 +0100

    Что успел

--- padawans.js
+++ padawans.js
@@ -1,4 +1,22 @@
+const fs = requaire('fs');
 
+const puthPad './padawans.txt';
+const patVar = fs readFileSync (puthPad,'utf 8').split ('\n').slice(0, -1);
+const puthScores = './data/scores.txt';
+const scoreVar = fs.readFileSync (puthScores,'utf 8').split ('\n').map Number;
+
+function getPadawanNames () {
+  return padVar;
+}
+function getLightsaberScores () {
+  return scoreVar;
+}
+function getStats () {
+ const statsVar = []
+ for (let i = 0; i < padVar.length i +=1){
+   statsVar.puch()
+ } 
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
