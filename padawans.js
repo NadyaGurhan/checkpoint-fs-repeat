35f00111832a8798d2442a0830a43ace4936b8e5
yxngbenji <bogdanshutko4job@gmail.com>
2022-03-08 10:06:41 +0300

    All the tests have been passed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,35 @@
+const fs = require('fs');
+const { get } = require('http');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n').map((e) => Number(e));
+  return scores;
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const stats = [];
+  for (let i = 0; i < padawans.length; i += 1) {
+    stats.push([padawans[i], scores[i]]);
+  }
+  return stats;
+}
+
+function writeStats() {
+  const stats = getStats();
+  for (let i = 0; i < stats.length; i += 1) {
+    if (i === stats.length - 1) {
+      fs.appendFileSync('data/stats.txt', `${stats[i].join(' ')}`);
+    } else fs.appendFileSync('data/stats.txt', `${stats[i].join(' ')}\n`);
+  }
+  console.log(stats);
+}
 
 module.exports = {
   getPadawanNames,
