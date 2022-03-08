4d11ded73aaaf3f184b2f1f79bc047b28db2baa1
Dmitriy <dim94@mail.ru>
2022-03-08 10:05:14 +0300

    all releases

--- padawans.js
+++ padawans.js
@@ -5,3 +5,46 @@ module.exports = {
   getStats,
   writeStats,
 };
+function getPadawanNames(){
+  const fs = require("fs")
+  let data = fs.readFileSync("data/padawans.txt", "utf8")
+  data = data.trim().split("\n")
+  //console.log(data)
+  return data
+}
+
+function getLightsaberScores(){
+  const fs = require("fs")
+  let data = fs.readFileSync("data/scores.txt", "utf8")
+  data = data.trim().split("\n")
+  data = data.map((a) => Number(a))
+  //console.log(data)
+  return data
+}
+
+function getStats(){
+  let mas1 = getPadawanNames()
+  let mas2 = getLightsaberScores()
+  let result = []
+  for (let i=0; i<mas1.length; i++){
+    result.push([mas1[i],mas2[i]])
+  }
+  return result
+  //console.log(result)
+}
+
+function writeStats(){
+  const fs = require("fs")
+  let mas1 = getPadawanNames()
+  let mas2 = getLightsaberScores()
+  for (let i=0; i<mas1.length; i++){
+    if(i===mas1.length-1){
+      fs.appendFileSync("data/stats.txt",`${mas1[i]} ${mas2[i]}`, "utf8")
+      continue;
+    }
+    fs.appendFileSync("data/stats.txt",`${mas1[i]} ${mas2[i]}\n`, "utf8")
+  }
+}
+
+//writeStats()
+
