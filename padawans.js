f100e49f3165559be57a07a3666f95a0f9ce6a8c
Shamil Omarov <s_omarov05@mail.ru>
2022-03-29 10:23:15 +0300

    checkpount

--- padawans.js
+++ padawans.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+let padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const names = padawans;
+
+function getPadawanNames() {
+  return names
+}
 
 module.exports = {
   getPadawanNames,
