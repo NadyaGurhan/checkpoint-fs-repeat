6f53d63274133d02bd6490c6f5cc21162571d34f
Shamil Omarov <s_omarov05@mail.ru>
2022-03-29 14:23:34 +0300

    release 1-2

--- padawans.js
+++ padawans.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
-let padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
-const padaw = padawans.trim().split(' ');
 
 function getPadawanNames() {
-  return padaw;
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const padawansArr = padawans.trim().split('\n');
+  return padawansArr;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const scoresArr = scores.trim().split('\n');
+  const finalArr = [];
+  for (let i = 0; i < scoresArr.length; i += 1) {
+    finalArr.push(parseFloat(scoresArr[i]));
+  }
+  return finalArr;
 }
 
 module.exports = {
   getPadawanNames,
-  // getLightsaberScores,
+  getLightsaberScores,
   // getStats,
   // writeStats,
 };
