699d1b1c185a5e4c1a6bb738faf881a5632110b6
Alex <stepanov-a.a@bk.ru>
2022-03-29 10:26:11 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const padawan = fs.readFileSync("./data/padawans.txt" , "utf8");
+const arr = padawan.trim().split('/n')
+// console.log(arr)
+function getPadawanNames () {
+return arr
+}
+
 
 module.exports = {
   getPadawanNames,
