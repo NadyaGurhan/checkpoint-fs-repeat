f99c356862fbf1b40729147d4afe98c45e3b7f55
Anton <anton.miroshnik@mail.ru>
2022-03-29 10:23:25 +0300

    Release for checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,36 @@
+const fs = require('fs');
 
+function getPadawanNames() {
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').filter((el) => el !== '');
+  return padawanNames;
+}
+function getLightsaberScores() {
+  const lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  for (let i = 0; i < lightsaberScores.length; i++) {
+    lightsaberScores[i] = +lightsaberScores[i];
+  }
+  return lightsaberScores;
+}
+function getStats() {
+  const arr = [];
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').filter((el) => el !== '');
+  const lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  for (let i = 0; i < padawanNames.length; i++) {
+    arr.push([padawanNames[i], +lightsaberScores[i]]);
+  }
+  return arr;
+}
+function writeStats() {
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').filter((el) => el !== '');
+  const lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  for (let i = 0; i < padawanNames.length; i++) {
+    if (i === padawanNames.length - 1) {
+      fs.appendFileSync('./data/stats.txt', `${padawanNames[i]} ${lightsaberScores[i]}`);
+    } else {
+      fs.appendFileSync('./data/stats.txt', `${padawanNames[i]} ${lightsaberScores[i]}\n`);
+    }
+  }
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
