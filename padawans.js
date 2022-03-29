605b9306cb85bc99b52f51ac4a48fe6f42e363e9
Artem <ha-chu@yandex.ru>
2022-03-29 14:23:13 +0700

    sprint1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,15 @@
+const fs = require('fs')
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8')
+const names = padawans.split('\n');
+
+const getPadawanNames = names.map(((item) => item.split(' ').
+
+console.log(getPadawanNames)
+
+
+
+
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +17,4 @@ module.exports = {
   getStats,
   writeStats,
 };
+
