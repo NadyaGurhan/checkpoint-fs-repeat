a0e2448c819ac695c2c90c03d0012f8aef847d13
Artem <ha-chu@yandex.ru>
2022-03-29 14:25:23 +0700

    small mistakes

--- padawans.js
+++ padawans.js
@@ -3,8 +3,7 @@ const fs = require('fs')
 const padawans = fs.readFileSync('./data/padawans.txt', 'utf8')
 const names = padawans.split('\n');
 
-const getPadawanNames = names.map(((item) => item.split(' ').
-
+const getPadawanNames = names.map((item) => item.split(' '))
 console.log(getPadawanNames)
 
 
