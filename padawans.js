b64ca688d7f49b641fb5a52218250c76b050399b
DrebnitskyKirill <mr.drebnitzky@yandex.ru>
2022-03-30 18:07:56 +0300

    All Release

--- padawans.js
+++ padawans.js
@@ -1,3 +1,32 @@
+const fs = require('fs');
+
+const padawanText = fs.readFileSync('./data/padawans.txt', 'utf8');
+const scoreText = fs.readFileSync('./data/scores.txt', 'utf8');
+
+function getPadawanNames() {
+  return padawanText.split('\n').slice(0, -1);
+}
+
+function getLightsaberScores() {
+  return scoreText.split('\n').map((el) => +el);
+}
+
+function getStats() {
+  const pada = getPadawanNames();
+  const sc = getLightsaberScores();
+  const arr = [];
+  for (let i = 0; i < pada.length; i++) {
+    arr.push([pada[i]]);
+  }
+  for (let i = 0; i < sc.length; i++) {
+    arr[i].push(sc[i]);
+  }
+  return arr;
+}
+
+function writeStats() {
+  return fs.writeFileSync('data/stats.txt', `${getStats().map((el) => el.join(' ')).join('\n')}`);
+}
 
 module.exports = {
   getPadawanNames,
