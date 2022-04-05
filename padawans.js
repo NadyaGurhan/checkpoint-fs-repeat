dbc1647ad3d4101acdcc19365c35fd0268a3ac2a
bazoftrope <4433800@gmail.com>
2022-04-05 10:17:02 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
+const fs = require('fs');
+
+// function getPadawanNames() {
+let getPadawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').slice(-1)
+console.log(getPadawanNames)
+// }
+let getLightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n')
+console.log(getLightsaberScores)
+
+let getStats = []
+let res = [];
+let i = 0;
+while (i < getPadawanNames.length) {
+  res.push(getPadawanNames[i], getLightsaberScores[i])
+  getStats.push(res)
+  i++
+}
+console.log(getStats)
+
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
