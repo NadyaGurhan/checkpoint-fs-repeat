9eea0cc40f2a2a48613fc1ae725dce593b16da4d
Boris Gadaborshev <BorisGadaborshev@gmail.com>
2022-04-05 12:48:09 +0300

    Release 2

--- padawans.js
+++ padawans.js
@@ -1,61 +1,44 @@
 const fs = require('fs');
 
 function getPadawanNames() {
-  let padawan = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
-  let padawans = padawan.filter(item => item !== '');
+  const padawan = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  const padawans = padawan.filter((item) => item !== '');
   return padawans;
 }
 function getLightsaberScores() {
-  let skill = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
-  for(let i = 0; i < skill.length; i++){
+  const skill = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  for (let i = 0; i < skill.length; i += 1) {
     skill[i] = +skill[i];
   }
   return skill;
 }
 
 function getStats() {
-  let padawan = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
-  let padawans = padawan.filter(item => item !== '');
-  let skill = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
-  let padSkill = [];
-  for(let i = 0; i < padawans.length; i++) {
-    let skills = []
-    for(let j = 0; j < 2; j++){
-      if (j === 0){
-        skills.push(padawans[i])
-      } else{
-        skills.push(+skill[i])
+  const padawans = getPadawanNames();
+  const skill = getLightsaberScores();
+  const padSkill = [];
+  for (let i = 0; i < padawans.length; i += 1) {
+    const skills = [];
+    for (let j = 0; j < 2; j += 1) {
+      if (j === 0) {
+        skills.push(padawans[i]);
+      } else {
+        skills.push(+skill[i]);
       }
-      
     }
-  padSkill.push(skills)
-  }
-  
-  return padSkill;
+    padSkill.push(skills);
   }
 
-  function writeStats() {
-    let padawan = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
-    let padawans = padawan.filter(item => item !== '');
-    let skill = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
-    let padSkill = [];
-    for(let i = 0; i < padawans.length; i++) {
-      let skills = []
-      for(let j = 0; j < 2; j++){
-        if (j === 0){
-          skills.push(padawans[i])
-        } else{
-          skills.push(+skill[i])
-        }
-        
-      }
-    padSkill.push(skills)
-    console.log(padSkill)
-    }
-    for( let i = 0; i < padSkill.length; i++) { 
-      fs.writeFileSync('./data/stats.txt', padSkill[i]);
-    }
-  }
+  return padSkill;
+}
+
+function writeStats() {
+  const skill = getStats();
+  console.log(skill);
+  skill.forEach((element) => {
+    fs.writeFileSync('./data/stats.txt', element);
+  });
+}
 
 module.exports = {
   getPadawanNames,
