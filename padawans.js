5a0f8e5706157f66247b83df7a3e2641e653371b
Anton <capycoder@gmail.com>
2022-04-05 08:35:55 +0200

    Done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs = require('fs');
+
+function getFileContent(path, file) {
+  return fs.readFileSync(`${path}/${file}`, 'utf8');
+}
+
+function getPadawanNames() {
+  return getFileContent('./data', 'padawans.txt').split('\n').filter((x) => (x));
+}
+
+function getLightsaberScores() {
+  return getFileContent('./data', 'scores.txt').split('\n').filter((x) => (x)).map((x) => +x);
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const scoreTable = padawans.map((x, i) => [x, scores[i]]);
+  return scoreTable;
+}
+
+function writeStats() {
+  fs.writeFileSync('./data/stats.txt', getStats().map((x) => x.join(' ')).join('\n'));
+}
+
 
 module.exports = {
   getPadawanNames,
