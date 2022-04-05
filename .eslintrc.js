94350b7817d6084666f5f435254cf842c5c7cb55
Milana <milanammk@gmail.com>
2022-04-05 10:15:19 +0300

    Release1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  plugins: [
-    'jest',
-  ],
-  rules: {
-  },
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
--- padawans.js
+++ padawans.js
@@ -1,7 +1,49 @@
+const fs = require("fs");
+const { writer } = require("repl");
+// import * as fs from 'fs';
+// const padawans = fs.readFileSync('./data/padawans.txt');
+const padawans = fs.readFileSync('./data/padawans.txt', "utf-8");
+const scores = fs.readFileSync('./data/scores.txt', "utf-8");
 
+function getPadawanNames() {
+    const arr = padawans.split("\n");
+    arr.pop();
+    return arr;
+}
+
+function getLightsaberScores() {
+    let arr = scores.split("\n");
+    return arr.map(str => +str);
+}
+// [
+// 	['Revan', 99.9],
+// 	['Bastila Shan', 92],
+// 	['Jolee Bindo', 87],
+// 	['Juhani', 82],
+// ]);
+function getStats() {
+    const padawansArr = getPadawanNames();
+    const scoresArr = getLightsaberScores();
+    const stat = [];
+    if (padawansArr.length === scoresArr.length) {
+        for (let index = 0; index < padawansArr.length; index++) {
+            const arr = [];
+            arr.push(padawansArr[index], scoresArr[index]);
+            stat.push(arr);
+        }
+    }
+    return stat;
+}
+
+const stats = getStats().join("\n");
+
+function writeStats() {
+    return fs.writeFileSync('data/stats.txt', stats);
+}
+console.log('Revan 99.9\nBastila Shan 92\nJolee Bindo 87\nJuhani 82', stats);
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+    getPadawanNames,
+    getLightsaberScores,
+    getStats,
+    writeStats,
 };
