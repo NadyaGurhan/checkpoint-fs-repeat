0024348bdb3ac6d52765af2574eaa814b2fa73e6
OskMurphy <oskarahm@ya.ru>
2022-04-05 10:13:54 +0300

    Re-test

--- padawans.js
+++ padawans.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+
+const getPadawanNames = () => padawans.slice(0, -1);
+
+const getLightsaberScores = () => scores.map(Number);
+
+const getStats = () => {
+//  const padawanNames = getPadawanNames();
+//  const LightsaberScores = getLightsaberScores();
+//  const stats = [];
+//  for (let i = 0, i<padawanNames.length; i++ ) {
+//    stats.push([padawanNames[i]]);
+//  }
+//  for (let i = 0, i < LightsaberScores.length; i++ ) {
+//   stats.push([LightsaberScores[i]]);
+// };
+// return stats;
+}
+
+const writeStats = () => fs.writeFileSync('./data/stats.txt', `${getStats().map((el) => el.join('')).join('\n')}`);
 
 module.exports = {
   getPadawanNames,
