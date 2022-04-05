6d9e11cf3ddfb7a9383beae079c85ef6f57661be
Dmitry <krasnovd@gmail.com>
2022-04-05 10:18:12 +0300

    test 1-2

--- padawans.js
+++ padawans.js
@@ -1,3 +1,27 @@
+const fs = require("fs");
+const padawans = fs.readFileSync("./data/padawans.txt", "utf8").split("\n");
+const saberScores = fs.readFileSync("./data/scores.txt", "utf8").split("\n");
+
+const getPadawanNames = (names) => {
+  const result = padawans;
+  result.pop();
+  return result;
+}
+
+const getLightsaberScores = (scores) => {
+  const result = saberScores.map((el) => el * 1);
+  return result;
+}
+
+
+const getStats = (stats) => {
+
+}
+
+const writeStats = (stats) => {
+
+}
+
 
 module.exports = {
   getPadawanNames,
