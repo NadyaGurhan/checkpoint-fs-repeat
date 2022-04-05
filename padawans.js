281dc2bf4569385aec8773d7db5979fdbe8d4e1e
Taras Gavrilyuk <tarastim@gmail.com>
2022-04-05 10:10:22 +0300

    checkpoint-fs (2 test done)

--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf8');
+
+function getPadawanNames() {
+  return padawans.trim().split(('\n'));
+}
+
+function getLightsaberScores() {
+  const arr = scores.split('\n').sort((a, b) => b - a).map((it) => Number(it));
+  return arr;
+}
+
+function getStats() {
+  return padavans
+}
+
+function writeStats() {
+  return padavans
+}
 
 module.exports = {
   getPadawanNames,
