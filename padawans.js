96acbeddfa6553c0611e073f95124f806e3165aa
a7heis7 <a7295@yandex.ru>
2022-04-05 10:11:30 +0300

    Release 0; Padawans Stats

--- padawans.js
+++ padawans.js
@@ -1,4 +1,17 @@
+const fs = require('fs');
 
+function getPadawanNames() {
+  fs.readFileSync('./data/padawans.txt', 'utf8');
+}
+function getLightsaberScores() {
+  fs.readFileSync('./data/padawans.txt', 'utf8');
+}
+function getStats() {
+  fs.readFileSync('./data/padawans.txt', 'utf8');
+}
+function writeStats() {
+  fs.writeFileSync('.data/stats.txt');
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
