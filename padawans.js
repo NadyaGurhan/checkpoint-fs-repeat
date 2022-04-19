bebde22723abc1f2124f44e13fd5d0084df8764e
Alexander Korobov <dktwho@mail.ru>
2022-04-19 10:10:08 +0300

    R1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
+ 
+ const fs = require('fs')
+
+ function getPadawanNames() {
+   const getName = fs.readFileSync("data/padawans.txt", "utf8"); 
+  let arr = []
+  arr.push(getName.split(''))
+  
+   return arr
+
+ }
+
+
+
+
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
+
