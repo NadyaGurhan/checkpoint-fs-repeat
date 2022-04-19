3669a5115571039e67461c1264a7482d7f0b3a90
Aldar <exalonv@yandex.ru>
2022-04-19 10:09:50 +0300

    fs_ checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
 
+
+function getPadawanNames() {
+  const arr = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const padawans = arr.split('\n')
+return padawans.slice(0,-1)
+};
+console.log(getPadawanNames())
+
+function getLightsaberScores
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+    getPadawanNames,
+  //   getLightsaberScores,
+  //   getStats,
+  //   writeStats,
 };
