e2fa8f1de695ea82fec4ed742078c59081977ee6
MaratEsiev <marik744056@gmail.com>
2022-04-19 10:11:25 +0300

    release mar

--- padawans.js
+++ padawans.js
@@ -1,3 +1,23 @@
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf8');
+
+function getPadawanNames() {
+  const arr = [];
+  for (let i = 0; i < value.length; i++) {
+    arr.push(value[i]);
+  }
+  return arr;
+}
+
+function getLightsaberScores(array) {
+  let arr = ''
+  for (let i = 0; i < array.length -1; i++) {
+    arr+= array[i]);
+  }
+  return arr.split('\n').slice(0, arr.length -2 )
+}
 
 module.exports = {
   getPadawanNames,
