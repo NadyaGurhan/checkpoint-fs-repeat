398c42e7e9b41240257b1e49ee8ecaa33e2181b9
ToxicYouth <81315171+ToxicYouth@users.noreply.github.com>
2022-04-19 10:15:58 +0300

    Release 1: сделал 2 задания

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt').toString().split('\n');
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt').toString().split('\n');
+  const lightsaberScores = scores.map((score) => +score);
+  return lightsaberScores;
+}
+
+function getStats() {
+  const padawans = fs.readFileSync('data/padawans.txt').toString().split('\n');
+  const result = [];
+  for (let i = 0; i < padawans.length; i++) {
+    result.push
+  }
+  return result;
+}
+
+console.log(getStats());
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
