70d538f66a5d424fc046f446f776cbfefefa852e
Павел <pkazhgin@gmail.com>
2022-04-19 10:10:23 +0300

    Test 2dw1p1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,20 @@
+const fs = require('fs');
 
+function getPadawanNames () {
+  const arrOfPadawan = fs.readFileSync('./data/padawans.txt', 'utf8');
+  return arrOfPadawan.trim().split('\n');
+};
+function getLightsaberScores () {
+  const arrOfMarks= fs.readFileSync('./data/scores.txt', 'utf8');
+  return arrOfMarks.split('\n').map(el => el = typeof 'number');
+}
+function getStats () {
+
+}
+function writeStats () {
+
+}
+ 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
