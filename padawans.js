a09b8d450eabb318d6ce7c48091e6834ab62f9d4
Andrey Runov <elefant86@bk.ru>
2022-04-19 10:10:39 +0300

    3 tests done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,37 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawanNames = fs.readFileSync('data/padawans.txt', 'utf-8').trim();
+  const arrPadawanNames = padawanNames.split('\n');
+  return arrPadawanNames;
+}
+
+function getLightsaberScores() {
+  const lightsaberScores = fs.readFileSync('data/scores.txt', 'utf-8').trim();
+  const arrlightsaberScores = lightsaberScores.split('\n');
+  return arrlightsaberScores.map((el) => el * 1);
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const res = [];
+
+  for (let i = 0; i < names.length; i += 1) {
+    res.push([names[i], scores[i]]);
+  }
+  return res;
+}
+
+function writeStats() {
+  const stats = getStats();
+  const res = stats.map((el) => {
+    return el.join(', ');
+  });
+  console.log(res);
+  
+  fs.appendFileSync('data/stats.txt', res, 'utf-8');
+}
 
 module.exports = {
   getPadawanNames,
