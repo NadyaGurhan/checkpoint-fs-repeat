e0016bbd493720835dd44275948500b3ac22e29b
EugeneS <audoken2125@gmail.com>
2022-04-19 10:09:54 +0300

    2.5/4

--- padawans.js
+++ padawans.js
@@ -1,7 +1,44 @@
+const fs = require('fs');
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+//   writeStats,
 };
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').toString().split('\n');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8').toString().split('\n');
+
+console.log(padawans);
+
+function getPadawanNames(arr = padawans) {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== '') output.push(arr[i]);
+  }
+  return output;
+}
+// console.log(scores);
+
+function getLightsaberScores(arr = scores) {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== '') output.push(Number(arr[i]));
+  }
+  return output;
+}
+
+// console.log(tab);
+
+function getStats(arr1 = padawans, arr2 = scores) {
+  const output = [];
+  for (let i = 0; i < arr2.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      output.push(arr1[i], arr2[j]);
+    }
+  }
+  return output;
+}
+
+console.log(getStats());
