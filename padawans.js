2ca9bec4d941e11f7a9c07b1a2c32c14edc1236b
Vladislav <wlladisla@gmail.com>
2022-04-19 10:10:15 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
+const fs = require('fs');
+let filePath = './data/padawans.txt';
+let data = fs.readFileSync(filePath,'utf8');
+
+// const padawanName = 
+function getPadawanNames(data){
+  let data = fs.readFileSync(filePath,'utf8');
+  let arr = [];
+
+  for (let i = 0; i < data ; i++) {
+    arr.push(data[i]);
+  }
+  return arr;
+}
+
+
+
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
