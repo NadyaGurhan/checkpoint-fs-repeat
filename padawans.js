563ba8335f9d6e4a6b4bb9ea11fc1dcad2d2cc31
Zookie <iushulgin@gmail.com>
2022-04-19 10:17:03 +0300

    Релиз

--- padawans.js
+++ padawans.js
@@ -1,3 +1,38 @@
+const fs = require('fs');
+
+const names = fs.readFileSync('data/padawans.txt', 'utf8').split('\n').filter(str => str.length > 0);
+const scores = fs.readFileSync('data/scores.txt', 'utf8').split('\n').filter(str => str.length > 0);
+const scoresNum = []
+const stats = []
+
+for (let i = 0; i < scores.length; i += 1) {
+  scoresNum.push(parseFloat(scores[i], 10))
+}
+
+for (let i = 0; i < names.length; i += 1) {
+  stats[i] = new Array;
+  stats[i].push(names[i]);
+  stats[i].push(scoresNum[i]);
+  // console.log(stats);
+}
+
+
+
+function getPadawanNames() {
+  return names;
+}
+
+function getLightsaberScores() {
+  return scoresNum;
+}
+
+function getStats() {
+  return stats;
+}
+
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
