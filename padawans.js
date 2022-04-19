0a071ba084a576a281f53e8a8be383b5dce71449
ABessonov <abess@mail.ru>
2022-04-19 10:15:59 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,38 @@
+const fs = require('fs');
+
+const padawanFilePath = './data/padawans.txt';
+const padawanFileData = fs.readFileSync(padawanFilePath, 'utf8');
+const padawanFileLines = padawanFileData.split('\n');
+
+const lightsaberFilePath = './data/scores.txt';
+const lightsaberFileData = fs.readFileSync(lightsaberFilePath, 'utf8');
+const lightsaberFileLines = lightsaberFileData.split('\n');
+
+function getPadawanNames() {
+  const PadawanNames = [];
+  for (let i = 0; i < padawanFileLines.length - 1; i += 1) {
+    PadawanNames[i] = padawanFileLines[i];
+  }
+  return PadawanNames;
+}
+function getLightsaberScores() {
+  const lightsaberNumbers = [];
+  for (let i = 0; i < lightsaberFileLines.length; i += 1) {
+    lightsaberNumbers[i] = Number(lightsaberFileLines[i]);
+  }
+  return lightsaberNumbers;
+}
+function getStats() {
+  const arrStats = [];
+  for (let i = 0; i < lightsaberFileLines.length; i += 1) {
+    arrStats[i][0] = padawanFileLines[i];
+    arrStats[i][1] = lightsaberFileLines[i];
+  }
+  return arrStats;
+}
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
