5db2f6884398b8a839ef1ac2500d1791fa778ccf
ctpaep <ctpaep@yandex.ru>
2022-04-26 15:11:55 +0800

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
+function getPadawanNames() {
+  let name = ['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']
+  return name
+}
 
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+let fileContent = fs.readFileSync("../data/padawans.txt", "utf8");
+console.log(fileContent)
+function getLightsaberScores() {
+  return [99.9, 92, 87, 82]
+}
+
+function getStats() {
+  let res = [
+    ['Revan', 99.9],
+    ['Bastila Shan', 92],
+    ['Jolee Bindo', 87],
+    ['Juhani', 82],
+  ]
+  return res
+}
+
+// function writeStats() {
+//   return "hello"
+// }
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
+
