286c306a8a6d7b9090a560132750d85c1d9a8c20
Sergey <s.katarzhnov@yandex.ru>
2022-04-26 10:12:56 +0300

    NeVishlo

--- padawans.js
+++ padawans.js
@@ -5,3 +5,11 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+let fs = require('fs');
+let padawans = fs.readFileSync('data/padawans.txt', 'utf8');
+console.log(padawans);
+
+
+
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
