87a85bfb8d6ae061eebb99d772fb78ac719b44c2
KuvaldinDmitry <kuvaldin2012@gmail.com>
2022-04-26 14:12:12 +0400

    fourth

--- padawans.js
+++ padawans.js
@@ -14,14 +14,20 @@ function getLightsaberScores() {
   let scores = fs.readFileSync("./data/scores.txt","utf-8")
   let arr = scores.split("\n")
   // console.log(arr);
-  return arr
+  let newArr = arr.map(x => +(x));
+  console.log(newArr);
+  return newArr
 }
+
+// getLightsaberScores()
+
+
 
 function getStats(){
   let names = getPadawanNames()
   let scores = getLightsaberScores()
-  console.log(names);
-  console.log(scores);
+  // console.log(names);
+  // console.log(scores);
   let status = []
   let person = []
   for (let i = 0; i < names.length; i++) {
@@ -32,29 +38,32 @@ function getStats(){
     person = []
 
   }
-  console.log(status);
+  // console.log(status);
   return status
 }
 
-// function writeStats(){
-//   let persons = getStats()
-//   fs.writeFileSync("data/stats.txt","utf-8")
-//   for (let index = 0; index < persons.length; index++) {
-//     fs.appendFileSync("data/stats.txt", `${persons[index][0]}`, "utf-8")
-//     fs.appendFileSync("data/stats.txt", `${persons[index][1]}\n`, "utf-8")
-//   }
-// }
-// getPadawanNames()
-
-// getPadawanNames()
-// getLightsaberScores()
-getStats()
 
 
+function writeStats(){
+  let persons = getStats()
+  fs.writeFileSync("data/stats.txt","")
+  for (let index = 0; index < persons.length; index++) {
+    fs.appendFileSync("data/stats.txt", `${persons[index][0]} `)
+    fs.appendFileSync("data/stats.txt", `${persons[index][1]}\n`)
+  }
+  let data = fs.readFileSync('data/stats.txt', 'utf8')
+  data.slice(0,-5);
+  fs.writeFileSync("data/stats.txt",`${data}`)
+  console.log(data);
+}
 
-// module.exports = {
-//   getPadawanNames,
-//   getLightsaberScores,
-//   getStats,
-//   writeStats,
-// };
+// writeStats()
+
+
+
+module.exports = {
+  getPadawanNames,
+  getLightsaberScores,
+  getStats,
+  writeStats,
+};
