62b311b15356aeac819a850e4ba9aa9ec1b9e950
Anastasiia Cherkashina <cherkashina.anastasiya@gmail.com>
2022-04-26 10:18:24 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
+const fs = require('fs');
+
+function getPadawanNames () {
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const padawans = padawanNames.trim().split('\n')
+  return padawans
+}
+
+function getLightsaberScores () {
+  const LightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const scores = LightsaberScores.trim().split('\n');
+  const arrOfNum = scores.map(str => {
+    return Number(str);
+  });
+  return arrOfNum
+}
+
+// function getStats () {
+//   let score = getLightsaberScores ();
+//   let names = getPadawanNames ();
+
+// }
+
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
