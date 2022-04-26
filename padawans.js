10abe9355c5f78c3da765f001ebd252938f6810c
Teolian <teolianwork@gmail.com>
2022-04-26 22:56:13 +0300

    commit 00

--- padawans.js
+++ padawans.js
@@ -1,27 +1,19 @@
-const fs = require("fs");
-const data = fs.readFileSync("./data/padawans.txt", "utf8");
-const padawans = data.split("\n")
+const fs = require('fs');
 
-const data1 = fs.readFileSync("./data/scores.txt", "utf8");
-const scores = data1.split("\n")
-let p1 = padawans.pop()
-let p2 = scores.map(el => +el)
- console.log(padawans);
- console.log(scores);
- console.log(p2);
-
-
-
-function getPadawanNames () {
-  return padawans
+function getPadawanNames() {
+  const data = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const padawans = data.split('\n');
+  padawans.pop();
+  return padawans;
 }
- 
 
-function getLightsaberScores () {
-  
-  return p2
+function getLightsaberScores() {
+  const data1 = fs.readFileSync('./data/scores.txt', 'utf8');
+  const scores = data1.split('\n')
+    .map((el) => Number(el));
+    return scores
 }
- 
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
