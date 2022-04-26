d12bd0be5fa75892a2066b193a3946ff39d34ac3
Nick <teolianwork@gmail.com>
2022-04-26 10:11:13 +0300

    2 tests only

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
+const fs = require("fs");
+const data = fs.readFileSync("./data/padawans.txt", "utf8");
+const padawans = data.split("\n")
 
+const data1 = fs.readFileSync("./data/scores.txt", "utf8");
+const scores = data1.split("\n")
+let p1 = padawans.pop()
+let p2 = scores.map(el => +el)
+ console.log(padawans);
+ console.log(scores);
+ console.log(p2);
+
+
+
+function getPadawanNames () {
+  return padawans
+}
+ 
+
+function getLightsaberScores () {
+  
+  return p2
+}
+ 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
