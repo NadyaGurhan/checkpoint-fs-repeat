57fc189448de930d8d9fcb7935835ae5346c80ff
Igor Yakushev <i19931@mail.ru>
2022-04-26 15:12:19 +0800

    chek

--- padawans.js
+++ padawans.js
@@ -1,3 +1,27 @@
+const fs = require('fs');
+
+function getPadawanNames(){
+  const names = fs.readFileSync('data/padawans.txt', 'utf8');
+  return names
+};
+console.log(names);
+
+function getLightsaberScores(){
+  const scores = fs.readFileSync('data/scores.txt', 'utf8');
+  return scores
+};
+
+function getStats(names, scores){
+  const stats = [];
+  for (let i = 0; i < names.length; i++){
+    stats.push(`${names[i]}, scores[i]`)
+  }
+  return stats
+}
+
+function writeStats(){
+  const write = fs.writeFileSync('data/stats.txt', stats.join('\n', 'utf8'))
+}
 
 module.exports = {
   getPadawanNames,
