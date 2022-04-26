a454e5b0cb006479928d5bacced5aa256c93132b
BeTaJI <lilysh994@mail.ru>
2022-04-26 10:09:04 +0300

    Не выполнено

--- padawans.js
+++ padawans.js
@@ -1,3 +1,29 @@
+const fs = require("fs");
+const getPadawanNames = () => {
+ 
+  const names = fs.readFileSync("./data/padawans.txt", "utf-8")
+  console.log(names)
+  let arrNames = names.split("\n")
+  // arr.push(names.split("\n"))
+  console.log(arrNames)
+
+
+  return arrNames
+}
+
+
+const getLightsaberScores = () => {
+
+}
+
+const getStats = () => {
+
+}
+
+const writeStats = () => {
+
+}
+
 
 module.exports = {
   getPadawanNames,
--- spec/padawans.spec.js
+++ spec/padawans.spec.js
@@ -6,6 +6,8 @@ const {
   writeStats,
 } = require('../padawans');
 
+
+
 describe('Статистика о падаванах', () => {
   it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
     const names = getPadawanNames();
