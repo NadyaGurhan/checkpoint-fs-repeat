75e4312756717e01fc86d52e4878099f38b4245b
lopasov89 <lopasov89@gmail.com>
2022-04-26 11:09:57 +0400

    Not to work(

--- padawans.js
+++ padawans.js
@@ -1,3 +1,14 @@
+const fs = require('fs');
+const files = fs.readdirSync('./data');
+// console.log(files);
+const names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+// console.log(typeof names);
+
+const getPadawanNames = () => {
+  const arrName = [];
+  arrName.push(names.split('\n'));
+  return arrName;
+};
 
 module.exports = {
   getPadawanNames,
