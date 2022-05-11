1e47ca9657a3893267991a78e2600c753a182262
Boris <borislo051060@gmail.con>
2022-05-11 15:44:57 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,41 @@
+const fs = require('fs');
+
+
+function getPadawanNames() {
+  let arr = [];
+  
+  str = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  arr = str.split('\n');
+  arr.pop();
+  return arr;
+}
+
+function getLightsaberScores() {
+  let arr = [];
+  
+  str = fs.readFileSync('./data/scores.txt', 'utf-8');
+  arr = str.split('\n');
+  return arr.map((el) => Number(el));
+}
+console.log(getLightsaberScores());
+
+function getStats() {
+  const arr_1 = getPadawanNames();
+  const arr_2 = getLightsaberScores();
+  let arr_3 = [];
+  for (let i = 0; i < arr_1.length; i += 1) {
+    arr_3[i] = [];
+    arr_3[i].push(arr_1[i], arr_2[i]);
+
+  }
+
+  return arr_3;
+}
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
