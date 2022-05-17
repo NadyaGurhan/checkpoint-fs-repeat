90e41250eef0e36e2ff6d029df111a3d6d2fd89f
BorisVargi <bbochkarev@yandex.ru>
2022-05-17 09:46:11 +0300

    Checkpoint_1

---
+++ test.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+function getPadawanNames () { //
+  let names = fs.readFileSync("./file.txt", "utf8").split('')
+  return getPadawanNames
+}
+
+function getLightsaberScores () { // 
+  let scores = 
+  for (i = 0; i < )
+}
+
+function getStats () { // 
+
+}
+
+function writeStats () { // запись в файл 
+  fs.writeFileSync("./file.txt", "New text")
+}
