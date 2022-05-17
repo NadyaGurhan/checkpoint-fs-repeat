e692e2f98a63c335b0563f70bee15ff253f7cda4
Senik <senikarakelyan@mail.ru>
2022-05-17 10:10:48 +0300

    s

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
+const fs = require('fs')
+
+function getPadawanNames(){
+let array = fs.readFileSync('./data/padawans.txt')
+
+
+return array.split('\n')
+}
+console.log(getPadawanNames())
+
+
+// function getLightsaberScores(){
+//   let array = fs.readFileSync('./data/scores.txt').toString().split('')
+
+
+// return array
+// }
+
+
+
+
+
+
+
+
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
