fbfa3c2ab4f331fc7b85c4a4490ef8265b5d76dc
Nikita <ookami.eq@gmail.com>
2022-05-17 10:05:49 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,56 @@
+const fs = require('fs')
+
+function getPadawanNames() {
+
+const readFile = fs.readFileSync('./data/padawans.txt', 'utf8').toString()
+console.log(readFile)
+const getPad = readFile.split('\n')
+getPad.pop()
+return getPad
+}
+
+const getPadName = getPadawanNames()
+
+function getLightsaberScores(){
+
+  const readScores = fs.readFileSync('./data/scores.txt', 'utf8').toString()
+  console.log(readScores)
+  const getScores = readScores.split('\n')
+  const scoresToNum = getScores.map(el => +el)
+  return scoresToNum
+
+}
+const getScoreName = getLightsaberScores()
+
+function getStats(){
+
+// const readFile = fs.readFileSync('./data/padawans.txt', 'utf8').toString()
+// const readScores = fs.readFileSync('./data/scores.txt', 'utf8').toString()
+
+for (let i = 0; i < getPadName.length; i++) {
+getPadName[i].concat(getScoreName[i])
+}
+console.log(newArr)
+
+
+// let newArr = []
+// for (let i = 0; i < getPadName; i++) {
+//   newArr.push(i)
+//   for (let j = 0; j < getScoreName; j++) {
+//     newArr.push(j)
+//   }
+  
+// }
+
+
+}
+getStats()
+
+function writeStats(){
+
+}
+writeStats()
+
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +58,4 @@ module.exports = {
   getStats,
   writeStats,
 };
+
