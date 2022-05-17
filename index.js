662c0114705b53f536f4846e7bb03d4db9a15725
Tymphoto <tymphoto@MacBook-Pro-tymphoto.local>
2022-05-17 09:52:28 +0300

    all release

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+function getPadawanNames(){
+  let arr = fs.readFileSync('./data/padawans.txt').toString().split('\n').filter(el => !el == ' ')
+  return arr
+}
+
+const fs = require('fs')
+function getLightsaberScores(){
+  let arr = fs.readFileSync('./data/scores.txt').toString().split('\n').filter(el => !el == ' ')
+  return arr
+}
+
+
+
+function getStats(){
+  let padawans = fs.readFileSync('./data/padawans.txt').toString().split('\n').filter(el => !el == ' ')
+  let scores = fs.readFileSync('./data/scores.txt').toString().split('\n').filter(el => !el == ' ')
+  let final = padawans.map((el, index) => {
+    return [el, scores[index]]
+  })
+  return final
+}
+
+
+module.exports = {
+  getPadawanNames,
+  getLightsaberScores,
+  getStats
+}
--- padawans.js
+++ padawans.js
@@ -1,7 +1,41 @@
+// const fs = require('fs')
+
+function getPadawanNames(){
+  let arr = fs.readFileSync('./data/padawans.txt').toString().split('\n').filter(el => !el == ' ')
+  return arr
+}
+
+const fs = require('fs')
+function getLightsaberScores(){
+  let arr = fs.readFileSync('./data/scores.txt').toString().split('\n').filter(el => !el == ' ').map(el => +el)
+  return arr
+}
+
+function getStats(){
+  let padawans = fs.readFileSync('./data/padawans.txt').toString().split('\n').filter(el => !el == ' ')
+  let scores = fs.readFileSync('./data/scores.txt').toString().split('\n').filter(el => !el == ' ').map(el => +el)
+  let final = padawans.map((el, index) => {
+    return [el, scores[index]]
+  })
+  return final
+}
+
+
+function writeStats() {
+  let padawans = fs.readFileSync('./data/padawans.txt').toString().split('\n').filter(el => !el == ' ')
+  let scores = fs.readFileSync('./data/scores.txt').toString().split('\n').filter(el => !el == ' ').map(el => +el)
+  let final = padawans.map((el, index) => {
+    return [el, scores[index]]
+  })
+  let result = final.map(el => el.join(' ')).join('\n')
+  // return result
+  fs.writeFileSync(`./data/stats.txt`, `${result}`)
+}
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  writeStats
 };
