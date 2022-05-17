ddde9e53d4d9e5b021dad5c14441344af7c0d21d
Banu <gula2798@mail.ru>
2022-05-17 10:07:51 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,4 +1,20 @@
+const fs = require("fs");
+function getPadawanNames() {
+  let arr = [];
+  let result = fs
+    .readFileSync("./data/padawans.txt", "utf8")
+    .toString()
+    .split("/n");
+  arr.push(result);
 
+  return arr;
+}
+
+getPadawanNames();
+
+function getLightsaberScores() {}
+function getStats() {}
+function writeStats() {}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
