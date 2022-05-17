eddf3537b4f53bf13c350fd46f37a25a91d8c65b
Maria <zaytseva.m.v@yandex.ru>
2022-05-17 10:06:28 +0300

    1 test

--- padawans.js
+++ padawans.js
@@ -1,3 +1,33 @@
+const fs = require('fs')
+
+function getPadawanNames() {
+  // const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const line = padawans.split('\n');
+  line.splice(-1, 1);
+  console.log(line);
+  return line;
+}
+
+function getLightsaberScores () {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  let line = scores.split('\n').join(', ');
+  console.log(line);
+  const result = line.split(', ');
+  console.log(result);
+  return result;
+
+}
+
+getLightsaberScores ()
+
+function getStats () {
+
+}
+
+function writeStats () {
+
+}
 
 module.exports = {
   getPadawanNames,
