0ecd7fce2ddda09f8da6d6152cb7b70dd636187b
KatyaGap <katyakate22@mail.ru>
2022-05-17 10:06:23 +0300

    2 tests

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs = require('fs');
+
+const str1 = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const arr1 = str1.split('\n');
+
+const str2 = fs.readFileSync('./data/scores.txt', 'utf-8');
+const arr2 = str2.split('\n');
+
+function getPadawanNames() {
+arr1.splice(-1,1);
+return arr1;
+};
+
+ function getLightsaberScores(){
+return arr2.join(' ').split(' ').map(item => Number(item))
+ }
+
+ function  getStats(){
+
+
+ }
+ function writeStats(){
+
+ }
+
 
 module.exports = {
   getPadawanNames,
