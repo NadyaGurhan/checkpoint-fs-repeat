1bb521209ea84d8e2a6c3a5e94a752dec3ceb9c5
Sony <alexandrikuskos@gmail.com>
2022-05-17 10:11:42 +0300

    2,5

--- padawans.js
+++ padawans.js
@@ -5,26 +5,21 @@ function getPadawanNames() {
 }
 console.log(getPadawanNames([]))
 
-const fs = require("fs")
+
 function getLightsaberScores() {
   let arr = fs.readFileSync('data/scores.txt').toString().split('\n')
   return arr
 }
 console.log(getLightsaberScores())
 
-const fs = require("fs")
+
 function getStats() {
   let arr = fs.readFileSync('data/padawans.txt').toString().split(',')
   let arr2 = fs.readFileSync('data/scores.txt').toString().split(',')
-  //let arr3 = [...arr, ...arr2].join('');
-  //let d = arr3.filter(function (item, pos) {return arr3.indexOf(item) == pos});
+
   let arr3 = [...new Set([...arr ,...arr2])];
 console.log(arr3);
-  //let arr3 = arr.concat(arr2);
 
- // for(let i = 0; i < arr.length; i++) {
- //   `${arr[i]}, ${arr2[i]}`
- // }
 return arr3.join('\n')
 }
 console.log(getStats())
@@ -33,5 +28,5 @@ module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  //writeStats,
 };
