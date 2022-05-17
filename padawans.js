2cbac99b79449605801b45bba759f4990d8045e8
IslamCod <IslamCod>
2022-05-17 10:13:33 +0300

    Relaese

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
+function getPadawanNames(){
+  let padawans = '';
+  const fs = require ('fs');
+  const padawansList = fs.readFileSync('./data/padawans.txt', 'utf8');
+    padawans = padawansList
+    padawans.split('/n').join(' ')
+    console.log(padawans)
+ return padawans
+}
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
