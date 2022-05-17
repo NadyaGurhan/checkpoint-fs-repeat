dfd6d2e8bb2bea40e6f54c485cc019613b19b720
Artyom <ihctuyd@mail.ru>
2022-05-17 10:04:49 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,34 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const result = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return result.split('\n').slice(0, 4);
+}
+
+function getLightsaberScores() {
+  const result = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').slice(0, 4);
+  const arrNew = result.map((elem) => Number(elem));
+  return arrNew
+}
+
+function getStats() {
+  const getLi = getLightsaberScores();
+  const getPad = getPadawanNames();
+  const arrNew = [];
+
+  for (let i = 0; i < getPad.length; i += 1) {
+    const arr = getPad[i].concat(`, ${Number(getLi[i])}`);
+    console.log(arr);
+    arrNew.push(arr.split(','))
+  }
+
+
+  console.log(arrNew.map((elem) ));
+}
+
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
