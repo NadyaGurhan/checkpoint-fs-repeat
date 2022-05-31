5cb0efd3ebd74a7a21397fc142e4c8dfb7597bf4
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-05-31 09:52:24 +0300

    3 tests done, 4th troubles with commas

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs = require('fs')
+
+const getPadawanNames = () => {
+  return fs.readFileSync('./data/padawans.txt','utf-8').split('\n')
+}
+
+const getLightsaberScores = () => {
+  return fs.readFileSync('./data/scores.txt','utf-8').split('\n').map(a=>+a)
+}
+
+const getStats = () => {
+  let arr = []
+  for (let i=0;i<4;i++){
+let padawan = []
+padawan.push(getPadawanNames()[i],getLightsaberScores()[i])
+arr.push(padawan)
+  }
+  return arr
+}
+
+const writeStats = (stats) => {
+  fs.appendFileSync(`./data/stas.txt`,`${stats.join('\n')}`,'utf-8')
+}
+
+//writeStats(getStats())
 
 module.exports = {
   getPadawanNames,
