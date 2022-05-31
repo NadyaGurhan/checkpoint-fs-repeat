438d778c2fe8258d1da597bf9f1bcbe8362e59e7
Stepan Mogilyov <st.mogilev@mail.ru>
2022-05-31 10:12:10 +0300

    Вообще ничего не понял

--- padawans.js
+++ padawans.js
@@ -1,7 +1,18 @@
-
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'UTF-8').split('\n');
+const scores = fs.readFileSync('./data/scores.txt', 'UTF-8').split('\n');
+
+console.log(scores)
+
+function getPadawanNames() {
+  const arr = padawans;
+
+}
