95844955503c535e2a8daca2ca3b6e46e58be1ea
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-05-31 12:42:18 +0300

    fix: refactor as for eslint

--- padawans.js
+++ padawans.js
@@ -1,26 +1,22 @@
-const fs = require('fs')
+const fs = require('fs');
 
-const getPadawanNames = () => {
-  return fs.readFileSync('./data/padawans.txt','utf-8').split('\n')
-}
+const getPadawanNames = () => fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
 
-const getLightsaberScores = () => {
-  return fs.readFileSync('./data/scores.txt','utf-8').split('\n').map(a=>+a)
-}
+const getLightsaberScores = () => fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((a) => +a);
 
 const getStats = () => {
-  let arr = []
-  for (let i=0;i<4;i++){
-let padawan = []
-padawan.push(getPadawanNames()[i],getLightsaberScores()[i])
-arr.push(padawan)
+  const arr = [];
+  for (let i = 0; i < 4; i += 1) {
+    const padawan = [];
+    padawan.push(getPadawanNames()[i], getLightsaberScores()[i]);
+    arr.push(padawan);
   }
-  return arr
-}
+  return arr;
+};
 
 const writeStats = (stats) => {
-  fs.appendFileSync(`./data/stats.txt`,`${stats.map(a=>a.join(' ')).join('\n')}`,'utf-8')
-}
+  fs.appendFileSync('./data/stats.txt', `${stats.map((a) => a.join(' ')).join('\n')}`, 'utf-8');
+};
 
 module.exports = {
   getPadawanNames,
