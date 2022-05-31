9672eaf39a1fbe65e9f3453a23088448921a2d26
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2022-05-31 17:12:01 +1000

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,23 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs.readFileSync('data/padawans.txt').toString().split('\n').slice(0, -1);
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('data/scores.txt')
+    .toString()
+    .split('\n')
+    .map((scoreString) => +scoreString);
+}
+
+function getStats() {
+  
+}
+
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
