9e8051a42594f71ed276ecea26846d6821be8de2
Lelikk <Lelikkon@gmail.com>
2022-05-31 10:26:49 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+	const data = fs.readFileSync('data/padawans.txt', 'utf8');
+	let arr = data.split('\n');
+	arr.pop();
+	console.log(arr);
+	//return arr;
+
+}
+//console.log(getPadawanNames());
+
+function getLightsaberScores() {
+	const data = fs.readFileSync('data/scores.txt', 'utf8');
+	const arr = data.split('\n');
+	return arr
+}
+console.log(getLightsaberScores());
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  //getStats,
+  //writeStats,
 };
