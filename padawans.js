2934426afc8a776ecdc845d1105dc512d0b6c29a
Danil <flip7474@gmail.com>
2022-05-31 15:14:18 +0800

    -

--- padawans.js
+++ padawans.js
@@ -1,3 +1,19 @@
+function getPadawanNames(name) {
+  let resultName = []
+  for (let i=0;i<name.length; i++) {
+    resultName.push(name(i))
+  }
+  return resultName
+}
+
+function getPadawanScores (num) {
+  let resultNum =[];
+  for (let i=0;i<num.length; i++) {
+     resultNum.push(num[i])
+  }
+  return resultNum
+}
+
 
 module.exports = {
   getPadawanNames,
