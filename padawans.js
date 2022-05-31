9b82eb94363ba884cf7f37afcb8d6d79673ca5dc
Sergey <s.katarzhnov@yandex.ru>
2022-05-31 09:51:37 +0300

    Release => Final

--- padawans.js
+++ padawans.js
@@ -1,7 +1,44 @@
-
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'UTF-8').split('\n');
+const scores = fs.readFileSync('./data/scores.txt', 'UTF-8').split('\n');
+
+function getPadawanNames() {
+  const arr = padawans;
+  if (arr.length > 4) {
+    arr.length -= 1;
+    return arr;
+  }
+
+  return arr;
+}
+
+function getLightsaberScores() {
+  const arr = scores;
+  const res = [];
+  arr.forEach((el) => res.push(Number(el)));
+  return res;
+}
+
+function getStats() {
+  const pad = getPadawanNames();
+  const score = getLightsaberScores();
+  const arr = [];
+  for (let i = 0; i < pad.length; i++) {
+    arr.push([pad[i], score[i]]);
+  }
+  return arr;
+}
+
+function writeStats() {
+  const res = getStats().map((el) => `${el[0]} ${el[1]}`).join('\n');
+  const path = 'data/stats.txt';
+  return fs.writeFileSync(path, res);
+}
