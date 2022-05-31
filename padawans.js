a32031b790efb2fe701dc748bce0c1b0c66da109
Maxim <maxpopov19@icloud.com>
2022-05-31 10:22:03 +0300

    1,2 passed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+const getPadawanNames = () =>{
+  let names = fs.readFileSync(`./data/padawans.txt`, `utf-8`);
+   names = names.split('\n');
+   names.pop();
+  return names;
+  
+}
+console.log(getPadawanNames());
+
+const getLightsaberScores = () =>{
+  let scores = fs.readFileSync(`./data/scores.txt`, `utf-8`)
+   scores =  scores.split('\n');
+  return scores.map(x => Number(x))
+}
+
+console.log(getLightsaberScores())
+const getStats = () =>{
+}
+const writeStats = () =>{
+
+}
+
 
 module.exports = {
   getPadawanNames,
