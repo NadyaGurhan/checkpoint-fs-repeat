242dbcc5b8979e3637b1d0ae6ef6037b27bf6e63
Sadan <daniel.sadkov12@gmail.com>
2022-05-31 09:50:29 +0300

    relise1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs = require('fs')
+let filePath1 = "./data/padawans.txt"
+let data1 = fs.readFileSync('./data/padawans.txt')
+
+function getPadawanNames(data1){
+let result = data1.toString()
+let result1 = result.split('/n')
+return result1
+}
+
+function getLightsaberScores(){
+
+}
+
+function getStats(){
+
+}
+
+function writeStats(){
+
+}
 
 module.exports = {
   getPadawanNames,
