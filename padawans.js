d6514f6057239c471561011292c3b91a61146801
Julia <sizova.julia.1990@gmail.com>
2022-05-31 10:14:01 +0300

    fail

--- padawans.js
+++ padawans.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+function getPadawanNames() {
+  
+  const array = fs.readFileSync("./data/padawans.txt", "utf8").split('\n')
+  
+  return array
+}
+function getLightsaberScores() {
+  return
+}
+function getStats() {
+  return
+}
+function writeStats() {
+  return
+}
 
 module.exports = {
   getPadawanNames,
