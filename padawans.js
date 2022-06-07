0e81e6ba2f7530a7f7b315819f7f10a0c909449e
Emiliya <emilya.korkunova@gmail.com>
2022-06-07 10:33:36 +0300

    Release 1: tests 1-3

--- padawans.js
+++ padawans.js
@@ -1,3 +1,53 @@
+const fs = require('fs');
+
+  function getPadawanNames() {
+    try {
+      const content = fs.readFileSync('./data/padawans.txt', 'utf-8');
+      const contentArr = content.split('\n');
+      const filteredArr = contentArr.filter((el) => el.trim() != '')
+      return (filteredArr);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  getPadawanNames();
+
+  function getLightsaberScores() {
+    try {
+      const scores= fs.readFileSync('./data/scores.txt', 'utf-8');
+      const scoresArr = scores.split('\n');
+      const filteredScoresArr = (scoresArr.filter((el) => el.trim() != '')).map((item) => +item)
+      return(filteredScoresArr);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  getLightsaberScores();
+
+
+function getStats() {
+  let statsArr = getPadawanNames().map((el) => {
+    return [el]
+  })
+  getLightsaberScores().forEach((item, index) => {
+    statsArr[index].push(item);
+  }) 
+  return (statsArr);
+}
+getStats()
+
+function writeStats(){
+  try {
+    getStats().forEach((name, i) => {
+     return fs.appendFileSync('data/stats.txt', i >= getStats().length-1? name.join(" ") : name.join(" ") +"\n")
+    })
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+writeStats()
+
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +55,5 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+
