0973e29273498f445063002912c5fdee930a29f4
Almas <Almaz.sgv1@mail.ru>
2022-06-07 10:50:21 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,12 @@
 
+const fs = require ('fs');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
+const getPadawanNames = fs.readFileSync('.data/padawans.txt', 'utf8');
+const getLightsaberScores = fs.getLightsaberScores('.data/scores.txt', 'utf8');
