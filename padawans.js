2db7e56d4cafb55998b40c4043da0972db3d1ad9
Ayub Achildiev <75487903+ayub-a@users.noreply.github.com>
2022-06-07 10:35:56 +0300

    checkpoint-fs/all-passed

--- padawans.js
+++ padawans.js
@@ -1,3 +1,49 @@
+const fs = require('fs');
+const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const lightSaberScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+// get padawan names
+function getPadawanNames() {
+  return padawanNames.split('\n');
+}
+
+// get light-saber scores
+function getLightsaberScores() {
+  const scores = lightSaberScores.split('\n');
+  const arr = [];
+
+  for (let num of scores) {
+    arr.push(+num);
+  }
+
+  return arr;
+}
+
+// get stats
+function getStats() {
+  const statics = [];
+
+  const padawanNames = getPadawanNames();
+  const lightSaberScores = getLightsaberScores();
+
+  for (let i = 0; i < padawanNames.length; i++) {
+    statics.push([padawanNames[i], lightSaberScores[i]]);
+  }
+
+  return statics;
+}
+
+// write stats
+function writeStats() {
+  const stats = getStats();
+  let data = '';
+
+  for (let elem of stats) {
+    data += `${elem.join(' ')}\n`;
+  }
+
+  fs.writeFileSync('./data/stats.txt', data.trim());
+}
 
 module.exports = {
   getPadawanNames,
