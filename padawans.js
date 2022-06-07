427956e31139ebbc6d1fbfa666b8e94990724556
Kristina <kotova.kristina@gmail.com>
2022-06-07 10:51:07 +0300

    /

--- padawans.js
+++ padawans.js
@@ -1,13 +1,15 @@
+const fs = require('fs');
+
 function getPadawanNames(){
-  let arr = fs.readFileSync('data/padawans.txt', 'utf8')
-  let nameBlock = [];
- for(let i = 0; i < arr.length; i++) {
-   nameBlock.push(arr[i])
- }
+  let arr = fs.readFileSync('./data/padawans.txt', 'utf-8');
+ return arr.split('/n').filter(elem => elem !== '')
 }
 
 function getLightsaberScores() {
-
+// let arr2 = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+// let score = [];
+// for(let i = 0; i < arr2.length; i++) {
+//   score.push(arr2[i])
 }
 
 function getStats(){
