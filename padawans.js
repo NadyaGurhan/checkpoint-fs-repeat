998b737c86ff203a4066aaa309e32a30c22421d8
Marina <kikkafikka90@gmail.com>
2022-06-07 11:39:18 +0400

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  let padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return padawans.slice(0, padawans.length - 1);
+}
+// console.log(getPadawanNames())
+
+function getLightsaberScores() {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  let scoresNum = [];
+  for (let i = 0; i < scores.length; i++) {
+    scoresNum.push(Number(scores[i]));
+  }
+  return scoresNum;
+}
+
+// console.log(getLightsaberScores())
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
