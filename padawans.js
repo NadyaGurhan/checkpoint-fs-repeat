e5072dc4991156b1d2f4bedb83e8b8844b81a3e1
Kristina <kotova.kristina@gmail.com>
2022-06-07 09:59:00 +0300

    proval

--- padawans.js
+++ padawans.js
@@ -1,3 +1,23 @@
+function getPadawanNames(){
+  let arr = fs.readFileSync('data/padawans.txt', 'utf8')
+  let nameBlock = [];
+ for(let i = 0; i < arr.length; i++) {
+   nameBlock.push(arr[i])
+ }
+}
+
+function getLightsaberScores() {
+
+}
+
+function getStats(){
+
+}
+
+function writeStats() {
+
+}
+
 
 module.exports = {
   getPadawanNames,
