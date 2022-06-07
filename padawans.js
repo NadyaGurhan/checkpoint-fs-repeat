4e2158e8a701d5c5276aec17ccf78de7ba1d0bce
Aleksandr <sasha-pticin@inbox.ru>
2022-06-07 10:47:20 +0300

    release 1. Checkpoint 40%

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
+const fs = require('fs');
+
+const getPadawanNames = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+
+const getPadawanScoresString = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+let getPadawanScores = [];
+for (let i = 0; i < getPadawanScoresString.length; i++) {
+  getPadawanScores.push(+getPadawanScoresString[i])
+};
+
+
+
+let getStats = [];
+for (let i = 0; i < getPadawanScores.length; i++) {
+  getStats.push([getPadawanNames[i], getPadawanScores[i]]);
+}
+console.log(getStats);
+
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
+  getPadawanScores,
   getStats,
-  writeStats,
+ // writeStats,
 };
+
