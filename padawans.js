c215b53c1e6f4a62330a579fd94f9b4d7d2227f5
BorisVargi <bbochkarev@yandex.ru>
2022-06-07 09:59:05 +0300

    1-3

--- padawans.js
+++ padawans.js
@@ -1,9 +1,41 @@
+const fs = require('fs')
+
+
+function getPadawanNames () { 
+  const names = fs.readFileSync('.data/padawans.txt', 'utf8').trim().split('\n')
+  return names
+  }
+  console.log(getPadawanNames())
+
+
+
+function getLightsaberScores () { 
+  const scores = fs.readFileSync('.data/scores.txt', 'utf8').trim().split('\n')
+  return scores
+}
+console.log(getLightsaberScores());
+
+
+
+function getStats () {
+  let stats = []
+  const scores = fs.readFileSync('.data/scores.txt', 'utf8').trim().split('\n')
+  const names = fs.readFileSync('.data/padawans.txt', 'utf8').trim().split('\n')
+  for (i = 0; i <= names.length; i ++) {
+    stats.push (names[i] + scores[i])
+  }
+  return stats
+ }
+console.log(getStats())
+
+
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
 
 
