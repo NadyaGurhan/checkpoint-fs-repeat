9b9a98f4e7b1e6cdeb54ca86917c378118ad8a33
Arina <arina_ageeva_1999@mail.ru>
2022-06-07 10:53:46 +0300

    Чекпоинт 1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,28 @@
+const fs = require('fs')
+
+function getPadawanNames(){
+  const text = fs.readFileSync('./data/padawans.txt','utf-8')
+  let arr = text.split('\n')
+  arr.pop()
+  return arr
+ 
+}
+
+function getLightsaberScores(){
+  const text = fs.readFileSync('./data/scores.txt','utf-8')
+  let arr = text.split('\n')
+  return arr.map(elem => Nomber(elem))
+  
+}
+
+function getStats(){
+  // const arr2 = 
+  return arr2
+}
+
+function writeStats(){
+
+}
 
 module.exports = {
   getPadawanNames,
