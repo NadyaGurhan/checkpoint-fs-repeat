788419ffdf843421d59e98f4501cb4ff9c962052
KH.Yaroslav <icpr.johni@gmail.com>
2022-06-07 10:55:17 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+// const str = fs.readFileSync('./data/padawans.txt', 'utf8');
+// console.log("🚀 ~ file: padawans.js ~ line 3 ~ str", str)
+
 
 module.exports = {
   getPadawanNames,
@@ -5,3 +9,14 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  let arr = [];
+  const str = fs.readFileSync('./data/padawans.txt', 'utf8');
+  for (let i = 0; i < str.length; i++ ) {
+    arr.push(str[i]);
+  }
+  return arr
+}
+console.log("🚀 ~ file: padawans.js ~ line 20 ~ str", str)
+console.log(getPadawanNames(str))
