28b0bb29696705f4486177239b657576607e5f9d
Ruslan <rrrruuus@yandex.ru>
2022-06-07 10:45:07 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,29 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const pad = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return pad.split('\n').slice(0, -1);
+}
+
+
+
+function getLightsaberScores() {
+  const score = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const score2 = score.split('\n');
+  const res = [];
+  for (let i = 0; i < score2.length; i += 1) {
+    res.push(+score2[i]);
+  }
+  return res;
+}
+
+
+
+
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  //getStats,
+ // writeStats,
 };
