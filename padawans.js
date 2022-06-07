e2b56655b612add4929e880f1c0166e7618a9fe6
Bayar <bayarik@bk.ru>
2022-06-07 10:34:57 +0300

    check w1d1 test1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,17 @@
+function getPadawanNames() {
+  const fs = require('fs');
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const arr = []
+  const slpitPadawans = padawans.split('\n');
+  for (let i = 0; i < slpitPadawans.length - 1; i++) {
+    arr.push(slpitPadawans[i]);
+  }
+  return arr;
+}
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
