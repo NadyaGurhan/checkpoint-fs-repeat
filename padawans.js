fda752df11835dd39948da02870dab22f7db33eb
U10 <uliaten10@gmail.com>
2022-06-07 10:06:04 +0300

    Костыльный релиз

--- padawans.js
+++ padawans.js
@@ -1,3 +1,36 @@
+const fs = require('fs');
+// 'getPadawanNames возвращает список падаванов из файла `data/padawans.txt`'
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return names.trim().split('\n');
+}
+
+console.log(getPadawanNames());
+
+//'getPadawanScores возвращает оценки владения световым мечом из файла `data/scores.txt`
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return scores.split('\n').map((el) => +el);
+}
+
+console.log(getLightsaberScores());
+
+//'getStats возвращает таблицу соответствия падавана и оценки владения световым мечом'
+
+function getStats() {
+  return getPadawanNames().map((name, i) => {
+    return [name, getLightsaberScores()[i]];
+  });
+}
+
+console.log(getStats());
+
+// 'writeStats сохраняет статистику в файл `data/stats.txt`'
+function writeStats () {
+  fs.writeFileSync('data/stats.txt', getStats().map(el => el.join(',').split(',').join(' ')).join('\n'))
+}
+
 
 module.exports = {
   getPadawanNames,
