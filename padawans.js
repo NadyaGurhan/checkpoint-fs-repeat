02c3f16f65bf54bff2e7d4c88a69e1c1d5815cd5
Artem Karasev <art.karasev@gmail.com>
2022-06-28 10:20:31 +0300

    check_point

--- padawans.js
+++ padawans.js
@@ -1,7 +1,35 @@
-
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+const fs = require('fs');
+
+// console.log(padawans);
+
+function getPadawanNames() {
+  let padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return padawans.split('\n');
+}
+//* Не сразу понял как какждый элемент перевести в number
+function getLightsaberScores() {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const getScore = scores.split('\n');
+  return getScore.map((el) => Number(el));
+}
+//* Вывел результат в массив, но не успел вывести каждую коллекцию в подмассив
+function getStats() {
+  let result = [];
+  const pad = getPadawanNames();
+  const score = getLightsaberScores();
+  for (let i = 0; i < pad.length; i++) {
+    result.push(pad[i], score[i]);
+  }
+  return result;
+}
+
+// console.log(getStats());
+
+function writeStats() {}
