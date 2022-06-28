7c33b16fd90d2fa2afc0a74ab27c7ebfc770a73b
GrishaSvir012 <grishasvir1996@gmail.com>
2022-06-28 10:19:51 +0300

    Всё плохо

--- padawans.js
+++ padawans.js
@@ -1,4 +1,14 @@
+const fs = require ('fs');
+const { toNamespacedPath } = require('path');
 
+function getPadawanNames() {
+  const arr1 =[]
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  for(let i =0; i<names.length; i++){
+    console.log (names[i])
+    // arr1.push(`$`)
+  }
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
