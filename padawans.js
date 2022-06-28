762286071826e4aa64cbf487426a662a34924eae
Aleksandr <gazmaster.msk@gmail.com>
2022-06-28 10:18:06 +0300

    Тесты не пробовал, старался успеть логику

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
+const fs = require('fs');
 
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+const getPadawanNames = () => {
+  let padawanList = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  let padawanListInline = padawanList.split('\n').join(' ');
+  console.log(typeof padawanList);
+  return padawanListInline;
 };
+
+const getLightsaberScores = () => {
+  let padawanScores = fs.readFileSync('./`data/scores.txt`', 'utf-8');
+  let scoresListInline = padawanScores.split('\n').join(' ');
+  return scoresListInline;
+};
+
+const getStats = (arrName, arrScores) => {
+  padawanList = padawanList.split(' ');
+  padawanScores = padawanScores.split(' ');
+  let table = [];
+  for (let i = 0; i < padawanList.length; i++) {
+    table.push[padawanList[i], padawanScores[i]]
+  }
+  return table;
+}
+
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
+console.log(getPadawanNames());
