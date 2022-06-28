86ef6bed3d70c354d589c7a8ef9326576b756052
Rina <sosnina.ek@yandex.ru>
2022-06-28 10:19:42 +0300

    .

--- padawans.js
+++ padawans.js
@@ -1,3 +1,27 @@
+const fs = require('fs');
+
+function getPadawanNames(){
+  let arrNames = [];
+  arrNames = fs.readFileSync('./data/padawans.txt', 'utf8').split("\n");
+  return(arrNames);
+}
+
+function  getLightsaberScores(){
+  let arrScores = [];
+  arrScores = fs.readFileSync('./data/scores.txt', 'utf8').split("\n");
+  return(arrScores);
+}
+
+function  getStats(){
+  const lines = fs.readFileSync('./data/padawans.txt', 'utf8');
+  return(lines);
+}
+
+function writeStats(){
+  const lines = fs.readFileSync('./data/padawans.txt', 'utf8');
+  return(lines);
+}
+
 
 module.exports = {
   getPadawanNames,
