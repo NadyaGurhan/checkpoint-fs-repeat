e2f0a90a2967a95ff4c0d6528a1ddb8c732046b6
Serg <zaytsergo@gmail.com>
2022-06-28 10:16:15 +0300

    epic fail

--- padawans.js
+++ padawans.js
@@ -1,7 +1,19 @@
+const fs = require('fs');
+const {
+  getPadawanNames
 
+}
+function getPadawanNames (num){
+ const nameArr = [];
+  for (let i = 0; i < num; i++){
+    nameArr.push()
+  }
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
