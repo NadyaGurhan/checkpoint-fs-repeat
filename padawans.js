a45b53671abd9ac894bef919be44a362d820773b
Artyom <alotofpainhaha@mail.ru>
2022-06-28 10:17:30 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,46 @@
+ const fs = require('fs')
+ const names = './data/padawans.txt'
+ const namesOfPad = fs.readFileSync(names, 'utf-8')
+ 
+ function getPadawanNames() {
+  newName = namesOfPad.split('\n')
+  let newArr = []
+  for (let i = 0; i < 4; i += 1){
+    newArr.push(newName[i])
+  }
+  return newArr
+ }
+
+console.log(getPadawanNames)
+
+const skill = './data/scores.txt'
+const skillLvl = fs.readFileSync(skill, 'utf-8')
+
+function getLightsaberScores() {
+  newSkill = skillLvl.split('\n')
+  let newArr1 = []
+  for (let i = 0; i < 4; i += 1){
+    newArr1.push(Number(newSkill[i]))
+}
+return newArr1
+}
+
+function getStats(){
+  const padNames = (getPadawanNames())
+  const score = (getLightsaberScores())
+  let newArr2 = []
+  for(let i = 0; i < 4; i +=1){
+    newArr2.push([padNames[i], score[i]])
+
+    }
+  return newArr2
+}
+
+
+function writeStats(){
+  fs.appendFileSync('./data/stats.txt', getStats())
+}
+
 
 module.exports = {
   getPadawanNames,
