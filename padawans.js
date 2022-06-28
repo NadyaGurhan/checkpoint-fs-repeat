fe5863c76e9ac64880bf403cd822c278c2ee9f3e
Anastasia <nastassi@list.ru>
2022-06-28 09:21:26 +0200

    релиз 1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
+const fs = require('fs');
+const padavansName = fs.readFileSync('./data/padawans.txt', 'utf-8');
+// console.log(padavansName);
+
+const padavansArr = padavansName.trim().split('\n');
+// console.log(padavansArr);
+
+function getPadawanNames() {
+  const personArray = [];
+  for (let i = 0; i < padavansArr.length; i += 1) {
+    personArray.push(padavansName.name());
+  }
+  return personArray;
+}
 
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
 };
