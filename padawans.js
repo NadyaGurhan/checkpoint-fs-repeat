034e9fd9c9a80c238e614cd0ca10d256d05ab1a7
Ivan Kosenko <jean.kosenko@gmail.com>
2022-06-28 10:17:19 +0300

    (

--- padawans.js
+++ padawans.js
@@ -1,3 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+function getPadawanNames(file) {
+  const namesFile = fs.readFileSync(path.join(__dirname, `../${file}`), 'utf-8');
+  const names = [];
+  return names.push(namesFile);
+}
+
+function getLightsaberScores(file) {
+  return fs.readFileSync(path.join(__dirname, `../${file}`), 'utf-8');
+}
+
+function getStats(file) {
+  return fs.readFileSync(path.join(__dirname, `../${file}`), 'utf-8');
+}
+
+const writeStats = () => fs.appendFileSync('./data/stats.txt');
 
 module.exports = {
   getPadawanNames,
