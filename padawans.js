3dbcd814b431341a3841d5b1a6817748a04cf6cc
Andrey Fesechko <andrey.fesechko@gmail.com>
2022-06-28 10:25:17 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,3 +1,37 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const readText = fs.readFileSync('./data/padawans.txt');
+  return readText.trim().split('\n');
+}
+
+getPadawanNames();
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return readNum = scores.split('\n').map((el) => +el);
+}
+
+getLightsaberScores();
+
+function getStats() {
+  let pad = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  pad = pad.trim().split('\n');
+  scores = scores.split('\n').map((el) => +el);
+  return pad.map((el, index) => [el, scores[index]]);
+}
+
+getStats();
+
+function writeStats() {
+  const result = getStats().join('\n').replace(/,/gmi, ' ');
+  const stats = fs.writeFileSync('./data/stats.txt', result);
+}
+
+writeStats();
+
+
 
 module.exports = {
   getPadawanNames,
