fc79011dedf6fd35f9533c689027b34f8540b5f1
Edgar <senior.golosnoj@gmail.com>
2022-06-28 10:21:22 +0300

    release super

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
-    node: true,
-    jest: true,
   },
   extends: [
     'airbnb-base',
   ],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: [
-    'jest',
-  ],
   rules: {
   },
 };
--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+
+fs.readFileSync('data/padawans.txt');
+
+function getPadawanNames() {
+  for (let i = 0; i <= fs.length; i += 1) {
+    return (fs.split(' '));
+  }
+}
+// console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  return fs.readFileSync('data/scores.txt');
+}
+
+function getStats() {
+}
+
+function writeStats() {
+
+}
 
 module.exports = {
   getPadawanNames,
