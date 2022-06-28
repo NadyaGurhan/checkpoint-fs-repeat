20b058dee4653b51bddd1fd97465054c5b11deef
Dzagoev Azamat <azamatdzag@mail>
2022-06-28 10:18:10 +0300

    fiasko

--- padawans.js
+++ padawans.js
@@ -1,7 +1,20 @@
+const fs = require("fs");
+
+function getPadawanNames() {
+  const names = fs.readFileSync("./data/padawans.txt", "utf-8");
+  return names.split(" ");
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+  const nam = fs.readFileSync(`data/scores.txt`, "utf-8");
+  return nam.split(" ");
+}
+getLightsaberScores();
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
