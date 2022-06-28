3aff69e47ddf19f5b8bdedd317da081ac499a76c
Vadim <Steg560@yandex.ru>
2022-06-28 10:25:17 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const read = fs.readFileSync('./data/padawans.txt', 'utf-8');
+
+  return read.trim().split('\n');
+}
+
+getPadawanNames();
+
+function getPadawanScores() {
+  const lightsabersRead = fs.readFileSync('./data/scores.txt', 'utf-8');
+  // console.log(lightsabersRead)
+  return lightsabersRead.trim().split('\n').map((el) => +el);
+}
+
+getPadawanScores();
+
+
+
+
 
 module.exports = {
   getPadawanNames,
