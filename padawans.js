072d1f208f4b663f5c2b37868e24f1f8c0d17e05
serkoo10 <sercoo1407@icloud.com>
2022-06-29 01:24:40 +0300

    chepoint

--- padawans.js
+++ padawans.js
@@ -1,3 +1,37 @@
+const fs = require('fs');
+  function getPadawanNames () {
+    const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+
+return padawans
+;
+}
+console.log(getPadawanNames());
+
+function getLightsaberScores () {
+  const scores = fs.readFileSync('./data/scores.txt','utf-8').trim().split('\n').map(el => +el); 
+  return scores;
+ };
+
+
+ function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  let stats = names.map((el,index) => {
+let masStats=[];
+masStats.push(names[index],scores[index]);
+return masStats;
+  })
+  return stats;
+ }
+ console.log(getStats())
+
+ function writeStats () {
+ const dataList = getStats().map(el => el.join(' ')).join('\n')
+ fs.writeFileSync('data/stats.txt', dataList)
+ return dataList;
+
+ }
+ console.log(writeStats());
 
 module.exports = {
   getPadawanNames,
