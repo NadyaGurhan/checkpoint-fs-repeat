20abae8b8196461bca66dece4cf0859d2b952a88
Павел Глухарев <pavelgluharev@MacBook-Air-Pavel.local>
2022-06-28 10:15:20 +0300

    padawan

--- padawans.js
+++ padawans.js
@@ -1,4 +1,12 @@
-
+function getPadawanNames(names) {
+const namesArr = [];
+const param = require('./data/padawans.txt')
+for (let i = 0; i < names.length; i += 1){
+namesArr.push(param())
+})
+}
+return namesArr;
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
