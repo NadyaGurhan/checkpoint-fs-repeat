82fd28e8ed57fcf98f09a6e48b3ba85e5509cb2b
Pavel <pvstarodubtsev@gmail.com>
2022-06-28 10:15:32 +0300

    ya ne molodec

--- padawans.js
+++ padawans.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawan = fs.readFileSync('./data/padawans.txt', 'utf8');
+ 
+  return padawan.split(' ');
+
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
+
+
 
 module.exports = {
   getPadawanNames,
