49309ace641090264a584bc56c15412cbde0d2de
Shamil <shamil.chuchaev@gmail.com>
2022-07-05 12:09:00 +0500

    Release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
-const fs = require('fs')
+/* eslint-disable indent */
+const fs = require('fs');
+const file = fs.readFileSync('data/padawans.txt', 'utf-8');
+console.log(file)
+
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+    getPadawanNames,
+    getLightsaberScores,
+    getStats,
+    writeStats,
+};
+
+function getPadawanNames() {
+    const arr = [];
+    return arr.push(file.toString);
 };
