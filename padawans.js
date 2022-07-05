08d0405bff767dbcf29afb34e9f9a6e283bd6ac9
Daria Zakharova <99590272+lunaaur@users.noreply.github.com>
2022-07-05 10:02:37 +0300

    Update padawans.js

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
-const fs = require('fs')
+const fs = require('fs');
+
+let name = '';
+
+const getPadawanNames = () => {
+  name = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return name;
+};
+console.log(getPadawanNames());
+
+let scores = '';
+const getLightsaberScores = () => {
+  scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return scores;
+};
+
+console.log(getLightsaberScores());
+
+let array = [];
+array = getPadawanNames().split(' ');
+const newArray = String(array).replace(/\n/gi, ' ');
+
+array = newArray.split(' ');
+
+const results = [];
+const getStats = () => {
+  for (let i = 0; i < array.length - 1; i += 1) {
+    const names = array[i];
+    results.push(names);
+  }
+  return results;
+};
+
+console.log(getStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
