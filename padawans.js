e98e7341946d2fa781da762f6e53770c3c404ff2
FARİZA <ahmetova.fariza@gmail.com>
2022-07-05 10:05:03 +0300

    NOTHİNG

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+
+getPadawanNames = () => {
+let result = [];
+let path = 'data/padawans.txt';
+let datas = fs.readFileSync(path, 'utf-8');
+for (let i = 0; i < datas.length; i++) {
+result.push(datas);
+}
+return result;
+};
+console.log(getPadawanNames());
+
+getPadawanNames = () => {
+
+};
+
+getStats = () => {
+
+};
+
+writeStats = () => {
+
+};
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
+  getPadawanNames,
   getStats,
   writeStats,
 };
