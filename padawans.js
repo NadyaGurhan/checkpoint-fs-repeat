97c6db0b988615d441cbd717af50ab354d3af8fd
Dmitriy Astafev <dim-as82@yandex.ru>
2022-07-05 11:52:38 +0500

    all done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,28 @@
 const fs = require('fs')
+
+function getPadawanNames () {
+  let arr = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+  return arr.slice(0, arr.length - 1)
+}
+
+function getLightsaberScores(){
+  let arr = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  return arr.map(item => +item) 
+}
+
+function getStats() {
+  let arr = []
+for (let i = 0; i < getPadawanNames().length; i += 1) {
+  arr.push([getPadawanNames()[i], getLightsaberScores()[i]])
+}
+return arr;
+}
+
+function writeStats() {
+  fs.writeFileSync('./data/stats.txt', getStats().map(item => item.join(' ')).join('\n'))
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
