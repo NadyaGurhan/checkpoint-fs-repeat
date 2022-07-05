f94040145df29fba87cc2e8ccdb28a5c8ee59233
Rashid <rnurshinov@gmail.com>
2022-07-05 12:06:00 +0500

    release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,15 @@
 const fs = require('fs')
+
+function getPadawanNames () {
+  let arr = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+  return arr.slice(0, arr.length - 1)
+}
+
+function getLightsaberScores(){
+  let arr = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  return arr.map(item => +item) 
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
