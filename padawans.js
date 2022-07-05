82576e10cdb36b730456d58285f65a399b00fa90
Valeriy <epifanovvaleriy@gmail.com>
2022-07-05 10:02:07 +0300

    padawans

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return names;
+}
+function getLightsaberScores() {
+  const scoreTxt = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return scoreTxt;
+}
+function getStats() {
+  const stats = fs.writeFileSync('./data/stats.txt', 'Revan 99.9\nBastila Shan 92\nJolee Bindo 87\nJuhani 82');
+  const data = fs.readFileSync('data/stats.txt', 'utf8').split(',');
+return data;
+}
+
+console.log(getStats());
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
