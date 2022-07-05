d50c2e28e5ea59879f1260eb5a73b0c7ffd665b0
Aleksandr Romanov <klaster39@mail.ru>
2022-07-05 10:04:44 +0300

    release0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,27 @@
 const fs = require('fs')
+
+const getPadawanNames = () => {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  const padawansArr = padawans.split('\n');
+  return padawansArr
+}
+
+const getLightsaberScores = () => {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8')
+  const scoresArr = scores.split('\n')
+  return scoresArr
+}
+
+// console.log(getLightsaberScores());
+
+const getStats = () => {
+
+}
+
+const writeStats = () => {
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
