88b1f1a93edec02d64d4025371d0ce9f5c96a107
Edouard <professionnel.baillot@gmail.com>
2022-07-05 09:04:58 +0200

    Final Release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,29 @@
 const fs = require('fs')
+const padavansList = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n').filter(padawan => padawan !== '');
+console.log(padavansList);
+const scoreOfKnife = fs.readFileSync('data/scores.txt', 'utf-8').split('\n').map(Number);
+console.log(scoreOfKnife);
+function getPadawanNames() {
+	return padavansList;
+}
+
+function getLightsaberScores() {
+	return scoreOfKnife
+}
+
+function getStats() {
+	const concatPadawan = padavansList.concat(scoreOfKnife)
+	console.log(concatPadawan);
+}
+function writeStats() {
+	const dataStats = fs.writeFileSync('data/stats.txt', getStats().toString(), 'utf-8');
+	return dataStats
+}
+
+
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+	getPadawanNames,
+	getLightsaberScores,
+	getStats,
+	writeStats,
 };
