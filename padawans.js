26195b4fa87512402446f8eb0f385fc77decd1e4
Aleksei Vlasov <aleks101989@mail.ru>
2022-07-05 15:02:09 +0800

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  let data = fs.readFileSync("data/padawans.txt", "utf-8");
+  let newData = data.split("\n");
+  return newData.slice(0, 4);
+}
+
+getPadawanNames();
+
+function getLightsaberScores() {
+  let scores = fs.readFileSync("data/scores.txt", "utf-8");
+  let newScores = scores.split("\n").map((Element) => Number(Element));
+  return newScores;
+}
+
+getLightsaberScores();
+
+function getStats() {}
+
+function writeStats() {}
