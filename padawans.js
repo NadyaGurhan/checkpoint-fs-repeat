ef59cd1a86335bb9847f45214237659c22b986ec
FARİZA <ahmetova.fariza@gmail.com>
2022-07-05 19:40:09 +0300

    1

--- padawans.js
+++ padawans.js
@@ -1,30 +1,22 @@
 const fs = require('fs');
 
-getPadawanNames = () => {
+const getPadawanNames = () => {
 let result = [];
-let path = 'data/padawans.txt';
-let datas = fs.readFileSync(path, 'utf-8');
-for (let i = 0; i < datas.length; i++) {
-result.push(datas);
-}
-return result;
+let path = './data/padawans.txt';
+let datas = fs.readFileSync(path, 'utf-8').trim().split('\n');
+return datas;
 };
 console.log(getPadawanNames());
 
-getPadawanNames = () => {
+const getStats = () => {
 
 };
 
-getStats = () => {
-
-};
-
-writeStats = () => {
+const writeStats = () => {
 
 };
 
 module.exports = {
-  getPadawanNames,
   getPadawanNames,
   getStats,
   writeStats,
