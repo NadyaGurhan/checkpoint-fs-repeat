59c7bd98e9c7e9832ce2dd22c89b251bda134d8b
Anton <anton_feller@mail.ru>
2022-07-05 10:05:01 +0300

    Почти 2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  let a = [];
+  a = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+  
+  return a;
+}
+function getLightsaberScores() {
+  return fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+}
+
+function getStats(){
+
+}
+
+function writeStats(){
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
