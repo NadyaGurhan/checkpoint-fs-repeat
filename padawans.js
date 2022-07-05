13a3c8a03d52fa2d07aed1beeea91c5bf53889d5
aybika <adamovaaybika@mail.ru>
2022-07-05 10:05:07 +0300

    iii

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+let names;
+function getPadawanNames(){
+   names = fs.readFileSync('padawans.txt');
+}
+
+function getLightsaberScores(){
+  const number = fs.readFileSync('scores.txt');
+}
+ function getStats(){
+for(let i = 0; i,i<names.length; i++){
+  
+
+
+}
+  
+ }
+ function writeStats(){
+
+ }
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
