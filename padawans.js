d751ef003a647a2982df8997b830dd14406f17f9
Anton <anton_feller@mail.ru>
2022-07-06 15:42:16 +0300

    Release all

--- padawans.js
+++ padawans.js
@@ -3,19 +3,43 @@ const fs = require('fs');
 function getPadawanNames() {
   let a = [];
   a = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
-  
+  delete a[4];
   return a;
 }
 function getLightsaberScores() {
-  return fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  let a = [];
+  a = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  for (let index = 0; index < a.length; index++) {
+    a[index] /= 1;
+  }
+  return a;
 }
 
-function getStats(){
-
+function getStats() {
+  let all1 = [];
+  let a = [];
+  a = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+  let b = [];
+  b = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  for (let index = 0; index < 4; index++) {
+    b[index] /= 1;
+    all1.push([a[index], b[index]]);
+  }
+  return all1;
 }
 
-function writeStats(){
 
+function writeStats() {
+  let all1 = [];
+  let a = [];
+  a = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+  let b = [];
+  b = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  for (let index = 0; index < 4; index++) {
+    b[index] /= 1;
+    all1.push([a[index], b[index]]);
+  }
+  return fs.writeFileSync('data/stats.txt', all1.join('\n').split(',').join(' '));
 }
 
 module.exports = {
@@ -24,4 +48,3 @@ module.exports = {
   getStats,
   writeStats,
 };
-
