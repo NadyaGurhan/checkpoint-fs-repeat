ac6175979eba63441d0495b55dd34fbc1bc91d43
Edgar <senior.golosnoj@gmail.com>
2022-07-07 17:59:14 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,24 +1,32 @@
 const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
+// const path = require('path');
 
-fs.readFileSync('data/padawans.txt');
-
+// fs.readFileSync('data/padawans.txt');
+//
 function getPadawanNames() {
-  for (let i = 0; i <= fs.length; i += 1) {
-    return (fs.split(' '));
-  }
+  const str = fs.readFileSync('./data/padawans.txt', 'utf8');
+  return str.split('\n').filter((el) => el !== '');
 }
 // console.log(getPadawanNames());
 
 function getLightsaberScores() {
-  return fs.readFileSync('data/scores.txt');
+  const scores = fs.readFileSync('data/scores.txt', 'utf8');
+  return scores.split('\n').map((el) => (+el));
 }
 
 function getStats() {
+  const arr = [];
+  for (let i = 0; i < getPadawanNames().length; i += 1) {
+    arr.push([getPadawanNames()[i], getLightsaberScores()[i]]);
+  }
+  return arr;
 }
 
-function writeStats() {
-
-}
+const writeStats = (personArray) => {
+  const result = getStats().join('\n').replace(/,/gmi, ' ');
+  fs.writeFileSync('./data/stats.txt', result);
+};
 
 module.exports = {
   getPadawanNames,
