f5f1b9e97b1dac0addd8e4b426013162ef94b9b0
Павел Глухарев <pavelgluharev@MacBook-Air-Pavel.local>
2022-07-19 10:09:09 +0300

    fs

--- padawans.js
+++ padawans.js
@@ -1,12 +1,39 @@
-function getPadawanNames(names) {
-const namesArr = [];
-const param = require('./data/padawans.txt')
-for (let i = 0; i < names.length; i += 1){
-namesArr.push(param())
-})
+const padawan = require('./data/padawans.txt');
+const score = require('./data/score.txt');
+
+function getPadawanNames(name) {
+  let namesArr = [];
+
+  for (let i = 0; i < name.length; i += 1) {
+    namesArr.push(padawan.names());
+  }
+  return namesArr;
+
 }
-return namesArr;
+
+getPadawanNames();
+
+function getLightsaberScores(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    result.push(score());
+  }
+  return result;
 }
+getLightsaberScores();
+
+function getStats() {
+  return
+}
+getStats();
+
+function writeStats() {
+  return fs.readFileSync('data/stats.txt', 'utf8');
+}
+writeStats()
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
