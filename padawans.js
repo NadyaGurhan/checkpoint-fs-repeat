9c52ad145ac18f2d21018ffe197a7ee15a114ef3
Michae1-Jackson <desotovs@gmail.com>
2022-07-19 09:48:10 +0300

    All done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,37 @@
-const fs = require('fs')
+/* eslint-disable no-plusplus */
+const fs = require('fs');
+const path = require('path');
+
+function getPadawanNames() {
+  const padawansDataPath = path.resolve(__dirname, 'data', 'padawans.txt');
+  const padawansData = fs.readFileSync(padawansDataPath, 'utf-8');
+  const padawans = padawansData.split('\n').filter((el) => el !== '');
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scoresDataPath = path.resolve(__dirname, 'data', 'scores.txt');
+  const scoresData = fs.readFileSync(scoresDataPath, 'utf-8');
+  const scores = scoresData.split('\n').filter((el) => el !== '').map((el) => Number(el));
+  return scores;
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const stats = [];
+  for (let i = 0; i < Math.min(padawans.length, scores.length); i++) {
+    stats.push([padawans[i], scores[i]]);
+  }
+  return stats;
+}
+
+function writeStats() {
+  const stats = getStats();
+  const statsData = stats.map((stat) => stat.join(' ')).join('\n');
+  fs.writeFileSync(`${__dirname}/data/stats.txt`, statsData);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
