0538b3238b6288b134ab86a58ed54a7eb5b91573
Yurytop <yakshin.mo@yandex.ru>
2022-07-19 10:11:17 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padavanTxt = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const arr = padavanTxt.split('\n').slice(0, -1);
+  return arr;
+}
+
+function getLightsaberScores() {
+  const txt = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const arr = txt.split('\n');
+  for (let i= 0; i < arr.length; i++) {
+    arr[i] = Number(arr[i]);
+  }
+  return arr;
+  console.log(arr);
+}
+console.log(getLightsaberScores());
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  /* getStats,
+  writeStats, */
 };
