a286d1e9925253ed0381f667f82cf6ea8b7ab43a
ArtBar9981 <scorp9981@gmail.com>
2022-07-19 10:08:58 +0300

    checkpoint is over

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
 const fs = require('fs')
+const padawansText = fs.readFileSync('./data/padawans.txt', 'utf8');
+const padawans = padawansText.split('\n');
+
+const scoresText = fs.readFileSync('./data/scores.txt', 'utf8');
+const scores = scoresText.split('\n');
+
+const getPadawanNames = () => {
+  return padawans.slice(0,4);
+}
+
+const getLightsaberScores = () => {
+return scores.map((a) => Number(a));
+}
+
+const getStats = () => {
+  let scores1 = scores.map((a) => ' ' + a);
+  return padawans[0].concat(scores1[0]);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
