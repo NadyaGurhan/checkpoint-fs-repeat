a22aa69028a8c957f9b2fb2026b2759b63bb20dc
Kirill <school13810@mail.ru>
2022-07-19 10:09:35 +0300

    release 1 and 2

--- padawans.js
+++ padawans.js
@@ -2,6 +2,20 @@ const fs = require('fs')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+
+function getPadawanNames() {
+  const padavanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').filter(word => word.length > 0)
+  return padavanNames
+};
+
+console.log(getPadawanNames())
+
+function getLightsaberScores() {
+  const sabersScore = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n')
+  return sabersScore
+};
+
