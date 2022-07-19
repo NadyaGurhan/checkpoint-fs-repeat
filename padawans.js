135867fe023ae9f71c80b5d3108622433e6da126
Alex_G <grebennikovaa75@gmail.com>
2022-07-19 10:08:57 +0300

    не понял как загружать содержимое файла, нужно пересмотреть лекции

--- padawans.js
+++ padawans.js
@@ -1,7 +1,19 @@
-const fs = require('fs')
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+const fs = require('fs');
+
+function getPadawanNames() {
+  const arr = [];
+  const filePath = './data/padawans.txt';
+  // const padawanNames = fs.readFile('data/padawans.txt');
+  console.log(file);
+  // for (let i = 0; i < 4; i++) {
+  // }
+  // return arr.push(padawanNames);
+}
+console.log(getPadawanNames());
+
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
