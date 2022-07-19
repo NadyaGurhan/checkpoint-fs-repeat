60fe9ca1cf5b7c33cd7820db52f8dab10d53253b
TolyashG <geppeneraa@yandex.ru>
2022-07-19 10:11:28 +0300

    try

--- padawans.js
+++ padawans.js
@@ -1,7 +1,39 @@
 const fs = require('fs')
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+let filePathPad = ".data/padawans.txt"
+let dataPad = fs.readFileSync(filePathPad, "utf8");
+let filePath = ".data/scores.txt"
+let dataSc = fs.readFileSync(filePathSc, "utf8");
+
+const getPadawanNames = () => {
+  let resultPad 
+  result = dataPad.split("\n")
+  return resultPad
+}
+
+const getLightsaberScores = () => {
+  let resultSc 
+  result = dataSc.split("\n")
+  return resultSc
+}
+
+const getStats = () => {
+  let stats 
+  for (let index = 0; index < resultPad.length; index++) {
+    stats[index].push(resultPad[index]);
+    stats[index].push(resultSc[index]);
+  }
+  return stats
+}
+
+const writeStats = () => {
+  for (i = 0; i < stats.length; i++) {
+    fs.appendFileSync(`./data/stats.txt`,`${stats[i]}\n`) 
+  }
+
+}
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
