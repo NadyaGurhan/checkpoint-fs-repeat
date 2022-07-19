597bf4f302a68a703661a04a1ea2300bdb854e34
megomax88 <huku.ukuk@yandex.ru>
2022-07-19 10:14:13 +0300

    ...

--- padawans.js
+++ padawans.js
@@ -1,7 +1,35 @@
 const fs = require('fs')
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+// const padNames = fs.readFileSync('checkpoint-fs/data/padawans.txt', 'utf-8')
+// const scores = fs.readFileSync('/checkpoint-fs/data/scores.txt', 'utf-8')
+
+function getPadawanNames () {
+  const padNames = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  let res = []
+  let resx = []
+  res.push(padNames.split('\n'))
+  for(let i = 0; i < res.length; i++){
+    let resOne = res[i].join()
+    resx.push(resOne)
+    console.log(resx)
+  }
+  
+  
+  return resX
+}
+console.log(getPadawanNames())
+
+// function getLightsaberScores () {
+//   const scores = fs.readFileSync('./data/scores.txt', 'utf-8')
+//   console.log(scores)
+//   let res = []
+//   res.push(scores)
+//   return res
+// }
+// console.log(getLightsaberScores)
+
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
