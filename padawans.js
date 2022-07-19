bdff517064c36739b0a0421bd624484033637aef
Alex_G <grebennikovaa75@gmail.com>
2022-07-19 23:13:15 +0300

    release 1, внес правки, но тест не проходит

--- padawans.js
+++ padawans.js
@@ -1,19 +1,32 @@
 const fs = require('fs');
 
 function getPadawanNames() {
-  const arr = [];
-  const filePath = './data/padawans.txt';
-  // const padawanNames = fs.readFile('data/padawans.txt');
-  console.log(file);
-  // for (let i = 0; i < 4; i++) {
-  // }
-  // return arr.push(padawanNames);
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n');
+  names.pop();
+  return `${names}`;
 }
-console.log(getPadawanNames());
 
-// module.exports = {
-//   getPadawanNames,
-//   getLightsaberScores,
-//   getStats,
-//   writeStats,
-// };
+function getLightsaberScores() {
+  const res = fs.readFileSync('./data/scores.txt', 'utf8').trim().split('\n');
+  return `${res}`;
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const stats = padawans.map((el, i) => [el, scores[i]]);
+  return stats;
+}
+
+function writeStats() {
+  const stats = getStats();
+  for (let i = 0; i < stats.length; i++) {
+    fs.writeFileSync('./data/stats.txt', `${stats[i]}\n`);
+  }
+}
+module.exports = {
+  getPadawanNames,
+  getLightsaberScores,
+  getStats,
+  writeStats,
+};
