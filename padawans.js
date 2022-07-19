f655fdcf490c7d6e03e0bde7e82398ab107ea13c
Filipp <philipp3gs@gmail.com>
2022-07-19 10:08:44 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+const fs = require('fs') 
+
+function getPadawanNames () {
+  let padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  padawans = padawans.split('\n');
+  padawans.pop();
+  return padawans;
+}
+
+function getLightsaberScores () {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  scores = scores.split('\n').map(Number);
+  return scores;
+}
+console.log(getLightsaberScores())
+
+function getStats () {
+  let stats = [].concat(padawans, scores);
+  
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
