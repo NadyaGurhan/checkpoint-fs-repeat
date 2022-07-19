3ec0b1bc242e283069b6e48d7c864199c35a2b94
Alex <a.omlchnko@gmail.com>
2022-07-19 10:08:47 +0300

    checkpoint1day

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    fs.appendFileSync('../data/padawans.txt');
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+
+// function padawanToArr() {
+//   const newArr = [];
+//   let data = fs.readFileSync('./data/padawans.txt')
+//   for (let i = 0; i < arr.length; i += 1) {
+//     newArr.push(arr[i]);
+//   }
+//   return newArr;
+// }
