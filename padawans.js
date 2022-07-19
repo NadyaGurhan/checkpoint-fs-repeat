7c0c6e0cf513c7c38555e0707fdbe4baa89b4860
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-07-19 10:12:02 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const data = fs.readFileSync('/Users/makedonskyy/Desktop/phase-1/checkpoint-fs/data/padawans.txt', 'utf-8');
+const scale = fs.readFileSync('/Users/makedonskyy/Desktop/phase-1/checkpoint-fs/data/scores.txt', 'utf-8');
+function getPadawanNames() {
+  const names = data.split('\n');
+  const allNames = names.slice(0, names.length - 1);
+  return allNames;
+}
+
+function getLightsaberScores() {
+  const score = scale.split('\n');
+  const allScore = scoreArr.map(Number);
+  return allScore;
+}
+console.log(getLightsaberScores());
+
+function getStats() {}
+function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
