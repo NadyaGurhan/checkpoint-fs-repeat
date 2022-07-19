4a0a3fdc1703bda94d672aeb7f4aba555128d92c
KrAnatoly <kranatoly@gmail.com>
2022-07-19 10:12:50 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+const getPN = padawans.split('\n').slice(0,4);
+const getLS = scores.split('\n').map((sc)=>+sc);
+
+function getPadawanNames() {
+return getPN
+}
+
+function getLightsaberScores() {
+  return getLS
+}
+
+function getStats() {
+  const arr=[]
+  for (let i=0; i<getPN.length; i++){
+   arr.push([getPN[i], getLS[i]])
+  }
+  return arr;
+}
+
+function writeStats() {
+  const wSt = './data/stats.txt'
+  return fs.writeFileSync(wSt, `${getStats().join(' \n')}`)
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
