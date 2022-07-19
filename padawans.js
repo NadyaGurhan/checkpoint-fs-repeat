a8fc034097f5697263b20452085620a5991e2bec
AynurMv <aynurmotigulov@gmail.com>
2022-07-19 10:09:27 +0300

    Ne poluchilos sdelat 3 zadanie

--- padawans.js
+++ padawans.js
@@ -1,7 +1,39 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const padawansArr = padawans.split('\n');
+padawansArr.pop();
+console.log(padawansArr);
+
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+const scoresArr = scores.split('\n');
+console.log(scoresArr);
+const scoresArrInt = [];
+for (let i = 0; i < scoresArr.length; i += 1) {
+  scoresArrInt.push(+scoresArr[i]);
+}
+
+function getPadawanNames() {
+  return padawansArr;
+}
+
+function getLightsaberScores() {
+  return scoresArrInt;
+}
+
+const statsArr = [];
+for (let i = 0; i < padawansArr.length; i += 1) {
+  const stats = [];
+  for (let j = 0; j < 2; j += 1) {
+    stats.push(padawansArr[i]).push(scoresArrInt[i]);
+  }
+  statsArr.push(stats);
+}
+console.log(statsArr);
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
