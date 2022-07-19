2b19b67336774319d8e141e0373f6cf009813ab4
Nona <rkholmatova@gmail.com>
2022-07-19 10:08:57 +0300

    not a final work

--- padawans.js
+++ padawans.js
@@ -1,7 +1,19 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  let str = '/.data/padawans.txt'
+  return str.split('\n');
+}
+
+getPadawanNames();
+
+// function getLightsaberScores ('/data/scores.txt') {
+//   return
+// }
