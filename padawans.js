a01cfc39b85329e0e7ab00657554ec9d2f1c62fe
KatePann <beljakova-kat@yandex.ru>
2022-07-26 10:09:52 +0300

    release0

--- padawans.js
+++ padawans.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs')
 module.exports = {
   getPadawanNames,
@@ -5,3 +6,17 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+const padawans = require('./data/padawans');
+const padawansFile = fs.readdirSync(padawans);
+
+for (let i = 0; i < padawansFile.length; i++);
+console.log(fs.statSync(`${padawansFile[i]}`))
+
+// describe('Статистика о падаванах', () => {
+//   it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
+//     const names = getPadawanNames();
+//     expect(names).toEqual(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']);
+//   });
+ 
+  
