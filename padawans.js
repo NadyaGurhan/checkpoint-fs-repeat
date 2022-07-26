207b9e6d0e3d302e2c6b1ba8ac73868037b23186
BlackAbyssFox <90621864+BlackAbyssFox@users.noreply.github.com>
2022-07-26 10:08:47 +0300

    Done

--- padawans.js
+++ padawans.js
@@ -5,3 +5,34 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+  let padawan;
+  let score;
+  let stats;
+  let scoreTable;
+
+  function getPadawanNames () {
+    padawan = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').filter(names => names.length > 0);
+    return padawan
+  }
+
+  function getLightsaberScores () {
+    score = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').filter(names => names.length > 0)
+    for (i = 0; i < score.length; i++) {
+      score[i] = score[i] * 1
+    }
+    return score
+  }
+
+  function getStats () {
+    for (i = 0; i < padawan.length; i++) {
+      stats[i] = padawan[i]
+      stats[i] = score[i]
+    }
+    return stats
+  }
+
+  function writeStats () {
+    scoreTable = fs.writeFileSync('./data/stats.txt', 'utf-8', stats)
+    return scoreTable
+  }
