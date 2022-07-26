2fc5d5f9d7bf1112138492b47ed73b27323da575
Alexandr <kalombyslolprox@gmail.com>
2022-07-26 10:09:03 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,15 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const padawan = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  let arr = []
+  let padawans = padawan.split('\n')
+  for (let i = 0; i < padawans.length - 1; i++){
+    arr.push(padawans[i])
+  } 
+  return arr
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
