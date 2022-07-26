b6367ed34c028011ef28b4546904389fb04814cf
Andrew <yau7237@gmail.com>
2022-07-26 11:58:50 +0500

    Done 3 passed

--- padawans.js
+++ padawans.js
@@ -1,4 +1,43 @@
-const fs = require('fs')
+const fs = require("fs");
+const padawans = fs.readFileSync("./data/padawans.txt", "utf-8");
+const scores = fs.readFileSync("./data/scores.txt", "utf-8");
+
+function getPadawanNames() {
+  return padawans.trim().split("\n");
+}
+
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  let scoresSplit = scores.split("\n");
+  let array = [];
+  for (let i = 0; i < scoresSplit.length; i++) {
+    array.push(+scoresSplit[i]);
+  }
+  return array;
+}
+
+console.log(getLightsaberScores());
+
+function getStats() {
+  let result = [];
+  let name = getPadawanNames();
+  let scores = getLightsaberScores();
+  for (let i = 0; i < name.length; i++) {
+    result.push([name[i], scores[i]]);
+  }
+  return result;
+}
+console.log(getStats());
+
+function writeStats() {
+  const stats = getStats();
+  for (let i = 0; i < stats.length; i++) {
+    fs.appendFileSync("./data/stats.txt", `${stats[i]}\n`);
+  }
+}
+writeStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
