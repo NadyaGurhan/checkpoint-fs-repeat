afacf53b9c711b2219cc36bdcdd7f5f331d8c28c
Darya <dariaavs@gmail.com>
2022-08-09 10:06:54 +0300

    nonono

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt');
+}
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt');
+}
+function getStats() {
+  const res = [];
+  for (let i = 0; i < 4; i++) {
+    res.push(fs.readFileSync('./data/padawans.txt'));
+  }
+}
+function writeStats() {
+  return fs.writeFileSync('./data/stats.txt', )
+}
+
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  getPadawanNames: getPadawanNames(),
+  getLightsaberScores: getLightsaberScores(),
+  getStats: getStats(),
+  writeStats: writeStats(),
 };
