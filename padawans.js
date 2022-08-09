393c3059fd0c0d5358453e7669e6c00f150b22b4
aav2581 <aav2581@gmail.com>
2022-08-09 10:05:55 +0300

    all done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padavans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').filter(item => item !== '');
+const score = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').filter(item => item !== '');
+
+function getPadawanNames() {
+	return padavans;
+}
+
+function getLightsaberScores() {
+	return score.map(el => +el);
+}
+
+function getStats() {
+	const results = padavans.map((item, index) => [item, +score[index]]); 
+	return results;
+}
+
+function writeStats() {
+	const resultsForTxt = getStats().map(el => el.join(' ')).join('\n');
+	fs.writeFileSync('./data/stats.txt', `${resultsForTxt}`);
+}
+
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+	getPadawanNames,
+	getLightsaberScores,
+	getStats,
+	writeStats,
 };
