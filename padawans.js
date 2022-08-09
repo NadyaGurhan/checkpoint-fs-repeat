3daf7782244e5c4b19f035a850477289b21de843
VSaveLoad <vs.pavlenko@mail.ru>
2022-08-09 10:11:45 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  padawanNames.pop();
+  return padawanNames;
+}
+
+function getLightsaberScores() {
+  let lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return lightsaberScores.map((el) => +(el));
+}
+
+function getStats() {
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  padawanNames.pop();
+  let lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  lightsaberScores = lightsaberScores.map((el) => +(el));
+  const stats = [];
+  for (let i = 0; i < padawanNames.length; i++){
+    let arr = [padawanNames[i], lightsaberScores[i]];
+    stats.push(arr);
+   }
+   return stats;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
