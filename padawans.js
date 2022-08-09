5fd5dc176fc79a4060454426f2142bc9d05c44af
Dmitri D <dmitridiakov@gmail.com>
2022-08-09 10:04:23 +0300

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -5,3 +5,9 @@ module.exports = {
   getStats,
   writeStats,
 };
+const padawanNames = require('./data')
+
+const getPadawanNames = function getPadawanNames() { return fs.readFileSync('./data/padawans.txt') };
+
+const getLightsaberScores = function getLightsaberScores() { return fs.readFileSync('./data/scores.txt');
+  }
