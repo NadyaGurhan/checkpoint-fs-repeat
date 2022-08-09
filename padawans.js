bc0c378d8b051f4b62f78fce3099e6e10869dd5f
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-08-09 13:27:20 +0300

    fixed

--- padawans.js
+++ padawans.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const padawans = fs.readFileSync('/Users/makedonskyy/Desktop/phase-1-repeate/w1d2/checkpoint-fs/data/padawans.txt', 'utf-8');
-const scores = fs.readFileSync('/Users/makedonskyy/Desktop/phase-1-repeate/w1d2/checkpoint-fs/data/scores.txt', 'utf-8');
+const padawans = fs.readFileSync('data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('data/scores.txt', 'utf-8');
 
 function getPadawanNames() {
   const res = padawans.split('\n');
@@ -25,10 +25,10 @@ getStats();
 
 
 function writeStats() {
-  // const stat = getStats();
-  // // let res = stat.flat().slice(0, 1).join('')
-  // const res = stat.join('\n').split(',').join(' ');
-  // fs.appendFileSync('./data/stats.txt', res, 'utf-8');
+  const stat = getStats();
+  // let res = stat.flat().slice(0, 1).join('')
+  const res = stat.join('\n').split(',').join(' ');
+  fs.writeFileSync('./data/stats.txt', res, 'utf-8');
 }
 
 module.exports = {
