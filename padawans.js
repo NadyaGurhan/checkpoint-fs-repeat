3a33c009610f6838dd5371de05c685064a9804b4
7iliama1 <7iliama1@gmail.com>
2022-08-09 10:11:35 +0300

    release 1 and 2 only

--- padawans.js
+++ padawans.js
@@ -1,7 +1,18 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return names.trim().split('\n');
+}
+
+function getLightsaberScores() {
+  const score = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return score.trim().split('\n').map((el) => +el);
+};
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,getLightsaberScore
 };
