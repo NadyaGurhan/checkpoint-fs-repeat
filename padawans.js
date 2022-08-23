e3443ce589633d7f1a9183ebdd35d5369a0d7834
Lyudmila <zoruk.ll@gmail.com>
2022-08-23 10:00:45 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padavan = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+
+function getPadawanNames() {
+  const padavanName = padavan.pop();
+  return padavanName;
+}
+
+const scores = fs.readFileSync('./data/scores.txt').split('\n');
+
+function getLightsaberScores() {
+  return scores.map((value) => +value);
+}
+
+function getStats() {
+  cconst arr = [];
+  for (let i=0; i<padavan.length; i++); {
+  let arr1 = [padavan[i], scores[i]];
+  arr1.push(arr)
+  }
+return arr1
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
