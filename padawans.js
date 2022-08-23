acf40bb094a69fc1526b67407f1952f188381f42
vVolodya <skinner.vova@gmail.com>
2022-08-23 16:41:37 +1000

    Tests completed

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
 const fs = require('fs')
+
+const getPadawanNames = () => fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+
+const getLightsaberScores = () => {
+  return fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+}
+
+function getStats() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+  return names.map((el, i) => [el, scores[i]]);
+}
+
+function writeStats() {
+  const stats = getStats();
+  const noComma = stats.map((el) => el.join(' '));
+  return fs.writeFileSync('./data/stats.txt', noComma.join('\n'));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
