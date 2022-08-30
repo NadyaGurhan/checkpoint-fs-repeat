317f9a9d45013729bd3220af0d6e6206dfa452a4
AndreyKonkin <parenekrus34@mail.ru>
2022-08-30 10:05:27 +0300

    release0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+}
+
+function getLightsaberScores() {
+  const read = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  const res = read.map((el) => Number(el));
+  console.log(res);
+  return res;
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const score = getLightsaberScores();
+  const res = padawans.map((el, index) => `${el}``+${score[index]}`);
+  console.log(res);
+  return res;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
