95a40a4384934bba587d7300a33954341bf9aae3
Mishanya <mikhail.olianenko@mail.ru>
2022-08-30 10:15:27 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
+
+function getPadawanNames() {
+  const padavans = fs.readFileSync(path.join(__dirname, 'data/padawans.txt'), 'utf-8').split('\n');
+  padavans.pop();
+  return padavans;
+}
+
+function getLightsaberScores() {
+  const light = fs.readFileSync(path.join(__dirname, 'data/scores.txt'), 'utf-8').split('\n');
+  const saber = light.map((el) => parseInt(el, 10));
+  return saber;
+}
+
+function getStats() {
+  return 1;
+}
+
+function writeStats() {
+  return 1;
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
