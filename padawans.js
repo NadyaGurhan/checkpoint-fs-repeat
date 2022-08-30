1691975588e8d6ef255187dacde32b85c132607c
Artem Polkovnikov <artem.polkovnikov93@yandex.ru>
2022-08-30 09:46:28 +0300

    tests done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,33 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawansNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  padawansNames.pop();
+  return padawansNames;
+}
+
+function getLightsaberScores() {
+  const lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return lightsaberScores.map((el) => Number(el));
+}
+
+function getStats() {
+  const a = getPadawanNames();
+  const b = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < a.length; i++) {
+    const final = [a[i], b[i]];
+    result.push(final);
+  }
+  return result;
+}
+
+function writeStats() {
+  const c = getStats().map((el) => el.join(' '));
+  //console.log(c);
+  fs.writeFileSync('data/stats.txt', c.join('\n'));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
