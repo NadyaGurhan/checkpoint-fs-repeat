d047afc09485db73cf7d7ddf32d62dae7d79c247
Petr <petr-akatnov@yandex.ru>
2022-08-30 09:43:43 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return names.filter((el) => el !== '');
+}
+
+function getLightsaberScores() {
+  const score = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return score.map((el) => +el);
+}
+
+function getStats() {
+  const res = [];
+  getPadawanNames().forEach((el, i) => {
+    res.push([el, getLightsaberScores()[i]]);
+  });
+  return res;
+}
+
+function writeStats() {
+  const stats = getStats().map((el) => el.join(' ')).join('\n');
+  fs.writeFileSync('./data/stats.txt', `${stats}`);
+  // console.log(stats);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
