185f77ba90cb5351ebe4262eac429976e9d096d2
Gashmar <semenov.georgy.v@yandex.ru>
2022-08-30 10:17:49 +0300

    -

--- padawans.js
+++ padawans.js
@@ -1,7 +1,15 @@
-const fs = require('fs')
+const fs = require('fs');
+// fs не подключился
+const filePath = './data/padawans.txt';
+const data = fs.readFileSync(filePath, 'utf8');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  return data.split('\n');
+}
