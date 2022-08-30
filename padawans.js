027babe94af7f1354605f22f7306e1cc8a9704eb
Anton Belozerov <a.a.belozerov4545@gmail.com>
2022-08-30 10:10:10 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,26 @@
-const fs = require('fs')
+const fs = require("fs");
+const names = fs.readFileSync("./data/padawans.txt").toString("utf-8");
+const scores = fs.readFileSync("./data/scores.txt").toString("utf-8");
+
+function getPadawanNames() {
+  const arr = [];
+  for (let i = 0; i < names.length; i++) {
+    arr.push(names[i]);
+  }
+  return arr.join("\n");
+}
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  const arr = [];
+  for (let i = 0; i < scores.length; i++) {
+    arr.push(scores[i]);
+  }
+  return arr.join("\n");
+}
+
+console.log(getLightsaberScores());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
