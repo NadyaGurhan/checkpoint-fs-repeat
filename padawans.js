c21d8d96f4c939b829ca414b98e73bfc75d6bad9
Xesha <privet.ksusha@list.ru>
2022-08-30 10:11:57 +0300

    3 из 4 тестов

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+const pada = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+const score = fs.readFileSync('data/scores.txt', 'utf-8').split('\n').map(el=>Number(el));
+
+function getPadawanNames() {
+return pada;
+}
+
+
+
+function getLightsaberScores(){
+return score
+}
+ function getStats(){
+let arr=[]
+for (i=0;i<pada.length;i++){
+  arr.push([pada[i], score[i]])
+}
+return arr
+ }
+
+
+ function writeStats(){
+ }
+ 
+ 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
