384a84bd005f74af9f348ed45db45c760d96e4b5
Darya <dariaavs@gmail.com>
2022-08-30 09:48:56 +0300

    all done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,33 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const names = fs.readFileSync( ('./data/padawans.txt'), 'utf-8').split('\n')
+  names.pop()
+  return names
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return scores.map((el) => Number(el));
+}
+
+function getStats () {
+  const stats = [];
+  for (let i = 0; i < getPadawanNames().length; i++) {
+    let padawanScore = []
+    padawanScore.push(getPadawanNames()[i], getLightsaberScores()[i])
+    stats.push(padawanScore)
+  }
+  return stats
+}
+
+function writeStats() {
+  const res = getStats().map((el) => el.join(' ')).join('\n');
+  // console.log(res);
+  fs.writeFileSync('./data/stats.txt', `${res}`);
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
