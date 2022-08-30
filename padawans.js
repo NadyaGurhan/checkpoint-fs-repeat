b1575398a7c06aa4a57f778c023e3e459356e6db
AnnieLoo <anna.s.lubenko@gmail.com>
2022-08-30 10:14:57 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const padawan = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+  padawan.pop();
+  return padawan;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  return scores;
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
