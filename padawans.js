3bd1152ea80f31eac015e9510fa1342dd1befee1
Maksim <kavmak2009@mail.ru>
2022-08-30 10:16:54 +0300

    Все плохо,нужно разобраться нормально

--- padawans.js
+++ padawans.js
@@ -1,4 +1,11 @@
 const fs = require('fs')
+
+fs.readFileSync(path, options?): string
+const result = fs.readFileSync("/etc/passwd", { encoding: "utf-8" })
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
