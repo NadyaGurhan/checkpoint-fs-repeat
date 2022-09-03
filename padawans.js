5c4b60c1f0561b07544e340d61641dc0c8fa80f9
Mishanya <mikhail.olianenko@mail.ru>
2022-09-03 12:02:56 +0300

    done

--- padawans.js
+++ padawans.js
@@ -9,17 +9,32 @@ function getPadawanNames() {
 
 function getLightsaberScores() {
   const light = fs.readFileSync(path.join(__dirname, 'data/scores.txt'), 'utf-8').split('\n');
-  const saber = light.map((el) => parseInt(el, 10));
+  const saber = light.map((el) => Number(el));
   return saber;
 }
 
 function getStats() {
-  return 1;
+  const padavans = fs.readFileSync(path.join(__dirname, 'data/padawans.txt'), 'utf-8').split('\n');
+  const light = fs.readFileSync(path.join(__dirname, 'data/scores.txt'), 'utf-8').split('\n');
+  const result = padavans.slice(0, padavans.length - 1);
+  const arr = [];
+  for (let i = 0; i < result.length; i += 1) {
+    arr.push([result[i], Number(light[i])]);
+  }
+  return (arr);
 }
 
 function writeStats() {
-  return 1;
+  const padavans = fs.readFileSync(path.join(__dirname, 'data/padawans.txt'), 'utf-8').split('\n');
+  const light = fs.readFileSync(path.join(__dirname, 'data/scores.txt'), 'utf-8').split('\n');
+  const result = padavans.slice(0, padavans.length - 1);
+  let str = '';
+  for (let i = 0; i < result.length; i += 1) {
+    str += `${result[i]} ${Number(light[i])}\n`;
+  }
+  fs.appendFileSync('data/stats.txt', `${str.slice(0, str.length - 1)}`);
 }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
