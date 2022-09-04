903e1a4d718714f5abc6914cf61a57be7cd891de
Pavel <KozhevnikovPA295@gmail.com>
2022-09-04 13:49:19 +0300

    Release Fixes: Test done on 100% from 2d day try! I grow stronger!)

--- padawans.js
+++ padawans.js
@@ -1,44 +1,67 @@
 const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
-
-function getPadawanNames(names) {
-  const nameList = fs.readFileSync('data/padawans.txt');
-  const result = [];
-  for (let i = 0; i < nameList.length; i++) {
-    result.push(nameList[i]);
+// console.log(nameList);
+function getPadawanNames() {
+  newArr = [];
+  const nameList = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+  for (let i = 0; i < nameList.length; i += 1) {
+    if (nameList[i].length > 0) {
+      newArr.push(nameList[i]);
+    }
   }
-  return result;
-}
-
-const scores = fs.readFileSync('data/scores.txt');
-function getLightsaberScores(scores) {
-  // const scores = fs.readFileSync('data/scores.txt');
-  const newArr = [];
-  for (let i = 0; i < scores.length; i++) {
-    newArr.push(scores[i]);
-  }
+  // console.log(newArr);
   return newArr;
 }
+getPadawanNames();
 
-const nameTiers = fs.readFileSync('data/padawans.txt');
-const scoreTiers = fs.readFileSync('data/padawans.txt');
-function getStats(nameTiers, scoreTiers) {
-  // const nameTiers = fs.readFileSync('data/padawans.txt');
-  // const scoreTiers = fs.readFileSync('data/padawans.txt');
-  const padawanTiers = [];
-  for (let i = 0; i < nameTiers; i++) {
-    padawanTiers[i] = `${nameTiers[i]}, ${scoreTiers[i]}`;
+function getLightsaberScores(scores) {
+  const scoreList = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  for (let i = 0; i < scoreList.length; i += 1) {
+    scoreList[i] = Number(scoreList[i]);
   }
-  return padawanTiers;
+  // console.log(scoreList);
+  return scoreList;
+}
+getLightsaberScores();
+
+function getStats() {
+  const newArr = getPadawanNames();
+  const scoreList = getLightsaberScores();
+  // console.log(scoreList);
+  // console.log(newArr);
+  const padawanList = [];
+  for (let i = 0; i < newArr.length; i += 1) {
+    padawanList.push([newArr[i], scoreList[i]]);
+  }
+  // console.log(padawanList);
+  return padawanList;
+}
+getStats();
+
+function writeStats() {
+  let resultArr = [];
+  const result = getStats();
+  //console.table(result);
+  // result.join('');
+  //console.log(result[1].join(' '));
+  for (let i = 0; i < result.length; i += 1) {
+    resultArr.push(result[i].join(' '));
+   // console.log(resultArr);
+  }
+  console.table(resultArr);
+  for (let i = 0; i < resultArr.length; i += 1) { 
+    if (i !== resultArr.length - 1){
+    fs.appendFileSync('data/stats.txt', `${resultArr[i]}\n`); 
+    } else {
+      fs.appendFileSync('data/stats.txt', `${resultArr[i]}`); 
+    }
+  }
 }
 
-
-function writeStats (string) {
-
-  // fs.appendFileSync('data/stats.txt', string);
-}
+//writeStats();
