6c8905df637b74791dbcf32b99cc1b562b8b6753
Ko.Kko <ko.kko@mrm.ru>
2022-09-20 10:01:39 +0300

    .

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const read = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const padawanNames = read.split('\n').pop();
+  return padawanNames;
+}
+
+function getLightsaberScores() {
+  const read = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const score = read.split('\n');
+  return score;
+}
+function getStats() {
+  let names = getPadawanNames();
+  let score = getLightsaberScores();
+  let stats = [];
+ 
+
+}
+function writeStats() {}
+function getStats() {}
+
