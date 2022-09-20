e4744beef3923234b2c374b811caac4e564d82cb
Daniil Balashov <danil.balashik.98@gmail.com>
2022-09-20 10:04:59 +0300

    chek

--- padawans.js
+++ padawans.js
@@ -5,3 +5,23 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames(){
+  const name = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const pName = name.split('\n');
+  pName.join();
+return pName;
+}
+
+function getLightsaberScores(){
+  const scoresT = fs.readFileSync('./data/scores.txt', 'utf8');
+  const scores = scoresT.split('\n');
+  scores.join();
+  return scores;
+}
+function getStats(){
+
+}
+function writeStats(){
+
+}
