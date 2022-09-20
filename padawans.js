30b0b8bad3ea5a8033cbbd798ccd6221c3c522e5
Kirill Prikhozhij <mentblead@gmail.com>
2022-09-20 10:01:39 +0300

    2 tests

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+return padawans;
+}
+function getLightsaberScores() {
+  let LightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n');
+  return LightsaberScores.map(el=> +el)
+}
+function getStats() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  let LightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n');
+  LightsaberScores = LightsaberScores.map(el => +el);
+  
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+//   getStats,
+//   writeStats,
 };
