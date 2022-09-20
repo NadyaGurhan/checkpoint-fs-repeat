0aab31c2addf66cc90d540eea4a543816a5a1930
Yaroslav <yaroslav.danilenko.0996@gmail.com>
2022-09-20 10:04:25 +0300

    1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+function getPadawanNames() {
+  const x = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const y = x.trim().split('\n');
+  return (y);
+}
+function getLightsaberScores() {
+  const numb = fs.readFileSync('./data/scores.txt', 'utf8');
+  const res = numb.trim().split('\n');
+  }
+  return res;
+}
+console.log (getLightsaberScores())
+function getStats() {
+
+}
+function writeStats() {
+
+}
