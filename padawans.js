9c508b434d8829e60a138e5f4e5037bb0fa5b8bd
Sarkis <sarkis_strelok@mail.ru>
2022-09-20 10:04:37 +0300

    sdf

--- padawans.js
+++ padawans.js
@@ -1,4 +1,13 @@
 const fs = require('fs')
+
+const getPadawanNames = fs.readFileSync('./data/padawans.txt', 'utf8');
+console.log([getPadawanNames()]);
+
+const getLightsaberScores = fs.writeFileSync('checkpoint-fs/data/scores.txt', 'utf8');
+console.log([getLightsaberScore()]);
+// const getStats,
+// const writeStats,
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
