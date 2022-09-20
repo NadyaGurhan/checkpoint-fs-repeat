6b4fc24205d754490fc472fd10388fa12576ae45
Ko.Kko <ko.kko@mrm.ru>
2022-09-20 14:22:34 +0300

    DONE :)

--- padawans.js
+++ padawans.js
@@ -9,22 +9,26 @@ module.exports = {
 
 function getPadawanNames() {
   const read = fs.readFileSync('./data/padawans.txt', 'utf-8');
-  const padawanNames = read.split('\n').pop();
+  const padawanNames = read.split('\n');
   return padawanNames;
 }
 
 function getLightsaberScores() {
   const read = fs.readFileSync('./data/scores.txt', 'utf-8');
   const score = read.split('\n');
-  return score;
+  return score.map((i) => Number(i));
 }
 function getStats() {
-  let names = getPadawanNames();
-  let score = getLightsaberScores();
-  let stats = [];
- 
-
+  const stats = [];
+  for (let i = 0; i < getPadawanNames().length; i++) {
+    stats.push([getPadawanNames()[i], getLightsaberScores()[i]]);
+  }
+  return stats;
 }
-function writeStats() {}
-function getStats() {}
+// console.log(getStats());
+function writeStats() {
+  const data = getStats().join('\n');
+  fs.writeFileSync('data/stats.txt', data.replace(/,/g, ' '));
+}
 
+// writeStats();
