b48b5afda42f940394ef999e56834eabc81b9709
Daniil <panchito97@bk.ru>
2022-09-20 10:04:18 +0300

    a

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const arr = [];
+  arr.push('Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani');
+  return arr;
+}
+
+
+function getLightsaberScores() {
+  const arr = [];
+  arr.push(99.9, 92 , 87 ,82);
+  return arr;
+}
+function getStats(){
+const names = ['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']
+const score =[99.9, 92 , 87 ,82]
+const newArr= []
+
+
+};
+
+// function writeStats(){
+
+// }:
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
