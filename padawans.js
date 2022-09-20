b6aaed656d5c5caa5e74341610ab10b983aa9c4c
LizaSP <elizavetasolodovnik12@mail.ru>
2022-09-20 10:02:00 +0300

    all tests: done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,35 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('data/padawans.txt', 'utf8').split('\n').filter((el) => el);
+  return names;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('data/scores.txt', 'utf8').split('\n').filter((el) => el).map((el) => +el);
+  return scores;
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const result = names.map((el, i) => [el, scores[i]]);
+  return result;
+}
+
+function writeStats(dataArray) {
+  let res = '';
+  dataArray.map((el, i, array) => {
+    if (i !== array.length - 1) {
+      res += `${el[0]} ${el[1]}\n`;
+    } else {
+      res += `${el[0]} ${el[1]}`;
+    }
+  });
+  fs.writeFileSync('data/stats.txt', res);
+  return res;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
