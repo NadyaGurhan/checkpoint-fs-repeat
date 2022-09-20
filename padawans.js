49736286b9b9573a46b76e1c5e64396bb97ec4b5
Valery <bovaeff@mail.ru>
2022-09-20 10:03:59 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,32 @@
 const fs = require('fs')
+function getPadawanNames() {
+const padawan = fs.readFileSync(`data/padawans.txt`,'utf8').split('\n')
+const pad = padawan.pop()
+return padawan
+}
+
+function getLightsaberScores() {
+const light = fs.readFileSync(`data/scores.txt`,'utf8').split('\n')
+//const li = light.pop()
+  return light.map((el)=>Number(el))
+  }
+
+  function getStats() {
+
+    for (let i=0;i<4;i++) {
+      let arr = []
+      arr.push([padawan[i],light[i]])
+    }
+    return arr
+}
+const write = fs.writeFileSync('data/stats.txt','New text')
+
+function writeStats() {
+return write
+}
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
