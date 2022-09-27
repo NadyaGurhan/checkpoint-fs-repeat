f493e1b20d77802be328102fe9ffa9e340bbfeb3
HdDanya <daniil24kiselev@gmail.com>
2022-09-27 10:03:35 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,19 @@
 const fs = require('fs')
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+ // getStats,
+  //writeStats,
 };
+
+function getPadawanNames(){
+  
+  const arr = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return arr.filter((str) => str !== '');
+};
+function getLightsaberScores(){
+  const arr = fs.readFileSync('./data/scores.txt', 'utf-8');
+  
+}
