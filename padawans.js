3cb40a0d24d62beed5a3238a4621948ce6a2782c
Пользователь <maria.trksh@gmail.com>
2022-09-27 10:00:08 +0300

    beda

--- padawans.js
+++ padawans.js
@@ -1,7 +1,18 @@
 const fs = require('fs')
+
+
+
+function getPadawanNames() {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n')
+  return names
+}
+
+getPadawanNames()
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
+
