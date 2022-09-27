b04a080e5096826b45f097ca40a1290824e5e219
Maria <17mashmallow@gmail.com>
2022-09-27 10:01:40 +0300

    release

--- padawans.js
+++ padawans.js
@@ -2,6 +2,20 @@ const fs = require('fs')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+// У меня получаются массивы, разделенные по пробелу,  понимаю, что это не то что требуется, но мне не хватает времени разобраться :(
+
+function getPadawanNames() {
+  let dir = 'data/padawans.txt';
+  const names = fs.readFileSync(`${dir}`, 'utf-8').split(' ');
+  return names;
+}
+
+function getLightsaberScores() {
+  let folder = 'data/scores.txt';
+  const scores = fs.readFileSync(`${folder}`, 'utf-8').split(' ');
+  return scores;
+}
