7e9f93bd4c063d074981e71e5835cb4c4cdeb1df
Pavel Urseev <urseev.pavel@gmail.com>
2022-09-27 09:47:44 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,30 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return padawanNames.filter((str) => str !== '');
+}
+
+function getLightsaberScores() {
+  const lightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return lightsaberScores.map((str) => +str);
+}
+
+function getStats() {
+  const padawanNames = getPadawanNames();
+  const lightsaberScores = getLightsaberScores();
+  return padawanNames.map((x, i) => [x, lightsaberScores[i]]);
+}
+
+function writeStats() {
+  const padawanNames = getPadawanNames();
+  const lightsaberScores = getLightsaberScores();
+  fs.writeFileSync('./data/stats.txt', `${padawanNames[0]} ${lightsaberScores[0]}`);
+  for (let i = 1; i < padawanNames.length; i++) {
+    fs.appendFileSync('./data/stats.txt', `\n${padawanNames[i]} ${lightsaberScores[i]}`);
+  }
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
