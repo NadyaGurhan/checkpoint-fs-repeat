0c7ab6b2616d90c08a17207271e0a7acaa656dd2
Vladislav <vlad.devidson.harle@gmail.com>
2022-09-27 10:00:20 +0300

    1test

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
 const fs = require('fs')
+function getPadawanNames(){
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return padawanNames.filter((str) => str !== '');
+}
+function getLightsaberScores(){
+  const luch = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return luch.filter((str) => +str)
+}
+function writeStats(){
+
+}
+function getStats(){
+
+}
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
