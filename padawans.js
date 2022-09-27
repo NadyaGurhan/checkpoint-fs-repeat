1ff93ffae13ab7755ec9eaa524836e80118cbc8d
ZHtmn <a_zhelobtsov@bk.ru>
2022-09-27 12:02:09 +0500

    (((

--- padawans.js
+++ padawans.js
@@ -1,7 +1,13 @@
 const fs = require('fs')
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
+
+function getPadawanNames() {
+  let arr = [];
+  arr.push(fs.readFileSync('./data/padawans.txt', 'utf-8'))
+  return arr
+}
