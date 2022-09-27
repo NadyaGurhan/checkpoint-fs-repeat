9df066ebe320f48e55647f7ab5fb88b9be2f4149
Andrew <andrewkotlov48@gmail.com>
2022-09-27 10:06:50 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,15 @@
-const fs = require('fs')
+/* eslint-disable no-use-before-define */
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawanNames = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+  padawanNames.pop();
+  return padawanNames;
+}
+function getLightsaberScores() {}
+function getStats() {}
+function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
