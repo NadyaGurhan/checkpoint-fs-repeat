3094cb56c9c19e1c74455c3a213526d741af877e
MariiaSV <sviridova.mahaon@gmail.com>
2022-10-11 09:55:40 +0300

    W1D2

--- padawans.js
+++ padawans.js
@@ -5,3 +5,41 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+
+const readNames = fs.readFileSync('./data/padawans.txt', 'utf-8')
+function getPadawanNames() {
+ let result = readNames.split('\n')
+ let fin = result.slice(0, result.length - 1)
+ return fin
+}
+
+// console.log(getPadawanNames())
+
+const readScores = fs.readFileSync('./data/scores.txt', 'utf-8')
+function getLightsaberScores() {
+  let result = readScores.split('\n').map((el) => Number(el))
+  return result
+}
+
+// console.log(getLightsaberScores());
+
+const names = getPadawanNames()
+const scores = getLightsaberScores()
+function getStats () {
+  let result = []
+  for(let i = 0; i < names.length; i++) {
+    result.push([names[i], scores[i]])
+  }
+  return result
+}
+
+// console.log(getStats());
+
+const stat = getStats()
+function writeStats() {
+  const finArr = stat.map((el) => el.join(' '))
+  return fs.writeFileSync('data/stats.txt', finArr.join('\n'))
+}
+
+// console.log(writeStats());
