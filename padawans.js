eaafe3157cd57903cf792b1aef12306f9cc0131d
Olga <olga67802@gmail.com>
2022-10-11 10:19:25 +0300

    Padawans

--- padawans.js
+++ padawans.js
@@ -1,4 +1,20 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const PadawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8');
+
+const getPadawanNames = () => {
+  let arr = [];
+  for (let i = 0; i <= PadawanNames.length; i += 1) {
+    arr.push(PadawanNames);
+  }
+  return arr;
+};
+
+const getLightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+console.log(getLightsaberScores);
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
