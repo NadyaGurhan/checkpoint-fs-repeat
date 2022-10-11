579d8b1ddc83f36a1e501aafbde4da12804774c0
Stepan Kirakosian <mailbox_35@mail.ru>
2022-10-11 10:22:49 +0300

    first test is passed

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
+
+// возвращает список падаванов из файла `data/padawans.txt`
+const getPadawanNames = () => {
+  const names = fs.readFileSync('data/padawans.txt', 'utf8');
+  return names.split('\n');
+};
+
+const getPadawanScores = () => {
+  const scores = fs.readFileSync('data/scores.txt', 'utf8');
+  return scores.split('\n'); // выводит массив из строк, а нужно преобразовать в массив из чисел
+};
+console.log(getPadawanScores())
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  getPadawanScores,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
