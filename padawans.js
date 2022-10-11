3e83fadd6e806bdab149a41fa3bd5fe4220f560d
AndreyKhmara <kelevra101smu@gmail.com>
2022-10-11 12:35:44 +0300

    relise 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
 const fs = require('fs')
+
+let padawans = fs.readFileSync("./data/padawans.txt", "utf8");
+
+
+function getPadawanNames(padawans){
+  
+  let list = padawans.split('\n')
+  
+  return list.splice(0,4)
+  
+}
+console.log(getPadawanNames(padawans));
+
+
+
+function getLightsaberScores(){
+
+}
+
+function getStats(){
+
+}
+
+function writeStats(){
+  
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
