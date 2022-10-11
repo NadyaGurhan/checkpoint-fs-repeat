bcec18d7e62c4f35a3d3ee02ac9c8fa1076c4214
Aleksandr Proshkov <all9100@mail.ru>
2022-10-11 10:18:58 +0300

    release ?

--- padawans.js
+++ padawans.js
@@ -1,4 +1,24 @@
 const fs = require('fs')
+
+const getPadawanNames = () => {
+  let res = fs.readFileSync('./data/padawans.txt','utf-8')
+ return res.split('\n')
+
+}
+const getLightsaberScores = () => fs.readFileSync('./data/scores.txt','utf-8')
+const getStats = (getPadawanNames,getLightsaberScores) => {
+  const newNames = getPadawanNames.split('\n')
+  const newScore = getLightsaberScores.split('\n')
+  res = []
+  for (let i = 0 ; i < newNames ; i++ ) {
+    res.push(newNames[i],newScore[i])
+  }
+}
+
+const writeStats = (res) => {
+  fs.writeFileSync('./data/stats.txt',res)
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
