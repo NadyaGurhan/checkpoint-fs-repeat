fe8cd8684166f4b4f42f590d8bbe71f17908ec66
Sesegma <anveegres@icloud.com>
2022-10-11 10:19:43 +0300

    :(

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+// let fileNames = "./data/padawans.txt";
+// let data = fs.readFileSync(fileNames, "utf8");
+// console.log(data);
+// data = data + "my text";
+// fs.writeFileSync(fileNames, data);
+
+function getPadawanNames () {
+  let arr = [];
+  let fileNames = ("./data/padawans.txt");
+  let data = fs.readFileSync(fileNames, "utf8");
+  console.log(data);
+  for(let i =0; i < data.length; i++){
+    arr.push(data[i])
+  }
+ return arr;
+}
+function getLightsaberScores () {
+
+}
+function getStats (){
+
+}
+function writeStats (){
+
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
