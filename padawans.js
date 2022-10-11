05608e00e02cd427787684bca252e80b2c00c493
alena <safronovaa1807@gmail.com>
2022-10-11 10:24:07 +0300

    chek-point

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
 const fs = require('fs')
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+   getLightsaberScores,
+   getStats,
+   writeStats,
 };
+const txt = fs.readFileSync ('./data/padawans.txt', 'utf-8')
+console.log(txt)
+function getPadawanNames (txt) {
+  let arr = []
+  for(let i =0; i < txt; i++){
+    arr.push(txt[i])
+  }
+  return arr
+  }
+ 
+
+
+const num = fs.readFileSync ('./data/scores.txt')
+function getLightsaberScores (num) {
+
+}
