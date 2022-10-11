ef320028d4d5426f972a9806e2dc27a8d934b23e
Nikita Kardopolov <reverdj@yandex.ru>
2022-10-11 10:22:24 +0700

    Nothing is done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = 'Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani';
+  const getNames = fs.writeFileSync('./data/padawans.txt', names.toString());
+  const name = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return [name];
+}
+// const names = getPadawanNames(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']);
+
+// function getLightsaberScores() {
+//   const getNames = fs.writeFileSync('/data/scores.txt');
+//   return fs.readFileSync('/data/scores.txt', 'utf-8');
+// }
+
+// function getStats() {
+
+// }
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
 };
