bb2cfa3bddc5e00b5b3f722efd8ee3d47787f0e3
Kirill <kirill@192.168.0.7>
2022-10-18 10:08:16 +0300

    point

--- padawans.js
+++ padawans.js
@@ -1,7 +1,22 @@
-const fs = require('fs')
+const fs = require("fs");
+
+const readNames = fs.readFileSync("./data/padawans.txt", "utf-8");
+function getPadawanNames() {
+  const result = readNames.split("\n");
+  const fin = result.filter((el) => el.length !== 0);
+  return fin;
+}
+
+const readScores = fs.readFileSync("./data/scores.txt", "utf-8");
+function getLightsaberScores() {
+  const result = readScores.split("\n").map((el) => Number(el));
+  return result;
+}
+// console.log(getLightsaberScores());
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
+  getLightsaberScores
   getStats,
   writeStats,
 };
