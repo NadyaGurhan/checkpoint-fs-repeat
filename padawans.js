40f141403cf5f2cde2a23f1530bd67bb5a6f0467
Dima <dima@dimas-mbp-2.home>
2022-10-18 08:03:27 +0100

    checkpoint

--- padawans.js
+++ padawans.js
@@ -2,6 +2,39 @@ const fs = require('fs')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  getStats ,
+  writeStats, 
 };
+
+ function getPadawanNames(){ 
+  let names =fs.readFileSync('./data/padawans.txt', 'utf8').split('\n')
+  const arr = names.filter((el) =>el.length !==0 )
+  return arr;
+
+  
+}   
+
+
+function getLightsaberScores(){
+  let names = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  console.log(names);
+}
+
+
+function getStats() {
+ stats = [];
+ let names =  getPadawanNames()
+ let scores = getLightsaberScores()
+ names.forEach(n =>{
+  n
+ })
+
+}
+
+function writeStats() {
+
+}
+
+
+
+getPadawanNames()
