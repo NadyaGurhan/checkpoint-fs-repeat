fc7b84af5f3b24c899935febcc384280d2d84cc5
Alex Doved <adoved.dev@gmail.com>
2022-11-01 10:34:54 +0300

    task: done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,44 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => {
+  const content = fs.readFileSync('data/padawans.txt', 'utf-8')
+    .split(/\r?\n/);
+
+  return content;
+};
+
+const getLightsaberScores = () => {
+  const content = fs.readFileSync('data/scores.txt', 'utf-8').split(/\r?\n/);
+  const result = content.map(Number);
+
+  return result;
+};
+
+const getStats = () => {
+  const resultArr = [];
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  for (let i = 0; i < padawans.length; i++) {
+    resultArr.push([padawans[i], scores[i]]);
+  }
+
+  return resultArr;
+};
+
+const writeStats = () => {
+  const filePath = './data/stats.txt';
+  let data = '';
+  const stats = getStats();
+  stats.forEach((element) => {
+    const tempValue = element.join(' ');
+    data += `${tempValue}\n`;
+  });
+
+  fs.writeFileSync(filePath, data.slice(0, -1));
+};
+
+writeStats();
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
