a9c5178dd69ad3abfef29fa75ad980bf91df7194
renyarenat <renyamazunin@gmail.com>
2022-11-01 10:26:29 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,38 @@
-const fs = require('fs')
+const fs = require('fs');
+const { get } = require('http');
+
+const padawans = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+const scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8');
+
+function getPadawanNames() {
+  return padawans.split('\n').splice(0, padawans.split('\n').length - 1);
+}
+
+function getLightsaberScores() {
+  return scores.split('\n').map((el) => +el);
+}
+
+console.log(getLightsaberScores())
+
+function getStats() {
+  const getStatsArr = [];
+  let newArr = [];
+  const a = getPadawanNames();
+  const b = getLightsaberScores();
+  for (let i = 0; i < a.length; i++) {
+    getStatsArr.push([a[i]]);
+    getStatsArr[i].push(b[i])
+  }
+  return getStatsArr;
+}
+
+function writeStats() {
+  
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
