68553ed48be7c8f0d8775cb391ee5f23ea7eac78
Алиса <tsorionovaalisa@mail.ru>
2022-11-01 10:30:21 +0300

    3 tests done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,40 @@
 const fs = require('fs')
+
+
+function getPadawanNames(){
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+  return padawans.trim().split('\n')
+}
+
+
+function getLightsaberScores(){
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8');
+  return scores.trim().split('\n').map(x=>Number(x));
+}
+
+function getStats(){
+  let names = getPadawanNames();
+  let scores = getLightsaberScores();
+  let stats = [];
+  let i = 0;
+  while (i<names.length){
+    let arr = [];
+    arr.push(names[i]);
+    arr.push(scores[i]);
+    if(arr.length){
+      stats.push(arr)
+    }
+    i++;
+  };
+  return stats
+}
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
