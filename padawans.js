d3e7b6e32eb452636bde28bc1169eae8e969421f
Daniela <dk96.dk96@gmail.com>
2022-11-01 10:16:30 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8').trim().split('\n');
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8').split('\n').map(el => +el);
+}
+
+function getStats() {
+  const names = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8').trim().split('\n');
+  const scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8').split('\n').map(el => +el);
+
+  return names.map((name, i) => [name, scores[i]]);
+}
+
+function writeStats() {
+  const names = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8').trim().split('\n');
+  const scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8').split('\n').map(el => +el);
+  fs.writeFileSync(`${__dirname}/data/stats.txt`, names.map((name, i) => [name, scores[i]]).map(el => el.join(' ')).join('\n'));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
