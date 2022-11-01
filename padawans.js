1397be8a8c18005691ce77d7870fdcaf0cde742b
Olga <olga67802@gmail.com>
2022-11-01 10:25:42 +0300

    Check1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => {
+  const read = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return read.trim().split('\n');
+};
+
+const getLightsaberScores = () => {
+  const read = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const read2 = read.trim().split('\n');
+  return read2.map((elem) => Number(elem));
+};
+
+const getStats = () => {
+
+};
+
+const writeStats = () => {
+
+};
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
