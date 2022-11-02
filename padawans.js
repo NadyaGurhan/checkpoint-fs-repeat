33da2dbc59c05c115ce6cfad1012b198cb8c5381
thorenDK <alexelkin17@yandex.ru>
2022-11-02 14:54:37 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawan = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const getLightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+function getPadawanNames() {
+  const arr = padawan.split('\n');
+  arr.pop();
+  return arr;
+}
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
