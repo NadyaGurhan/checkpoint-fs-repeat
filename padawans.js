ffba99d4de23d55797570ea5f8b38e87bfd2c6b2
Olga-Alieva <olga.a.alieva@gmail.com>
2022-11-14 17:28:37 -0500

    3/4 tests done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const file = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const score = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+function getPadawanNames() {
+  const arr = file.split('\n');
+  arr.pop();
+  return arr;
+}
+function getLightsaberScores() {
+  const arr = score.split('\n');
+  const newArr = arr.map((str) => Number(str));
+  return newArr;
+}
+function getStats() {
+  const padNames = getPadawanNames();
+  const scores = getLightsaberScores();
+  const newArr = [];
+  for (let i = 0; i < padNames.length; i += 1) {
+    newArr.push([padNames[i], scores[i]]);
+  }
+  return newArr;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
