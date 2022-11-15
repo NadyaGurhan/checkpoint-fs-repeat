b332e2189b015ea3f03bad3eb987f483a030241f
AgathaCott <agatha.cott@gmail.com>
2022-11-15 10:14:55 +0300

    Try

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n').slice(0, 4);
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+
+function getPadawanNames() {
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scoresNum = scores.map((elem) => Number(elem));
+  return scoresNum;
+}
+
+function getStats() {
+}
+
+function writeStats() {
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
