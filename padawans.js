ea5cb487d0767cbd0cdbdf78c45433b2dbc530bb
MaxM55 <mikovmi55@gmail.com>
2022-11-15 12:14:51 +0500

    no comment

--- padawans.js
+++ padawans.js
@@ -5,3 +5,23 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const names = [];
+  names.push(fs.readFileSync('data/padawans.txt', 'utf8'));
+  return names
+}
+
+function getLightsaberScores() {
+  const names = [];
+  names.push(fs.readFileSync('data/scores.txt', 'utf8'));
+  return names
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
