af537d6a663c4e84c1ffdc090214b9a9a89d8af2
MarinaShustrova <averc.marina@yandex.ru>
2022-11-15 10:55:33 +0400

    checkpoint done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,33 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const readNames = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const result = readNames.split('\n');
+  const fin = result.slice(0, result.length - 1);
+  return fin;
+}
+
+function getLightsaberScores() {
+  const readScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const scores = readScores.split('\n').map((el) => Number(el));
+  return scores;
+}
+
+const names = getPadawanNames();
+const scores = getLightsaberScores();
+function getStats() {
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push([names[i], scores[i]]);
+  }
+  return result;
+}
+const stat = getStats();
+function writeStats() {
+  const finArray = stat.map((el) => el.join(' '));
+  return fs.writeFileSync('data/stats.txt', finArray.join('\n'));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
