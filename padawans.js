876170624e4a1e79f417d822fae1cb4cbe4859c6
Aleksandr_Iarmak <iarmak01.71@yandex.ru>
2022-11-15 10:06:24 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,27 @@
 const fs = require('fs')
+
+function getPadawanNames(){
+   const fileContent = fs.readFileSync(`data/padawans.txt`, "utf-8");
+   console.log(fileContent);
+   return fileContent;
+};
+function getPadawanScores(){
+   const fileContentScores = fs.readFileSync(`data/scores.txt`, "utf-8");
+   return fileContentScores;
+}
+
+const padavanNames = getPadawanNames();
+const padavanScores = getPadawanScores();
+
+function getStats(){
+   let arr[];
+   for ( let i = 0; i < padavanNames.length; i++){
+      arr.push(padavanNames[i] + padavanScores[i])
+    
+   };
+   console.log(arr);
+   return arr;
+};
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
