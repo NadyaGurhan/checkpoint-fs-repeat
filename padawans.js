15d07ed6610559967ba46c8ee89fad5f661f075f
Ivan Kolpakov <vanjaklp95@gmail.com>
2022-11-15 10:07:59 +0300

    two tests done

--- padawans.js
+++ padawans.js
@@ -5,3 +5,30 @@ module.exports = {
   getStats,
   writeStats,
 };
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+const jedi = fs.readFileSync('./data/padawans.txt', 'utf-8');
+
+function getPadawanNames() {
+  const jediArr = jedi.split('\n');
+  const jedies = jediArr.slice(0, -1);
+  return jedies;
+}
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  const scoresArr = scores.split('\n');
+  const res = [];
+  for (let i = 0; i < scoresArr.length; i++) {
+    res.push(Number(scoresArr[i]));
+  }
+  return res;
+}
+console.log(getLightsaberScores());
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
