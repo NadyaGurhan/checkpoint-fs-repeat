08c95a724cf2f683a15dde80822b7844a162ee30
Hronius <xpohuyc@gmail.com>
2022-11-15 10:21:20 +0300

    release0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+
+// const myFile = fs.readFileSync('data/padawans.txt', 'utf-8');
+// console.log(myFile.split('\n'));
+function getPadawanNames() {
+  const myFile = fs.readFileSync('data/padawans.txt', 'utf-8');
+  return myFile.trim().split('\n');
+}
+function getLightsaberScores() {
+  const myFile = fs.readFileSync('data/scores.txt', 'utf-8');
+  return myFile.trim().split('\n').map(Number);
+}
+console.log(getLightsaberScores());
+function getStats() {
+  const myFile = fs.readFileSync('data/padawans.txt', 'utf-8');
+  const myFile1 = fs.readFileSync('data/scores.txt', 'utf-8');
+  return myFile.trim().split('\n');
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
