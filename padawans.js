a03f0ab6da12aada88ee2c9cae41a7a029a886f4
viz-A-viz <64863859+viz-A-viz@users.noreply.github.com>
2022-11-15 11:52:38 +0500

    Release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,56 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  names.pop();
+  return names;
+}
+
+function getLightsaberScores() {
+  const arr = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  const scores = arr.map((el) => +el);
+  return scores;
+}
+
+function getStats() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  names.pop();
+
+  const arr = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  const scores = arr.map((el) => +el);
+
+  const stats = Array.from({ length: names.length }, () => []);
+
+  for (let i = 0; i < names.length; i += 1) {
+    stats[i].push(names[i]);
+    stats[i].push(scores[i]);
+  }
+
+  return stats;
+}
+
+function writeStats() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  names.pop();
+
+  const arr = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  const scores = arr.map((el) => +el);
+
+  const stats = Array.from({ length: names.length }, () => []);
+
+  for (let i = 0; i < names.length; i += 1) {
+    stats[i].push(names[i]);
+    stats[i].push(scores[i]);
+  }
+
+  let data = stats.map((el) => el.join(' '));
+  data = data.join('\n');
+  fs.writeFileSync('./data/stats.txt', data);
+}
