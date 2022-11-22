48328aa5658d2b81e7222d6c856f1d7be887a9a1
Зори Захарян <Zori33@mail.ru>
2022-11-22 10:17:37 +0300

    first

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
 const fs = require('fs')
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
+
+
+function getPadawanNames(){
+  let arr = []
+    let names = `"Revan",
+    "Bastila Shan",
+    "Jolee Bindo",
+    "Juhani"`
+    
+  arr.push(names) 
+     return arr
+  }
+  
+  console.log(getPadawanNames())
--- spec/padawans.spec.js
+++ spec/padawans.spec.js
@@ -10,6 +10,7 @@ describe('Статистика о падаванах', () => {
   it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
     const names = getPadawanNames();
     expect(names).toEqual(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']);
+    
   });
   it('getPadawanScores возвращает оценки владения световым мечом из файла `data/scores.txt`', () => {
     const names = getLightsaberScores();
