5c2a1fbcc8cba7a6c232bb55694429c63f862d00
Aartem <7933813@gmail.com>
2022-11-22 10:08:44 +0300

    1st sprint

--- padawans.js
+++ padawans.js
@@ -5,3 +5,24 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+
+const getPadawanNames() {
+let arr = []
+let padawansNames = fs.readFileSync('data/padawans.txt','utf-8')
+arr.push[padawansNames]
+}
+
+const getLightsaberScores() {
+let arr = []
+let saberScore = fs.readFileSync('data/scores.txt','utf-8')
+arr.push[saberScore]
+}
+
+
+const getStats() {}
+
+
+const writeStats() {
+fs.appendFileSync('data/stats.txt',`${getStats().join('\n')}`,'utf8')
+}
