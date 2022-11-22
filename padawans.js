9325a4c8071a2c875c735a14c04145fbaea3cc62
Said <Saidqwerty1221@gmail.com>
2022-11-22 10:17:31 +0300

    pochti all

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
-const fs = require('fs')
+const { Certificate } = require('crypto');
+const fs = require('fs');
+const { join } = require('path');
+
+function getPadawanNames(){
+const getName = fs.readFileSync('./data/padawans.txt', 'utf-8')
+return getName.trim().split('\n')
+
+}
+
+function getLightsaberScores(){
+ return fs.readFileSync('./data/scores.txt', 'utf-8')
+ .split('\n')
+ .map((el)=> Number(el))
+}
+function getStats(){
+  const name = getPadawanNames()
+  const score = getLightsaberScores()
+  return name.map((el, index)=> [el, score[index]])
+}
+function writeStats(){
+  let creatFile = fs.writeFileSync('./data/stats.txt', getStats().join('\n').replace(/,/g, ' '))
+ return creatFile
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
