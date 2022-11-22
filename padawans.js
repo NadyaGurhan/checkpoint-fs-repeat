ce2ad4134ef0532e570e874769be73c342c3aa41
JmprXDrk <piskar2008@yandex.ru>
2022-11-22 10:20:22 +0300

    done checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,45 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs
+    .readFileSync('./data/padawans.txt', 'utf-8')
+    .split('\n')
+    .slice(0, -1);
+  return padawans;
+}
+
+getPadawanNames();
+
+function getLightsaberScores() {
+  const saberScores = fs
+    .readFileSync('./data/scores.txt', 'utf-8')
+    .split('\n')
+    .map((string) => Number(string));
+  return saberScores;
+}
+
+getLightsaberScores();
+
+function getStats() {
+  let padawans = getPadawanNames();
+  let saberScores = getLightsaberScores();
+  let result = [];
+  for (let i = 0; i < padawans.length; i += 1) {
+    result.push(padawans[i].split(',').concat(saberScores[i]));
+  }
+  return result;
+}
+
+getStats();
+
+function writeStats() {
+  let result = getStats();
+  const data = fs.writeFileSync('./data/stats.txt', `${result.join(' ')}\n`);
+  return data;
+}
+
+writeStats();
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
