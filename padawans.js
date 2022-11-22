e4606f707a0e82162b19c87c94f0c0c7c99d0cc5
Anastasiya <iinokayo@mail.ru>
2022-11-22 09:33:27 +0300

    Function getPadawanNames passed

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const padawansList = padawans.split('\n');
+  padawansList.splice(padawansList.length - 1, 1);
+
+  return padawansList;
+};
+
+const getLightsaberScores = () => {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const scoresList = scores.split('\n');
+  // scoresList = scoresList.
+};
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+console.log(getLightsaberScores());
