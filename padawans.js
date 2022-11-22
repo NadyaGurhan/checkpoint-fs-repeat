e86570d371a57773c6597640ddbd89401ce4035a
thorenDK <alexelkin17@yandex.ru>
2022-11-22 10:17:44 +0300

    release ZERO

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawan = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+function getPadawanNames() {
+  const arr = padawan.split('\n');
+  arr.pop();
+  return arr;
+}
+
+function getLightsaberScores() {
+  const arr = scores.split('\n');
+  return arr.map((el) => (Number(el)));
+}
+function getStats() {
+return padawan.map((name, i)=> )
+}
+function writeStats() {
+  fs.writeFileSync('./data/stats.txt', getStats().join('\n'));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
