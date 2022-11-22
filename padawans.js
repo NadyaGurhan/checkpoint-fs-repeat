359433a0126b67f11e5985a68199e5a75de7a0ec
AndreyNguen <goonikjke@gmail.com>
2022-11-22 10:19:00 +0300

    ---

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
+const padawans = require('./data/padawans.txt');
+const scores = require('./data/scores.txt');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const list = fs.readFileSync('./data/padawans.txt');
+  return list;
+  console.log(list);
+  // const list = [];
+  // list.push(padawans);
+  // list.forEach((el) => el);
+  // return el;
+}
+
+function getLightsaberScores() {
+
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
