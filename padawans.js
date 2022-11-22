15895f040d3cd8e86b3fd41f42665b08077bb7ec
Dovran <dseminov@gmail.com>
2022-11-22 10:17:32 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,22 @@
-const fs = require('fs')
+const fs = require('fs');
+
+console.log(fs.readFileSync('./data/padawans.txt', 'utf8'));
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf8');
+}
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf8');
+}
+console.log(getLightsaberScores());
+
+function getStats() {
+  const arr = [];
+  for(let i)
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
