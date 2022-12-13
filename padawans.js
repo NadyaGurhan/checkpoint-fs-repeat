61964244f77c6a9896448de02a73a8de9215585e
pavel <pasha_nikolaev@bk.ru>
2022-12-13 10:15:38 +0300

    none

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadavanNames(count) {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    result.push(i);
+  }
+  return result;
+}
