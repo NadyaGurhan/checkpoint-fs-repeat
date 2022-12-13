69a90f11f51927796cdaaad3858a3527b3519cbb
Vasily <ololol2113@mail.ru>
2022-12-13 10:09:12 +0300

    ph1/day2/checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const readPad = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return readPad.split('\n');
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+  const readScore = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  const arr = [];
+  for (let i = 0; i < readScore.length; i += 1) {
+    arr.push(Number(readScore[i]));
+  }
+  return arr;
+}
+
+getLightsaberScores();
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
