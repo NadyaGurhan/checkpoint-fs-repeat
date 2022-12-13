85fb779033a2b463d255d3eef7b9428878b28d24
Leonid660 <leonid_zolotovky@bk.ru>
2022-12-13 10:24:10 +0300

    release debug

--- padawans.js
+++ padawans.js
@@ -1,36 +1,36 @@
-const fs = require('fs')
+const fs = require('fs');
 
-function getPadawanNames(){
-  let names = fs.readFileSync('./data/padawans.txt', "utf8")
-  return names.split("\n")
-              .map((i) => i.slice(0,-1))
-              .slice(0,-1)
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8');
+  return names.split('\n')
+    .map((i) => i.slice(0, -1))
+    .slice(0, -1);
 }
 
-function getLightsaberScores(){
-  let lightsaberScore = fs.readFileSync('./data/scores.txt', "utf8")
-  return lightsaberScore.split("\n")
-                        .map((i) => Number(i))                                      
+function getLightsaberScores() {
+  const lightsaberScore = fs.readFileSync('./data/scores.txt', 'utf8');
+  return lightsaberScore.split('\n')
+    .map((i) => Number(i));
 }
 
-
-function getStats(){
-  let namesArr = getPadawanNames()
-  let lightsaberScoreArr = getLightsaberScores()
-  let arr =[]
-   for(let i=0; i< namesArr.length; i+=1){
-    arr.push([namesArr[i],lightsaberScoreArr[i]])
-   }
-   return arr
+function getStats() {
+  const namesArr = getPadawanNames();
+  const lightsaberScoreArr = getLightsaberScores();
+  const arr = [];
+  for (let i = 0; i < namesArr.length; i += 1) {
+    arr.push([namesArr[i], lightsaberScoreArr[i]]);
+  }
+  return arr;
 }
 
-function writeStats(){
-  let arrStar = getStats().map((i) => i+"\n")
-  return fs.writeFileSync("./data/stats.txt",`${arrStar}`)
+function writeStats() {
+  const arrStar = getStats();
+  const arrMap = arrStar.map((i) => i.join(' '));
+  return fs.writeFileSync('./data/stats.txt', arrMap.join('\n'));
 }
-console.log(writeStats())
-writeStats
+
 module.exports = {
+  writeStats,
   getPadawanNames,
   getLightsaberScores,
   getStats,
