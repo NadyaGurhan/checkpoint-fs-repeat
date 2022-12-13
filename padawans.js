3f4eaf9bc3d548e099ae5170987d552f39c690c8
Maksim Grushetskiy <mega_ymbetov@mail.ru>
2022-12-13 10:11:33 +0300

    done 50/50

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+
+function getPadawanNames() {
+  const padav = fs.readFileSync('data/padawans.txt', 'utf-8');
+  return padav.split('\n').slice(0, 4);
+}
+
+function getLightsaberScores() {
+  const score = fs.readFileSync('data/scores.txt', 'utf-8');
+  return score.split('\n').map((el) => Number(el));
+}
+
+function getStats() {
+  const pad = getPadawanNames();
+  const scr = getLightsaberScores();
+  const all = [...pad, ...scr];
+  const newArr = [];
+  // res.push([pad[i], scr[i]])
+  console.log(all.map((el) => el));
+  for (let i = 0; i < all.length; i++) {
+    console.log(newArr.push(pad[i], scr[i]));
+  }
+}
