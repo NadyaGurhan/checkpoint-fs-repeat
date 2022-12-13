3bd7ea8beaf2d834d4fc60c212338198f979618f
Andrey Vorozhtsov <pilottu154b2@gmail.com>
2022-12-13 10:15:21 +0300

    chekpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
+
+// console.log(getPadawanNames)
+// let padawansArr = padawans.split('\n')
+// console.log(padawansArr)
+function getPadawanNames() {
+  const padawans = fs.readFileSync("data/padawans.txt", 'utf8');
+  let arrPadawans = padawans.split('\n')
+console.log(arrPadawans)
+}
+getPadawanNames()
+// let getPadawanNames = 'data/padawans.txt';
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
