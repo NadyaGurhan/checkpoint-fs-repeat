372f076bf4be94ada6fe79a5e1c067631b8025fd
hamurapy <mr.skifcha007@mail.ru>
2022-12-13 10:09:49 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const arr = [];
+  const read = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\r\n').slice(0, -1);
+  arr.push(read);
+  return arr.flat();
+}
+
+function getLightsaberScores() {
+  const lvl = fs.readFileSync('./data/scores.txt', 'utf-8').split('\r\n');
+  return lvl.map((el) => Number(el));
+}
+
+// function getStats() {
+
+// }
+
+// function writeStats() {
+
+// }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
