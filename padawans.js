41d6251913ce10e4ebd0e45fdeb2544760fd311f
Pavel <pavel.bezugliy@mail.ru>
2022-12-13 12:07:52 +0500

    Check0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,51 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n')
+  const newScores = []
+  for(let i = 0; i <scores.length; i++){
+    newScores.push(Number(scores[i]))
+  }
+  return newScores
+
+}
+
+function getStats() {
+
+}
+function writeStats() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
