7fe947992ff8aa1225d438b8199f0352da5ec9dc
PenRomain <romandreevich@icloud.com>
2022-12-13 17:38:12 +0300

    release all

--- padawans.js
+++ padawans.js
@@ -7,18 +7,26 @@ function getPadawanNames() {
 }
 
 function getLightsaberScores() {
-  const read = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
-  console.log(read);
+  const read = fs
+    .readFileSync('./data/scores.txt', 'utf-8')
+    .split('\n')
+    .map((el) => Number(el));
   return read;
 }
 function getStats() {
-  const arr = [];
-  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
-  const stats = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
-  arr.push(names, stats);
-  console.log(names);
+  const names = getPadawanNames();
+  const stats = getLightsaberScores();
+  return names.map((el, ind) => [names[ind], stats[ind]]);
 }
-function writeStats() {}
+function writeStats(stats) {
+  const arr = getStats();
+  arr.map((el) => {
+    if (!fs.existsSync('./data/stats.txt')) {
+      return fs.writeFileSync('./data/stats.txt', `${el[0]} ${el[1]}`);
+    }
+    fs.appendFileSync('./data/stats.txt', `\n${el[0]} ${el[1]}`);
+  });
+}
 
 module.exports = {
   getPadawanNames,
