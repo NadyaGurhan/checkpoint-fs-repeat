87f47422e2472b8affd40afb09d02f587130bd9c
EvgenyShv1 <evgeny0xa4a@gmail.com>
2022-12-20 10:05:23 +0300

    relise

--- padawans.js
+++ padawans.js
@@ -1,4 +1,34 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const names = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  .split('\n')
+  .slice(0, -1);
+
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8')
+  .split('\n')
+  .map((el) => Number(el));
+
+// const data = fs.writeFileSync('data/stats.txt');
+// console.log(data)
+
+const getPadawanNames = () => names;
+
+const getLightsaberScores = () => scores;
+
+const getStats = () => {
+  const stats = [];
+  for (let i = 0; i < names.length; i++) {
+    stats.push([names[i], scores[i]]);
+  }
+  return stats;
+};
+
+const writeStats = () => {
+  const res = getStats().join('\n');
+  fs.writeFileSync('data/stats.txt', res, 'utf8');
+  return res;
+};
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
