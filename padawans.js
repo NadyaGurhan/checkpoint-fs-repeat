418448fb23766dda335704ae2c7df9edbb89a0de
isii <irudenkoilya@gmail.com>
2022-12-20 11:06:01 +0400

    first checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+
+function getPadawanNames() {
+  const list = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  return list;
+}
+
+function getLightsaberScores() {
+  const list = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+  return list;
+}
+
+function getStats() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+  return padawans.concat(scores);
+}
