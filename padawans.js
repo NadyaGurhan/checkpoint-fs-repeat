980c9e8ef44357d875bd10edf562059d5ce6fb7b
Ruslan Rybka <ruslanrybka10@gmail.com>
2022-12-20 10:02:22 +0300

    Tests 1-3

--- padawans.js
+++ padawans.js
@@ -1,7 +1,37 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const read = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\r\n').slice(0, -1);
+  const names = [];
+  names.push(read);
+  return names.flat();
+}
+
+getPadawanNames();
+
+function getLightsaberScores() {
+  const score = fs.readFileSync('./data/scores.txt', 'utf-8').split('\r\n').map((el) => Number(el));
+  return score;
+}
+
+getLightsaberScores();
+
+function getStats() {
+const padawans = getPadawanNames();
+const lightsaberScores = getLightsaberScores();
+
+  const stats = [];
+  for (let i = 0; i < padawans.length; i += 1) {
+    stats.push([padawans[i], lightsaberScores[i]]);
+  }
+  return stats;
+}
+
+getStats();
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
