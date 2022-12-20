1c2076f374d1864fc81754b7bf3e0a3cb2322fc5
Timir Ammosov <timiramm@gmail.com>
2022-12-20 13:01:39 +0600

    Done

--- padawans.js
+++ padawans.js
@@ -5,3 +5,30 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  return names;
+}
+
+function getLightsaberScores() {
+  const names = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n');
+  return names.map(index => Number(index));
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  let stats = []
+  let i = 0;
+  while (names[i] && scores[i]) {
+    stats.push([names[i], scores[i]]);
+    i += 1;
+  } 
+  return stats;
+}
+
+function writeStats() {
+  let stats = getStats();
+  const data = fs.writeFileSync('./data/stats.txt', stats.map(index => index.join(' ')).join('\n'));
+}
