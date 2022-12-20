76e3b8a1574603c190fd5cb5a924cdf88d746bdd
AnatolySeleznev <toly1986@mail.ru>
2022-12-20 10:07:56 +0300

    ckeck_fail

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
 const fs = require('fs')
+
+const getPadawanNames = fs.readFileSync('./data/padawans.txt','utf-8')
+ 
+
+
+const getLightsaberScores = fs.readFileSync('./data/scores.txt', 'utf-8')
+
+
+// fs.writeFileSync('./data/stats.txt',getPadawanNames)
+
+// fs.appendFileSync('./data/stats.txt',getLightsaberScores)
+
+
+// const getStats = fs.readdirSync('./data/')
+// console.log(getStats)
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
