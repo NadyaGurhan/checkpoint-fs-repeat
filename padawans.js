9c148b6b586b40f365044e8ae064e791446db3a1
User <user@MacBook-Air-User.local>
2022-12-20 10:05:39 +0300

    no

--- padawans.js
+++ padawans.js
@@ -1,4 +1,10 @@
-const fs = require('fs')
+const fs = require("fs");
+function getPadawanNames() {
+  return fs.readFileSync("data/padawans.txt", "utf-8").split("/n");
+}
+function getLightsaberScores() {}
+function getStats() {}
+function writeStats() {}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
