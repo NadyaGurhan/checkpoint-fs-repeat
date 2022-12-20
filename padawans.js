63366a6b624bd085b9f5d8948025733fc438597b
Dmitriy Vyaznikov <dmitri.vyaznikov@gmail.com>
2022-12-20 11:01:33 +0400

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,43 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const fileSync = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const arr = fileSync.split('\n');
+
+const fileSync2 = fs.readFileSync('./data/scores.txt', 'utf-8');
+const arr2 = fileSync2.split('\n');
+
+function getPadawanNames() {
+  arrRes = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arrRes.push(arr[i]);
+  }
+  return arrRes;
+}
+getPadawanNames(arr);
+
+function getLightsaberScores() {
+  arrRes2 = [];
+  for (let i = 0; i < arr2.length; i++) {
+    arrRes2.push(Number(arr2[i]));
+  }
+  return arrRes2;
+}
+getLightsaberScores(arr2);
+
+function getStats() {
+  // arrRes3 = [];
+  // for (let i = 0; i < arr.length - 1; i++) {
+  //   for (let j = 0; j < arr2.length - 1; j++) {
+  //     arrRes3.push(arr[j]);
+  //   }
+  // }
+  return arrRes3;
+}
+
+console.log(getStats(arr, arr2));
+
+function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
