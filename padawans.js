bc1774a28ee8e7a461180cd78cf5b0bec028d71b
semochkaelse <semochkaelse@gmail.com>
2022-12-20 10:50:37 +0400

    Tests passed

--- padawans.js
+++ padawans.js
@@ -1,7 +1,40 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawan = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const padawanList = padawan.trim().split('\n');
+
+const score = fs.readFileSync('./data/scores.txt', 'utf-8');
+const scoreList = score.trim().split('\n');
+
+function getPadawanNames() {
+  return padawanList;
+}
+
+function getLightsaberScores() {
+  const result = scoreList.map((a) => Number(a));
+  return result;
+}
+
+function getStats() {
+  const result = [];
+  const numbersList = getLightsaberScores();
+  for (let i = 0; i < padawanList.length; i += 1) {
+    result.push([padawanList[i], numbersList[i]]);
+  }
+  return result;
+}
+
+function writeStats() {
+  const stats = getStats();
+  fs.writeFileSync('./data/stats.txt', stats.join('\n').replaceAll(',', ' '));
+}
+
+console.log(getStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
