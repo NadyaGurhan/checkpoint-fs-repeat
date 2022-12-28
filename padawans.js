f7ea6a3ef13b8f7a058a1efe1486dc3c0b9dd00e
pavel <pasha_nikolaev@bk.ru>
2022-12-28 19:28:49 +0300

    first check done

--- padawans.js
+++ padawans.js
@@ -1,16 +1,31 @@
 const fs = require('fs');
 
+function getPadawanNames() {
+  const result = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return result;
+}
+// console.log(getPadawanNames());
+function getLightsaberScores() {
+  const result = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => +el);
+  return result;
+}
+// console.log(getLightsaberScores());
+function getStats() {
+  const stats = [];
+  for (let i = 0; i < getPadawanNames().length || i < getLightsaberScores().length; i++) {
+    stats.push([getPadawanNames()[i], getLightsaberScores()[i]]);
+  }
+  return stats;
+}
+// console.log(getStats());
+function writeStats(stats) {
+  const result = stats.map((el) => el.join(' ')).join('\n');
+  fs.writeFileSync('./data/stats.txt', result, 'utf8');
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
-
-function getPadavanNames(count) {
-  const result = [];
-  for (let i = 0; i < count; i++) {
-    result.push(i);
-  }
-  return result;
-}
