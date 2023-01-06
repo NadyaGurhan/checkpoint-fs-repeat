d2698ea0cffb07f089cea0e1e9865d2e676b9870
Petr <petristomin@gmail.com>
2023-01-06 21:42:55 +0200

    retest

--- padawans.js
+++ padawans.js
@@ -4,44 +4,38 @@ const padawans = fs.readFileSync('./data/padawans.txt', 'UTF-8');
 const scores = fs.readFileSync('./data/scores.txt', 'UTF-8');
 
 function getPadawanNames() {
-  const padawansArr = padawans.split('\n');
-
-  const arr = [];
-
-  for (let index = 0; index < padawansArr.length - 1; index++) {
-    arr.push(padawansArr[index]);
-  }
-  return arr;
+  return padawans.split('\n').slice(0, 4);
 }
 
 function getLightsaberScores() {
-  const padawansArr = padawans.split('\n');
-  const scoresArr = scores.split('\n');
-
-  const arr = [];
-
-  for (let index = 0; index < padawansArr.length - 1; index++) {
-    // arr.push(padawansArr[index],scoresArr[index]);
-    arr.push(+scoresArr[index]);
-  }
-  return arr;
+  return scores.split('\n').map((el) => +el);
 }
 
 function getStats() {
-  const padawansArr = padawans.split('\n');
-  const scoresArr = scores.split('\n');
-
+  const padawansNew = padawans.split('\n').slice(0, 4);
+  const scoresNew = scores.split('\n').map((el) => +el);
   const arr = [];
-
-  for (let index = 0; index < padawansArr.length - 1; index++) {
-    arr.push([padawansArr[index], +scoresArr[index]]);
+  for (let i = 0; i < padawansNew.length; i++) {
+    arr.push([padawansNew[i], scoresNew[i]]);
   }
   return arr;
 }
 
-function writeStats(arr3) {
-  fs.appendFileSync('./data/stats.txt', `${arr3}\n`);
+function writeStats() {
+  const arr = [];
+  const padawansNew = padawans.split('\n').slice(0, 4);
+  const scoresNew = scores.split('\n');
+  for (let i = 0; i < padawansNew.length; i++) {
+    arr.push(`${padawansNew[i]} ${scoresNew[i]}`);
+  }
+  arr.join('\n');
+  fs.appendFileSync('./data/stats.txt', arr, 'utf8');
 }
+console.log(writeStats());
+
+// function writeStats(arr3) {
+//   fs.appendFileSync('./data/stats.txt', `${arr3}\n`);
+// }
 
 module.exports = {
   getPadawanNames,
@@ -49,3 +43,33 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+// const padawansArr = padawans.split('\n');
+
+//   const arr = [];
+
+//   for (let index = 0; index < padawansArr.length - 1; index++) {
+//     arr.push(padawansArr[index]);
+//   }
+//   return arr;
+
+// const padawansArr = padawans.split('\n');
+//   const scoresArr = scores.split('\n');
+
+//   const arr = [];
+
+//   for (let index = 0; index < padawansArr.length - 1; index++) {
+//     // arr.push(padawansArr[index],scoresArr[index]);
+//     arr.push(+scoresArr[index]);
+//   }
+//   return arr;
+
+// const padawansArr = padawans.split('\n');
+//   const scoresArr = scores.split('\n');
+
+//   const arr = [];
+
+//   for (let index = 0; index < padawansArr.length - 1; index++) {
+//     arr.push([padawansArr[index], +scoresArr[index]]);
+//   }
+//   return arr;
