1b97bb8941845d974e353319ce544023522848a2
Alexey <ragnarek.1488@mail.ru>
2023-01-17 09:52:30 +0300

    real

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('data/padawans.txt', 'utf8');
+  const padawansList = padawans.trim().split('\n');
+  return padawansList;
+}
+function getLightsaberScores() {
+  const scores = fs.readFileSync('data/scores.txt', 'utf-8');
+  const scoreList = scores.trim().split('\n');
+  console.log(scoreList);
+  return scoreList.map((el) => Number(el));
+}
+function getStats() {
+  const padawanNam = getPadawanNames();
+  const lightScore = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < padawanNam.length; i += 1) {
+    result.push([padawanNam[i], lightScore[i]]);
+  }
+  return result;
+}
+function writeStats() {
+  const arrStars = getStats();
+  const arrMap = arrStars.map((i) => i.join(' '));
+  return fs.writeFileSync('./data/stats.txt', arrMap.join('\n'));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
