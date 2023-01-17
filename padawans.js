98d7e5a38ddd66d444916fc3fea7ece4ca432109
Samurai <a.i.kamenev90@icloud.com>
2023-01-17 10:05:43 +0300

    2,5 tests

--- padawans.js
+++ padawans.js
@@ -1,4 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+function getPadawanNames() {
+  const padawans = names.split('\n').slice(0, -1);
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const sabers = scores.split('\n').map((el) => Number(el));
+  return sabers;
+}
+
+function getStats() {
+  getPadawansNames().
+  return stat;
+}
+
+function writeStats() {
+  fs.appendFileSync(`/data/stats.txt`, `${getStats()[i]}\n`)
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
