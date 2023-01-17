b57f26412f2ca5e01e93e84ea106e3ccc89d6e36
Eugene Perov <perovevgenij879@gmail.com>
2023-01-17 10:07:02 +0300

    Не сделал(((

--- padawans.js
+++ padawans.js
@@ -1,4 +1,39 @@
-const fs = require('fs')
+const fs = require('fs');
+
+
+function getPadawanNames(){
+  let name=fs.readFileSync("./data/padawans.txt", "utf8");
+  return name;
+}
+getPadawanNames();
+
+function getLightsaberScores(){
+  let name=fs.readFileSync("./data/scores.txt", "utf8");
+  return name;
+}
+
+getLightsaberScores();
+
+
+function getStats(){
+  let name=fs.readFileSync("./data/padawans.txt", "utf8");
+  console.log('пока ничего');
+}
+getStats();
+
+
+function writeStats(){
+  let name=fs.readFileSync("./data/padawans.txt", "utf8");
+  console.log('пока ничего');
+
+}
+
+
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
