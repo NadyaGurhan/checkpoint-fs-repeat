34db5756b35cdec267299bfe7b652f2ad7a8af94
Uadim4ik <uadim4ik9696@gmail.com>
2023-01-31 11:09:41 +0400

    only managed the first two tests

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => {
+  const inerTxt = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const trimTxt = inerTxt.trim();
+  return trimTxt.split('\n');
+};
+
+const getLightsaberScores = () => {
+  const inerNum = fs.readFileSync('./data/scores.txt', 'utf8');
+  return inerNum.split('\n').map(function (element) {
+    return Number(element);
+  });
+};
+
+// const getStats = () => {
+  // const inerTxtTwo = fs.readFileSync('./data/padawans.txt', 'utf8');
+  // const inerNumTwo = fs.readFileSync('./data/scores.txt', 'utf8');
+
+// };
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
