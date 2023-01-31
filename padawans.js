2f7f5083294812f3d813e874be1d7aa128385e16
Denis <luchsound@gmail.com>
2023-01-31 10:13:28 +0300

    так торопился,, что запутался и ничего не успел -_-

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+const fs = require("fs");
+const { argv } = require("process");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+function getPadawanNames() {
+  const res = []; // typeof object ?!
+  res.push(fs.readFileSync("data/padawans.txt", "utf8"));
+  return res;
+}
+
+getPadawanNames();
+
+function getLightsaberScores() {
+  const result = []; // typeof object ?!
+  result.push(fs.readFileSync("data/scores.txt", "utf8"));
+  console.log(result);
+  return result;
+}
+getLightsaberScores();
+
+// getStats();
+
+// writeStats();
