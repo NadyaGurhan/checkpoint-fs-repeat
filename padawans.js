115b603f5b8c0f477aca44c4801bb99fabcabac9
Svyatoslav <svyatoslav.gryadyuga@gmail.com>
2023-01-31 10:55:02 +0400

    All tests passed

--- padawans.js
+++ padawans.js
@@ -1,4 +1,34 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  return fs
+    .readFileSync(`${__dirname}/data/padawans.txt`, "utf8")
+    .split("\n")
+    .slice(0, -1);
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync(`${__dirname}/data/scores.txt`, "utf8")
+    .split("\n")
+    .map((item) => +item);
+}
+
+function getStats() {
+  const result = [];
+  for (let i = 0; i < getPadawanNames().length; i++) {
+    result[i] = [getPadawanNames()[i], getLightsaberScores()[i]];
+  }
+  return result;
+}
+
+function writeStats() {
+  const data = getStats()
+    .map((item) => item.join(" "))
+    .join("\n");
+  fs.writeFileSync("data/stats.txt", data, "utf8");
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
