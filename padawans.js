c7bf93c8e4e5d7e859f6f0ce542275c936009bb7
vladimir <vladimir.dill@mail.ru>
2023-01-31 12:01:50 +0500

    reliase 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,21 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  const names = readDataFromTxtFile("padawans.txt");
+
+  return names;
+}
+console.log(getPadawanNames());
+
+function readDataFromTxtFile(fileName) {
+  return fs
+    .readFileSync(`../data/${fileName}`, "utf-8")
+    .split("\n")
+    .slice(0)
+    .map((el) => el.split(", "));
+}
+console.log(readDataFromTxtFile("padawans.txt"));
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
