0d95828711e391aa9fcfe0de24069849b5dd8612
caf435 <caftales@gmail.com>
2023-01-31 16:01:16 +0900

    не получилось

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  let result = [];
+  let padaw = fs.readFileSync("data/padawans.txt", "utf8");
+  for (let i = 0; i < padaw; i++) {
+    result.push(padaw);
+  }
+  return result;
+}
+
+function getLightsaberScores() {
+  let result = [];
+  let sabers = fs.readFileSync("data/scores.txt", "utf8");
+  for (let i = 0; i < sabers; i++) {
+    result.push(sabers);
+  }
+  return result;
+}
+const getStats = 0;
+const writeStats = 0;
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
