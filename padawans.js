6522fb36396c49b510475617e608b1585ba8e40d
Isa <isa_akashev@mail.ru>
2023-02-07 10:09:24 +0300

    work

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+const text = fs.readFileSync(__dirname + "/data/padawans.txt", "utf8");
+const textAr = text.split("\n");
+const text2 = fs.readFileSync(__dirname + "/data/scores.txt", "utf8");
+const textAr2 = text2.split("\n");
+
+function getPadawanNames() {
+  const arr = [];
+  textAr.forEach((element) => {
+    arr.push(element);
+  });
+  return arr;
+}
+
+function getLightsaberScores() {
+  const arr = [];
+  textAr2.forEach((n) => {
+    arr.push(Number(n));
+  });
+  return arr;
+}
