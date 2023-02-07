ef343df53a1fab80fa94c1104ebfde507e616f98
Aleksandr Yurkovskiy <alus03@mail.ru>
2023-02-07 09:50:02 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,37 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  return fs
+    .readFileSync(__dirname + "/data/padawans.txt", "utf8")
+    .split("\r\n");
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync(__dirname + "/data/scores.txt", "utf8")
+    .split("\r\n")
+    .map((el) => +el);
+}
+
+function getStats() {
+  names = getPadawanNames();
+  scores = getLightsaberScores();
+  let res = [];
+  for (ind in names) {
+    res.push([names[ind], scores[ind]]);
+  }
+
+  return res;
+}
+
+function writeStats() {
+  let res = getStats().map((el) => el.join(" "));
+  fs.writeFileSync(__dirname + "/data/stats.txt", res.join("\n"));
+}
+console.log(getPadawanNames());
+console.log(getLightsaberScores());
+console.log(getStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
