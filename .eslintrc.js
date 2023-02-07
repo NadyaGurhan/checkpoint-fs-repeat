f9bc32f633a5c659d72d4a450498718441ca84f6
vtaeke <vtaeke@gmail.com>
2023-02-07 10:05:36 +0300

    endCommit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,15 @@
 module.exports = {
   env: {
-    browser: true,
     commonjs: true,
     es2021: true,
     node: true,
-    jest: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: 'airbnb-base',
+  overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
   },
-  plugins: [
-    'jest',
-  ],
   rules: {
   },
 };
