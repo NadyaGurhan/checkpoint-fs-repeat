aa2c1e26153236dca4dfc1da0a90d99aca35cc86
Nikolay Yakushenko <pycckuupc@gmail.com>
2023-02-07 10:05:19 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,47 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padPath = '/data/padawans.txt';
+const data = fs.readFileSync(__dirname + padPath, 'utf8');
+const padawans = data.split('\n');
+padawans.pop();
+
+const scorePath = '/data/scores.txt';
+const scoreData = fs.readFileSync(__dirname + scorePath, 'utf8');
+const scores = scoreData.split('\n');
+
+function getPadawanNames() {
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const result = [];
+  scores.forEach((e) => result.push(+e));
+  return result;
+}
+
+function getStats() {
+  const result = [];
+  padawans.map((el, i) => {
+    result.push([el]);
+    result[i].push(+scores[i]);
+  });
+  return result;
+}
+
+const savePath = './data/stats.txt';
+
+function writeStats() {
+  const arr = getStats();
+  const newArr = [];
+  let result;
+  arr.forEach(() => {
+    newArr.join(',');
+  });
+  // newArr.forEach((el) => {
+  // result.join()
+  // })
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
