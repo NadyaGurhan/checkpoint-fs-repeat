7125c2045ae11a3a8e5263af38d2343297947eb7
Max Romanov <maxromanov@MacBook-Pro-Maksim.local>
2023-02-07 10:30:58 +0300

    3/4 checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,41 @@
-const fs = require('fs')
+const fs = require('fs');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  return fs
+    .readFileSync('./data/padawans.txt', 'utf-8')
+    .split('\n')
+    .slice(0, -1);
+}
+
+function getLightsaberScores() {
+  const num = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return num.map(Number);
+}
+
+function getStats() {
+  const jedi = getPadawanNames();
+  const lightSaber = getLightsaberScores();
+  const scores = [];
+
+  for (let i = 0; i < jedi.length; i++) {
+    const score = [jedi[i], lightSaber[i]];
+    scores.push(score);
+  }
+
+  return scores;
+}
+
+function writeStats() {
+  const text = getStats()
+    .map((el) => {
+      el.join(' ');
+    })
+    .join('\n');
+  fs.writeFileSync('./data/stats.txt', text);
+}
