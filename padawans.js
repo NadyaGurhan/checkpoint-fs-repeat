730f3eaea787ece749a826413ffdd5cb5d84a7a2
Dimitri Sukhoff <sukhovdk@gmail.com>
2023-02-08 08:43:20 +0300

    Checkpoint-fs done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
-const fs = require('fs')
+const fs = require('fs');
+const {EOL} = require('os')
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+  const padawansArr = padawans.split(EOL).slice(0, -1)
+  return padawansArr;
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8');
+  const scoresArr = scores.split(EOL).map(el => Number(el))
+return scoresArr
+}
+getLightsaberScores()
+
+function getStats() {
+  const padawans = getPadawanNames()
+  const scores = getLightsaberScores() 
+  const stats = padawans.map((name, index) => [name, scores[index]])
+ 
+return stats
+}
+getStats() 
+
+function writeStats() {
+  const stat = getStats()
+  const statArr = stat.map(line => line.join(' '))
+  const statStr = statArr.join('\n')
+  const statsData = fs.writeFileSync(`${__dirname}/data/stats.txt`, statStr)
+}
+
+writeStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
