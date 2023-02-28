8122ff8e7ef9b3a2d8b2d013395f49658634e77a
P Z <pz@MacBook-Air-P.local>
2023-02-28 10:09:10 +0300

    checkpoint is done

--- padawans.js
+++ padawans.js
@@ -2,6 +2,19 @@ const fs = require('fs')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+ function getPadawanNames () {
+  const arrNames = fs.readFileSync(`./data/padawans.txt`, "utf8");
+  const names = arrNames.trim().split("\n");
+  return names;
+ };
+ function getLightsaberScores () {
+  let arrScores = fs.readFileSync(`./data/scores.txt`, "utf8");
+  
+  const scores = arrScores.split("\n");
+
+  return scores;
+ }
+ 
