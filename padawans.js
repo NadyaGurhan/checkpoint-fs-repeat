7983d86bb90e5e58a678a0e26e575165eeea61b6
Maksim Maslov <max.tmfc@gmail.com>
2023-02-28 10:08:54 +0300

    Release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawanNames = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+const padawanScores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8');
+
+function getPadawanNames() {
+  const resArr = padawanNames.split('\n').slice(0, 4);
+  return resArr;
+}
+
+function getLightsaberScores() {
+  const scoresArr = padawanScores.split('\n');
+  const resScoresArr = scoresArr.map(elem => Number(elem));
+  return resScoresArr;
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+// Я не понимаю, почему не проходят успешно тесты по первым двум функциям. 
+// Если вывести их результат в консоль, он соответствует тому, что указано в резудьтатах тестов.
