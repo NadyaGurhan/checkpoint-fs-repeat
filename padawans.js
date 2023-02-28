e8fe9e5a4cfc98889dcb46038c98aeb1c222083a
chyrgal <cobernadoss@gmail.com>
2023-02-28 10:02:41 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,40 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+const popped = padawans.pop();
+// console.log(popped);
+// console.log(scores);
+// console.log(padawans);
+
+function getPadawanNames() {
+  return padawans.filter((el) => el);
+}
+// console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  return scores.map((el) => +el);
+}
+// console.log(getLightsaberScores());
+
+function getStats() {
+  const res = [];
+  for (i = 0; i < scores.length; i++) {
+    res.push([padawans[i], +scores[i]]);
+  }
+  return res;
+}
+// console.log(getStats());
+
+// console.log(ultraRes);
+// console.log(finalRes);
+function writeStats() {
+  const finalRes = getStats();
+  const resRes = finalRes.map((el) => el.join(' '));
+  const ultraRes = resRes.join('\n');
+  fs.writeFileSync('./data/stats.txt', ultraRes, 'utf8');
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
