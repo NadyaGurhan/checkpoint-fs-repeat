bac4455ec81265023d41220384eaaa5d2956f83e
Mihail <misha17_74@mail.ru>
2023-02-28 10:21:04 +0300

    не смотрите

--- padawans.js
+++ padawans.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
+
+//я понимаю что мне надо сделать так чтобы fs читал текстовые файлы но не знаю как 
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+//пока думал как сделато то что написал выше не успел сделать остальное
