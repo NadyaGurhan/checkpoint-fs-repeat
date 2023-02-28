8c599c945208975731f7615806d166ed8687013f
Anna <a_karvatskaya@mail.ru>
2023-02-28 10:11:14 +0300

    My

--- padawans.js
+++ padawans.js
@@ -1,7 +1,11 @@
-const fs = require('fs')
+const fs = require("fs");
+let namePadawans = fs.readFileSync("./data/padawans.txt", "utf-8");
+
+const getPadawanNames = (namePadawans) => {};
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+getPadawanNames()
