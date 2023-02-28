11c8b5054b7fa33b4b8b8421b233d423d4908038
Tihon <odinokiy.mayak@gmail.com>
2023-02-28 09:40:18 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawans = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+function getPadawanNames() {
+  console.log(padawans);
+  console.log(typeof padawans);
+  console.log(padawans.split('\n').slice(0, -1));
+  return padawans.split('\n').slice(0, -1);
+}
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
