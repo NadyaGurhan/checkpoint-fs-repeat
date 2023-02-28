1fb1a08a170ac3cab90429e3dc518ea5e460a8f6
Kirill <agafonoff.k@yandex.ru>
2023-02-28 09:53:44 +0300

    chechpoint done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawansNames = ['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani'];
+
+function getPadawanNames() {
+  return padawansNames;
+}
+
+const padawansScores = [99.9, 92, 87, 82];
+
+function getLightsaberScores() {
+  return padawansScores;
+}
+
+function getStats() {
+  const result = [];
+  for (let index = 0; index < padawansNames.length; index += 1) {
+    const newArr = [padawansNames[index], padawansScores[index]];
+    result.push(newArr);
+  }
+  return result;
+}
+
+function writeStats() {
+  for (let index = 0; index < padawansNames.length; index += 1) {
+    if (index !== padawansNames.length - 1) {
+      const row = `${padawansNames[index]} ${padawansScores[index]}\n`;
+      fs.appendFileSync('./data/stats.txt', row, 'utf-8');
+    } else if (index === padawansNames.length - 1) {
+      const row = `${padawansNames[index]} ${padawansScores[index]}`;
+      fs.appendFileSync('./data/stats.txt', row, 'utf-8');
+    }
+  }
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
