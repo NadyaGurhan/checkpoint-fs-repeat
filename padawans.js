ec9dc3cdfdea8e295b2b6b1beda6d1a1301e6879
P Z <pz@MacBook-Air-P.local>
2023-03-04 09:54:19 +0300

    checkpoint 1.2 is done

--- padawans.js
+++ padawans.js
@@ -1,20 +1,50 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  // getStats,
-  // writeStats,
+  getStats,
+  writeStats,
 };
- function getPadawanNames () {
+function getPadawanNames() {
   const arrNames = fs.readFileSync(`./data/padawans.txt`, "utf8");
   const names = arrNames.trim().split("\n");
   return names;
- };
- function getLightsaberScores () {
-  let arrScores = fs.readFileSync(`./data/scores.txt`, "utf8");
-  
-  const scores = arrScores.split("\n");
+}
+function getLightsaberScores() {
+  const arrScores = fs.readFileSync(`./data/scores.txt`, "utf8");
 
-  return scores;
- }
- 
+  let scores = arrScores.split("\n");
+  // scores = [];
+  let x = [];
+  for (let i = 0; i < scores.length; i++) {
+    x.push(+scores[i]);
+  }
+
+  return x;
+}
+
+function getStats() {
+  const padawan = getPadawanNames();
+  const saberScores = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < 4; i++) {
+    result.push([padawan[i], saberScores[i]]);
+  }
+  return result;
+}
+function writeStats() {
+  const padawan = getPadawanNames();
+  const saberScores = getLightsaberScores();
+  let result = "";
+  for (let i = 0; i < 4; i++) {
+    // if (i <= 2) {
+    //   result += `${padawan[i]} ${saberScores[i]}\n`;
+    // } else {
+    //   result += `${padawan[i]} ${saberScores[i]}`;
+    // }
+    i <= 2
+      ? (result += `${padawan[i]} ${saberScores[i]}\n`)
+      : (result += `${padawan[i]} ${saberScores[i]}`);
+  }
+  fs.writeFileSync("./data/stats.txt", result, "utf8");
+}
