2a78dcc27dfec3a8d191331b98b864daeb369ee8
Alexander Lokshin <av_lokshin81@mail.ru>
2023-03-21 10:17:04 +0300

    fail to solve

--- padawans.js
+++ padawans.js
@@ -1,4 +1,10 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+   let filePath = 'data/padawans.txt';
+   let data = fs.readFileSync(filePath, 'utf8').split(/\r?\n/);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
