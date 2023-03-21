ff82fe057619e32b4d2af67055c8a7213bbdc225
Anton Yusupov <an87yu@gmail.com>
2023-03-21 10:18:42 +0300

    release 1.0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('data/padawans.txt', 'utf8');
+  const res = names.split('\n');
+  return res;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('data/scores.txt', 'utf8');
+  scores.split('\n');
+}
+
+// function getStats() {
+
+// }
+
+// function writeStats() {
+
+// }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
