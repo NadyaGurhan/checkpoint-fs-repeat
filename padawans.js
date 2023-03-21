f9655500e8b0db29ac58c6d63c2f1ccab62fcbc2
Evgeniy <shichkov97@gmail.com>
2023-03-21 10:17:59 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,22 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const arr = [];
+  const result = fs.readFileSync("data/padawans.txt", 'utf8');
+  const names = result.split('\n').filter(name => name.trim() !== '');
+  arr.push(result);
+  return names;
+}
+function getLightsaberScores() {
+  const funcX = getPadawanNames();
+  const e = fs.readFileSync("data/scores.txt")
+ funcX.forEach(el => el.push(e))
+  return funcX
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
