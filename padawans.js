aba5bb381949b1088a56ebcee6a94b4d60e46fd6
Andrey Burov <andrey-valeriy.burov@mail.ru>
2023-03-21 10:08:44 +0300

    all done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,47 @@
-const fs = require('fs')
+/* eslint-disable no-plusplus */
+const fs = require('fs');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return data.split('\n').slice(0, -1);
+}
+
+// console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return data.split('\n').map((el) => Number(el));
+}
+
+// console.log(getLightsaberScores());
+
+function getStats() {
+  const names = getPadawanNames();
+  const skills = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push([names[i], skills[i]]);
+  }
+  return result;
+}
+
+// console.log(getStats());
+
+function writeStats() {
+  const data = getStats().join('\n');
+  let result = '';
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] === ',') {
+      result += ' ';
+    } else {
+      result += data[i];
+    }
+  }
+  fs.writeFileSync('data/stats.txt', result, 'utf-8');
+}
+
+console.log(writeStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
