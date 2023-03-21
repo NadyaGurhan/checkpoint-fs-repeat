34b2a2701738d0a5d3dd7c06885d95e109a9d32b
Alena <alena58@mail.ru>
2023-03-21 10:18:50 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => {
+  const r = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  // const a = require('./data/padawans.txt'); 
+  const r1 = a.split('\n');
+  // const r2 = []
+  // r2.push(r1)
+  return r1;
+};
+// const getLightsaberScores = () => {
+//   const f = fs.readFileSync('./data/scores.txt', 'utf-8');
+//   const e = f.split('\n');
+//   // const e = [];
+//   // return e.push(f);
+//   return e;
+// };
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
