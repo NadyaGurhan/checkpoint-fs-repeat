104043fa9f592a12d9f583d0d05e4b7bd2348562
Vardges <charhvargs@gmail.com>
2023-03-21 10:20:27 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,4 +1,22 @@
-const fs = require('fs')
+const fs = require("fs");
+
+const padawans = fs.readFileSync("data/padawans.txt", "utf-8");
+const scrores = fs.readFileSync('data/scores.txt', 'utf-8');
+const score = scrores.split('\n');
+
+console.log(score)
+
+function getPadawanNames() {
+  const names = padawans.split("\n").slice(0, 4);
+  return names;
+}
+function getLightsaberScores() {
+  const score = scrores.split('\n');
+  return score;
+}
+function getStats() {}
+function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
