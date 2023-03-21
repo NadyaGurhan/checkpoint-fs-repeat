a502465e778d7d4a2f8c5b008122992218b4aa45
Artem <asaygashkin@gmail.com>
2023-03-21 10:21:51 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
-const fs = require('fs')
+const fs = require("fs");
+
+const padawans = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+
+const scores = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+
+function getPadawanNames() {
+  return padawans.filter((el) => el !== "");
+}
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  return scores.map((el) => Number(el));
+}
+console.log(getLightsaberScores());
+
+// function getStats(){
+// let stats =[]
+
+// }
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
