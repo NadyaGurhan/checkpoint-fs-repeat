fdb2bac906115e89418d160f9bf5cab4bcfc2db2
DjoDik <djodi046@gmail.com>
2023-03-21 10:18:45 +0300

    checkPoint1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  plugins: [
-    'jest',
-  ],
-  rules: {
-  },
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+        node: true,
+        jest: true,
+    },
+    extends: ["airbnb-base"],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    plugins: ["jest"],
+    rules: {
+        "linebreak-style": ["error", "windows"],
+        quotes: ["error", "double"],
+        indent: ["error", 4],
+        "prefer-destructuring": 0,
+    },
 };
