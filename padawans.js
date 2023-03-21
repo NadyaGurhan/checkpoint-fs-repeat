185f87dc88d41ccde6bf5d534b7d68894c08debe
Markuha98 <markyha18@mail.ru>
2023-03-21 10:19:55 +0300

    Padawans

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+const arrInDataP = fs.readFileSync('./data/padawans.txt', 'utf-8').split('/n');
+const arrInDataS = fs.readFileSync('./data/scores.txt`', 'utf-8').split('/n');
+// 1
+function getPadawanNames() {
+  return arrInDataP;
+}
+// console.log(getPadawanNames())
+
+// 2
+function getLightsaberScores() {
+  return arrInDataS;
+}
+
+// 3
+function getStats(array) {
+  const arr = [];
+  for (let i = 0; i < array.length; i++) {
+    arr.push(arrInDataP[i].concat(arrInDataS[i]));
+  }
+  return arr;
+}
