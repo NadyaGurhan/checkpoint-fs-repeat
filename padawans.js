60d5dbd93801e548143835da9130998a4f7edc32
Markuha98 <markyha18@mail.ru>
2023-03-25 13:30:43 +0300

    padawans_change

--- padawans.js
+++ padawans.js
@@ -7,24 +7,31 @@ module.exports = {
   writeStats,
 };
 
-const arrInDataP = fs.readFileSync('./data/padawans.txt', 'utf-8').split('/n');
-const arrInDataS = fs.readFileSync('./data/scores.txt`', 'utf-8').split('/n');
+// const arrInDataP = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+// const arrInDataS = fs.readFileSync('./data/scores.txt`', 'utf-8').split('\n');
 // 1
 function getPadawanNames() {
-  return arrInDataP;
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\r\n');
 }
 // console.log(getPadawanNames())
 
 // 2
 function getLightsaberScores() {
-  return arrInDataS;
+  return fs.readFileSync('./data/scores.txt', 'utf-8').split('\r\n').map(el => +el);
 }
+// console.log(getLightsaberScores());
 
 // 3
-function getStats(array) {
-  const arr = [];
-  for (let i = 0; i < array.length; i++) {
-    arr.push(arrInDataP[i].concat(arrInDataS[i]));
-  }
-  return arr;
-}
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  return padawans
+    .map((el, index) => [padawans[index], scores[index]]);
+};
+// console.log(getStats());
+
+// 4
+function writeStats() {
+const stats = getStats().map(el => el.join(' ')).join('\n');
+return fs.writeFileSync('data/stats.txt', stats);
+};
