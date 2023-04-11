894c496379921e17bea27a61482b1065b01b3678
Tatiana <batyukova.tatyana@gmail.com>
2023-04-11 10:04:40 +0300

    check point

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+const fs = require("fs");
+
+const getPadawanNames = () =>
+  fs
+    .readFileSync(`${__dirname}/data/padawans.txt`, "utf-8")
+    .split("\n")
+    .slice(0, 4);
+
+const getLightsaberScores = () =>
+  fs
+    .readFileSync(`${__dirname}/data/scores.txt`, "utf-8")
+    .split("\n")
+    .map((el) => el * 1);
+
+const getStats = () => {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const skills = padawans.concat(scores);
+  let result = [];
+  for (let i = 0; i <= skills.length; i += 1) {
+    result.push(skills[i] + skills[i + 4]);
+  }
+
+  return result;
 };
+
+module.exports = { getPadawanNames, getLightsaberScores, getStats };
