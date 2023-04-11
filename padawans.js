fe36964568825d8ddae3e9aac0e6353b38ca6ca1
Ilia <iliya.britvin@gmail.com>
2023-04-11 10:21:06 +0300

    r2

--- padawans.js
+++ padawans.js
@@ -1,4 +1,34 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawanNamesArr = fs
+    .readFileSync('./data/padawans.txt', 'utf-8')
+    .split('\n');
+
+  const result = padawanNamesArr.slice(0, -1);
+  return result;
+}
+
+function getLightsaberScores() {
+  const result = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+
+  return result;
+}
+
+// function getStats() {
+//   const padawanNamesArr = fs
+//     .readFileSync('./data/padawans.txt', 'utf-8')
+//     .split('\n');
+
+//   const scoresArr = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+
+//   const result;
+
+//   return result;
+// }
+
+// function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
