d7318a119546fb2250f636d83d2cb8c991074159
Alena <aa_bow@mail.ru>
2023-05-02 10:09:54 +0300

    not well done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+let padawans = fs.readFileSync("../padawans.txt", "utf-8");
+
+function getPadawanNames(text) {
+  let arr = [];
+  for (let i = 0; i < text.length; i += 1) {
+    return arr.push(padawans);
+  }
+}
