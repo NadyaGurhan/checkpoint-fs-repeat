0a8d76fc39b956eb26df02ad0131372e77d91a87
Struzhanov <struzhanov.a@yandex.ru>
2023-05-30 10:00:05 +0300

    relise0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => {
+  const padawans = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+  return padawans
+    .trim()
+    .split('\n');
+};
+
+const getLightsaberScores = () => {
+  const scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8')
+    .split('\n').map((el) => +el);
+  return scores;
+};
+
+const getStats = () => {
+  const scores = getLightsaberScores();
+  const padawans = getPadawanNames();
+  const result = [];
+  for (let i = 0; i < padawans.length; i += 1) {
+    result.push(`${padawans[i]} , ${scores[i]}`);
+  }
+  console.log(result);
+};
+
+const writeStats = () => {
+
+};
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
