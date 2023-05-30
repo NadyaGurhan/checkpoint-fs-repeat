a25e23aa18bbbd57b53dba8eecd86b57d75a1d7a
ds <d555601@ya.ru>
2023-05-30 10:00:53 +0300

    ok

--- padawans.js
+++ padawans.js
@@ -1,7 +1,17 @@
 const fs = require('fs')
+
+function getPadawanNames(){
+  const text = fs.writeFileSync("__dirname + '/data/padawans.txt", "utf-8");
+  return console.log(text);
+  
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
