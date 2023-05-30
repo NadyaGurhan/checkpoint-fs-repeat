56f34322674db742e9c12e590fb845e816d8b10b
Vyacheslav Galkin <galkin.slava@list.ru>
2023-05-30 09:57:01 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,35 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', { encoding: 'utf8', flag: 'r' });
+  return padawans.split('\n');
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', { encoding: 'utf8', flag: 'r' });
+  return scores.split('\n').map((el) => Number(el));
+}
+
+function getStats() {
+  const padawans = fs.readFileSync('./data/padawans.txt', { encoding: 'utf8', flag: 'r' });
+  const scores = fs.readFileSync('./data/scores.txt', { encoding: 'utf8', flag: 'r' });
+  const arrPadawans = padawans.split('\n');
+  const arrScores = scores.split('\n');
+  const stats = [];
+  for (let i = 0; i < arrPadawans.length; i++) {
+    const padawan = [];
+    padawan.push(arrPadawans[i], Number(arrScores[i]));
+    stats.push(padawan);
+  }
+
+  return stats;
+}
+
+function writeStats() {
+  const toWrite = getStats().map((el) => el.join(' '));
+  fs.appendFileSync('data/stats.txt', toWrite.join('\n'));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
