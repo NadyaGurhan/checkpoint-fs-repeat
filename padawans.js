916d73326d5bc9fbd11cadbd419b4637032b436d
ds <d555601@ya.ru>
2023-05-30 19:50:33 +0300

    ok

--- padawans.js
+++ padawans.js
@@ -1,11 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
 
-function getPadawanNames(){
-  const text = fs.writeFileSync("__dirname + '/data/padawans.txt", "utf-8");
+function getPadawanNames() {
+  const text = fs.writeFileSync('./data/padawans.txt', 'utf-8');
   return console.log(text);
-  
 }
 
+function getLightsaberScores() {
+  const text1 = fs.writeFileSync('./data/scores.txt', 'utf-8');
+  return console.log(text1);
+}
+
+function getStats() {
+  // const text = fs.writeFileSync('./data/padawans.txt', 'utf-8');
+  // const text1 = fs.writeFileSync('./data/scores.txt', 'utf-8');
+  const stat = getPadawanNames.map(((el, i) => el + getLightsaberScores[i]));
+  return stat;
+}
+console.log(getStats());
+
+const writeStats = () => {};
 
 module.exports = {
   getPadawanNames,
@@ -13,5 +26,3 @@ module.exports = {
   getStats,
   writeStats,
 };
-
-
