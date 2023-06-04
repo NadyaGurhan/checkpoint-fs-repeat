47f333edd1a6b9fcee3ec9819545ddee05bafb4d
Dmitry <d555601@yandex.ru>
2023-06-04 14:19:49 +0300

    Add files via upload
    не понял почему?: 
    Revanundefined
    Bastila Shanundefined
    Jolee Bindoundefined
    Juhaniundefined
    
    у ф-ции function writeStats()

--- padawans.js
+++ padawans.js
@@ -1,4 +1,34 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const text = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+  const array = text.split('\n');
+  return array;
+}
+
+function getLightsaberScores() {
+  const text = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8');
+  const array = text.split('\n')
+    .map((el) => Number(el));
+  return array;
+}
+
+function getStats() {
+  const stat = getPadawanNames().map(((el, i) => el + getLightsaberScores[i]));
+  return stat;
+}
+console.log(getStats());
+
+function writeStats() {
+  // const stats = getStats().map((el) => el.join(' '));
+  const stats = getStats();
+  const data = stats.join('\n')
+
+  fs.writeFileSync('./data/stats.txt', data)
+  return stats;
+}
+writeStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
