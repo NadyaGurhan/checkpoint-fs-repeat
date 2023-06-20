cb9060ec1bf229a682799acb60498fc2672fe9d8
AEvgeniyB <aeb1986@yandex.ru>
2023-06-20 10:02:04 +0300

    Чекпоинт-1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,35 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+const dataPadavans = fs.readFileSync("./data/padawans.txt", "utf-8");
+const scores = fs.readFileSync("./data/scores.txt", "utf-8");
+
+function getPadawanNames() {
+  return dataPadavans.trim().split("\n");
+}
+
+function getLightsaberScores() {
+  const arr = scores.trim().split("\n");
+  return arr.map((item) => +item);
+}
+
+function getStats() {
+  const padawan = getPadawanNames();
+  const lightsabel = getLightsaberScores();
+  let answer = [];
+  for (let i = 0; i < padawan.length; i++) {
+    answer.push([padawan[i], lightsabel[i]]);
+  }
+  return answer;
+}
+
+function writeStats() {
+  const data = getStats();
+  const result = data.map((item) => item.join(" "));
+  fs.writeFileSync("./data/stats.txt", result.join("\n"));
+}
