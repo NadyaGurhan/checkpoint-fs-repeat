6f33b5b78a22ef33d54e9ebc401abd658e3b401a
ViktorRM <viktorrmbox@gmail.com>
2023-06-20 10:07:43 +0300

    testt

--- padawans.js
+++ padawans.js
@@ -5,3 +5,18 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+
+function getPadawanNames (){
+
+  let padawansFile = fs.readFileSync('./data/padawans.txt', 'utf8');
+  let crewCapArrs = padawansFile.split('\n').map((item) => item);
+  lastcrewCapArrs = crewCapArrs.pop()
+  return crewCapArrs
+}
+
+function getLightsaberScores (){
+  let Lightsaber = fs.readFileSync('./data/sco.txt', 'utf8');
+  let Lightsaber = padawansFile.join((item) => item);
+  return Lightsaber
+}
