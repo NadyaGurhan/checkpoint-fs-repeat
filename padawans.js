e76fc56034b08dd12a6804bbe4372967118de041
Dmitry <d555601@yandex.ru>
2023-06-20 09:48:31 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,34 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const text = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const array = text.split('\n');
+  return array;
+}
+
+function getLightsaberScores() {
+  const text = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const array = text.split('\n')
+    .map((el) => Number(el));
+  return array;
+}
+
+function getStats() {
+  const stat = getPadawanNames().map(((el, i) => el + getLightsaberScores[i]));  // непойму почему возвращает undefined
+  return stat;
+}
+
+
+function writeStats() {
+  // const stats = getStats().map((el) => el.join(' '));
+  const stats = getStats();
+  const data = stats.join('\n')
+
+  fs.writeFileSync('./data/stats.txt', data)
+  return stats;
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
