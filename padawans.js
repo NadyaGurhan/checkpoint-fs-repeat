16e627d3cf319cdca251b9908cac890ad1b05ed8
erdnikektyshov@bk.ru <erdnikektyshov@bk.ru>
2023-06-20 10:09:22 +0300

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const filePath = './data/padawans.txt';
+  const names = fs.readFileSync(filePath, 'utf8');
+  return names.split('\n');
+}
+console.log(getPadawanNames());
+function getLightsaberScores() {
+  const arr = [];
+  const filePath = './data/scores.txt';
+  const scores = fs.readFileSync(filePath, 'utf8');
+  arr.push(scores);
+  return arr;
+}
+
+// console.log(getLightsaberScores())
+
+function getStats() {
+
+}
+function writeStats() {
+
+}
