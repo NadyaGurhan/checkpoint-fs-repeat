d575c50a4930f86f916c70fa3d19ba86cd1931b8
IlyasMahmudov <ilyas.mahmudov@list.ru>
2023-06-27 10:01:58 +0300

    1 checkpoint Ilyas

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
 const fs = require('fs')
+const padawans = fs.readFileSync('./data/padavans.txt','utf8')
+const scores = fs.readFileSync('./data/scores.txt','utf8')
+
+function getPadawanNames() {
+  return padawans.split('\n')
+}
+
+function getLightsaberScores() {
+  return scores.split('\n')
+}
+
+function getstats() {
+
+}
+
+
+function writeStats() {
+
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
