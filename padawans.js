c18b969dc791dd9931303d0e69a6141bac8bb235
alastark <sandro.korovitskiy@yandex.ru>
2023-06-27 09:58:01 +0300

    checkpoint 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,22 @@
-const fs = require('fs')
+const fs = require("fs");
+
+const padawans = fs
+  .readFileSync(__dirname + "/data/padawans.txt", "utf8")
+  .split("\r\n");
+
+const scores = fs
+  .readFileSync(__dirname + "/data/scores.txt", "utf8")
+  .split("\r\n");
+
+function getPadawanNames() {
+  return padawans.slice(0, 4);
+}
+function getLightsaberScores() {
+  return scores.map((el) => +el);
+}
+function getStat() {
+  return padawans.push(scores);
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
