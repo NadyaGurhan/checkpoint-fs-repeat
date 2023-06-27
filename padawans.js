f3f85a9c39c300c265d1f18513bb986109aa61fa
MatissKrayt <MatissKrayt@gmail.com>
2023-06-27 10:01:21 +0300

    release 1/4

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
-const fs = require('fs')
+const fs = require("fs");
+
+const padawans = fs.readFileSync("./data/padawans.txt", "utf-8");
+const scores = fs.readFileSync("./data/scores.txt", "utf-8");
+
+const getPadawanNames = () => padawans.split('\n').slice(0, -1);
+console.log(getPadawanNames())
+const getLightsaberScores = () => scores.trim().split("\n").map((el) => +el);
+
+const getStats = () => {}
+
+const writeStats = () => {
+  fs.writeFileSync(`./data/stats.txt`,`${getStats}`,"utf8");
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
+  // getStats,
   writeStats,
 };
