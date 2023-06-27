afcd4429e9a78a1e1aeb7908523896eff9219aef
LubeninStanislav <lubeninstanislav@gmail.com>
2023-06-27 12:03:15 +0500

    d

--- padawans.js
+++ padawans.js
@@ -1,4 +1,20 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const getPadawanNames = fs.readFileSync('data/padawans.txt', utf8);
+
+}
+function getLightsaberScores() {
+  const getLightsaberScores = fs.readFileSync('./data/scores.txt');
+}
+function getStats() {
+
+}
+
+function writeStats(){
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
