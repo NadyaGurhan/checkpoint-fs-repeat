780a346f460ea275987ac3a90c217559fd8562cd
Roman Ko <roman_ko@gmail.com>
2023-06-27 10:02:40 +0300

    p1w1d2 - сheckpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,35 @@
-const fs = require('fs')
+/* eslint-disable no-plusplus */
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return padawans.split('\n').filter(Boolean);
+}
+
+function getLightsaberScores() {
+  const lightsaber = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return lightsaber.split('\n').map(parseFloat);
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const lightsaber = getLightsaberScores();
+  const result = [];
+
+  for (let i = 0; i < padawans.length; i++) {
+    const name = padawans[i];
+    const score = lightsaber[i];
+    result.push([name, score]);
+  }
+
+  return result;
+}
+
+function writeStats(result) {
+  const statistics = result.map(([name, score]) => `${name} ${score}`).join('\n');
+  fs.writeFileSync('./data/stats.txt', statistics, 'utf-8');
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
