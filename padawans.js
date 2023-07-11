b70804f33a01818d371a42b8e213084abcf82e82
AntonA320 <kulaginad320@gmail.com>
2023-07-11 19:15:16 +0300

    reliase 2

--- padawans.js
+++ padawans.js
@@ -2,24 +2,42 @@ const fs = require('fs')
 
 function getPadawanNames(){
   let arr = [];
- let name = fs.readFileSync('./data/padawans.txt','utf8')
+ let name = fs.readFileSync('./data/padawans.txt','utf8').split('\n')
  for (let i = 0; i< name.length; i+=1){
-   arr. push (name[i])
+   arr. push ((name[i]))
  }
 return arr
 };
 
+
+
+
 function getLightsaberScores (){
   let arr =[]
-  let score = fs.readFileSync('./data/scores.txt','utf8')
+  let score = fs.readFileSync('./data/scores.txt','utf8').split('\n')
   for (let i = 0; i< score.length; i+=1){
-    arr. push (score[i])
+    arr. push (+(score[i]))
   }
   return arr;
 }
 
+function getStats(){
+  let name = getPadawanNames();
+  let score = getLightsaberScores ();
+  let arr = []
+  for(let i = 0;i< name.length;i+=1){
+    arr.push([name[i],score[i]])
+  }
+  return arr
+}
 
+function writeStats(){
 
+  let stats = getStats().join('\n')
+   fs.writeFileSync('./data/stats.txt', stats)
+}
+
+writeStats()
 
 
 
@@ -27,7 +45,7 @@ function getLightsaberScores (){
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  
- 
+  getStats,
+  writeStats
 };
 //  writeStats,getStats,
