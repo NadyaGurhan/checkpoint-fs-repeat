a9a6507195f7a36315458e2de9ec2df4a9f949f0
Anastasiya Gitlina <lottaanastasia@gnail.com>
2023-07-11 10:08:34 +0300

    Нашла как вытащить файлы из data, но запаниковала и не успела сообразить как правильно из вывести, в тестах видны данные первых 2 функций но с ошибкой, мне не хватило времени

--- padawans.js
+++ padawans.js
@@ -1,4 +1,30 @@
 const fs = require('fs')
+
+function getPadawanNames () {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+  // for(i = 0; i < names.length; i += 1) {
+  //   printNames.push(names[i]);
+  // }
+  return names;
+} 
+
+function getLightsaberScores () {
+  let scores = fs.readFileSync('./data/scores.txt', 'utf8').split();
+  // for (i = 0; i < scores.length; i += 1) {
+  //   printScores.push(scores);
+  // }
+  return scores;
+}
+
+function getStats () {
+
+}
+
+function writeStats () {
+
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
