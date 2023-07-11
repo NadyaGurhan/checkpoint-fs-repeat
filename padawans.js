0a5e184282e3c441de82c1ca4541f1b1e8c6b7b0
Ekaterina Bakhmeteva <ekaterina.kukhorevich@gmail.com>
2023-07-11 09:48:26 +0300

    tests are ok

--- padawans.js
+++ padawans.js
@@ -1,7 +1,35 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const arr = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8').split(EOL).slice(0, -1);
+  return arr;
+}
+
+function getLightsaberScores() {
+  const score = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8').split(EOL);
+  return score.map((el) => +el);
+}
+
+function getStats() {
+  const arr = getPadawanNames();
+  const score = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    result[i] = [arr[i], +score[i]];
+  }
+  return result;
+}
+
+function writeStats() {
+  const arrToResult = getStats().map((el) => el.join(' '));
+  const result = fs.writeFileSync(`${__dirname}/data/stats.txt`, arrToResult.join(EOL));
+  return result;
+}
+
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+    getPadawanNames,
+    getLightsaberScores,
+    getStats,
+    writeStats,
 };
