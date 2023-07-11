f647d9ae50db62cd83f45096c2005f72063a5555
hegyth <hegyth1236@gmail.com>
2023-07-11 10:09:42 +0300

    eto pechalno

--- padawans.js
+++ padawans.js
@@ -1,3 +1,4 @@
+const { isUtf8 } = require('buffer');
 const fs = require('fs')
 module.exports = {
   getPadawanNames,
@@ -5,3 +6,29 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames () {
+  const zero = ""
+  const result = []
+  let string = fs.readFileSync('./data/padawans.txt', { encoding: 'utf8'})
+  result.push(string.split('\n'))
+  const resultFilter = result.filter((string) => string !== zero)
+  return resultFilter
+}
+function getLightsaberScores () {
+  const result = []
+  let string = fs.readFileSync('./data/scores.txt', { encoding: 'utf8'})
+  let num = string.split('\n')
+  result.push(Number(num))
+  // for (let i=0; i<result.length; i+=1) {
+  //   +result[i]
+  // }
+  // let num = Number(string.split('\n'))
+  return result
+}
+function getStats () {
+  
+}
+function writeStats () {
+  
+}
