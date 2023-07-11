df6242a720bb423ece9ecb6641108471354396c0
Irinabalerina <medvedeva.irinav@yandex.ru>
2023-07-11 10:10:08 +0300

    Release 1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
 const fs = require('fs')
+const padawans = fs.readFileSync(__dirname + './data/padawans.txt','utf8')
+const scores = fs.readFileSync(__dirname + './data/scores.txt','utf8')
+
+function getPadawanNames () {
+const padawanNames = padawans.split('\n')
+return padawanNames
+}
+
+function getLightsaberScores () {
+  const lightsaberScores = scores.split('\n')
+  return lightsaberScores
+}
+
+function getStats () {
+const 
+}
+
+  
+// function writeStats () {
+// //   for (let i = 0; i < arr.length; i += 1) {
+// //     fs.appendFileSync(path, `${arr[i]}\n`)
+// // }
+// }
+  
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
