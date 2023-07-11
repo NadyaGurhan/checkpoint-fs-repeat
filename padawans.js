8f8daf13a685738588476cee7b4443f1d3ba0cd9
mtvbel <mtvbel@yandex.ru>
2023-07-11 10:11:07 +0300

    final

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  let names = fs.readFileSync('./data/padawans.txt', 'utf8');
+  let result = [];
+  let str = '';
+
+  //str = names.replace('/\n/g', '');
+
+  for (let i = 0; i < names.length; i += 1) {
+    if (names[i] !== '\n') {
+      str += names[i];
+    } else {
+      str += ' ';
+    }
+  }
+
+  return str;
+}
+
+function getLightsaberScores() {}
+
+function getStats() {}
+
+function writeStats() {}
+
+console.log(getPadawanNames());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
