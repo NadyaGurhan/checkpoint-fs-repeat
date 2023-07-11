f0b59cc6ef0f392a6bad18ae8cd3e73a704a9256
Vazgen <Kirakosyan_Vazgen@mail.ru>
2023-07-11 10:09:23 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,24 @@
 const fs = require('fs')
+
+function getPadawanNames(){
+  const newFs = fs.readFileSync('/data/padawans.txt', 'utf-8').split(' ')
+  return newFs
+}
+
+function getLightsaberScores(){
+  const newLight = fs.readFileSync('/data/scores.txt', 'utf-8').split(' ')
+  
+  return newLight
+}
+
+function getStats(){
+
+}
+
+function writeStats(){
+  
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
