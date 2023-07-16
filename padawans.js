76649103699c1d7e3eda4b4157f72a633a3166f7
Irinabalerina <medvedeva.irinav@yandex.ru>
2023-07-16 13:00:56 +0300

    Test

--- padawans.js
+++ padawans.js
@@ -1,32 +1,57 @@
-const fs = require('fs')
-const padawans = fs.readFileSync(__dirname + './data/padawans.txt','utf8')
-const scores = fs.readFileSync(__dirname + './data/scores.txt','utf8')
+const fs = require("fs");
 
-function getPadawanNames () {
-const padawanNames = padawans.split('\n')
-return padawanNames
+function getPadawanNames() {
+  const padawans = fs
+    .readFileSync("./data/padawans.txt", "utf8")
+    .trim()
+    .split("\n")
+    .map((el) => el.trim("\r"));
+
+  return padawans;
 }
 
-function getLightsaberScores () {
-  const lightsaberScores = scores.split('\n')
-  return lightsaberScores
+function getLightsaberScores() {
+  const scores = fs
+    .readFileSync("./data/scores.txt", "utf8")
+    .trim()
+    .split("\n")
+    .map((el) => Number(el.trim("\r")));
+
+  return scores;
 }
 
-function getStats () {
-const 
+function getStats() {
+  const statsArr = [];
+
+  const padawans = fs
+    .readFileSync("./data/padawans.txt", "utf8")
+    .trim()
+    .split("\n")
+    .map((el) => el.trim("\r"))
+    .map((el) => {
+      statsArr.push(el.split());
+    });
+
+  const scores = fs
+    .readFileSync("./data/scores.txt", "utf8")
+    .trim()
+    .split("\n")
+    .map((el) => el.trim("\r"));
+  for (i = 0; i < scores.length; i++) {
+    statsArr[i].push(Number(scores[i]));
+  }
+  return statsArr;
 }
 
-  
-// function writeStats () {
-// //   for (let i = 0; i < arr.length; i += 1) {
-// //     fs.appendFileSync(path, `${arr[i]}\n`)
-// // }
-// }
-  
+function writeStats(statsArr) {
+  for (let i = 0; i < statsArr.length; i++) {
+    fs.appendFileSync("data/stats.txt", `${statsArr[i]}\n`);
+  }
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
-
