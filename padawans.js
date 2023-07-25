92538b3e3e1f96a70e75bfadd1729df3ff762e35
niko0018 <bullettt018@gmail.com>
2023-07-25 10:07:09 +0300

    checkpoiunt

--- padawans.js
+++ padawans.js
@@ -1,7 +1,22 @@
-const fs = require('fs')
+const fs = require("fs");
+
+
+function getPadawanNames() {
+  const data = fs.readFileSync("./data/padawans.txt", "utf-8");
+  return data.split("\r\n").slice(0, -1);
+}
+
+function getLightsaberScores() {
+  const arr = [];
+  const data = fs.readFileSync("data/scores.txt", "utf-8");
+  arr.push(data);
+  return arr;
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
