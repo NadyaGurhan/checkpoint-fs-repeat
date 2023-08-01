f632dfe080990c5928e2edd55b68b6b2fc9b39ac
deson <sakhlike@gmail.com>
2023-08-01 10:18:21 +0300

    11

--- padawans.js
+++ padawans.js
@@ -1,4 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+const { dirname } = require('path');
+
+function getPadawanNames() {
+  const text = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+  const padawans = text.split('\n').slice(0, -1);
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8');
+  const scores1 = scores.split('\n').map((scores) => Number(scores));
+  return scores1;
+}
+function getStats() {
+  const allpadawans = getPadawanNames;
+  const allscores = getLightsaberScores;
+  const stats = [];
+  const stat = allpadawans.map((el, i) => stats.push([el, scores[i]]));
+  return stats;
+}
+console.log(getStats);
+
+function writeStats() {
+  const allstats = getStats();
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
