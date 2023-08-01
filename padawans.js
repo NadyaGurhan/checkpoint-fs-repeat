693ddccbfd9ef006f792fcaba8565c6cb01d9fa4
MiKhail <mihailevsyukov@mail.ru>
2023-08-01 10:21:02 +0300

    -

--- padawans.js
+++ padawans.js
@@ -1,7 +1,20 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
+  function getPadawanNames() {
+   const names = getPadawanNames(fs.readFileSync(".data/padawans.txt", "New text"););
+   return $`[names]`;
+}
+  function getLightsaberScores() {
+    const scores = getLightsaberScores(fs.readFileSync("data/scores.txt", "utf8");)
+    return $`[scores]`;
+  }
+ function getStats(){
+  const names = getStats(fs.readFileSync(".data/padawans.txt", "New text");
+  const scores = getLightsaberScores(fs.readFileSync("data/scores.txt", "utf8");)
+  const stats = names.push('scores')
+  // Полагаю, что здесь следует использовать цикл, но сделать уже не успеваю)
+
+ }
   writeStats,
-};
+}
