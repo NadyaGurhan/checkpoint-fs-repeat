7bb222a45034f855f340729154dfc320c55fb358
IgorOkK <e-barista@yandex.ru>
2023-08-01 12:35:51 +0300

    finished

--- padawans.js
+++ padawans.js
@@ -1,4 +1,12 @@
 const fs = require('fs')
+function getPadawanNames(){
+  const name =[];
+  const quary = 'select name from padawans.txt'
+  const result = database.quary(quary);
+  while(result.next()){
+    
+  }
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
