9891b21f31b6ea28c38667903b92dee4ff42d108
darya <atomicleda@mail.ru>
2023-08-01 14:28:44 +0300

    chek-fs-2

--- padawans.js
+++ padawans.js
@@ -2,24 +2,31 @@ const fs = require('fs');
 
 function getPadawanNames() {
   const names = fs.readFileSync('data/padawans.txt').toString('utf8');
-  return names.split(',');
+  return names.split('\n');
 }
 
 function getLightsaberScores() {
-  const scores = fs.readFileSync('data/scores.txt').toString('utf8');
-  return scores.split(',');
+  const scores = fs.readFileSync('data/scores.txt', 'utf8');
+  return scores.split('\n').map(Number);
 }
 
 function getStats() {
   const array1 = getPadawanNames();
   const array2 = getLightsaberScores();
-  const newArray = array1.concat(array2);
-  const newText = newArray.join(',');
-  fs.writeFileSync('data/stats.txt', newText);
+  const newArray = array1.map((value, index) => [value, array2[index]]);
+
+  return newArray;
+}
+
+function writeStats() {
+  const dataF = getStats();
+  const newInfo = fs.writeFileSync('data/stats.txt', dataF, 'utf8');
+  return newInfo;
 }
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
+  writeStats,
 };
