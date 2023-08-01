3e0f3d494bdfc905d244612eda92adbd3846f7d6
darya <atomicleda@mail.ru>
2023-08-01 10:12:13 +0300

    check fs

--- padawans.js
+++ padawans.js
@@ -1,27 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('data/padawans.txt').toString('utf8');
+  return names.split(',');
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('data/scores.txt').toString('utf8');
+  return scores.split(',');
+}
+
+function getStats() {
+  const array1 = getPadawanNames();
+  const array2 = getLightsaberScores();
+  const newArray = array1.concat(array2);
+  const newText = newArray.join(',');
+  fs.writeFileSync('data/stats.txt', newText);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
 };
-
-
-const names = fs.readFileSync('./data/padawans.text');
-getPadawanNames () {
-  const arrNames = names.split("\n")
-  return arrNames;
-};
-
-
-const scores = fs.readFileSync('./data/scores.text');
-getLightsaberScores () {
-  const getScores = scores.split("\n");
-  return getScores;
-};
-
-getStats () {
-  const namesP = getPadawanNames();
-  const scoresP = getLightsaberScores();
-  const stats = namesP.map(scoresP) 
-}
