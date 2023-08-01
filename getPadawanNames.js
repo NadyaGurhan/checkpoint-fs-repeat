78df534b69f94caa69d2065b6f28f4eab06927a7
root <root@DESKTOP-K9JPN4F.localdomain>
2023-08-01 11:17:21 +0300

    end

--- getPadawanNames.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const filePath = './data/padawans.txt';
-
-function getPadawanNames() {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const fullNames = fileContent.split('\n').map((name) => name.trim());
-    return fullNames;
-}
-module.exports = getPadawanNames;
--- padawans.js
+++ padawans.js
@@ -7,8 +7,15 @@ module.exports = {
 };
 
 function getPadawanNames() {
-  const file = './data/padawans.txt'
-  const fileContent = fs.readFileSync(file, 'utf8');
-  const result = fileContent.split('\n');
-  return result;
+  const fileNames = './data/padawans.txt';
+  const fileContent1 = fs.readFileSync(fileNames, 'utf8');
+  const result1 = fileContent1.split('\n');
+  return result1;
+}
+
+function getLightsaberScores() {
+  const fileLights = './data/scores.txt';
+  const fileContent2 = fs.readFileSync(fileLights, 'utf8');
+  const result2 = fileContent2.split('\n');
+  return result2;
 }
