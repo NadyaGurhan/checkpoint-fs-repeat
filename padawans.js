e375297ba97e603d26334c57a33294f37cb02432
Home <home@MacBook-Air-Ekaterina.local>
2023-08-01 10:16:17 +0300

    было бы чуть больше времени)

--- padawans.js
+++ padawans.js
@@ -5,3 +5,29 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  padawans.pop();
+  return padawans;
+};
+getPadawanNames();
+
+function getLightsaberScores() {
+  const spec = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  const padawan = getPadawanNames()
+  
+  console.log(padawan)
+  // for (let i = 0; i < spec.length; i++){
+
+  // }
+};
+getLightsaberScores();
+
+function getStats(){
+
+};
+
+function writeStats (){
+
+};
