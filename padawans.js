3272ea173bbf75c3d0a388b4cee45f699e22b25e
IvanonVadim <xsagax@yandex.ru>
2023-08-22 10:28:14 +0300

    checkp1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,15 @@
 const fs = require('fs')
+
+ 
+
+ function getPadawanNames () {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split('\r\n').slice(0,4);
+  
+ }
+ console.log (getPadawanNames())
+ const getLightsaberScores = '';
+ const getStats = '';
+ const writeStats = '';
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
