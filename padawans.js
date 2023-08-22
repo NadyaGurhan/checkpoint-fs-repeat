c9ea9de5839d57579c618691721f628b0580e4d4
MukhammadSalyaf17 <muhammedalikotikov@gmail.com>
2023-08-22 10:17:07 +0300

    cheakpoint p1w1d2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,33 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+
+function getPadawanNames() {
+  const fileRead = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n');
+  return fileRead;
+}
+
+function getLightsaberScores() {
+  const array = [];
+  const fileReadScores = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  for (let i = 0; i < fileReadScores.length; i += 1) {
+    array.push(fileReadScores[i] * 1);
+  }
+  return array;
+}
+
+function getStats() {
+  const arr = [];
+  const getNames = getPadawanNames();
+  const getScores = getLightsaberScores();
+
+  for (let i = 0; i < getScores.length; i += 1) {
+    arr.push([getNames[i], getScores[i]]);
+  }
+  return arr;
+}
