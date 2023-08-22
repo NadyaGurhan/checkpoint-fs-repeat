87e1d018e2243bcfef1bb0f391b539359894ca26
HaritonovEvgenyi <luckydjone@inbox.ru>
2023-08-22 10:14:12 +0300

    badChek

--- padawans.js
+++ padawans.js
@@ -5,3 +5,8 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+const array = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+console.log(array);
+
+"ниче не понял, и я и соседи, обратная связь когда будет - я наверно уже забуду что тут было и она будет без кпд"
