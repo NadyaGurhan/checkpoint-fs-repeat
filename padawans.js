bff0038e3bc6af0592b132b6a8189b266d9a0f6a
Разият <raziat@MacBook-AirRaziyat.local>
2023-08-22 10:15:02 +0300

    Work

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getNames() {
+const arr = 
+}
+console.log(getNames);
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+// Я поняла, что нужно найти ошибку в тесте и исправить ее. Проблема в том, что я даже не знаю как начать. Я запустила npm test, 
+//увидела ошибку, а что дальше делать я не знаю. 
