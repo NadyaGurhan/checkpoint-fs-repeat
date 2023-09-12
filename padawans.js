ac3057d8263f02a51148fbaa97bfd6ae1012800a
Artyom Savenkov <orgtaktik@yandex.ru>
2023-09-12 10:01:01 +0300

    failed at all :(

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  // const arr = [];
+  const list = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const nameList = list.split('\n').splice(-1);
+  console.log(nameList);
+
+  return nameList;
+  
+}
+
+
+function getLightsaberScores() {
+  // const arr = [];
+  const list = fs.readFileSync('./data/scores.txt', 'utf8');
+  const scoreList = list.split('\n');
+  
+  console.log(scoreList);
+
+  return arr.push(scoreList);
+
+};
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
