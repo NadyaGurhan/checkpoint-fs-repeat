b46689b6c53a68a938bd3dbc21442960113a691b
HaritonovEvgenyi <luckydjone@inbox.ru>
2023-09-12 09:59:50 +0300

    3-4

--- padawans.js
+++ padawans.js
@@ -1,4 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const getPad = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+
+  return getPad;
+}
+
+function getLightsaberScores() {
+  const read = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+
+  return read;
+}
+
+function getStats() {
+  const getPad = getPadawanNames();
+  const read = getLightsaberScores();
+  const arr = [];
+  for (let i = 0; i < getPad.length; i++) {
+    arr.push([getPad[i], read[i]]);
+  }
+  return arr;
+}
+
+function writeStats() {
+
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
