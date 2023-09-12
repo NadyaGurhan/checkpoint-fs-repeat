b2df3b5e7444a00df82e78ffbfe64f79f385bb72
Sergei Komarov <komarovs87@mail.ru>
2023-09-12 10:12:35 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,36 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+const path = require('path');
+const dirname = path.join(__dirname);
+const padawans = fs
+  .readFileSync(`${dirname}/data/padawans.txt`, 'utf-8')
+  .split(EOL);
+const scores = fs
+  .readFileSync(`${dirname}/data/scores.txt`, 'utf-8')
+  .split(EOL)
+  .map((elem) => Number(elem));
+function getPadawanNames() {
+  return padawans;
+}
+function getLightsaberScores() {
+  return scores;
+}
+function getStats() {
+  let newArr = [];
+  for (let i = 0; i < padawans.length; i += 1) {
+    let arr = [];
+    arr.push(padawans[i], scores[i]);
+    newArr.push(arr);
+  }
+  return newArr;
+}
+function writeStats() {
+  let stats = getStats()
+    .map((elem) => elem.join(' '))
+    .join(EOL);
+  return fs.writeFileSync(`${dirname}/data/stats.txt`, stats);
+}
+writeStats();
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
