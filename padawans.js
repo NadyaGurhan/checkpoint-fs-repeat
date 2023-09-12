6a1822adc19ee01587fa59584b82f3e16bc5b04d
Vallydei <obedkova.v@rambler.ru>
2023-09-12 10:00:20 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,44 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
+function getPadawanNames() {
+  let getNames = fs.readFileSync(__dirname + "/data/padawans.txt", {
+    encoding: "utf-8",
+  });
+  const padavans = getNames.split("\n").slice(0, -1);
+
+  return padavans;
+}
+
+function getLightsaberScores() {
+  let getScores = fs.readFileSync(__dirname + "/data/scores.txt", {
+    encoding: "utf-8",
+  });
+  const scores = getScores.split("\n");
+  const scoresNum = scores.map((elem) => Number(elem));
+
+  return scoresNum;
+}
+
+function getStats() {
+  let names = getPadawanNames();
+  let scores = getLightsaberScores();
+  let stat = [];
+  for (let i = 0; i < names.length; i += 1) {
+    stat.push([names[i], scores[i]]);
+  }
+  return stat;
+}
+
+let path = `data/stats.txt`
+function writeStats(path, stat) { 
+  if (!stat && !path && !stat.length) return;
+  fs.writeFileSync(path, stat); 
+}
+
