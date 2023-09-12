db06ba2f131d3044143a73c41c2b9a58d236e334
Irina <i.med.torr@gmail.com>
2023-09-12 12:38:51 +0300

    Release1.1

--- padawans.js
+++ padawans.js
@@ -7,8 +7,10 @@ module.exports = {
 };
 
 const padawans = fs.readFileSync("data/padawans.txt", "utf-8").split("\n");
-const scores = fs.readFileSync("data/scores.txt", "utf-8").split("\n").map((el) => Number(el));
-
+const scores = fs
+  .readFileSync("data/scores.txt", "utf-8")
+  .split("\n")
+  .map((el) => Number(el));
 
 function getPadawanNames() {
   return padawans.filter((el) => el.length > 0);
@@ -20,14 +22,17 @@ function getLightsaberScores() {
 
 function getStats() {
   const res = [];
+  let temp = [];
 
   for (let i = 0; i < scores.length; i += 1) {
-    res.push([padawans[i], scores[i]]);
+    temp.push(padawans[i], scores[i]);
+    res.push(temp);
+    temp = [];
   }
 
   return res;
-};
+}
 
 function writeStats() {
-
+  return fs.writeFileSync('data/stats.txt', getStats().join('\n').replaceAll(',', ' '));
 }
