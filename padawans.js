1b246d9b238f3ee7bd4efb98fca81c4ac9b682d8
mqki1 <137097102+mqki1@users.noreply.github.com>
2023-09-12 09:57:44 -0700

    done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf8');
+  return data.trim().split('\n');
+}
+getPadawanNames();
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf8');
+  return data.split('\n').map(Number);
+}
+getLightsaberScores();
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  return names.map((name, index) => [name, scores[index]]);
+}
+getStats();
+ writeStats() {
+  fs.writeFileSync('data/stats.txt', 'utf-8')
+
+ }
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+ writeStats,
 };
