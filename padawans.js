f0cee3d222d7f6d64affcb5165434f00952c4b16
IvanMinich <vane4ko@gmail.com>
2023-09-12 09:57:46 +0300

    3/4

--- padawans.js
+++ padawans.js
@@ -1,4 +1,49 @@
-const fs = require('fs')
+const { log } = require("console");
+const fs = require("fs");
+const dataScore = fs.readFileSync(__dirname + "/data/scores.txt", {
+  encoding: "utf-8",
+});
+
+const scoreArr = dataScore.split(" ");
+
+function getPadawanNames() {
+  const data = fs.readFileSync(__dirname + "/data/padawans.txt", {
+    encoding: "utf-8",
+  });
+  const padawanArr = data.trim().split('\n');
+  const namesArr = [];
+  padawanArr.forEach((element) => {
+    namesArr.push(element);
+  });
+  return namesArr;
+}
+function getLightsaberScores(){
+  const data = fs.readFileSync(__dirname + "/data/scores.txt", {
+    encoding: "utf-8",
+  });
+  const Arr = data.trim().split('\n');
+  const csoresArr = [];
+  Arr.forEach((element) => {
+    csoresArr.push(+element);
+  });
+  return csoresArr;
+}
+
+function getStats(){
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  let arr = [];
+  let res = []
+  for (let i = 0; i<padawans.length; i++){
+    arr.push(padawans[i], Number(scores[i]));
+    res.push(arr);
+    arr = [];
+  }
+  return res
+}
+function writeStats(){
+
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
