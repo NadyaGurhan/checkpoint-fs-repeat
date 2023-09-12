97c52d7e9f73147290890d365ecdefe196e40882
IlonaKorosteleva <ilona-korosteleva@mail.ru>
2023-09-12 09:57:30 +0300

    чек поинт

--- padawans.js
+++ padawans.js
@@ -1,4 +1,21 @@
 const fs = require('fs')
+function getPadawanNames(){
+  const data = fs.readFileSync('data/padawans.txt', 'utf8');
+  return data.split('\n').filter(name => name.trim() !== '');
+}
+getLightsaberScores(){
+  const data = fs.readFileSync('data/scores.txt', 'utf8');
+  const scores = data.split('\n').map(score => parseFloat(score));
+  return scores;
+}
+getStats(){
+  const padawanNames = getPadawanNames();
+  const lightsaberScores = getLightsaberScores(); 
+  const stats = padawanNames.map((name, index) => [name, lightsaberScores[index]]);
+  return stats;
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
