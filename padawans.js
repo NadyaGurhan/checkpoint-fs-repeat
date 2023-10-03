63c2cadd93fa6f9fd72558c502fad6fab4303d2b
Kretov222 <Kretov222@yandex.ru>
2023-10-03 10:06:53 +0300

    check 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames(){
+  return fs.readFileSync ('./data/padawans.txt','utf8').split('\n');
+  
+
+};
+
+function getLightsaberScores(){
+
+};
+
+
+function getStats(){
+
+};
+
+function writeStats(){
+
+};
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
