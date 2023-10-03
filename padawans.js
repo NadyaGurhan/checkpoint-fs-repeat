93e46790481bc41251bc53ebf1788225a454623d
EvPir <evpirozh@rambler.ru>
2023-10-03 10:09:39 +0300

    p1w1d2 checkpoint-fs

--- padawans.js
+++ padawans.js
@@ -2,6 +2,19 @@ const fs = require('fs')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+function getPadawanNames() {
+  const names = fs.readFileSync("./data/padawans.txt", "utf8").split('\r\n');
+  return names;
+}
+console.log(getPadawanNames())
+
+
+function getLightsaberScores() {
+  const grades = fs.readFileSync("data/scores.txt", "utf8").split('\r\n');
+  return grades;
+}
+console.log(getLightsaberScores())
