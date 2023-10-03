85c98435554c60a406257f3f119e28fa14f24464
Alish1993 <ali.sarin.93@mail.ru>
2023-10-03 10:06:49 +0300

    checkpoint1

--- padawans.js
+++ padawans.js
@@ -5,3 +5,31 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const filePath = 'data/padawans.txt';
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const names = fileContent.split('\n').map(name => name.trim());
+  return names;
+};
+
+function getLightsaberScores() {
+  const filePath = 'data/scores.txt';
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const scores = fileContent.split('\n').map(score => parseFloat(score.trim()));
+  return scores;
+};
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const stats = names.map((name, index) => [name, scores[index]]);
+  return stats;
+};
+
+function writeStats(stats) {
+  const filePath = 'data/stats.txt';
+  const data = stats.map(([name, score]) => `${name} ${score}`).join('\n');
+  fs.writeFileSync(filePath, data);
+};
+
