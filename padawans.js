a9e25264ee68cb9ace75ca8a0a4984caffb46f09
dustern <ldusternl@gmail.com>
2023-10-03 10:07:22 +0300

    checkpoint1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
+  getPadawanNames() {
+    const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf8');
+    return padawanNames.split(' ');
+  },
+  getLightsaberScores() {
+    return fs.readFileSync('./data/score.txt', 'utf8');
+  },
   getStats,
   writeStats,
 };
