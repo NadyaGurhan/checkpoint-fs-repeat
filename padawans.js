f2f4cae33c537661f05b47dd9f03569953791e55
viktoria <viktoria_19_1@mail.ru>
2023-10-03 10:09:47 +0300

    Chekpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
 const fs = require('fs')
+
+function getPadawanNames(){
+  let filePath = './data/padawans.txt';
+  let data = fs.readFileSync(filePath, "utf8");
+  return data.split('\n')
+}
+getPadawanNames();
+
+
+function getPadawanScores(){
+
+}
+
+function getLightsaberScores(){
+  
+}
+
+function getStats (){
+
+}
+
+function writeStats (){
+
+}
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
