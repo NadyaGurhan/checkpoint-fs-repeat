e3b518a8927e0cb0985bbde3c4fdb64ae99c485b
shout68 <alexey@DESKTOP-MFK1GN3.localdomain>
2023-10-03 10:09:11 +0300

    releas1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,39 @@
 const fs = require('fs')
+
+const getPadawanNames = () => {
+  const res = fs.readFileSync("./data/padawans.txt", 'utf-8').split("\n")
+  return res
+  
+  
+};
+
+
+const getLightsaberScores = () => {
+ const res1 = fs.readFileSync("./data/scores.txt", "utf-8").split('\n')
+ const test = []
+ for(let i = 0;i<res1.length; i++){
+  test.push(parseInt(res1[i]))
+ }
+
+ return test
+}
+const getStats = () => {
+
+}
+const writeStats = () => {
+
+}
+
+
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
