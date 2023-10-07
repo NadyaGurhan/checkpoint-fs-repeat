e33cd912734539638ac81948467267b79dceec70
viktoria <viktoria_19_1@mail.ru>
2023-10-07 13:36:47 +0300

    Checkpont

--- padawans.js
+++ padawans.js
@@ -1,30 +1,40 @@
-const fs = require('fs')
+const fs = require('fs');
 
-function getPadawanNames(){
-  let filePath = './data/padawans.txt';
-  let data = fs.readFileSync(filePath, "utf8");
-  return data.split('\n')
+function getPadawanNames() {
+  const filePath = './data/padawans.txt';
+  const data = fs.readFileSync(filePath, 'utf8');
+  return data.split('\n');
 }
 getPadawanNames();
 
+function getLightsaberScores() {
+  const filePath1 = './data/scores.txt';
+  const data1 = fs.readFileSync(filePath1, 'utf8');
+  const newArr = data1.split('\n');
+  for (let i = 0; i < newArr.length; i++) {
+    newArr[i] = Number(newArr[i]);
+  }
+  return newArr;
+}
+getLightsaberScores();
 
-function getPadawanScores(){
-
+function getStats() {
+  const arr1 = getPadawanNames();
+  const arr2 = getLightsaberScores();
+  const array = [];
+  for (let i = 0; i < arr1.length; i++) {
+    array.push([arr1[i], arr2[i]]);
+  }
+  return array;
 }
 
-function getLightsaberScores(){
-  
+function writeStats() {
+  const arr = getStats();
+  const newstr = arr.join('\n').replaceAll(',', ' ');
+  console.log(newstr);
+  fs.writeFileSync('./data/stats.txt', newstr);
 }
-
-function getStats (){
-
-}
-
-function writeStats (){
-
-}
-
-
+console.log(writeStats());
 
 module.exports = {
   getPadawanNames,
