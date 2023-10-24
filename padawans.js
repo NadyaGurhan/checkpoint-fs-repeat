0d13e8e85f66cff9f64d5564f5d86cacac90645a
viktoria <viktoria_19_1@mail.ru>
2023-10-24 09:57:17 +0300

    fs

--- padawans.js
+++ padawans.js
@@ -1,7 +1,34 @@
 const fs = require('fs')
+
+function getPadawanNames(){
+  const file = fs.readFileSync('./data/padawans.txt', 'utf8')
+  return file.split('\n')
+}
+
+function getLightsaberScores(){
+  const file = fs.readFileSync('./data/scores.txt', 'utf8')
+  const arr = file.split('\n')
+  return arr.map((el) => Number(el))
+}
+
+function getStats(){
+  const file1 = getPadawanNames()
+  const file2 = getLightsaberScores()
+  const arr = []
+  for(let i = 0; i < file1.length; i++){
+    arr.push([file1[i], file2[i]])
+  }
+  return arr
+}
+
+function writeStats(){
+  const stats = getStats()
+  fs.writeFileSync('./data/stats.txt',stats.join('\n').replaceAll(',', ' '),  'utf8')
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+   writeStats,
 };
