47dc75d225de70825758ebb17f82f88705be344a
0racl <oleg.sir-tokarev@yandex.ru>
2023-10-24 10:16:57 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync("./data/padawans.txt", "utf-8");
+  return names.split("\n");
+}
+function getLightsaberScores() {
+  
+}
+
+// function getStats() {
+//   const names = getPadawanNames();
+//   const scores = getLightsaberScores();
+//   const stats = names.map((name, index) => [name, scores[index]]);
+//   return stats;
+// }
+
+// function writeStats(stats) {
+//   const statsString = stats.map(([name, score]) => `${name} ${score}`).join('\n');
+//   fs.writeFileSync('data/stats.txt', statsString);
+// }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
