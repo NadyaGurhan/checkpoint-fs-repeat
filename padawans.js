05488a6bdd3d12404779c43c39d3fa62278567cb
andrey <andrey.burlyaev@gmail.com>
2023-10-24 10:15:12 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
+
+
+
+function getPadawanNames() {
+  const result = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  
+  return result.split('\n').pop()
+
+}
+
+function getLightsaberScores() {
+
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
