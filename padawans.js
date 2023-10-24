298f40f4601a9d7d419c1b83e70a00a96612100b
nat <natalbka.p92@gmail.com>
2023-10-24 10:09:19 +0300

    releaseNew

--- padawans.js
+++ padawans.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
@@ -6,28 +6,35 @@ module.exports = {
   writeStats,
 };
 
-
-function getPadawanNames () {
-  
-  let names = fs.readFileSync("./data/padawans.txt", "utf8");
-  console.log (names);
-  
-  let arr = names.join("\n");
-  
-
-
+function getPadawanNames() {
+  const fileName = "./data/padawans.txt";
+  const namesPadavan = fs.readFileSync(fileName, "utf8");
+  return namesPadavan.split("\n").slice(0, -1);
 }
-
-  
-
 
 function getLightsaberScores() {
-
+  const fileName = "./data/scores.txt";
+  const scorePadavan = fs.readFileSync(fileName, "utf8");
+  return scorePadavan.split("\n").map((el) => +el);
 }
 
-function getStats () {
-
+function getStats() {
+  const name = getPadawanNames();
+  const score = getLightsaberScores();
+  let arr = [];
+  const result = [];
+  for (let i = 0; i < name.length; i++) {
+    arr.push(name[i], score[i]);
+    result.push(arr);
+    arr = [];
+  }
+  return result;
 }
-function writeStats () {
-  
+function writeStats() {
+  const data = getStats().join("\n");
+  //console.log(data);
+  const replace = data.replaceAll(",", " ");
+  //console.log(replace);
+  const fileName = "data/stats.txt";
+  fs.writeFileSync(fileName, replace, "utf-8");
 }
