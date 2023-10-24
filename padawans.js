d7f2d159202499d2bce186f0b869ef997a63e6ae
winnig181 <otiumpost@yandex.ru>
2023-10-24 10:17:35 +0300

    wip

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
 const fs = require('fs')
+const data = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf8');
+const data1 = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf8');
+
+function getPadawanNames() {
+  return data.split('\n').slice(0,-1);
+}
+
+function getLightsaberScores() {
+  const res = data1.split('\n');
+  const resF = res.map((el)=>Number(el));
+  return (resF);
+}
+
+function getStats() {
+  const arr = data.split('\n');
+  const arr1 = data1.split('\n');
+  return ();
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
