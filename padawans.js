603cdd2bec7915d94cbf172c710c0bda1378fcd7
Dmitriy <dim0on@mail.ru>
2023-10-24 10:15:43 +0300

    50%

--- padawans.js
+++ padawans.js
@@ -1,7 +1,46 @@
-const fs = require('fs')
+
+const fs = require('fs');
+
+const allPadawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+
+function getPadawanNames() {
+  padsArray = allPadawans.split('\n');
+  padsArray.pop();
+  return padsArray;
+}
+
+const padawansScores = fs.readFileSync('./data/scores.txt', 'utf8');
+
+function getLightsaberScores() {
+  scoreArray = padawansScores.split('\n');
+  const scoreNumbers = []
+  scoreArray.forEach((element) => {
+    scoreNumbers.push(Number(element));
+  });
+  return scoreNumbers;
+}
+
+function getStats(){
+  
+  const arrOfPad = getPadawanNames()
+  const arrOfSpecs = getLightsaberScores()
+  const allStats = []
+
+for (let i = 0; i < arrOfPad.length; i++){
+ 
+let newEl = [arrOfPad[1], arrOfSpecs[1]]
+  
+   console.log (newEl)
+}
+
+  const createFile = fs.writeFileSync('./data/scores.txt', allStats,  'utf8')
+
+}
+getStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+//   getStats,
+//   writeStats,
 };
