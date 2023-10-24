2f218f25f87d9e0c0456fd4eac2e32b7f953bb42
Tseren108 <tseren108@gmail.com>
2023-10-24 10:18:40 +0300

    Chekpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,30 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+return(fs.readFileSync('./data/padawans.txt', 'utf8').trim().split(('\n')))
+
+}
+
+
+function getLightsaberScores() {
+return(fs.readFileSync('./data/scores.txt', 'utf8'))
+
+}
+
+
+function getStats() {
+
+
+}
+
+const fs = require('fs');
+function writeStats(stats, path) {
+fs.writeFileSync(path, stats, 'utf8')
+writeStats(stats.join('\n'), './data/stats.txt');
+
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
