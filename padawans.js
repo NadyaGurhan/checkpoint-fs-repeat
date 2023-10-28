24d1d7959eb3663a2d1747110eb23cbaca591925
Alexander Samarin <146456270+samarskey@users.noreply.github.com>
2023-10-28 16:16:04 +0300

    completed

--- padawans.js
+++ padawans.js
@@ -1,49 +1,56 @@
-const fs = require('fs')
+const fs = require('fs');
 
-
-function getPadawanNames(){
-  const padawanList = fs.readFileSync('data/padawans.txt', 'utf8').split('\n')
-  let padawans = []
-  for (let i = 0; i<padawanList.length; i++){
-    if (padawanList[i]){
-      padawans.push(padawanList[i])
+function getPadawanNames() {
+  const padawanList = fs.readFileSync('data/padawans.txt', 'utf8')
+    .split('\n');
+  let padawans = [];
+  for (let i = 0; i < padawanList.length; i++) {
+    if (padawanList[i]) {
+      padawans.push(padawanList[i]);
     }
   }
-  return padawans
+  return padawans;
 }
-let arr1 = getPadawanNames()
-console.log(getPadawanNames())
 
-function getLightsaberScores(){
-  const swordsScores = fs.readFileSync('data/scores.txt', 'utf8')
-  const freshData = swordsScores.split('\n')
-  const lightSaberScore = Array.from(freshData)
-  return lightSaberScore
+function getLightsaberScores() {
+  const swordsScores = fs.readFileSync('data/scores.txt', 'utf8');
+  const freshData = swordsScores.split('\n');
+  let abc = freshData.map((el) => (Number(el)));
+  return abc;
 }
-let arr2 = getLightsaberScores()
-console.log(getLightsaberScores())
 
-// function getStats(arr1, arr2){
-//   let stats = []
-//   for (let i = 0; i<arr1.length; i++)
-//     stats.push(arr1[i]+" "+arr2[i])
-//     if (arr1[i]===undefined||arr2[i]===undefined){
-//       stats.push('over9000')
-//     }
-//   return stats
-// }
-// console.log(getStats(arr1,arr2))
+function getStats() {
+  let newArr = [];
+  let arr1 = getPadawanNames();
+  let arr2 = getLightsaberScores();
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] && arr2[i]) {
+      newArr.push([arr1[i]]);
+      newArr[i].push(arr2[i]);
+    }
+  }
+  return newArr;
+}
 
-// let stats = writeStats(arr1,arr2)
-// function writeStats(data, path){
-//   fs.writeFileSync('./data/stats.txt', getStats())
-// }
+// console.log(getStats());
+
+function writeStats(path, data) {
+  let newData = getStats();
+  let abc = []
+  for (let i = 0; i < newData.length; i++) {
+    newData[i] = newData[i].join(' ')
+    abc.push(newData[i])
+  }
+  // return abc
+  fs.writeFileSync('./data/stats.txt', abc.join('\n'));
+}
+// console.log(writeStats())
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  // getStats,
-  // writeStats,
+  getStats,
+  writeStats,
 };
 
 
