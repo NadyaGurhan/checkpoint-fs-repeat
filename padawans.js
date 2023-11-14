917322901325089a20768e35e480bf907546e8c8
MayONez98 <MayONez98@gmail.com>
2023-11-14 10:26:18 +0300

    chekpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,14 @@
-const fs = require('fs')
+const fs = require('fs');
+const { get } = require('http');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+f
+const padawansNames = "./data/padawans.txt"
+let data = fs.readFileSync(getPadawanNames, 'utf-8')
+console.log(data)
+console.log(padawansNames)
