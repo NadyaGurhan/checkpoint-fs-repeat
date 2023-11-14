335d963e23dd5c491850b5bbc9b7ab71b05e637c
Nikolas81 <riazanovna@mail.ru>
2023-11-14 08:34:38 +0300

    CHECKPOINT-FS

---
+++ spec/getPadawanNames.js
@@ -0,0 +1,7 @@
+function getPadawanNames() {
+    let filePath = "./padawans.txt";
+    newArr = [];
+    newArr.split(filePath);
+}
+console.log(getPadawanNames);
+module.exports = {getPadawanNames};
