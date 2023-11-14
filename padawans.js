9ebe7c355491ef9d6af26e033d92bd0f5e19154f
starikova-a <a.starikova.ru@gmail.com>
2023-11-14 10:15:48 +0300

    Отчаянно мало времени

--- padawans.js
+++ padawans.js
@@ -2,6 +2,23 @@ const fs = require('fs')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  //getStats,
+  //writeStats,
 };
+
+function getPadawanNames(){
+  let name = fs.readFileSync("./data/padawans.txt", "utf8")
+  return name.push()
+}
+console.log(getPadawanNames())
+
+
+function getLightsaberScores(){
+  return fs.readFileSync("./data/scores.txt", "utf8")
+}
+console.log(getLightsaberScores())
+
+
+// function getStats(){
+// for (let i=0; i<)
+// }
