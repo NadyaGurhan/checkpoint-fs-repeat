dda492fe17332433f1535e2fac4243326d862f46
IlETONI <letoni1996@gmail.com>
2023-11-14 10:01:51 +0300

    fuction 1-2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,35 @@
-const fs = require('fs')
+const fs = require("fs");
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const data = fs.readFileSync("./data/padawans.txt", "utf8");
+  const arr = data.split("\n");
+  let resultArr = [];
+  arr.forEach((el) => {
+    if (el.length > 0) {
+      resultArr.push(el);
+    }
+  });
+  return resultArr;
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync("./data/scores.txt", "utf8");
+  const arr = data.split("\n");
+  const resultArr = [];
+
+  arr.forEach((el) => {
+    resultArr.push(Number(el));
+  });
+
+  return resultArr;
+}
+
+function getStats() {}
+
+function writeStats() {}
