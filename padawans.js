de2e90dd2c685a7bab5c4ba1cfea9ca3186eb831
cybereg0r <uk-one@mail.ru>
2023-11-14 10:04:53 +0300

    1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,27 @@
 const fs = require('fs')
+
+
+function getPadawanNames() {
+  const filePath = 'data/padawans.txt'
+  const arr = []
+  let data = fs.readFileSync(filePath, 'utf-8')
+  arr.push(data)
+    return arr
+}
+
+function getLightsaberScores() {
+  const filePath = 'data/scores.txt'
+  const arr = []
+  let data =
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
