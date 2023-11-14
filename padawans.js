33c2c5ff5db9ff816cd33c5fb1ac8e0a4906082a
EgorSmelov <smelov909@mail.ru>
2023-11-14 10:16:16 +0300

    check1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,18 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+  return padawanNames;
+}
+
+function getLightsaberScores() {
+  const saberScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+  return saberScores;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
