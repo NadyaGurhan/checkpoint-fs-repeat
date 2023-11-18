208fce82961b052c90d9f8d20eb65046cf93f7c0
EgorSmelov <smelov909@mail.ru>
2023-11-18 12:18:24 +0300

    done

--- padawans.js
+++ padawans.js
@@ -7,12 +7,33 @@ function getPadawanNames() {
 
 function getLightsaberScores() {
   const saberScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
-  return saberScores;
+  const arr = [];
+  for (let i = 0; i < saberScores.length; i += 1) {
+    arr.push(Number(saberScores[i]));
+  }
+  return arr;
+}
+
+function getStats() {
+  const padawanNames = getPadawanNames();
+  const saberScores = getLightsaberScores();
+  const arrScores = [];
+  for (let i = 0; i < saberScores.length; i += 1) {
+    arrScores.push([padawanNames[i], saberScores[i]]);
+  }
+  return arrScores;
+}
+
+function writeStats() {
+  const stats = getStats();
+  const str = stats.map((el) => el.join(' ')).join('\n');
+  const data = fs.writeFileSync('data/stats.txt', str);
+  return data;
 }
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  // getStats,
-  // writeStats,
+  getStats,
+  writeStats,
 };
