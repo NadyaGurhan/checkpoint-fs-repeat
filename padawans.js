48e54152c885edfb98fe6fae4876b24997b23bdb
Ayana888 <abbolotova2024@gmail.com>
2023-11-20 23:45:58 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,55 @@
 const fs = require('fs')
+const { EOL } = require('os');
+
+
+function getPadawanNames() {
+      const names = fs.readFileSync(`./data/padawans.txt`, 'utf-8').trim().split(EOL);
+      return names;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync(`./data/scores.txt`, 'utf-8').split(EOL);
+  for (let i = 0; i < scores.length; i++) {
+    scores[i] = +scores[i];
+  }
+  return scores;
+}
+
+function getStats() {
+  const newArr = [];
+  const padawanNames = getPadawanNames();
+  const LightsaberScores = getLightsaberScores();
+  for (let i = 0; i < padawanNames.length; i++) {
+  const newArrAll = [];
+  const name = padawanNames[i];
+  const scor = LightsaberScores[i];
+
+  newArrAll.push(name);
+  newArrAll.push(scor);
+  newArr.push(newArrAll);
+  }
+  return newArr;
+}
+
+function writeStats(stats) {
+  stats.forEach((stat) => {
+    fs.appendFile(``)
+  });
+  //console.log(stats.join(EOL));
+//const scores = fs.readFileSync('data/stats.txt', 'utf8').split(EOL);
+
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
+        
+
+
+
