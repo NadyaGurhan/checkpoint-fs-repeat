f504c90c3546dcfe7ef926c19fd8b9cfec2c0e6d
Rusla Shmelev <ruslan_developer96@mail.ru>
2023-12-05 12:06:02 +0300

    check point

--- padawans.js
+++ padawans.js
@@ -1,4 +1,40 @@
-const fs = require('fs')
+const fs = require("fs");
+const path = require("path");
+
+const dataFolderPath = path.join(__dirname, "data");
+
+const fileOne = fs.readFileSync(
+  path.join(dataFolderPath, "padawans.txt"),
+  "utf8"
+);
+const fileTow = fs.readFileSync(
+  path.join(dataFolderPath, "scores.txt"),
+  "utf8"
+);
+let names;
+function getPadawanNames() {
+  names = fileOne.split("\n").slice(0, -1);
+  return names;
+}
+console.log(getPadawanNames());
+let score;
+function getLightsaberScores() {
+  score = fileTow.split("\n");
+  return score.map((ele) => +ele);
+}
+console.log(getLightsaberScores());
+
+function getStats() {
+  let arr = [];
+  for (let i = 0; i < names.length; i++) {
+    arr.push([names[i], score[i]]); // не успел приобразовать score
+  }
+  return arr;
+}
+
+console.log(getStats());
+
+function writeStats() {}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
