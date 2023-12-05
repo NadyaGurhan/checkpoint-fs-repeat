4842117c5f89a9639f61488bde1e04e9149391b7
matv88r <matv66rd@gmail.com>
2023-12-05 09:48:49 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,36 @@
-const fs = require('fs')
+const fs = require("fs");
+const path = require("path");
+
+const getPadawanNames = () => {
+  let names = fs
+    .readFileSync(path.join(__dirname, "./data/padawans.txt"), "utf-8")
+    .split("\n");
+  return names;
+};
+
+const getLightsaberScores = () => {
+  let scores = fs
+    .readFileSync(path.join(__dirname, "./data/scores.txt"), "utf-8")
+    .split("\n");
+  return scores.map((el) => Number(el));
+};
+
+const getStats = () => {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  let arr = [];
+  for (let i = 0; i < padawans.length; i++) {
+    arr.push([padawans[i], scores[i]]);
+  }
+  return arr;
+};
+
+let stats = getStats();
+const writeStats = (stats) => {
+  let str = stats.join("\n").replaceAll(",", " ");
+  fs.writeFileSync(path.join(__dirname, "./data/stats.txt"), str);
+};
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
