221abf17446de2fe6648013b32f5107aed2500c1
Mark <mark@MacBook-Pro-Mark.local>
2023-12-05 11:58:42 +0300

    release 2/4

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
 const fs = require('fs')
+const path = require('path')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+function getPadawanNames () {
+  const fileName = './padawans.txt';
+  const pathToFile = path.join(__dirname, 'data', fileName);
+  const readMe = fs.readFileSync(pathToFile, 'utf-8').trim();
+  const arr = readMe.split('\n')
+  return arr;
+};
+getPadawanNames();
+
+function getLightsaberScores () {
+  const fileName = ('./scores.txt');
+  const pathToFile = path.join(__dirname, 'data', fileName)
+  const readMe = fs.readFileSync(pathToFile, 'utf-8').trim()
+  const arr = readMe.split('\n').map(Number)
+  return arr;
+}
+// getLightsaberScores()
+
+// getStats
+
+// writeStats
