47e036469e40166a89979de540a5db139531ceeb
«GugnarevYuriy» <gugnarev.art@gmail.com>
2023-12-05 10:13:08 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,4 +1,19 @@
-const fs = require('fs')
+const fs = require("fs");
+
+let getPadawanNames = () => {
+  let result = fs.readFileSync("./data/padawans.txt", "utf8");
+  return result.split('\n').slice(0,-1);
+};
+let getLightsaberScores =() =>{
+
+}
+let   getStats = () => {
+  
+}
+ let writeStats =() => {
+
+ }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
