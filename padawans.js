c6f8746b21d17d454428237d608f2d85a808b8ac
starikova-a <a.starikova.ru@gmail.com>
2023-12-05 09:51:08 +0300

    Повтор идет штатно

--- padawans.js
+++ padawans.js
@@ -2,21 +2,44 @@ const fs = require('fs')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  //getStats,
-  //writeStats,
+  getStats,
+  writeStats,
 };
 
 function getPadawanNames(){
-  let name = fs.readFileSync("./data/padawans.txt", "utf8")
-  return name.push()
+  const name = fs.readFileSync("./data/padawans.txt", "utf8").split("\n")
+return name.slice(0,-1)
+
 }
-console.log(getPadawanNames())
+// console.log(getPadawanNames())
 
 
 function getLightsaberScores(){
-  return fs.readFileSync("./data/scores.txt", "utf8")
+  return fs.readFileSync("./data/scores.txt", "utf8").split("\n").map((el)=> Number(el))
 }
-console.log(getLightsaberScores())
+
+
+
+function getStats(){
+  const arrNames = getPadawanNames()
+  const arrScores = getLightsaberScores()
+  const stats = []
+  let arr = []
+  for( let i =0;i<arrNames.length;i++){
+    arr.push(arrNames[i], arrScores[i])
+    stats.push(arr)
+    arr= []
+  }
+
+  return stats
+}
+
+function writeStats(){
+  const data = getStats().join('\n').replaceAll(',',' ')
+   return fs.writeFileSync('./data/stats.txt',data,'utf-8')
+}
+
+// console.log(getLightsaberScores())
 
 
 // function getStats(){
