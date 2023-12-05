9e2b4e5ba44c3343aa9317e457a63b55237b6ac2
Nasya4p <qwertikexon@gmail.com>
2023-12-05 09:52:30 +0300

    checkpoint 1.1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,39 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const arr = fs.readFileSync('data/padawans.txt', 'utf-8').split('\n').slice(0, -1);
+  return arr;
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+  const readFile = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  return readFile.map((el) => Number(el));
+}
+getLightsaberScores();
+
+function getStats() {
+  const arr1 = [];
+  const arr2 = [];
+  for (let i = 0; i < getPadawanNames.length; i++) {
+    arr1.push(getPadawanNames[i]);
+    arr1.push(getLightsaberScores[i]);
+    arr2.push(arr1);
+  }
+  return arr2;
+}
+getStats();
+
+function writeStats() {
+  const stats = getStats();
+  const data = fs.writeFileSync('data/stats.txt', `${stats}`);
+  return data;
+}
+writeStats();
