8950f8aaa9b368d348f6eaf287117e235e736351
fist333 <mr.achaban@gmail.com>
2023-12-05 10:07:05 +0300

    relise

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
-const fs = require('fs')
+const { log } = require("console");
+const fs = require("fs");
+const path = require("path");
+
+const getPadawanNames = fs.readFileSync("./data/padawans.txt", "utf8");
+console.log(getPadawanNames);
+
+// const getPadawanNames = () =>{
+//   const padawanRead = fs.readFileSync(path.join(__dirname, '../data/padawans.txt'));
+//   console.log(padawanRead);
+// }
+// const allNames = []
+// function Names(string) {
+//   for (let i = 0; i < num; i++) {
+//     result.push(allNames);
+// }
+//  console.log(allNames);
+// const readFile = () => {
+//   const readData = fs.readFileSync(
+//     path.join(__dirname, '../data/padawans.txt'),
+//     'utf8',
+//   );
+//   console.log(readData);
+// };const readFile = () => {
+//   const readData = fs.readFileSync(
+//     path.join(__dirname, '../data/padawans.txt'),
+//     'utf8',
+//   );
+//   console.log(readData);
+// };
+// const readDirectory = () => {
+//   const fileNames = fs.readdirSync(path.join(__dirname, '../data/padawanns.txt'));
+//   console.log(fileNames);
+// };
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
