bdd35fe72407e6dd30298a441758ddd94ecd5f1f
alyonaosipova <alyonaosipova@gmail.com>
2023-12-05 12:13:38 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,17 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  const namesStr = fs.readFileSync("./data/padawans.txt", "utf-8").trim();
+  names = namesStr.split("\n");
+  return names;
+}
+
+function getLightsaberScores() {}
+
+function getStats() {}
+
+function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
