5caf57b24bbe2bc128a69df4edf769ab63ee0675
XMas <azar0vdv@yandex.ru>
2023-12-05 10:06:40 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,19 @@
-const fs = require('fs')
+const { log } = require("console");
+const fs = require("fs");
+
+let str1 = fs.readFileSync("./data/padawans.txt", "utf8");
+let arr1 = str1.split("\n");
+
+let str2 = fs.readFileSync("./data/scores.txt", "utf8");
+let arr2 = str2.split("\n");
+
+const getPadawanNames = (arr) => arr;
+
+const getLightsaberScores = (arr) => arr;
+
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+	getPadawanNames,
+	getLightsaberScores,
+	getStats,
+	writeStats,
 };
