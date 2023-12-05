5a551bc9f4402fee3eced3a70b74c5b1b0a37636
Emil <em.novodvorsky@gmail.com>
2023-12-05 10:21:17 +0300

    w1d1

--- padawans.js
+++ padawans.js
@@ -1,3 +1,6 @@
+const randomNames = require('random-profile-generator');
+const random = require( '@stdlib/random' );
+
 const fs = require('fs')
 module.exports = {
   getPadawanNames,
@@ -5,3 +8,35 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+//я вообще подумал что надо сгенерировать сначала имена
+// function getPadawanNames(count) {
+//   let resultArr = [];
+//   for (let i = 0; i < count; i++) {
+//   const names = randomNames.name();
+//      resultArr.push(names)
+//   }
+//   return resultArr;
+// }
+
+// console.log(getPadawanNames(4));
+
+
+function getPadawanNames (data) {
+  const writeNames = fs.writeFileSync("./data.padawans.txt", "utf8");
+}
+
+
+
+function getLightsaberScores () {
+
+}
+
+
+function getStats() {
+
+}
+
+function writeStats () {
+
+}
