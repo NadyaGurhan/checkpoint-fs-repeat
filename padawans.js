a6e8fc11374335bff12160ef8f5ce22195f37184
Ruslan Khatipov <rmk.educ@gmail.com>
2023-12-05 10:06:03 +0300

    done 2 tests

--- padawans.js
+++ padawans.js
@@ -5,3 +5,30 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const readPadawans = fs.readFileSync(__dirname + '/data/padawans.txt', 'utf8'); //чтение данных из файла
+  console.log(readPadawans);
+  const padawanArr = readPadawans.split('\n'); // преобразование списка падаванов в массив
+  console.log(padawanArr);
+  return padawanArr;
+}
+getPadawanNames()
+
+function getLightsaberScores() {
+  const readScores = fs.readFileSync(__dirname + '/data/scores.txt', 'utf8');
+  console.log(readScores);
+  const scoresArr = readScores.split('\n');
+  for (let i = 0; i < scoresArr.length; i++) {
+    scoresArr[i] = +scoresArr[i];
+      };
+      console.log(scoresArr)
+  return scoresArr;
+}
+getLightsaberScores()
+function getStats() {
+
+}
+function writeStats() {
+
+}
