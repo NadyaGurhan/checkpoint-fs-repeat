422020772d56e7f240398ee31d423b116f4b94c7
sashanoviy <lamp.lamp.lamp@yandex.ru>
2023-12-05 15:20:24 +0300

    1.5 done

--- padawans.js
+++ padawans.js
@@ -2,6 +2,25 @@ const fs = require('fs')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+function getPadawanNames () {
+  const names = fs.readFileSync("./data/padawans.txt", "utf8");
+  return names.split("\r\n").slice(0, -1)
+}
+console.log (getPadawanNames ());
+
+function getLightsaberScores () {
+  const score = fs.readFileSync("./data/scores.txt", "utf8");
+  return score.split("\r\n").slice(0, -1)
+}  
+console.log (getLightsaberScores ())
+
+// function getStats () {
+//   const namesScore = [];
+//   const names = fs.readFileSync("./data/padawans.txt", "utf8");
+//   const score = fs.readFileSync("./data/scores.txt", "utf8");
+//   for (let i = 0; 
+//   namesScore.push
+// }
