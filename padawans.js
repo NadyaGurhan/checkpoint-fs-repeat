557af0886ad65302359f32fff8b9dd1a8b43a5bf
beetleOf <beet1le@mail.ru>
2024-01-09 09:58:19 +0300

    checkpoint not done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,24 @@
 const fs = require('fs')
+const getPadawanNames = () => {
+ return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n')
+}
+const getLightsaberScores = () => {
+ return fs.readFileSync('./data/scores.txt', 'utf-8').split()
+
+ 
+}
+
+const getStats = () => {
+  fs
+}
+
+const writeStats = () => {
+  const stats = getStats();
+  fs.writeFileSync('./data/stats.text', stats, 'utf-8')
+}
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
