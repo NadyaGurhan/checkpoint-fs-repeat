de0fcfef4b535069362f45a8efd9f70f0e4ee37d
xarukibuu21 <khapkhaan@gmail.com>
2024-01-09 09:59:37 +0300

    test

--- padawans.js
+++ padawans.js
@@ -1,4 +1,26 @@
 const fs = require('fs')
+
+
+const readFile = (path) =>{
+  const data = fs.readFileSync(`./data/padawans.txt`,'utf-8');
+  return data;
+}
+
+
+
+const getPadawanNames = (path) => fs.readdirSync(path){
+
+
+  function (readFile) {
+   const data = fs.readFileSync(`./data/padawans.txt`, 'utf-8')
+   return data
+  }
+}
+
+
+// const getPadawanNames =(path) 
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
