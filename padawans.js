4509beec590122f2c135ce4e07b886e1f1e39603
sashanoviy <lamp.lamp.lamp@yandex.ru>
2024-01-09 10:03:22 +0300

    3 iz 4 dance macabre

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
 const fs = require('fs')
+
+function getPadawanNames () {
+  const padawans = fs.readFileSync ('./data/padawans.txt', "utf-8").trim().split('\r\n')
+  return padawans
+}
+
+function getLightsaberScores () {
+  const scores = fs.readFileSync ('./data/scores.txt', "utf-8")
+  return scores.split('\r\n').map ((el) => Number(el))
+}
+
+function getStats () {
+  const padawans = getPadawanNames ();
+  const scores = getLightsaberScores ();
+  const results = scores.map ((el, i) => [padawans[i], el])
+  return results
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
