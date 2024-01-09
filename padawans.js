edd7e3c8f617e3eab4c55497af11f56efc41f374
OyunaSanieva <fairy.oyuna01@gmail.com>
2024-01-09 09:52:20 +0300

    ch1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  return names;
+}
+function getPadawanScores() {
+  const stats = fs.readFileSync('data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+  return stats;
+}
+function getStats() {
+  const arr = [];
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  const stats = fs.readFileSync('data/scores.txt', 'utf-8').split('\n');
+  arr.push(names, stats);
+  return arr;
+}
+function writeStats() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  const data = fs.writeFileSync('./data/stats.txt', names);
+  return data
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
