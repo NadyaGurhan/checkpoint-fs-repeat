eecdd98d9e66f951c21c3e01aa9da495eb24b057
Pafy88 <opossumkresh2@mail.ru>
2024-01-09 10:03:20 +0300

    my check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n').map((el) => Number(el));
+  return scores;
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  let status = padawans.map()
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
