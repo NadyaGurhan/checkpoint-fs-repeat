e145a873f10b44f401e75846cbc1a1f285b8de6b
zavarec <abdulaevham@gmail.com>
2024-01-09 10:06:43 +0300

    release1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return names.split('\n');
+}
+
+function getLightsaberScores() {
+  const stat = fs.readFileSync('./data/scores.txt', 'utf-8')
+  return stat.split('\n')
+
+}
+
+function getStats() {
+
+}
+function writeStats() {
+
+}
+
+console.log(getLightsaberScores());
