caff2a290aee468f966fcb7fa68ffc4d8c00bd8a
mr.advert <mr.advert@yandex.ru>
2024-01-09 09:48:47 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawanInfo = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf8');
+  const splitRes = padawanInfo.split('\n');
+  splitRes.pop();
+  return splitRes;
+}
+
+
+function getLightsaberScores() {
+  const scoresInfo = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf8');
+  const splitRes = scoresInfo.split('\n');
+  const final = parseInt(splitRes);
+  return final;
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
