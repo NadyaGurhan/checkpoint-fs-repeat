eda030367a73cf1806aa881c46d5c7eb32addd47
Karen <kareniki@mail.ru>
2024-01-09 10:57:21 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -4,4 +4,14 @@ module.exports = {
   getLightsaberScores,
   getStats,
   writeStats,
-};
+}; 
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n')
+}
+
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return scores.split('\n').map((score) => Number(score));
+}
