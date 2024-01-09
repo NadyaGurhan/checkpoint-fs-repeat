31314ac04d77aeb234e6de187c061ac762ffd728
Grimaliak <tirisfallen@gmail.com>
2024-01-09 09:59:08 +0300

    check d1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
-const fs = require('fs')
+const { isUtf8 } = require('buffer');
+const fs = require('fs');
+const path = require('path');
+
+function readFile() {
+  return fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+}
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+  const result = padawans.split('\n');
+  result.pop();
+  return result;
+}
+
+function getLightsaberScores() {
+  const score = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8').split('\n');
+  return score;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
