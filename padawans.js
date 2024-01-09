38db7dc8b5eefb62b4128301723fe2f81f2c3c1e
ZloyDoc13 <zloydoc.k@yandex.ru>
2024-01-09 10:53:11 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,14 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+};
+
+
 module.exports = {
+  // eslint-disable-next-line no-use-before-define
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
