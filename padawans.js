c023a79591272d30ef0a8ce5035b6d38a5678939
Denis Ignatenko <d.i.ignatenko@gmail.com>
2024-01-09 09:55:59 +0300

    feat: p1w1d2 Chechpoint 1. Checkpoint-fs, 4 tests passed

--- padawans.js
+++ padawans.js
@@ -1,4 +1,49 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
+
+function readFile(filename) {
+  return fs
+    .readFileSync(path.join(__dirname, filename), 'utf8')
+    .trim()
+    .split('\n');
+}
+
+function writeFile(filename, data) {
+  return fs.writeFileSync(path.join(__dirname, filename), data, 'utf8');
+}
+
+function getPadawanNames() {
+  return readFile('data/padawans.txt');
+}
+
+function getLightsaberScores() {
+  return readFile('data/scores.txt').map(Number);
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  for (let i = 0; i < padawans.length; i++) {
+    padawans[i] = [padawans[i], scores[i]];
+  }
+  return padawans;
+}
+
+function writeStats() {
+  let padawans = getStats();
+  let formattedStats = '';
+  for (let i = 0; i < padawans.length; i++) {
+    formattedStats += padawans[i][0] + ' ' + padawans[i][1] + '\n';
+  }
+
+  writeFile('data/stats.txt', formattedStats.trim());
+}
+
+console.log(getPadawanNames());
+console.log(getLightsaberScores());
+console.log(getStats());
+console.log(writeStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
