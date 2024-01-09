68fc4b9dbb0795e0137784522a5b3cc18bb58e90
EAKovtuenko <3884985@bk.ru>
2024-01-09 09:59:42 +0300

    Release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => {
+  const names = fs.readdirSync('./data/padawans.txt');
+  return names;
+};
+
+const getLightsaberScores = () => {
+  const names = fs.readdirSync('./data/scores.txt');
+  return names;
+};
+
+// const getStats = () => {
+//   const names = fs.readdirSync('./data/padawans.txt');
+//   const scores = fs.readdirSync('./data/scores.txt');
+//   const stats = 
+// }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
