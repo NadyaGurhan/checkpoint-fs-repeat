f1d9926f48632c1f1a3f18d22f74e59f232c0e76
maksiiik <maksin8008@yandex.ru>
2024-01-30 10:02:04 +0300

    hjkk

--- padawans.js
+++ padawans.js
@@ -11,7 +11,9 @@ function getPadawanNames() {
    return fileConect.split('\n').filter(name => name.trim() !== '') ;
 }
 function getLightsaberScores() {
-  
+  const fileConect = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return fileConect.split('\n').map(score => parseFloat(score))
+
 }
 
 function getStats() {
