d21013d6b6bb4d7076d04030175c3d62f3086bdb
Ann-Zobnina <azobnina97@mail.ru>
2024-01-30 09:53:51 +0300

    checkpoint Fs

--- padawans.js
+++ padawans.js
@@ -1,4 +1,33 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n');
+
+const getLightsaberScores = () => {
+  const data = fs.readFileSync('./data/scores.txt', 'utf8').trim().split('\n');
+  return data.map((elem) => +elem);
+};
+
+const getStats = () => {
+  const padawansData = getPadawanNames();
+  const scoresData = getLightsaberScores();
+
+  const result = [];
+  for (let i = 0; i < padawansData.length; i += 1) {
+    result.push([padawansData[i], scoresData[i]]);
+  }
+
+  return result;
+};
+
+const writeStats = () => {
+  const statData = getStats();
+  const result = statData.map((elem) => `${elem[0]} ${elem[1]}`);
+
+  fs.writeFileSync('data/stats.txt', result.join('\n'));
+};
+
+writeStats();
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
