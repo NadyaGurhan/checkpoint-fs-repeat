550657cb6ae7c266b80a249974bc4cfa3212987c
IrmaEzhik <irmawalerewna@gmail.com>
2024-01-30 10:04:19 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf8');
+  return data.split('\n').filter(name => name.trim() !== '');
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf8');
+  return data.split('\n').map(score => parseFloat(score));
+}
+
+function getStats() {
+  
+}
+
+function writeStats() {
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
