d5cea2640c2cf4f0c0e71b262d020183001112fd
ElizavetaKartashova <kartashova.liza@icloud.com>
2024-01-30 10:02:27 +0300

    padawan

--- padawans.js
+++ padawans.js
@@ -1,7 +1,22 @@
 const fs = require('fs')
+
+  function getPadawanNames() {
+    const data = fs.readFileSync('data/padawans.txt', 'utf8');
+    const names = data.split('\n').map(name => name.trim());
+
+    // for (let i = 0; i < names.length; i++) {
+     
+      return names;
+    
+  }        
+
+
+  
+
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
