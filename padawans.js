1b0f99c4f04cec62196af40276334a2e419bf66b
maksiiik <maksin8008@yandex.ru>
2024-01-30 09:58:20 +0300

    releas0

--- padawans.js
+++ padawans.js
@@ -5,3 +5,19 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const fileConect = fs.readFileSync('./data/padawans.txt', 'utf-8');
+   return fileConect.split('\n').filter(name => name.trim() !== '') ;
+}
+function getLightsaberScores() {
+  
+}
+
+function getStats() {
+  
+}
+
+function writeStats() {
+  
+}
