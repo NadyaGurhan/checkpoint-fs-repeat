a23b72aaa615db06977935df7d6082678170ef4c
Остяков Роман <152424557+Roman-Ostyakov@users.noreply.github.com>
2024-01-30 09:57:51 +0300

    checkpoint 30.01.24

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
 const fs = require('fs')
+
+//возвращает список падаванов из файла `data/padawans.txt
+function getPadawanNames () { 
+  let arr = fs.readFileSync(`./data/padawans.txt`,'utf-8').split('\r\n');
+  arr.splice(-1,1)
+return arr;
+}
+
+
+function getLightsaberScores () { 
+  let arr = fs.readFileSync(`./data/scores.txt`,'utf-8').split('\r\n');
+  arr = arr.map((item) => Number(item));
+return arr;
+}
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
