cc81d7d930531b9fb98e2fe20b061f1695a14a57
EkaterinaZakh <zakharovaea95@yandex.ru>
2024-01-30 10:07:55 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,29 @@
 const fs = require('fs')
+
+
+
+function getPadawanNames(){
+
+  const allPadawans = fs.readFileSync("./data/padawans.txt", "utf8");
+  newArr = allPadawans.split("\n");
+  return newArr
+
+}
+
+
+
+function getLightsaberScores (){
+
+  const scores = fs.readFileSync("./data/scores.txt", "utf8");
+  newArr = scores.split("\n");
+
+  return newArr
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
