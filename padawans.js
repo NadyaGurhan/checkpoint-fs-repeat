53ecd67ffdc86797c9b445ca6ac30dcb8f6b2161
VictorS <ruwebman@gmail.com>
2024-02-01 00:40:26 +0300

    checkpoint solution

--- padawans.js
+++ padawans.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const fs = require('fs');
 
 function getPadawanNames() {
@@ -8,9 +9,36 @@ function getPadawanNames() {
 
 function getLightsaberScores() {
   const point = fs.readFileSync('./data/scores.txt', 'utf8');
-  //const res = padawans.split('\n');
-  return (+point);
+  const res = point.split('\n').map(Number);
+  return res;
 }
+//console.log(getLightsaberScores());
+
+// const keys  = getPadawanNames();
+//console.log(keys);
+// const values = getLightsaberScores();
+//console.log(values);
+
+// let obj = {};
+//  const res = keys.forEach((key, index) => {
+// obj[key] = values[index]
+// });
+//console.log(res)
+//console.log(obj)
+
+function getStats() {
+  const keys  = getPadawanNames();
+  const values = getLightsaberScores();
+
+  let obj = {};
+ const res = keys.forEach((key, index) => {
+obj[key] = values[index]
+});
+return obj;
+};
+// console.log(getPadawanNames());
+// console.log(getLightsaberScores());
+//console.log(getStats());
 
 module.exports = {
   getPadawanNames,
