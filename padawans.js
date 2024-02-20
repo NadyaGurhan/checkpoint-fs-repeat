86deb6b865eacf0457664fdddfcf3a6940c776f8
Bagrat Ashibokov <bagrat_ashba@mail.ru>
2024-02-20 10:05:30 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,16 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  let arr = fs.readFileSync(`./data/padawans.txt`, "utf-8").split("\n");
+  arr.splice(-1, 1);
+  return arr;
+}
+
+function getLightsaberScores() {
+  let arr = fs.readFileSync(`./data/padawans.txt`, "uft-8").split("\n");
+  arr = arr.map((item) => Namber(item));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
