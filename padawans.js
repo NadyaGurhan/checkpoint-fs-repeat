30b52f6bc9c91a550def3f04f1edc69239af4bd5
Ilya <papondreo@mail.ru>
2024-02-20 09:59:24 +0300

    test

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
+
+const padavansX = fs.readFileSync(path.join(__dirname, '/home/ilya/checkpoint-fs/checkpoint-fs/data/padawans.txt'), 'utf8');
+const scoresX = fs.readFileSync(path.join(__dirname, './data/scores.txt'), 'utf8');
+
+
+function getPadawanNames(){
+  let arr = [];
+  let mas = padavansX.split('\r\n').join();
+ for(let el of mas){
+   arr.push(el);
+ }
+ return arr;
+}
+
+function getLightsaberScores(){
+  let arr = [];
+  let mas = scoresX.split('\n');
+ for(let el of mas){
+   arr.push(el);
+ }
+ return arr;
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
