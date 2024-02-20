a2ba82ccf0ac696faad35bce0ba5ced3fe5f0a39
Alexey-Plekhanov <plehanov234@gmail.com>
2024-02-20 10:00:31 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,20 @@
-const fs = require('fs')
+const fs = require('fs');
+
+
+const getPadawanNames = () => fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+
+const getLightsaberScores = () => fs.readFileSync('./data/scores.txt', 'utf-8')
+.trim()
+.split('\n')
+.map((el) => Number(el));
+
+// const getStats = (getPadawanNames(), getLightsaberScores()) => [getPadawanNames(), getLightsaberScores()];
+
+const writeStats = (getStats()) => fs.writeFileSync('./data/stats.txt', getStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
+  // getStats,
   writeStats,
 };
