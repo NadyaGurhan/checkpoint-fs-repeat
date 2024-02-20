6430329fa62be421284c5d59c1e62f172119aa6c
VladislavChobanu <m.akckaunt@yandex.ru>
2024-02-20 10:09:44 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,24 @@
 const fs = require('fs')
+const names = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf8').split('\n')
+const sabersString = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf8').split('\n')
+const sabers = sabersString.map((saber) => parseFloat(saber))
+
+function getPadawanNames() {
+  return names
+}
+
+function getLightsaberScores() {
+  return sabers
+}
+
+function getStats() {
+  return scores
+}
+
+const scoresArray = names.map((x) => x.split(','))
+
+console.log(scores)
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
