9a5ecad0a54c75789bcc52a9e3a6c8034fef0550
VladislavChobanu <m.akckaunt@yandex.ru>
2024-02-20 18:21:51 +0300

    Релиз 2

--- padawans.js
+++ padawans.js
@@ -1,23 +1,30 @@
-const fs = require('fs')
-const names = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf8').split('\n')
-const sabersString = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf8').split('\n')
-const sabers = sabersString.map((saber) => parseFloat(saber))
+const fs = require('fs');
+const path = require('path');
+
+const names = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf8').split('\n');
+const sabersString = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf8').split('\n');
+const sabers = sabersString.map((saber) => parseFloat(saber));
+const scores = names.map((name) => name.split(','));
+scores.map((score) => score.push(sabers[scores.indexOf(score)]));
+console.log(scores);
 
 function getPadawanNames() {
-  return names
+  return names;
 }
 
 function getLightsaberScores() {
-  return sabers
+  return sabers;
 }
 
 function getStats() {
-  return scores
+  return scores;
 }
 
-const scoresArray = names.map((x) => x.split(','))
+function writeStats(data) {
+  fs.writeFileSync(path.join(__dirname, 'data/stats.txt'), data.join('\n').replaceAll(',', ' '), 'utf8');
+}
 
-console.log(scores)
+writeStats(scores);
 
 module.exports = {
   getPadawanNames,
