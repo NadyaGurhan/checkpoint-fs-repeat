67ecd78b7c9109400b1937e0e41e3cc156d6874d
sashaserov2 <sashaserov2@gmail.com>
2024-03-12 10:01:07 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,37 @@
 const fs = require('fs')
+const path = require('path')
+
+const getPadawanNames = () => {
+  const result = fs.readFileSync(path.join(__dirname, './data/padawans.txt'),
+  'utf-8',
+);
+const names = result.split('\r\n')
+const arr = []
+for (let i = 0; i<names.length -1; i++) {
+  arr.push(names[i])
+}
+return arr
+
+}
+console.log(getPadawanNames())
+
+
+const getLightsaberScores = () => {
+  const result = fs.readFileSync('./data/scores.txt',
+  'utf-8'
+);
+  const score = result.split('\r\n')
+  const arr = []
+  for (let i = 0; i < score.length; i++){
+    arr.push(+(score[i]))
+  }
+  return arr
+}
+console.log(getLightsaberScores())
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
