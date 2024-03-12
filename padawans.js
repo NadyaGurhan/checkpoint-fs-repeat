df43edb0b2f1945796e76e422ac65f45aae61195
Ilya Osipov <ilyaoa19@gmail.com>
2024-03-12 10:08:27 +0300

    2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
 const fs = require('fs')
+
+const readFile = () => {
+  const names = fs.readFileSync(__dirname + '/data/padawans.txt', 'utf-8');
+  return names;
+}
+
+const readScore = () => {
+  const names = fs.readFileSync(__dirname + '/data/scores.txt', 'utf-8');
+  return names;
+}
+
+const getPadawanNames = (str) => {
+  const newArr = str.split('\n');
+  newArr.pop();
+  return newArr
+}
+const getLightsaberScores = (str) => {
+  const newArr = str.split('\n');
+  return newArr;
+}
+
+console.log(getPadawanNames(readFile()));
+console.log(getLightsaberScores(readScore()))
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
