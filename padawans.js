c51185e54a50907351dc825760615752862fbddc
belek1233 <belek12333@gmail.com>
2024-03-12 14:59:27 +0300

    bezdar-parch-2.0.0

--- padawans.js
+++ padawans.js
@@ -1,31 +1,45 @@
+/* eslint-disable linebreak-style */
 const fs = require('fs');
 const { get } = require('http');
-const path = require('path');
 
-function getPadawanNames () {
-  // const readResult = fs.readFileSync(
-  //   path.join(__dirname, './data/padawans.txt'),
-  //   'utf-8',);
-  //   console.log(readResult.split('\" \"'));
-
-    const readResult = fs.readFileSync(
-      './data/padawans.txt',
-      'utf-8');
-    return readResult.split(" ");
+function getPadawanNames() {
+  const readResult = fs.readFileSync(
+    './data/padawans.txt',
+    'utf-8',
+  );
+  const result = readResult.replaceAll(/\r/gi, '').trim().split('\n');
+  return result;
 }
 
 function getLightsaberScores() {
   const scores = fs.readFileSync(
     './data/scores.txt',
-    'utf-8');
-  console.log(scores.split(" "));
+    'utf-8',
+  );
+  const result = scores.replaceAll(/\r\n/gi, ' ').split(' ').map((el) => Number(el));
+  return result;
+}
+function getStats() {
+  const asd = [];
+  const qwe = getPadawanNames();
+  const zxc = getLightsaberScores();
+  for (let i = 0; i < qwe.length; i++) {
+    asd.push([qwe[i], zxc[i]]);
+  }
+  return asd;
+}
+
+function writeStats() {
+  const input = getStats();
+  fs.writeFileSync('./data/stats.txt', `${input.join("\n").replaceAll(",", " ")}`);
+  console.log(input)
 }
 
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  // getStats,
-  // writeStats,
+  getStats,
+  writeStats,
 };
-getPadawanNames();
-getLightsaberScores();
+
+writeStats();
