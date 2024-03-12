b96b62b4110926308d8ec578fe860f31fd571b89
belek1233 <belek12333@gmail.com>
2024-03-12 10:01:09 +0300

    yaBezdar

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+const { get } = require('http');
+const path = require('path');
+
+function getPadawanNames () {
+  // const readResult = fs.readFileSync(
+  //   path.join(__dirname, './data/padawans.txt'),
+  //   'utf-8',);
+  //   console.log(readResult.split('\" \"'));
+
+    const readResult = fs.readFileSync(
+      './data/padawans.txt',
+      'utf-8');
+    return readResult.split(" ");
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync(
+    './data/scores.txt',
+    'utf-8');
+  console.log(scores.split(" "));
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+getPadawanNames();
+getLightsaberScores();
