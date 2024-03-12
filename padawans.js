fbc51364ea3b5d67b544efeea3fe23d37ad6bc42
nikita <nikita-ka@yandex.ru>
2024-03-12 09:46:21 +0300

    allDone

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+// const path = require('path');
+
+const getPadawanNames = () => {
+  const data = fs.readFileSync('./data/padawans.txt', 'utf8');
+  return data.split('\n');
+};
+
+const getLightsaberScores = () => {
+  const data = fs.readFileSync('./data/scores.txt', 'utf8');
+  return data.split('\n').map((element) => Number(element));
+};
+const getStats = () => {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+
+  const array = [];
+  for (let i = 0; i < names.length; i += 1) {
+    array.push([names[i], scores[i]]);
+  }
+  return array;
+};
+const writeStats = () => {
+  const stats = getStats().map((stat) => stat.join(' ')).join('\n');
+  //console.log(stats)
+  fs.writeFileSync('data/stats.txt', stats, 'utf8');
+};
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
