d81df28991a6fc4398cc335f3f27885a1ee714f8
AndreyBoykov <yaroslavl1992@gmail.com>
2024-04-02 09:58:56 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8');
+  return names.split(' ');
+}
+
+function getLightsaberScores() {
+  const names = fs.readFileSync('./data/scores.txt', 'utf8');
+  return names.split(' ');
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
+//вчера не делал эти тесты, не успел, не до конца догоняю что нужно делать) 
