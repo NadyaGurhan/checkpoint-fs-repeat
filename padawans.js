eba04b37a37f873058694a7a7fb4fb95cf052fab
belek1233 <belek12333@gmail.com>
2024-04-02 09:31:16 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,12 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const getPadawanNames = () => (fs.readFileSync('./data/padawans.txt', 'utf-8')).trim().split('\n');
+
+// it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
+const getLightsaberScores = () => (fs.readFileSync('./data/scores.txt', 'utf-8')).split('\n').map((el) => Number(el));
+const getStats = () => getPadawanNames().map((el, i) => [el, getLightsaberScores()[i]]);
+const writeStats = () => fs.writeFileSync('./data/stats.txt', getStats().join('\n').replaceAll(',', ' '), 'utf-8');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
