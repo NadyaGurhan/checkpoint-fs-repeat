20a71ffe4e4afc269b6a95b95c63dc0e3de1185b
varyayaru <vgya00@mail.ru>
2024-04-02 10:00:58 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const namesArr = fs.readFileSync('./data/padawans.txt', 'utf-8').toString().split('\n');
+  namesArr.length = namesArr.length - 1
+  return namesArr;
+}
+
+function getLightsaberScores() {
+  const scoresArr =fs.readFileSync('./data/scores.txt', 'utf-8').toString().split('\n');
+  return scoresArr;
+}
+
+function getStats() {
+  const namesArr = fs.readFileSync('./data/padawans.txt', 'utf-8').toString().split('\n');
+  const scoresArr = fs.readFileSync('./data/scores.txt', 'utf-8').toString().split('\n');
+  const newArr = [[], [], [], []];
+  for (let i = 0; i < namesArr.length; i++) {
+    
+    newArr.push();
+  }
+}
+
+function writeStats() {
+
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
