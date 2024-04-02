b6285f414107d76fafe7c8e8481a8fa54ac45de3
NikitaUsachev <nikitatekknik@yandex.ru>
2024-04-02 09:58:29 +0300

    я не понимаю

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  return fs.writeFileSync('./data/padawans.txt').split('');
+}
+
+function getLightsaberScores() {
+
+}
+
+function getStats() {
+
+}
+
+function writeStats() {
+
+}
