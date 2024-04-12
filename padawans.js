ef3048f271c9594d484c343d5b741e91ea91f89f
ygp188 <michiko696@gmail.com>
2024-04-12 15:05:33 +0300

    test

--- padawans.js
+++ padawans.js
@@ -1,4 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return padawans.trim().split('\n');
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return scores.split('\n').map((score) => Number(score));
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push([names[i], Number(scores[i])]);
+  }
+  return result;
+}
+
+function writeStats(stats) {
+  const result = stats.join('\n').replaceAll(',', ' ');
+  fs.writeFileSync('data/stats.txt', result);
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
