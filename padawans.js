1623843442438093c7c13a9b70d4bcc0721c1135
KonkoffAV <konkoffav@gmail.com>
2024-04-22 21:39:10 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,4 +1,22 @@
 const fs = require('fs')
+const Padawans = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+const Scores = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8');
+
+
+function getPadawanNames() {
+const padawansArr = Padawans.trim().split('\n');
+return padawanArr;
+}
+
+function getLightsaberScores() {
+  const scoresarr = Scores.trim().split('\n');
+  const arr = []
+  for (let i=1, i<arr.length, i++){
+    arr.push(Number(arr[i]))
+  } 
+ return arr
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
