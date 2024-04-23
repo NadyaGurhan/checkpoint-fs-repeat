0ded592011c725c82ad86bef08a21d5bc1ac91e9
Kirill-begemot <kkostykov@gmail.com>
2024-04-23 09:56:38 +0300

    0

---
+++ spec/getPadawanNames.js
@@ -0,0 +1,7 @@
+const getPadawan = require('data/padawans.');
+const fs = require('fs');
+
+function getPadawanName(arr) {
+  return arr.join();
+}
+console.log(getPadawanName(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']));
