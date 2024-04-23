e7186ff37c098c423c4bc684605c20119823b188
TermedKitten <ruizhkow@gmail.com>
2024-04-23 09:26:27 +0300

    release1 add getPadawanNames

--- padawans.js
+++ padawans.js
@@ -1,7 +1,15 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padNames = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf8');
+  const arrReturn = padNames.split('\r\n');
+  arrReturn.pop();
+  return arrReturn;
+}
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
