644112f2d2487b115b9ebd517cfa58a732cf25c6
1replica5 <antonvolsky@gmail.com>
2024-04-23 10:03:15 +0300

    test 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n');
+  return names;
+}
+
+function getLightsaberScores() {
+  newArr = [];
+  const names = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  names.map((el) => newArr.push(Number(el)));
+  return newArr;
+}
+function getStats() {
+  const stats = [];
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n');
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  for (let i = 0; i < names.length; i += 1) {
+    stats.push([names[i], Number(scores[i])]); // потом принять функции
+  }
+  return stats;
+}
+function writeStats(stats) {
+  const data = stats.join('\n').replaceAll(',', ' ');
+  return fs.writeFileSync('data/stats.txt', data);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
