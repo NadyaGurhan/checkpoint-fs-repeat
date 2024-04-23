fcc313fb942ac11f642969238e896bfc81001906
Sema <sem.ilyukhin@gmail.com>
2024-04-23 10:00:41 +0300

    Check1

--- padawans.js
+++ padawans.js
@@ -5,3 +5,25 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+let filePath = "./data/padawans.txt";
+let data = fs.readFileSync(filePath,"utf-8");
+
+function getPadawanNames() {
+  let arr = [];
+  for (let i = 0; i < data.length; i++){
+    arr.push(data[i])
+  }
+  return arr;
+}
+
+let beda = "./data/scores.txt";
+let voobshebeda = fs.readFileSync(beda, "utf-8");
+
+function getLightsaberScores(){
+  let scores = [];
+  for (let i = 0; i < voobshebeda.length; i++){
+    scores.push(voobshebeda[i])
+  }
+  return scores
+}
