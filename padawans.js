f936e91137adb78cac878ec6f0ef3e067bca9298
Fagoh <ranaray95@mail.ru>
2024-04-23 09:56:12 +0300

    1 and 2 test

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+const padawansArr = padawans.split('\n').slice(0, padawans.split('\n').length - 1);
+const scoresArr = scores.split('\n').map(el => Number(el));
+
+
+
+getPadawanNames = () => padawansArr;
+getLightsaberScores = () => scoresArr;
+
+console.log(getPadawanNames())
+console.log(getLightsaberScores())
+
+
+
+
+getStats = () => {}
+writeStats = () => {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
