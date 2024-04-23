c9369cf5151a655e85d78848c7566a583b94beea
Dava13 <dava-13-05@mail.ru>
2024-04-23 09:59:40 +0300

    2/4

--- padawans.js
+++ padawans.js
@@ -1,4 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawansTxt = fs.readFileSync(`${__dirname}/data/padawans.txt`, 'utf-8');
+const scoresTxt = fs.readFileSync(`${__dirname}/data/scores.txt`, 'utf-8');
+
+function getPadawanNames() {
+  const arr = padawansTxt.trim().split('\n');
+  return arr;
+}
+
+function getLightsaberScores() {
+  const array = [];
+  const arr = scoresTxt.trim().split('\n');
+  for (let i = 0; i < arr.length; i ++) {
+    array.push(Number(arr[i]));
+  }
+  return array;
+}
+
+function getStats () {
+
+}
+
+function writeStats () {
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
