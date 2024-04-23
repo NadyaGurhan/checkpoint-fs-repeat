03ab4896a266f11b1a55b6d3cde6d2fe0e1ee55d
ViktorLid <viks130293@gmail.com>
2024-04-23 10:09:26 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,34 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+const findNames= fs.readFileSync(`${__dirname}/data/padawans.txt`, "utf-8");
+
+const findScores= fs.readFileSync(`${__dirname}/data/scores.txt`, "utf-8");
+
+
+function getPadawanNames() {
+ const arr = findNames.trim().split('\n')
+return arr;
+}
+
+
+
+function getLightsaberScores {
+
+}
+
+function getStats {
+
+}
+
+function writeStats {
+
+
+}
+
