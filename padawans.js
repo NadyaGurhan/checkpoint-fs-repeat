a7e27620ec756f90b009abaec374c159d315238d
1replica5 <antonvolsky@gmail.com>
2024-04-24 14:28:48 +0300

    check Release 1

--- padawans.js
+++ padawans.js
@@ -1,25 +1,41 @@
 const fs = require('fs');
 
 function getPadawanNames() {
-  const names = fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n');
-  return names;
+  return fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n');
 }
+console.log(getPadawanNames());
 
 function getLightsaberScores() {
-  newArr = [];
+  const newArr = [];
   const names = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
-  names.map((el) => newArr.push(Number(el)));
+  names.forEach((el) => newArr.push(Number(el)));
   return newArr;
 }
-function getStats() {
-  const stats = [];
-  const names = fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n');
-  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
-  for (let i = 0; i < names.length; i += 1) {
-    stats.push([names[i], Number(scores[i])]); // потом принять функции
-  }
-  return stats;
-}
+console.log(getLightsaberScores());
+
+// const getLightsaberScores = () => fs
+//   .readFileSync('./data/scores.txt', 'utf8')
+//   .trim()
+//   .split('\n')
+//   .map((el) => Number(el));
+
+const getStats = () => {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  return padawans.map((padawan, index) => [padawan, scores[index]]);
+};
+console.log(getStats());
+
+// function getStats() {
+//   const stats = [];
+//   const names = fs.readFileSync('./data/padawans.txt', 'utf8').trim().split('\n');
+//   const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+//   for (let i = 0; i < names.length; i += 1) {
+//     stats.push([names[i], Number(scores[i])]); // потом принять функции
+//   }
+//   return stats;
+// }
+
 function writeStats(stats) {
   const data = stats.join('\n').replaceAll(',', ' ');
   return fs.writeFileSync('data/stats.txt', data);
