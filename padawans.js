dfacfaab3312c5175365f81152714a7d1f1c5781
1replica5 <antonvolsky@gmail.com>
2024-04-24 14:33:03 +0300

    check Release 2

--- padawans.js
+++ padawans.js
@@ -6,18 +6,24 @@ function getPadawanNames() {
 console.log(getPadawanNames());
 
 function getLightsaberScores() {
-  const newArr = [];
   const names = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
-  names.forEach((el) => newArr.push(Number(el)));
-  return newArr;
+  return names.map((el) => (Number(el)));
 }
-console.log(getLightsaberScores());
+
+// function getLightsaberScores() {
+//   const newArr = [];
+//   const names = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+//   names.forEach((el) => newArr.push(Number(el)));
+//   return newArr;
+// }
 
 // const getLightsaberScores = () => fs
 //   .readFileSync('./data/scores.txt', 'utf8')
 //   .trim()
 //   .split('\n')
 //   .map((el) => Number(el));
+
+console.log(getLightsaberScores());
 
 const getStats = () => {
   const padawans = getPadawanNames();
