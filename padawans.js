6ddfdb97369ea7f18d3fccff3c239ba0ad0865e3
VladimirKuznetsov1990 <fixer2026@gmail.com>
2024-05-21 10:00:54 +0300

    checkPoint 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,13 @@
 const fs = require('fs')
+
+const getPadawanNames = () => fs.readFileSync('./data/padawans.txt', 'utf8').split('\n').slice(0, 4);
+
+const getLightsaberScores = () => fs.readFileSync('./data/scores.txt', 'utf8').split('\n').map(item => +item)
+
+const getStats = () => {}
+
+const writeStats = () => {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
