c80667cd21eb14ed4e02c57c0ef385001d91ad82
DariaVor <daria_voronina@list.ru>
2024-05-21 10:00:30 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,45 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync("data/padawans.txt", "utf8");
+  const padArr = padawans.split("\n");
+  let newArr = [];
+  for (let i = 0; i < padArr.length; i++) {
+    if (padArr[i].trim()) {
+      newArr.push(padArr[i].trim());
+    }
+  }
+  return newArr;
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync("data/scores.txt", "utf8");
+  const scoresArr = scores.split("\n");
+  let newArr = [];
+  for (let i = 0; i < scoresArr.length; i++) {
+    if (scoresArr[i].trim()) {
+      newArr.push(Number(scoresArr[i].trim()));
+    }
+  }
+  return newArr;
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  let newArr = [];
+
+  for (let i = 0; i < padawans.length; i++) {
+    newArr.push([padawans[i], scores[i]]);
+  }
+  return newArr;
+}
+
+function writeStats() {
+  const stats = getStats();
+  fs.writeFileSync(`data/stats.txt`, `${stats.join("\n")}`, "utf-8");
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
