f52b6493584c096c250a822b1a7e39119f05c7e3
AlexeyKudr <renenkton123@yandex.ru>
2024-05-21 10:02:50 +0300

    check

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const listPadawans = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const padawanArray = listPadawans.split('\n');
+  const filteredPadawanArray = padawanArray.filter((name) => name.trim() !== '');
+  return filteredPadawanArray;
+}
+
+function getLightsaberScores() {
+  const listScores = fs.readFileSync('./data/scores.txt', 'utf8');
+  const scoresArray = listScores.split('\n');
+  const filteredScores = scoresArray.filter((name) => name.trim() !== '');
+  const numberScores = filteredScores.map(Number);
+  return numberScores;
+}
+
+function getStats() {
+  const names = getPadawanNames;
+  const score = getLightsaberScores;
+
+  const padawanData = names.map((name, i) => [name, score[i]]);
+  return padawanData;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
