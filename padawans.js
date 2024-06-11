fa3b60e7b86f473d4708584b3ebf7a3190bbe779
zmkt <zmkt@mail.ru>
2024-06-11 10:00:14 +0300

    не знаю почему 2й тест не прошел, вроде все нормально

--- padawans.js
+++ padawans.js
@@ -1,7 +1,45 @@
-const fs = require('fs')
+const fs = require("fs");
+const readFile = () => {
+  const data = fs.readFileSync("./data/padawans.txt", "utf-8");
+  return data;
+};
+const writeStats = (path, data) => {
+  fs.appendFileSync(path, data);
+};
+
+const scores = () => {
+  const data = fs.readFileSync("./data/scores.txt", "utf-8");
+  return data;
+};
+const table = readFile().split("\n").slice(0, -1);
+const scoresNum = scores().split("\n");
+console.log(scoresNum);
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  return table;
+}
+const resGetPadawanNames = getPadawanNames();
+
+function getLightsaberScores() {
+  let res = [];
+  let arr = [];
+  for (let i = 0; i < resGetPadawanNames.length; i++) {
+    arr.push(resGetPadawanNames[i]);
+    arr.push(+scoresNum[i]);
+    res.push(arr);
+    arr = [];
+  }
+  return res;
+}
+let c = getLightsaberScores()
+console.log(c)
+function getStats(){
+  writeStats("./data/stats.txt", c);
+}
