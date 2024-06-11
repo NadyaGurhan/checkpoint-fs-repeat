29f8d65e0f50b8ab4a68e79f1cd4faa6c7f39cb2
Mary38Z <3_mary@bk.ru>
2024-06-11 10:01:45 +0300

    3 func done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,46 @@
 const fs = require('fs')
+
+function getPadawanNames(){
+  const newNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n'); // уточнить почему с trim не работает
+  const result = [];
+  for (let i = 0; i < newNames.length-1; i ++){
+    result.push(newNames[i]);
+  }
+  return result;
+}
+
+function getLightsaberScores(){
+  const newScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n'); // для себя - глянуть почему не работает с .map(Number);
+  const result = [];
+  for (let i = 0; i < newScores.length; i ++){
+    result.push(+newScores[i]);
+  }
+  return newScores;
+}
+
+function getStats() {
+  const newNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n'); 
+  const newScores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+
+  const result = [];
+  for (let i = 0; i < newScores.length; i ++){
+    result.push(newNames[i], +newScores[i] );
+  }
+
+return result; 
+}
+
+function writeStats() {
+//тут точно надо fs.appendFile
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
+getStats()
