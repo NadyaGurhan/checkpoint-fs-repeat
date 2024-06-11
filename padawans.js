24c206783c383121ee295b951660ef02058f9cc3
q-zin <kuzin.esq@gmail.com>
2024-06-11 09:59:56 +0300

    not ok

--- padawans.js
+++ padawans.js
@@ -1,4 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
+const os = require('os');
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split(os.EOL);
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf-8').split(os.EOL).map((el) => Number(el));
+}
+
+function getStats() {
+  const res = [];
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  for (let i = 0; i < padawans.length; i += 1) {
+    for (let j = 0; j < scores.length; j += 1) {
+      let arr = [];
+      arr.push(padawans[i], scores[j]);
+      res.push(arr);
+    }
+  }
+  return res;
+}
+console.log(getStats());
+function writeStats() {
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
