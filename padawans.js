5a2cecbfd635257ad31cd9b6b56dee1fbcf6a238
Fan-95 <Fantom2000@bk.ru>
2024-06-11 09:59:18 +0300

    done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,45 @@
-const fs = require('fs')
+const fs = require("fs");
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  writeStats
 };
+
+const padawansData = fs
+  .readFileSync("./data/padawans.txt", "utf-8")
+  .split("\n");
+const scoresData = fs.readFileSync("./data/scores.txt", "utf-8").split("\n");
+
+function getPadawanNames() {
+  const padawans = [];
+
+  for (let i = 0; i < padawansData.length - 1; i++) {
+    padawans.push(padawansData[i]);
+  }
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const scores = [];
+  for (let i = 0; i < scoresData.length; i++) {
+    scores.push(+scoresData[i]);
+  }
+  return scores;
+}
+
+function getStats() {
+  const stats = [];
+  for (let i = 0; i < scoresData.length; i++) {
+    stats.push([padawansData[i], +scoresData[i]]);
+  }
+  return stats;
+}
+
+function writeStats() {
+  // надо ещё разделить с помощью \n не успел дописть...
+  fs.writeFileSync("./data/stats.txt", `${getStats()}`);
+};
+
+console.log(writeStats());
