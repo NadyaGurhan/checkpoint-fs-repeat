ab887e3869daa3362d6effadce72db85faf7097d
OlegUrasinov <UrasinovOB@gmail.com>
2024-06-11 09:59:44 +0300

    fail

--- padawans.js
+++ padawans.js
@@ -1,7 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const data1 = fs.readFile('./data/padawans', 'utf-8');
+  const padawans = data1.split('\n');
+  console.log(padawans);
+  return padawans;
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+  const data2 = fs.readFile('./data/scores', 'utf-8');
+  const scores = data2.split('\n');
+  console.log(scores);
+  return scores;
+}
+getLightsaberScores();
+
+function getStats() {
+
+}
+getStats();
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
