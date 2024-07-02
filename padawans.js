574fec07c38b223b5d7c2bae9f4fbb9b5c576aad
kokomimainr <akmatovamantur097@gmail.com>
2024-07-02 18:43:31 +0300

    Чекпоинт

--- padawans.js
+++ padawans.js
@@ -1,7 +1,44 @@
-const fs = require('fs')
+/* eslint-disable linebreak-style */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable space-before-blocks */
+/* eslint-disable import/newline-after-import */
+/* eslint-disable space-in-parens */
+/* eslint-disable quotes */
+/* eslint-disable semi */
+/* eslint-disable no-use-before-define */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+const fs = require('fs');
+function getPadawanNames() {
+  return fs.readFileSync('C:/Users/User/Desktop/Elbrus_HomeWork😭😍😜/Phase_1/Codes/D1/checkpoint-fs/data/padawans.txt', 'utf-8').split("\r\n").filter((s) => s !== '' )
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('C:/Users/User/Desktop/Elbrus_HomeWork😭😍😜/Phase_1/Codes/D1/checkpoint-fs/data/scores.txt', 'utf-8').split('\r\n').filter((s) => s !== '' ).map((num) => Number(num))
+}
+
+function getStats(){
+  const arr1 = getPadawanNames
+  const arr2 = getLightsaberScores
+  return arr1().map((items, index) => [items, arr2()[index]])
+}
+
+const stats = getStats()
+
+function writeStats(){
+  return fs.writeFileSync('./data/stats.txt', getStats().join('\n').replace(/,/g, ' '))
+}
+
+/* console.log(getPadawanNames());
+console.log(getLightsaberScores());
+console.log(getStats()); */
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
--- spec/padawans.spec.js
+++ spec/padawans.spec.js
@@ -1,33 +1,42 @@
-const fs = require('fs');
-const {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-} = require('../padawans');
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable linebreak-style */
+/* eslint-disable operator-linebreak */
+/* eslint-disable object-curly-newline */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
 
-describe('Статистика о падаванах', () => {
-  it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
+const fs = require("fs");
+const { getPadawanNames, getLightsaberScores, getStats, writeStats } 
+= require("../padawans");
+
+describe("Статистика о падаванах", () => {
+  it("getPadawanNames возвращает список падаванов из файла `data/padawans.txt`", () => {
     const names = getPadawanNames();
-    expect(names).toEqual(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']);
+    expect(names).toEqual(["Revan", "Bastila Shan", "Jolee Bindo", "Juhani"]);
   });
-  it('getLightsaberScores возвращает оценки владения световым мечом из файла `data/scores.txt`', () => {
+  it("getLightsaberScores возвращает оценки владения световым мечом из файла `data/scores.txt`", () => {
     const names = getLightsaberScores();
     expect(names).toEqual([99.9, 92, 87, 82]);
   });
-  it('getStats возвращает таблицу соответствия падавана и оценки владения световым мечом', () => {
+  it("getStats возвращает таблицу соответствия падавана и оценки владения световым мечом", () => {
     const stats = getStats();
     expect(stats).toEqual([
-      ['Revan', 99.9],
-      ['Bastila Shan', 92],
-      ['Jolee Bindo', 87],
-      ['Juhani', 82],
+      ["Revan", 99.9],
+      ["Bastila Shan", 92],
+      ["Jolee Bindo", 87],
+      ["Juhani", 82],
     ]);
   });
-  it('writeStats сохраняет статистику в файл `data/stats.txt`', () => {
+  it("writeStats сохраняет статистику в файл `data/stats.txt`", () => {
     const stats = getStats();
     writeStats(stats);
-    const data = fs.readFileSync('data/stats.txt', 'utf8');
-    expect(data).toBe('Revan 99.9\nBastila Shan 92\nJolee Bindo 87\nJuhani 82');
+    const data = fs.readFileSync("data/stats.txt", "utf8");
+    expect(data).toBe("Revan 99.9\nBastila Shan 92\nJolee Bindo 87\nJuhani 82");
   });
 });
