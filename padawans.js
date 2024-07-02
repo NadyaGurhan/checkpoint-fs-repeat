5c4a9cce8a0133323bb271a4cfe0154579001637
Your Name <you@example.com>
2024-07-02 18:42:45 +0300

    w1d1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,47 @@
-const fs = require('fs')
+const fs = require("fs");
+const { EOL } = require("os");
+
+
+
+
+const getPadawanNames = () => {
+  return fs
+    .readFileSync("./data/padawans.txt", "utf-8")
+    .split(`${EOL}`)
+    .filter((el) => el !== "");
+};
+
+const getLightsaberScores = () =>
+  fs
+    .readFileSync("./data/scores.txt", "utf-8")
+    .split(`${EOL}`)
+    .map((el) => Number(el));
+
+    
+const getStats = () => {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  let result = [];
+  for (let i = 0; i < padawans.length; i++) {
+    if (padawans[i]) {
+      result.push([padawans[i], scores[i]]);
+    }
+  }
+  return result;
+};
+
+const writeStats = () => {
+  const stats = getStats().join(`\n`)
+  let re = /,/gi
+ return fs.writeFileSync('./data/stats.txt', stats.replace(re, ' '))
+};
+ console.log(writeStats())
+ writeStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+// console.log(getPadawanNames);
