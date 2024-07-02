daad960544b3159c9b51003560cdf980a1aec27f
Buianto-03 <buyanto678@gmail.com>
2024-07-02 11:58:25 +0300

    release0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,32 @@
-const fs = require('fs')
+const fs = require("fs");
+
+const padawansArr = fs.readFileSync("./data/padawans.txt", "utf8").split("\n");
+padawansArr.splice(padawansArr.length - 1);
+function getPadawanNames() {
+  return padawansArr;
+}
+
+const scoreArr = fs
+  .readFileSync("./data/scores.txt", "utf8")
+  .split("\n")
+  .map((el) => +el);
+function getLightsaberScores() {
+  return scoreArr;
+}
+
+function getStats() {
+  const res = [];
+  for (let i = 0; i < padawansArr.length; i++) {
+    res.push([padawansArr[i], scoreArr[i]]);
+  }
+  return res;
+}
+
+function writeStats() {
+  const res = getStats().map((el) => el.join(" "));
+  fs.writeFileSync("./data/stats.txt", res.join("\n"), "utf8");
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
