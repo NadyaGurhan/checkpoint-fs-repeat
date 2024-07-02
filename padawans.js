56cc61abf0846c5b3bb38d9c78f9168ba55cdd6c
nikitakryuchkoff <theal22898@gmail.com>
2024-07-02 12:01:13 +0300

    checkpoint1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  return [...fs.readFileSync("./data/padawans.txt", "utf-8").split("\n")];
+}
+
+function getLightsaberScores() {
+  return [...fs.readFileSync("./data/scores.txt", "utf-8").split("\n")].map(
+    (score) => Number(score)
+  );
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+
+  return new Array(scores.length)
+    .fill(0)
+    .map((_, index) => [padawans[index], +scores[index]]);
+}
+
+function writeStats() {
+  const stats = getStats();
+  const data = stats.map((stat) => stat.join(" ")).join("\n");
+
+  fs.writeFileSync("./data/stats.txt", data, "utf8");
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
