f042a64ceeccd9bb97e3671e8d1854422da2559b
«bonimary» <emelyanova.m.r@gmail.com>
2024-07-02 12:06:26 +0300

    чек поинт w1d2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const padawanNames = Array(fs.readFileSync('data/padawans.txt', 'utf8'));
+
+  const result = [];
+
+  for (let i = 0; i < padawanNames.length; i += 1) {
+    result.push(padawanNames[i]);
+  }
+  return result;
+}
+
+function getLightsaberScores() {
+  const lightsaberScores = Array(fs.readFileSync('data/scores.txt', 'utf8'));
+
+  const result = [];
+  for (let i = 0; i < lightsaberScores.length; i += 1) {
+    result.push(lightsaberScores[i]);
+  }
+
+  return result;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
