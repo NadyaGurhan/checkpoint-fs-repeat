70ffbe9a7f4c994b6cf2ebfd7f45d44b353402b9
Sergey Antipov <antipovarmy@gmail.com>
2024-07-23 11:36:04 +0300

    feat: Release

--- padawans.js
+++ padawans.js
@@ -1,4 +1,39 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  return fs
+    .readFileSync("./data/padawans.txt", "utf-8")
+    .split("\n")
+    .filter((item) => item !== "");
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync("./data/scores.txt", "utf-8")
+    .split("\n")
+    .filter((item) => item !== "")
+    .map((item) => +item);
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+
+  return padawans.reduce((acc, item, index) => {
+    acc.push([item, scores[index]]);
+    return acc;
+  }, []);
+}
+
+function writeStats() {
+  const result = getStats();
+  fs.writeFileSync(
+    "./data/stats.txt",
+    result.map((item) => item.join(" ")).join("\n"),
+    "utf-8"
+  );
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
