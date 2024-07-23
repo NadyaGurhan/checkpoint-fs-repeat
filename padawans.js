205dd655495bbd92695773a365f0f9789bef021b
Danil <danilizosimov@gmail.com>
2024-07-23 11:55:58 +0300

    4 passed

--- padawans.js
+++ padawans.js
@@ -1,4 +1,28 @@
 const fs = require('fs')
+
+function getPadawanNames () {
+  return  fs.readFileSync('data/padawans.txt', 'utf-8').trim().split('\n')
+}
+function getLightsaberScores () {
+  return  fs.readFileSync('data/scores.txt', 'utf-8').trim().split('\n').map(el => +el)
+}
+function getStats () {
+  const padawansArr = [...getPadawanNames()]
+  const scoresArr = [...getLightsaberScores()]
+  const newArr = []
+  for (let i = 0; i < padawansArr.length; i++){
+    newArr.push([padawansArr[i], scoresArr[i]])
+    // console.log(padawansArr[])
+  }
+  return newArr
+}
+
+function writeStats() {
+  const statsArr = [...getStats()].map(el => el.join(' ')).join('\n')
+  return fs.writeFileSync('./data/stats.txt', `${statsArr}`, 'utf-8')
+}
+writeStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
