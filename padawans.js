a3c233b6df7eb5a2a579bc05b1319278def0be3f
feodosiadi <feodosiadi@gmail.com>
2024-07-23 12:16:12 +0300

    Финал

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
-const fs = require('fs')
+const fs = require("fs");
+
+const padawansList = fs
+  .readFileSync("./data/padawans.txt", "utf-8")
+  .split("\n")
+  .filter((padawan) => padawan !== "");
+
+const scoreList = fs
+  .readFileSync("./data/scores.txt", "utf-8")
+  .split("\n")
+  .map((score) => Number(score))
+  .filter((score) => score !== "");
+
+
+function getPadawanNames() {
+  return padawansList;
+}
+
+function getLightsaberScores() {
+  return scoreList;
+}
+
+function getStats() {
+  const padawansListArr = padawansList.map((el) => el.split())
+  for (let i = 0; i < padawansListArr.length; i+=1) {
+    padawansListArr[i].push(scoreList[i])
+  }
+  return padawansListArr
+}
+
+function writeStats() {
+  fs.writeFileSync('./data/stats.txt', getStats().map((el)=> el.join (' ')).join('\n'), 'utf8')
+}
+writeStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
