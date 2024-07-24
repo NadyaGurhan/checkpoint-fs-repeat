02baa8c51ebe0258b0850d88212a36c0d4014a54
wastor3332 <serebrinv@gmail.com>
2024-07-24 14:56:13 +0300

    checkpoin 1.1

--- padawans.js
+++ padawans.js
@@ -1,23 +1,48 @@
+const { log } = require('console');
 const fs = require('fs')
-// module.exports = {
-//   getPadawanNames,
-//   getLightsaberScores,
-//   getStats,
-//   writeStats,
-// };
+module.exports = {
+  getPadawanNames,
+  getLightsaberScores,
+  getStats,
+  writeStats,
+};
 
-const getPadawanNames = require('./getPadawanNames');
 
-// describe('Статистика о падаванах', () => {
-//   it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
-//     const names = getPadawanNames();
-//     expect(names).toEqual(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']);
-//   });
+function getPadawanNames() {
 
-  function Padawan() {
-          const names = generateNames();
-          expect(names).toEqual(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']);
-      }
-  
-  
+  const padawans = fs.readFileSync('data/padawans.txt', 'utf-8')
+  const reTakePadawans = padawans.trim().split('\n')
+  return reTakePadawans
 
+};
+console.log(getPadawanNames())
+
+function getLightsaberScores() {
+  const score = fs.readFileSync('data/scores.txt', 'utf-8')
+  const reTakeScore = score.split('\n').map(Number)
+  return reTakeScore
+};
+console.log(getLightsaberScores())
+
+function getStats() {
+  const padawansStats = getPadawanNames();
+  const scoreStats = getLightsaberScores();
+
+  const allStats = [];
+  for (let i = 0; i < padawansStats.length; i++) {
+    allStats.push([padawansStats[i], scoreStats[i]]);
+  }
+
+  return allStats;
+}
+
+console.log(getStats());
+
+function writeStats() {
+  const allStats = getStats()
+  const formatData = allStats.map(el => el.join('')).join('\n')
+  const write = fs.writeFileSync('data/stats.txt', formatData, 'utf8')
+  return write
+}
+
+console.log(writeStats());
