4c7893be843360a0af2382e46901562bf5f0b382
Excomunicad0 <e_zhul@mail.ru>
2024-08-12 14:27:48 +0300

    checkpoint-fs

--- padawans.js
+++ padawans.js
@@ -1,4 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\n');
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\n').map(el => parseFloat(el));
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  return names.map((el, i) => [el, scores[i]]);
+}
+
+function writeStats() {
+  const result = getStats();
+  const filtr = result.map(([names, scores]) => `${names} ${scores}`).join('\n');
+  fs.writeFileSync('data/stats.txt', filtr);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
