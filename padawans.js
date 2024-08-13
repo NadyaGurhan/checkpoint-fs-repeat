9bd90933d7daf55aa59e1504023d1b3f1d386b56
MaryVictor1225 <yakovleva.mariya.89@gmail.com>
2024-08-13 12:56:28 +0300

    Done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
-const fs = require('fs')
+const fs = require("fs");
+
+const padawanNames = fs
+  .readFileSync("./data/padawans.txt", "utf-8")
+  .trim()
+  .split("\n");
+
+const lightsaberScores = fs
+  .readFileSync("./data/scores.txt", "utf-8")
+  .trim()
+  .split("\n")
+  .map((el) => Number(el));
+
+function getPadawanNames() {
+  return padawanNames;
+}
+
+function getLightsaberScores() {
+  return lightsaberScores;
+}
+
+function getStats() {
+  const arrPadawanScores = [];
+  for (let i = 0; i < padawanNames.length; i++) {
+    arrPadawanScores.push([padawanNames[i], lightsaberScores[i]]);
+  }
+  return arrPadawanScores;
+}
+
+function writeStats() {
+  const statistics = getStats().join("\n").replaceAll(",", " ");
+  const readStatistics = fs.writeFileSync("./data/stats.txt", statistics);
+  return readStatistics;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
