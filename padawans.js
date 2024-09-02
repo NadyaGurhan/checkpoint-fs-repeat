c80b704c7f741cbe1a83e5e2b3b604335f2cc877
MaryV10 <mary10@list.ru>
2024-09-02 16:35:27 +0300

    My checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,46 @@
-const fs = require('fs')
+const { log } = require("console");
+const fs = require("fs");
+const { EOL } = require("os");
+
+function getPadawanNames() {
+  const newText = fs.readFileSync("./data/padawans.txt", "utf-8");
+  const newTextArr = newText.split(EOL);
+  newTextArr.pop();
+  return newTextArr;
+}
+
+function getLightsaberScores() {
+  const scoresText = fs.readFileSync("./data/scores.txt", "utf-8");
+  return scoresText.split(EOL).map(Number);
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+
+  const result = [];
+
+  for (let i = 0; i < names.length; i++) {
+    result.push([names[i], scores[i]]);
+  }
+  return result;
+}
+
+function writeStats() {
+  const stats = getStats();
+
+  const newStats = stats.map((i) => i.join(" "));
+  const newStatsText = newStats.join("\n");
+  const createStats = fs.writeFileSync("./data/stats.txt", newStatsText);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+// console.log(('Revan 99.9\nBastila Shan 92\nJolee Bindo 87\nJuhani 82'))
+
+console.log(writeStats());
