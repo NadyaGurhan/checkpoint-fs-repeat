0994591b9f4539196a1b4ab25e15f5cc0f4c4188
Darya <dasha.sakharr@gmail.com>
2024-09-24 12:29:55 +0300

    Test 24.09

--- padawans.js
+++ padawans.js
@@ -1,7 +1,33 @@
 const fs = require('fs')
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+const padowans = fs.readFileSync('./data/padawans.txt', 'utf-8')
+const grade = fs.readFileSync('./data/scores.txt', 'utf-8')
+
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
+
+
+function getPadawanNames() {
+
+  return padowans.split('\r\n')
+
+}
+console.log(getPadawanNames());
+
+
+function getLightsaberScores() {
+
+const newGrade = grade.split('\r\n')
+let num = []
+
+for (let i = 0; i < newGrade.length; i++) {
+  num.push(Number(newGrade[i]))
+}
+
+return num
+}
+console.log(getLightsaberScores());
+
