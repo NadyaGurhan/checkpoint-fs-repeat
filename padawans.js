62e3dc4b0a2d2dc05e6fc4c841c02d3d48aaa0e5
nateeth <toaigel@gmail.com>
2024-09-24 12:45:24 +0300

    Поправила третью функцию

--- padawans.js
+++ padawans.js
@@ -1,29 +1,33 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function getPadawanNames() {
-  let newArr = fs.readFileSync('./data/padawans.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
+  const newArr = fs
+    .readFileSync("./data/padawans.txt", { encoding: "utf8", flag: "r" })
+    .split("\n");
   newArr.pop();
   return newArr;
 }
 
 function getLightsaberScores() {
-  let newArr = fs.readFileSync('./data/scores.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
-  return newArr.map(el => Number(el))
+  const newArr = fs
+    .readFileSync("./data/scores.txt", { encoding: "utf8", flag: "r" })
+    .split("\n");
+  return newArr.map((el) => Number(el));
 }
 
-function getStats () {
-  let padawansArr = getPadawanNames();
-  let scoresArr = getLightsaberScores();
-  let result = []
-  for (let i=0; i < padawansArr.length; i++) {
-    result.push(padawansArr[i] + ',' + scoresArr[i])
+function getStats() {
+  const padawansArr = getPadawanNames();
+  const scoresArr = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < padawansArr.length; i++) {
+    result.push(`${padawansArr[i]},${scoresArr[i]}`);
   }
-  let splited = result.map(el => el.split(','))
 
-  for (let i=0; i < splited.length; i++) {
-    Number(splited[i][1]) // почему-то не работает
-  }
-  return splited;
+  const splited = result.map((el) => el.split(","));
+
+  newArr = splited.map(([str, number]) => [str, Number(number)])
+
+  return newArr;
 }
 
 console.log(getStats());
