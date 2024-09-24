6ff365cc71281a44608a6cba49cc82daae9f585b
pugsparty <luka61097@yandex.ru>
2024-09-24 12:26:50 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
-const fs = require('fs')
+const fs = require("fs");
+
+function getPadawanNames() {
+  const data = fs.readFileSync("data/padawans.txt", "utf8");
+  return data.split("\n");
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync("data/scores.txt", "utf8");
+  return data.split("\n").map(Number);
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  return names.map((name, index) => [name, scores[index]]);
+}
+
+function writeStats(stats) {
+  const data = stats.map(([name, score]) => `${name} ${score}`).join("\n");
+  fs.writeFileSync("data/stats.txt", data);
+}
+console.log(getPadawanNames());
+console.log(getLightsaberScores());
+console.log(getStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
