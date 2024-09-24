d3e4dcae6f555baae07b27b7a9c5c52e9c47901e
EvUP <9258669363@mail.ru>
2024-09-24 12:30:13 +0300

    some

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
+const { clear } = require('console');
 const fs = require('fs')
+
+function getPadawanNames () {
+ const padawans = fs.readFileSync("./data/padawans.txt", "utf8")
+  let names = [];
+  for (let i = 0; i < padawans.length; i++) {
+    names = names.push(padawans[i]).split(", ")
+  }
+  return names
+}
+
+const function getLightsaberScores () {
+  const scores = fs.readFileSync("./data/scores.txt", "utf8")
+  let score = []
+  for (let i = 0; i < scores.length; i++) {
+    score = score.push(scores[i]).split(", ")
+  }
+return score
+}
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
