2d1fa02c20fa442917d5dc254a777e8331f34cf7
Basov21 <id.basov@yandex.ru>
2024-09-24 12:28:40 +0300

    Не проходит третий тест

--- padawans.js
+++ padawans.js
@@ -1,7 +1,47 @@
 const fs = require('fs')
+
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+
+function getPadawanNames() {
+  padawans.pop();
+  return padawans;
+}
+
+function getLightsaberScores() {
+  const result = [];
+  scores.forEach((string) => {
+    result.push(Number(string))
+  });
+  return result;
+}
+
+function getStats() {
+  const numScores = [];
+  scores.forEach((string) => {
+    numScores.push(Number(string))
+  });
+
+  padawans.pop();
+  for (let i = 0; i < padawans.length; i++) {
+    padawans[i] = padawans[i].split(', ')
+  }
+
+  for (let i = 0; i < padawans.length; i++) {
+    padawans[i].push(numScores[i])
+  }
+
+  return padawans;
+}
+// console.log(getStats())
+
+// function writeStats() {
+  
+// }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+//   writeStats,
 };
