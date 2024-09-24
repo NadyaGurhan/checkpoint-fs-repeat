aa35ef3323db17a6609c23c8df0dfc944d3d4be6
nateeth <toaigel@gmail.com>
2024-09-24 12:29:51 +0300

    Result of checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,36 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  let newArr = fs.readFileSync('./data/padawans.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
+  newArr.pop();
+  return newArr;
+}
+
+function getLightsaberScores() {
+  let newArr = fs.readFileSync('./data/scores.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
+  return newArr.map(el => Number(el))
+}
+
+function getStats () {
+  let padawansArr = getPadawanNames();
+  let scoresArr = getLightsaberScores();
+  let result = []
+  for (let i=0; i < padawansArr.length; i++) {
+    result.push(padawansArr[i] + ',' + scoresArr[i])
+  }
+  let splited = result.map(el => el.split(','))
+
+  for (let i=0; i < splited.length; i++) {
+    Number(splited[i][1]) // почему-то не работает
+  }
+  return splited;
+}
+
+console.log(getStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
