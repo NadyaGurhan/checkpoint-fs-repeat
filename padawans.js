c209d0ebc0229ac306dc911dec1c464270700ab9
Katerina <katerina_mart131@mail.ru>
2024-09-24 12:27:19 +0300

    CheckPoint

--- padawans.js
+++ padawans.js
@@ -5,3 +5,22 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const getPadawan = fs.readFileSync('./date/padawans.txt')
+  return getPadawan.split('\n')
+}
+
+function getLightsaberScores(){
+  const getScores = fs.readFileSync('./data/scores.txt')
+  const num = getScores.split('\n')
+  return Number(num)
+}
+
+function getStats() {
+
+}
+
+function writeStats(){
+  
+}
