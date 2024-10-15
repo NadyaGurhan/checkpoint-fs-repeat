6b61621d817912492c414e4c298fd3fac0c338ef
tamara.dzhikiya <djandzh.77@gmail.com>
2024-10-15 12:03:13 +0300

    Чекпоинт Fs

--- padawans.js
+++ padawans.js
@@ -1,4 +1,33 @@
 const fs = require('fs')
+
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8');
+  
+  return data.trim().split('\n');
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf-8');
+
+  return data.trim().split('\n').map(Number);
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const stats = [];
+
+  for (let i = 0; i < names.length; i++) {
+    stats.push([names[i], scores[i]]);
+  }
+  return stats;
+}
+
+function writeStats() {
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
