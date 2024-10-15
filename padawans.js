0f4aac32d78bf684385de9d8b782d3c855c110d8
Pavelbrshh <brshh1703@gmail.com>
2024-10-15 12:01:42 +0300

    ready2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+
+const fs = require('fs');
+
+function getPadawanNames() {
+  const text = fs.readFileSync('./data/padawans.txt', 'utf8'); 
+  const newArr = text.split('\n').slice(0,-1); 
+
+  return newArr;
+}
+
+function getLightsaberScores () {
+  const text = fs.readFileSync('./data/scores.txt', 'utf8'); 
+  const newArr = text.split('\n')
+  .map(score => Number(score))  
+return newArr;
+}
+
+function getStats () {
+
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
