792b84d0bb7b1848d7b7040127b85eb12f2e35b8
thefuge <thefuge@hotmail.com>
2024-10-15 12:06:00 +0300

    cp

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
 const fs = require('fs')
+function getPadawanNames() {
+  const padawans = fs.readFileSync("./data/padawans.txt", "utf8");
+  return padawans.split("\n");
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync("./data/scores.txt", "utf8");
+  return data.split("\n").map(Number);
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  return names.map((name, index) => [name, scores[index]]);
+}
+
+
+function writeStats(resultData) {
+  fs.writeFileSync(
+    "./data/stats.txt",
+    resultData.join("\n").replace(/,/g, " ")
+  );
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
