9c22230d5956d38298ba3dbc6eb0e0c67a36fa57
Ekaterina <katsoina95@gmail.com>
2024-10-15 12:00:04 +0300

    чек поинт

--- padawans.js
+++ padawans.js
@@ -1,4 +1,10 @@
 const fs = require('fs')
+function getPadawanNames(){
+  return fs.readFileSync("./padawans.txt", "utf8");
+} 
+function getLightsaberScores(){
+  return fs.readFileSync("./scores.txt", "utf8");
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
