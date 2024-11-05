5af38f727527e1d0ee67ff0164526fc51d0034ca
system <toropchin20@gmail.com>
2024-11-05 12:32:48 +0300

    chekpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,36 @@
 const fs = require('fs')
+const name = fs.readFileSync('./data/padawans.txt','utf-8')
+const nomer = fs.readFileSync('./data/scores.txt','utf-8')
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
+function getPadawanNames (){
+  const names = name.split('\n')
+  const name1 = names.pop()
+  return names;
+
+}
+
+function getLightsaberScores (){
+  const nomer1 = []
+  const nomers = nomer.split('\n')
+  for( let i = 0; i < nomers.length; i+=1){
+    nomer1.push(Number(nomers[i]))
+  }
+  
+  return nomer1;
+
+}
+
+function getStats(){
+
+}
+
+function writeStats(){
+
+}
