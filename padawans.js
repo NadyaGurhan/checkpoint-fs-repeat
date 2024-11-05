9f2fee5ca896f0fe99ee8a169f003e701d489375
effect-rus <lapshin.artemm@gmail.com>
2024-11-05 12:32:52 +0300

    Checkpoint1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  /* getStats,
+  writeStats, */
 };
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return padawans.trim().split('\n');
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+  const arr = scores.split('\n');
+  return arr.map((el) => Number(el));
+}
+
+function getStats() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+}
