659268ccfbb39c134f6d187e54648a746727b8ad
deniiishka <deni.sulimanoff@gmail.com>
2024-11-05 13:45:56 +0300

    p1w1d2_checkpoint

--- padawans.js
+++ padawans.js
@@ -1,14 +1,15 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
 
 function getPadawanNames () {
   const padawansList = fs.readFileSync('./data/padawans.txt', 'utf-8');
-  const padawansArr = padawansList.trim().split('\n');
+  const padawansArr = padawansList.trim().split(EOL);
   return padawansArr;
 }
 
 function getLightsaberScores () {
   const scoresList = fs.readFileSync('./data/scores.txt', 'utf-8');
-  const scoresArr = scoresList.split('\n').map((el) => +el);
+  const scoresArr = scoresList.split(EOL).map((el) => +el);
   return scoresArr;
 }
 
