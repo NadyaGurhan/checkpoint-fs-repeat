762b5fead3d92da9f8ae0d31212e64b2b4a5ef93
dashanazimko <zimko1995@icloud.com>
2024-12-17 10:00:02 +0300

    res

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
 const fs = require('fs')
+const {EOL} = require('os')
+function getPadawanNames(){
+  const padawans = fs.readFileSync('./data/padawans.txt','utf-8').trim().split(EOL)
+  return padawans
+}
+function getLightsaberScores(){
+  const score =fs.readFileSync('./data/scores.txt','utf-8').split(EOL).map((el)=>+el)
+  return score
+  
+}
+function getStats(){
+  const padawans =getPadawanNames()
+  const score =  getLightsaberScores()
+  const stats =[];
+  for(let i =0;i<stats.length;i++){
+  
+  stats.push([padawans[i],score[i]])
+  }
+    return stats
+
+  }
+  function writeStats(stats){
+    return fs.writeFileSync('./data/stats.txt',stats.map((el) =>el.join(' ')).join(EOL))
+
+
+  }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
