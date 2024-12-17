5fab6c8f8fac291f43d82106c2e10aff6ab8165b
Nikita-Petrov01 <petrovn.46.22@gmail.com>
2024-12-17 09:59:05 +0300

    Check point

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const total = [];
+  const newData = fs.readFile('data/padawans.txt', 'utf8');
+  total.push(newData)
+  return total
+}
+
+function getLightsaberScores() {
+  const newArr = [];
+   const data = fs.readFile('data/scores.txt', 'utf8');
+   newArr.push(data)
+   return newArr;
+}
+
+function getStats () {
+
+}
+
+function writeStats () {
+  
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
