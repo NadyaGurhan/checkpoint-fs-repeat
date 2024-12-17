fea89b0388354a654f206bd0c3462707aabd8af9
Nuri-engineer <abdurahmon_1989@mail.ru>
2024-12-17 09:58:14 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
-const fs = require('fs')
+const { log } = require('console');
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf8').split(EOL);
+  return padawans;
+  // console.log(padawans);
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split(EOL);
+  const result = scores.map((el) => Number(el));
+  return result;
+}
+getLightsaberScores();
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores()
+  const stats = [];
+  for (let i=0; i<4; i++) {
+    stats.push(padawans, scores);
+  }
+  return stats
+}
+getStats();
+
+function writeStats() {
+  const stats = getStats();
+  return fs.writeFileSync('./data/stats.txt', stats.join(EOL));
+}
+writeStats();
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
