1a26e4325d3a06f283336287ece86e7bc3daf778
zzfeduk <zfeduk@yandex.ru>
2025-01-21 09:56:26 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,20 @@
 const fs = require('fs')
+const { EOL } = require('os')
+
+function getPadawanNames() {
+  const padawans = [];
+  const padawansNames = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  const names = padawansNames.split(EOL);
+  for (let i = 0; i < names.length; i++) {
+    padawans.push(names[i])
+  }
+  return padawans;
+}
+
+function getLightsaberScores() {
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
