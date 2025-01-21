40c2f3590ad418687149ac14591418db4256e3f3
Your Name <you@example.com>
2025-01-21 09:56:39 +0800

    1,2

--- padawans.js
+++ padawans.js
@@ -1,7 +1,40 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+function getPadawanNames() {
+  const padavan = fs.readFileSync('./data/padawans.txt', 'utf8').split(EOL).slice(0, -1);
+  return padavan
+}
+function getLightsaberScores(){
+  const result = [];
+  const score = fs.readFileSync('./data/scores.txt', 'utf-8').split(EOL);
+  for (let i = 0; i < score.length; i++) {
+    score[i] = Number(score[i]);
+    result.push(score[i]);
+  }
+  return result;
+}
+function getStats (){
+    const result = [];
+    const sc1 = fs.readFileSync('./data/scores.txt', 'utf-8').split(EOL);
+    const sc2 = fs.readFileSync('./data/padawans.txt', 'utf-8').split(EOL)
+  
+    for (let i = 0; i < sc2.length; i++) {
+      const arr = [];
+      for (let j = 0; j < sc1.length; j++) {
+        if (i === j) {
+          sc1[j] = Number(sc1[j]);
+          arr.push(sc2[i], sc2[j]);
+        }
+      }
+      result.push(arr);
+    }
+    return result;
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
