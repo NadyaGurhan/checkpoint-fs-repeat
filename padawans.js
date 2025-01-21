cf159c455d8b416a8bc0ba58a12a6ad1f1a4e48e
yanebylitsa <fable.prod@gmail.com>
2025-01-21 12:13:20 +0300

    all done

--- padawans.js
+++ padawans.js
@@ -12,27 +12,23 @@ function getLightsaberScores() {
   return scores.split(EOL).map((item) => Number(item));
 }
 
-function getStats() {  
+function getStats() {
   const arrPadawans = padawans.split(EOL);
-  const arrScores=scores.split(EOL).map((item) => Number(item));
+  const arrScores = scores.split(EOL).map((item) => Number(item));
   const result = new Array(arrPadawans.length);
-for (let i=0; i <arrPadawans.length; i++){
-  result[i] = [];
-   result[i].push(arrPadawans[i])
-   result[i].push(arrScores[i])
-}
-return result;
+  for (let i = 0; i < arrPadawans.length; i++) {
+    result[i] = [];
+    result[i].push(arrPadawans[i]);
+    result[i].push(arrScores[i]);
+  }
+  return result;
 }
 
 function writeStats() {
-
-    fs.writeFileSync(`./data/stats.txt`, getStats().join(EOL));
+  const arr = getStats();
+  const result = arr.map((el) => `${el[0]} ${el[1]}`);
+  fs.writeFileSync(`./data/stats.txt`, result.join(EOL));
 }
-
-writeStats()
-
-
-
 
 module.exports = {
   getPadawanNames,
