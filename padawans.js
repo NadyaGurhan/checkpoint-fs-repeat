d4938714388f4eb5d9ae927a3fff4469e72d5d93
Remishok <tanya20003009@mail.ru>
2025-01-21 09:56:23 +0300

    test

--- padawans.js
+++ padawans.js
@@ -1,4 +1,16 @@
 const fs = require('fs')
+const {EOL} = require('os');
+
+const getPadawanNames = () {
+const data = fs.readFileSyn('./data/padawans.txt' , 'utf-8')
+const names= data.split(EOL)
+const result = [];
+ for ( let i = 0; i < names.length; i ++) {
+  names.push(names[i].trim())
+}
+return names
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
