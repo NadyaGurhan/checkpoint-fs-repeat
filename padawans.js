afb55a1959d57a8177063736fc361c087daa7839
zzfeduk <zfeduk@yandex.ru>
2025-02-01 16:19:31 +0300

    checkpointUpdate

--- padawans.js
+++ padawans.js
@@ -1,19 +1,56 @@
-const fs = require('fs')
-const { EOL } = require('os')
+const fs = require('fs');
+const { EOL } = require('os');
 
 function getPadawanNames() {
   const padawans = [];
-  const padawansNames = fs.readFileSync('./data/padawans.txt', 'utf-8')
-  const names = padawansNames.split(EOL);
+  const padawansNames = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const names = padawansNames.trim().split(EOL);
   for (let i = 0; i < names.length; i++) {
-    padawans.push(names[i])
+    padawans.push(names[i]);
   }
   return padawans;
 }
+getPadawanNames();
 
 function getLightsaberScores() {
+  const result = [];
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split(EOL);
+  for (let i = 0; i < scores.length; i++) {
+    result.push(+scores[i]);
+  }
 
+  return result;
 }
+getLightsaberScores();
+
+function getStats() {
+  const result = [];
+  const saber = getLightsaberScores();
+  const padawans = getPadawanNames();
+  for (let i = 0; i < saber.length; i++) {
+    result.push([padawans[i], saber[i]]);
+  }
+  // console.log(result);
+  return result;
+  
+}
+
+getStats();
+
+
+function writeStats(arr) {
+  let string = '';
+  for(let i = 0; i<arr.length; i++){
+    string += arr[i].join(' ')+EOL
+  }
+fs.writeFileSync('./data/stats.txt', string.trim())
+}
+writeStats([
+  [ 'Revan', 99.9 ],
+  [ 'Bastila Shan', 92 ],
+  [ 'Jolee Bindo', 87 ],
+  [ 'Juhani', 82 ]
+])
 
 module.exports = {
   getPadawanNames,
