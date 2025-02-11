32097ed5f0ab1a042d7bdc6c29fccb757f1e3d67
37Lizz <elizavetapozdniakova37@gmail.com>
2025-02-11 10:01:46 +0300

    finish

--- padawans.js
+++ padawans.js
@@ -1,4 +1,38 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const arr = data.split(EOL);
+  return arr;
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('./data/scores.txt', 'utf8');
+  const arr = data.split(EOL).map((el) => Number(el));
+  return arr;
+}
+
+function getStats() {
+  const data1 = fs.readFileSync('./data/padawans.txt', 'utf8');
+  const arr1 = data1.split(EOL);
+  const resultarr = arr1.map((el)=> el.split(' '));
+  for (let i = 0; i< resultarr.length; i++){
+    const data2 = fs.readFileSync('./data/scores.txt', 'utf8');
+    const arr2 = data2.split(EOL).map((el) => Number(el));
+    resultarr[i].push(arr2[i])
+  }return resultarr
+}
+
+
+function writeStats() {
+const result = getStats()
+const resultstr = result.join(EOL)
+fs.writeFileSync(resultstr, "data/stats.txt', 'utf8'")
+
+}
+
+console.log(writeStats());
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
