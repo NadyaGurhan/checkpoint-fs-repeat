f8d63f0d22ffd5f36a6a8b841402f850310d54e5
heijegersia <grechanova00@gmail.com>
2025-02-11 09:58:58 +0300

    padawans

--- padawans.js
+++ padawans.js
@@ -1,4 +1,14 @@
-const fs = require('fs')
+const fs = require('fs');
+const EOL = require('EOL');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('../padawans.txt', 'utf8');
+  const people = names.split(EOL);
+  return people;
+}
+
+console.log(getPadawanNames());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
