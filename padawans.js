1d22df00174ebe7980573e2cd21932923d347e4a
DoKTopBaHTyc <denisoid93@gmail.com>
2025-02-11 09:55:43 +0300

    Не справился с запятой в последнем задании(

--- padawans.js
+++ padawans.js
@@ -1,7 +1,36 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return padawans.trim().split(EOL);
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+  return scores.split(EOL).map((el) => Number(el));
+}
+
+function getStats() {
+  const newArray = [];
+  for (let i = 0; i < getPadawanNames().length; i++) {
+    newArray.push([getPadawanNames()[i], getLightsaberScores()[i]]);
+  }
+  return newArray;
+}
+
+function writeStats() {
+  const stats = getStats();
+  fs.writeFileSync(
+    'data/stats.txt',
+    stats.join(EOL),
+    'utf8',
+  );
+}
