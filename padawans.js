ba4c4506b11759081a22f9bf4f01694d264c5a22
mukhinaa89 <mukhinaa89@yandex.ru>
2025-03-04 08:02:33 +0100

    test1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
+
+function getPadawanNames() {
+  const filePath = path.join(__dirname, 'data', 'padawans.txt');
+
+  if (!fs.existsSync(filePath)) {
+    console.log('Файл не найден');
+    return [];
+  }
+
+  const data = fs.readFileSync(filePath, 'utf8');
+
+  const lines = data.split('\n');
+  const names = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    const name = lines[i].trim();
+    if (name !== '') {
+      names.push(name);
+    }
+  }
+
+  return names;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
