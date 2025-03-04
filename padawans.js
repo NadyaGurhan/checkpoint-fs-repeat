39b3f3db5f41b9d9037b4de8751d5f101808621b
annabett <ehhe@yandex.ru>
2025-03-04 07:56:11 +0100

    done

--- padawans.js
+++ padawans.js
@@ -1,4 +1,37 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+}
+getPadawanNames();
+
+function getLightsaberScores() {
+  const data = fs
+    .readFileSync('./data/scores.txt', 'utf-8')
+    .split('\n')
+    .map((el) => Number(el));
+  return data;
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const stat = [];
+  for (let i=0; i<padawans.length; i++){
+    stat.push([padawans[i], scores[i]])
+  }
+  // console.log(stat);
+  return stat;
+}
+getStats()
+
+function writeStats() {
+const data = getStats().join('\n').split(',').join(' ');
+console.log(data);
+fs.writeFileSync('./data/stats.txt', data)
+}
+writeStats()
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
