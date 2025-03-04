787b83bf8f71a7f0c219689e54173408097b5ad8
wellgetsu <sevryukov1995@inbox.ru>
2025-03-04 09:58:36 +0300

    release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,13 @@
-const fs = require('fs')
+const fs = require('fs');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const padawan = fs.readFileSync('data/padawans.txt', 'utf-8');
+  const padawanNames = padawan.split('\n');
+  return padawanNames;
+}
