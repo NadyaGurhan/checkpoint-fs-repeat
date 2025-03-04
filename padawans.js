83766c621c61bf471f26b4c1891e2532f85d7056
Влад Смотров <smotrov20001420@gmail.com>
2025-03-04 09:59:51 +0300

    .

--- padawans.js
+++ padawans.js
@@ -1,4 +1,22 @@
 const fs = require('fs')
+function getPadawanNames(){
+  return fs.readFileSync("data/padawans.txt", "utf-8").trim().split('\n')
+}
+
+function getLightsaberScores(){
+  return fs.readFileSync("data/scores.txt", "utf-8").trim().split('')
+}
+
+function getStats(){
+  return fs.readFileSync("data/padawans.txt", "utf-8").trim().split('')
+}
+
+function writeStats(){
+  return fs.readFileSync("data/padawans.txt", "utf-8").trim().split('')
+}
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
