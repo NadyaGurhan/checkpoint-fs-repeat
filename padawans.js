664d368a1478f777fbd66569b0cc6ee16944a93d
zadorin <k.o.zadorin@gmail.com>
2025-03-04 09:57:56 +0300

    Zadorin Kostya

--- padawans.js
+++ padawans.js
@@ -1,4 +1,20 @@
 const fs = require('fs')
+function getPadawanNames (){
+  const text = fs.readFileSync('./data/padawans.txt', 'utf-8').trim().split('\r\n')
+  console.log(text)
+  return text
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').trim().split('\r\n')
+  console.log(scores)
+  return scores
+}
+
+function getStats() {
+  const table = 
+}
+getPadawanNames()
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
