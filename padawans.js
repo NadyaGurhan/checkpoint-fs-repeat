beec8a7114ad07239115d584f17c304c973084f8
Tihon <Ltihonol@yandex.ru>
2025-03-04 09:55:57 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,15 @@
 const fs = require('fs')
+const { EOL } = require('os');
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8').split(EOL)
+
+function getPadawanNames() {
+  return padawans
+}
+
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
