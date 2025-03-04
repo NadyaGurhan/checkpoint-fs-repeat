0c5b25a904ff32d31130a66e1da70b1ad1c58940
IvanAgafon <ivanagafon95@gmail.com>
2025-03-04 09:55:44 +0300

    :(

--- padawans.js
+++ padawans.js
@@ -1,7 +1,19 @@
+const { EOL } = require('os');
 const fs = require('fs')
+const data = fs.readFileSync('./data/padawans.txt', 'utf-8')
+const dataArr = data.split(EOL)
+console.log(dataArr)
+
+function getPadawanNames() {
+  let padawansArr = [] 
+  padawansArr.push(data)
+  return padawansArr
+}
+// console.log(getPadawanNames())
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
