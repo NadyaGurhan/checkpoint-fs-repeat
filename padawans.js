ec1b6fda522c3fedb6da3c7e7cad58357a9995c2
TatianaVoznichik <kim_t.a@bk.ru>
2025-03-04 14:00:03 +0700

    release test

--- padawans.js
+++ padawans.js
@@ -1,7 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  fs.readFileSync('../data/padawans.txt', 'utf-8');
+}
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  fs.readFileSync('../data/scores.txt', 'utf8');
+}
+console.log(getLightsaberScores());
+
+function getStats() {
+  const first = getPadawanNames()
+  const second = getLightsaberScores()
+  fs.writeFileSync('../data/stats.txt', first, second)
+}
+
+function writeStats() {
+  fs.appendFileSync ('./data/stats.txt')
+}
+console.log(writeStats())
