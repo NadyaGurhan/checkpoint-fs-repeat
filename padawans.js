374d406722b297914f738e3fd6ee3fb1b068a21b
Erdnya23 <erdnyabastaev@mail.ru>
2025-03-24 15:15:05 +0300

    test

--- padawans.js
+++ padawans.js
@@ -1,4 +1,19 @@
-const fs = require('fs')
+const fs = require('fs');
+const { console } = require('inspector');
+
+
+function getPadawanNames(){
+  const files = fs.readdirSync('./data');
+  const padawansFile = files.find (name=>name === 'padawans.txt');
+  const fileData = fs.readFileSync(`./data/${padawansFile}`, 'utf-8');
+  const name = fileData
+  .split('\n')
+  .map(name=>name.trim())
+  .filter(Boolean)
+
+  return name
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
