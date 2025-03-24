ebf0d1672c3509df66cbb2723c4fcdd18c89a596
alx-almkn <alimkin1990@gmail.com>
2025-03-24 15:06:37 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,29 @@
-const fs = require('fs')
+const fs = require('fs')    
+
+function getPadawanNames(){
+  const data = fs.readFileSync('data/padawans.txt', 'utf8');
+  return data.split('\n')
+  .map(name => name.trim())
+  .filter(Boolean);
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('data/scores.txt', 'utf8')
+    .split('\n')
+    .map(Number) 
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  return names.map((name, index) => [name, scores[index]]);
+}
+
+function writeStats(stats) {
+  const data = stats.map(([name, score]) => `${name} ${score}`).join('\n');
+  fs.writeFileSync('data/stats.txt', data, 'utf8');
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
