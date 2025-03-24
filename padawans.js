1d2b50b8879494b0f653b1be6a51a6addebaa5f9
Влад Смотров <smotrov20001420@gmail.com>
2025-03-24 14:53:46 +0300

    padawans.js/test-done

--- padawans.js
+++ padawans.js
@@ -1,18 +1,27 @@
 const fs = require('fs')
+const { EOL } = require('os');
+ 
 function getPadawanNames(){
-  return fs.readFileSync("data/padawans.txt", "utf-8").trim().split('\n')
+  return fs.readFileSync("data/padawans.txt", "utf-8").trim().split(`${EOL}`).map(name => name.trim())
 }
 
 function getLightsaberScores(){
-  return fs.readFileSync("data/scores.txt", "utf-8").trim().split('')
+  return fs.readFileSync("data/scores.txt", "utf-8").trim().split('\n').map(Number)
 }
 
 function getStats(){
-  return fs.readFileSync("data/padawans.txt", "utf-8").trim().split('')
+const names = getPadawanNames();
+const scores = getLightsaberScores();
+
+console.log("Names", names);
+console.log("Scores", scores);
+
+return names.map((name, index) => [name, scores[index]])
 }
 
-function writeStats(){
-  return fs.readFileSync("data/padawans.txt", "utf-8").trim().split('')
+function writeStats(stats){
+  const data = stats.map(([name, score]) => `${name} ${score}`).join("\n")
+  fs.writeFileSync("data/stats.txt", data, "utf-8")
 }
 
 
