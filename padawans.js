853b3eabdbfe158ecdf4a1f74ab5c8b03f5ea539
YanaKor0505 <yyanchik@inbox.ru>
2025-03-24 15:24:59 +0300

    checkpoint-1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,19 @@
 const fs = require('fs')
+const { EOL } = require('os');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  
+  
+   return names.trim().split('\n') 
+}
+
+console.log(getPadawanNames())
