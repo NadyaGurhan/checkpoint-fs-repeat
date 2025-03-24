d58c282f9a62f8f4f45f3243b7121819ad9b08bf
GlebUrv <punkhoygleb@mail.ru>
2025-03-24 17:07:33 +0500

    release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,37 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const nameRead = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return nameRead.split(`${EOL}`).filter(Boolean);
+}
+
+function getLightsaberScores() {
+  const scoreRead = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const resultArray = scoreRead.split(`${EOL}`);
+  return resultArray.map((el) => Number(el));
+}
+
+function getStats() {
+  const result = [];
+  const nameArr = getPadawanNames();
+  const scoreArr = getLightsaberScores();
+  for (let i = 0; i < nameArr.length; i++) {
+    result.push([nameArr[i], scoreArr[i]]);
+  }
+  return result;
+}
+
+function writeStats() {
+  const stats = getStats();
+  const res1 = [];
+  for (const i of stats) {
+    res1.push(i.join(' '));
+  }
+  const result = res1.join('\n');
+  fs.writeFileSync('./data/stats.txt', `${result}`);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
