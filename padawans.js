bfc42b9459c0565f8fa67f40cb1f19b58ba9433f
YanaKor0505 <yyanchik@inbox.ru>
2025-03-24 16:22:25 +0300

    checkpoint-1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 const { EOL } = require('os');
 
 module.exports = {
@@ -8,12 +8,28 @@ module.exports = {
   writeStats,
 };
 
-
 function getPadawanNames() {
-  const names = fs.readFileSync('./data/padawans.txt', 'utf-8')
-  
-  
-   return names.trim().split('\n') 
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+
+  return names.trim().split('\n');
 }
 
-console.log(getPadawanNames())
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+  return scores.split('\n').map((el) => +el);
+}
+
+console.log(getLightsaberScores());
+
+function getStats() {
+  const newArr = [];
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+
+  names.forEach((name, i) => newArr.push([name, scores[i]]));
+  return newArr;
+}
+console.log(getStats());
