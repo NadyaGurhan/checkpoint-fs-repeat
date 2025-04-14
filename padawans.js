0dd4059c7b87934058a780f2b30daf2d8d3167d4
AlexTskhe <tskhealexander@gmail.com>
2025-04-14 15:08:38 +0300

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -5,3 +5,25 @@ module.exports = {
   getStats,
   writeStats,
 };
+
+const padawans = console.log(fs.readFileSync('./data/padawans.txt', 'utf-8'))
+function getPadawanNames () {
+  padawans.join
+}
+
+getPadawanNames()
+
+const evaluation = console.log(fs.readFileSync('./data/scores.txt', 'utf-8'))
+function getLightsaberScores () {
+  evaluation.join
+}
+
+getLightsaberScores()
+
+function getStats () {
+  table.push({getPadawanNames : getLightsaberScores})
+}
+
+getStats()
+
+
