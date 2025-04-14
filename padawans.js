1961b2e689b46359aff89989ad90db1acdf33adc
7Eighty <igrovoi.ac@yandex.ru>
2025-04-14 14:58:56 +0300

    MondayCheck

--- padawans.js
+++ padawans.js
@@ -1,7 +1,20 @@
-const fs = require('fs')
+const fs = require('fs');
+const { get } = require('http');
+
+function getPadawanNames(){
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split(', ').map(name => name.trim())
+}
+
+function getLightsaberScores(){
+  return fs.readFileSync('./data/scores.txt', 'utf-8').split(', ').map(name => name.trim())
+}
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
