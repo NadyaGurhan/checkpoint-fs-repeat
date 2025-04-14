c334f573bae517e91f479dad956d25b3995f634a
DenisSadikov <juniorletto03092001@gmail.com>
2025-04-14 15:03:12 +0300

    Sadikov

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+
+function getPadawanNames() {
+  return (fs.readFileSync('./data/padawans.txt', 'utf-8')).split(/\r?\n/);
+}
+
+function getLightsaberScores() {
+  return (fs.readFileSync('./data/scores.txt', 'utf-8')).split(/\r?\n/);
+}
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+function getStats() {
+  const arrPadawans = [];
+  const arrScores = [];
+  arrPadawans.push(padawans)
+  arrScores.push(scores)
+  
+  console.log(arrPadawans);
+  console.log(arrScores);
+}
+
+console.log(getStats())
