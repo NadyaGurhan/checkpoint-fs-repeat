7d346671977cd5892f9ac7808b94e21ac5b4e854
nick123554 <kolya21@mail.ru>
2025-04-14 18:50:01 +0300

    fail

--- padawans.js
+++ padawans.js
@@ -1,7 +1,26 @@
 const fs = require('fs')
+const {EOL} = require('os')
+
+
+
+function getPadawanNames() {
+  const data = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return data.split(EOL)
+    .map(name => name.trim())
+    .filter(name => name.length > 0);
+}
+
+console.log(getPadawanNames())
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
