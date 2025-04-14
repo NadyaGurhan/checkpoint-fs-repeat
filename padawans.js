d739fa5f7c8ec8a24e6954c66780e33e235afe81
IvanKrestonoshin <mrkrestpavuk@gmail.com>
2025-04-14 16:58:25 +0500

    StarWars

--- padawans.js
+++ padawans.js
@@ -1,7 +1,32 @@
 const fs = require('fs')
-module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
-};
+const { EOL } = require('os')
+// module.exports = {
+//   getPadawanNames,
+//   getLightsaberScores,
+//   getStats,
+//   writeStats,
+// };
+
+const names = fs.readFileSync('./data/padawans.txt', 'utf-8')
+
+function getPadawanNames () {
+  let arrNames = names.split(EOL)
+  return arrNames
+}
+
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8')
+
+function getLightsaberScores () {
+    let arrScores = scores.split(EOL)
+    return arrScores
+}
+// console.log(getLightsaberScores())
+
+function getStats () {
+  let total = []
+  total.push(getPadawanNames())
+  total.push(getLightsaberScores())
+  return total
+  //здесь необходим цикл, который я не успел рализовать
+}
+console.log(getStats())
