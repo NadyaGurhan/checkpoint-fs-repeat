46501206bdf56e7c969a39d2019ab19c9fd86741
Savenen <nikitasavonin@mail.ru>
2025-04-14 16:59:36 +0500

    checkpoint-fs

--- padawans.js
+++ padawans.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const padawans = fs.readFileSync('.data/padawans.txt', 'utf-8');
+  return padawans
+};
+getPadawanNames()
