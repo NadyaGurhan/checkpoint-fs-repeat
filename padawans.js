7f94e00a986ea62971e0c5d3365a7892c36dcc1a
Denis-Araev <araevd@icloud.com>
2025-04-15 10:02:06 +0300

    не успел проверить

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+function getPadawanNames () {
+  const data = fs.readFileSync('./data/padawans.txt', 'utf-8')
+return data.split(EOL);
+}
+// console.log(getPadawanNames())
+
+function getLightsaberScores () {
+  const data = fs.readFileSync('./data/scores.txt', 'utf-8')
+  return data.split(EOL).map(Number);
+}
+
+
