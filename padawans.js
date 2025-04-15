49a325cacc11d3299e3872ec8f5d2be3589e5b54
IvaTAT <ivan208@mail.ru>
2025-04-15 10:06:04 +0300

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,35 @@
-const fs = require('fs')
+const fs = require('fs');
+const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+
+function getPadawanNames() {
+  return padawanNames.filter((el) => el !== '');
+}
+console.log(getPadawanNames());
+
+function getLightsaberScores() {
+  return scores.map((el) => Number(el));
+}
+console.log(getLightsaberScores());
+
+function getStats() {
+  const count = [];
+  for (let i = 0; i < scores.length; i++) {
+    count.push([padawanNames[i], Number(scores[i])]);
+  }
+  return count;
+}
+console.log(getStats());
+
+function writeStats() {
+  const count = [];
+  for (let i = 0; i < scores.length; i++) {
+    count.push([padawanNames[i], scores[i]]).join(' ');
+  }
+  return count.join('\n');
+}
+console.log(writeStats());
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
