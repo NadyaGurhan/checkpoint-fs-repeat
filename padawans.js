cb4dc78f1d13503f47d048aa12ca048e5e83bb71
Alena <kirlena95@gmail.com>
2025-04-15 10:03:57 +0300

    bad release

--- padawans.js
+++ padawans.js
@@ -1,7 +1,20 @@
-const fs = require('fs')
+const fs = require('fs');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames(arr) {
+  const padavans = fs.readFile('padawans.txt', 'utf-8')
+  padavans.split(' ')
+}
+console.log(getPadawanNames(arr))
+
+
+
+
+function writeStats(arrName) {
+  fs.writeFileSync(`./data/ stats-names.txt`, arrName.join('\n'));
+}
