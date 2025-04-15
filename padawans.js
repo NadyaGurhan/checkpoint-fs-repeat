40e7d8614fc2725f5066259fafa19cdb48260b90
Kamil Gadzhiev <kamilgadzhiev2001@icloud.com>
2025-04-15 10:05:14 +0300

    rqer

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os')
+const { toNamespacedPath } = require('path');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('data/padawans.txt')
+  return names.toString().split(EOL)
+}
+console.log(getPadawanNames())
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+ // getLightsaberScores,
+  //getStats,
+  //writeStats,
 };
