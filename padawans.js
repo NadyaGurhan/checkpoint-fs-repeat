de1444356f7af432dbf755b9e8e24904eb6bd220
mlshvaa <malyshevaa01@mail.ru>
2025-04-15 10:00:14 +0300

    Релиз 0: ничего не получилось :((

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  function getPadawanNames() {
+    const padawansNames = fs.readFileSync('./data/padawans.txt', 'utf-8');
+    return names = padawansNames.map((name) => name.trim()).split(EOL);
+  };
+
+  function getLightsaberScores() {
+    const padawansScores = fs.readFileSync('./data/scores.txt', 'utf-8');
+    return scores = padawansScores.map((score) => score.trim()).split(EOL);
+  };
+
+  function getStats() {
+    const padawans = getPadawanNames();
+    const scores = getLightsaberScores();
+    const padawansScores = [];
+
+    for (let i = 0; i < padawans.length; i++) {
+    padawansScores.push([padawans[i], scores[i]])
+    };
+    
+    return padawansScores;
+  };
+
+  function writeStats() {
+
+  };
 };
