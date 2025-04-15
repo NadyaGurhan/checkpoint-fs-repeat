bd7f00b0ac92e6d7c12a682e570e99345de362a2
UNCLT <drdizel@mail.ru>
2025-04-15 09:49:46 +0300

    Checkpoint-FS-Tuesday-PH1-W1 - Ready

--- padawans.js
+++ padawans.js
@@ -1,4 +1,32 @@
-const fs = require('fs')
+const fs = require('node:fs');
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf8').split('\n');
+  return scores.map((elem) => Number(elem));
+}
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf8').split('\n');
+  return names.filter((elem) => elem !== '');
+}
+
+function getStats() {
+  const result = [];
+  for (let i = 0; i < getPadawanNames().length; i += 1) {
+    result.push([getPadawanNames()[i], getLightsaberScores()[i]]);
+  }
+  return result;
+}
+
+function writeStats() {
+  fs.writeFileSync(
+    './data/stats.txt',
+    getStats()
+      .map((elem) => elem.join(' '))
+      .join('\n'),
+  );
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
