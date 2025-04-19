d45abb8d2b0c528294d7917fcb24c298bfcdc815
IvaTAT <ivan208@mail.ru>
2025-04-19 13:21:26 +0300

    My honwwork weekend

--- padawans.js
+++ padawans.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const padawanNames = fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
 const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n');
+// console.log(padawanNames);
+// console.log(scores);
 
 function getPadawanNames() {
   return padawanNames.filter((el) => el !== '');
 }
-console.log(getPadawanNames());
+// console.log(getPadawanNames());
 
 function getLightsaberScores() {
   return scores.map((el) => Number(el));
 }
-console.log(getLightsaberScores());
+// console.log(getLightsaberScores());
 
 function getStats() {
   const count = [];
@@ -19,16 +21,17 @@ function getStats() {
   }
   return count;
 }
-console.log(getStats());
+// console.log(getStats());
 
 function writeStats() {
   const count = [];
   for (let i = 0; i < scores.length; i++) {
-    count.push([padawanNames[i], scores[i]]).join(' ');
+    count.push([padawanNames[i], scores[i]].join(' '));
   }
-  return count.join('\n');
+  fs.writeFileSync('data/stats.txt', count.join('\n'));
+  // return count.join('\n');
 }
-console.log(writeStats());
+// console.log(writeStats());
 
 module.exports = {
   getPadawanNames,
