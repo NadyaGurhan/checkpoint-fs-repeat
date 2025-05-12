7cbc6829ecf4210c77c4de7265ff51b2ebe4dc18
MrZurkov <kad1261993@gmail.com>
2025-05-12 14:59:37 -0700

    checkpoint_1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,14 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+function getPadawanNames() {
+  const file = fs.readFileSync('./data/padawans.txt', "utf-8")
+  const result = file.trim().split('\r\n')
+  return result
+}
