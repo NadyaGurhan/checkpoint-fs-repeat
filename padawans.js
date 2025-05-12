bee6838d9cbdd358aa5569a794a965e11e81ab66
Artur-oa <arturogan@yandex.ru>
2025-05-12 20:31:26 +0300

    tests 1-3  done

--- padawans.js
+++ padawans.js
@@ -1,16 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
 
-function  getPadawanNames() {
-  const data = fs.readFileSync('./data/padawans.txt', "utf-8")
-  const result = data.trim().split('\r\n')
+function getPadawanNames() {
+  const data = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const result = data.trim().split('\r\n');
   return result;
-  
 }
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const result = data.trim().split('\r\n').map(Number);
+  return result;
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push([names[i], scores[i]]);
+  }
+  return result;
+}
+
 
 module.exports = {
   getPadawanNames,
-  // getLightsaberScores,
-  // getStats,
+  getLightsaberScores,
+  getStats,
   // writeStats,
 };
-
