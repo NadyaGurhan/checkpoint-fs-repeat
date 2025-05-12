618b0efaa6b9b711afb523f3804d4d908db6e963
bolatchiev <bolatchiev.azamat@bk.ru>
2025-05-12 14:47:18 +0300

    p1w1d1Checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf8');
+  return data.split(EOL).filter((name) => name.trim() !== '');
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf-8');
+  return data.split(EOL).filter((score) => score.trim() !== '').map(Number);
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  return names.map((name, index) => [name, scores[index]]);
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
