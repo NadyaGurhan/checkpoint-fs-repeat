6b8032b71b888530a0feb8ff77c7542feeae634f
medvtanya <medvtanya13@gmail.com>
2025-05-12 14:59:42 +0300

    checkpoint-FS

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
 const fs = require('fs')
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8').trim()
+  const names = data.split('\r\n')
+  return names
+}
+
+function getLightsaberScores(){
+  const data = fs.readFileSync('data/scores.txt', 'utf-8').trim()
+  const scores = data.split('\r\n')
+  const names = scores.map(Number)
+  return names
+}
+
+function getStats (){
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8').
+
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
