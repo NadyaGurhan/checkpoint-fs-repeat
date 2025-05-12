03ece902c4d93409ea190d760d256536f485a215
kvashniVD <vdkvashnin74@gmail.com>
2025-05-12 16:59:40 +0500

    Checkpoint-fs

--- padawans.js
+++ padawans.js
@@ -1,4 +1,35 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const order = fs.readFileSync('./data/padawans.txt', 'utf-8')
+  return order.split(EOL)
+}
+
+
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split(EOL).map(Number)
+  return scores
+}
+
+
+function getStats() {
+  const order = fs.readFileSync('./data/padawans.txt', 'utf-8').split(EOL)
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split(EOL).map(Number)
+ 
+ 
+
+
+}
+
+
+function writeStats() {
+
+}
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
