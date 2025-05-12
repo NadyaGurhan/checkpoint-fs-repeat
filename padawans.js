73afa262e9834bc577a1b8d7f0cdb7b939a0d5e0
Artur-oa <arturogan@yandex.ru>
2025-05-12 14:59:32 +0300

    test1 - done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
 const fs = require('fs')
+
+function  getPadawanNames() {
+  const data = fs.readFileSync('./data/padawans.txt', "utf-8")
+  const result = data.trim().split('\r\n')
+  return result;
+  
+}
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
+
