0b512173e07a6d8dfa47f76ec5ada7a307671341
Savenen <nikitasavonin@mail.ru>
2025-05-12 17:01:32 +0500

    Всё плохо

--- padawans.js
+++ padawans.js
@@ -1,7 +1,31 @@
 const fs = require('fs')
+
+let pNames = [];
+
+function getPadawanNames() { 
+  // pNames.push(fs.readFileSync('./data/padawans.txt', 'utf-8'));
+  // pNames.join(", ")
+  // result = [...pNames]
+  // return result                                           //не работает
+
+  const result = [fs.readFileSync('./data/padawans.txt', 'utf-8')]; //не работает
+  return result
+}
+
+function getLightsaberScores() { 
+  let res = fs.readFileSync('./data/scores.txt', 'utf-8');
+  let res1 = res.slice()
+  return res1
+}
+
+function getStats() { }
+
+function writeStats() { }
+  
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+console.log(pNames);
