88db14e428eceed620b644f29b99dad8d1428de8
alexxeyich <alexey.m1khailov@yandex.ru>
2025-05-12 15:00:22 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,4 +1,41 @@
-const fs = require('fs')
+// Что-то ооочень тяжело...
+
+const fs = require('node:fs');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8');
+
+  return data.trim().split('\n');
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf-8');
+
+  const result = [];
+  data.trim().split('\n').forEach(item => {
+    result.push(Number(item));
+  })
+
+  return result; 
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+
+  const result = [];
+  for (let i = 0; i < padawans.length; i ++) {
+    result.push([padawans[i], scores[i]])
+  }
+
+  return result;
+}
+
+function writeStats() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8');
+
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
