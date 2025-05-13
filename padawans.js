066e9ec5c15200ec4ff4e8b3c021330e881bb4d0
Eonian-boop <eoniancake@gmail.com>
2025-05-13 09:57:33 +0300

    release 1

--- padawans.js
+++ padawans.js
@@ -1,4 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
+const padawansLines = fs.readFileSync('./data/padawans.txt', 'utf8');
+const scoresLines = fs.readFileSync('./data/scores.txt', 'utf8');
+
+function getPadawanNames() {
+  const padawansNames = padawansLines.split('\n');
+  return padawansNames.slice(0, padawansNames.length - 1);
+}
+
+function getLightsaberScores() {
+  const saberScores = scoresLines.split('\n');
+  return saberScores.map((number) => Number(number));
+}
+
+function getStats() {
+  const stats = [getPadawanNames()[0], getLightsaberScores()[0]];
+  return stats;
+}
+
+console.log(getStats());
+
+function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
