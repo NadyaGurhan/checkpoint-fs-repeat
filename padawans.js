932532767d48fdb044e7c84463ec9e1d28bf9a16
geym-eugene <geym.eugene@gmail.com>
2025-05-13 09:57:43 +0300

    add functions

--- padawans.js
+++ padawans.js
@@ -1,7 +1,42 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
+
+const newPadawans = padawans.split('\n');
+const newScores = scores.split('\n');
+
+function getPadawanNames() {
+  return newPadawans;
+}
+
+function getLightsaberScores() {
+  const numbersScores = [];
+  for (let i = 0; i < newScores.length; i++) {
+    numbersScores.push(+newScores[i])
+  }
+  return numbersScores;
+}
+
+// function getStats() {
+//   const newArr = [];
+
+//   for (let i = 0; i < newPadawans.length; i++) {
+
+//   }
+//   console.log(newArr);
+// }
+
+// getStats();
+
+// function writeStats() {
+
+// }
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
