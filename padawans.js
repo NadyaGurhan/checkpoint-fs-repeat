d1b9576951c95e06b98dff42c8ad6ac78e33edda
MaxPod98 <ma.podlipnov@yandex.ru>
2025-06-02 15:17:58 +0300

    My checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return names.split(EOL).filter(el => el.trim())
+}
+
+function getLightsaberScores() {
+  const numbers = fs.readFileSync('./data/scores.txt', 'utf-8');
+  const numArray = numbers.split(EOL).map((el) => Number(el));
+  return numArray;
+}
+
+function getStats() {
+
+}
+
+function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
