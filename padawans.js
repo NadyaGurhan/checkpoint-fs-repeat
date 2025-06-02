b0d35dcf469b35754e8dd3677a4d889a81a464cf
Saveliy <borisov.saveliy@mail.ru>
2025-06-02 19:22:06 +0700

    Checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
+const os = require('os');
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split(os.EOL).filter(string => string.trim() !== '');
+}
+
+function getLightsaberScores() {
+  return fs.readFileSync('./data/scores.txt', 'utf-8').split(os.EOL).map(score => Number(score))
+}
+
+function getStats() {
+  const names = getPadawanNames()
+  const score = getLightsaberScores()
+  
+  return names.map((name, index) => [name, score[index]])
+}
+
+function writeStats() {
+  const stats = getStats();
+  
+  return fs.writeFileSync('./data/stats.txt', stats)
+  // не сделал
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
