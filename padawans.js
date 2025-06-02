056d399b65995891c2f3a99f925240000bab0f5b
Gans-Shtraus <pugachev.2013@yandex.ru>
2025-06-02 15:17:32 +0300

    Pugachev

--- padawans.js
+++ padawans.js
@@ -1,4 +1,22 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf-8');
+  return data.split(EOL).filter((name) => name.trim());
+}
+function getLightsaberScores() {
+  const nums = fs.readFileSync('data/scores.txt', 'utf-8');
+  return nums.split(EOL).map((el) => Number(el));
+}
+function getStats() {
+const newAr = [];
+const name = fs.readFileSync('data/padawans.txt', 'utf-8');
+const nums = fs.readFileSync('data/scores.txt', 'utf-8');
+
+}
+function writeStats() {}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
