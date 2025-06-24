f7c0993e91e9a2567213cf1e35b3ba07ed919510
Lena4e <lechertova3.14@gmail.com>
2025-06-24 10:08:55 +0300

    CheckPoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,23 @@
 const fs = require('fs')
+
+function getPadawanNames(){
+  const padavans = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  return padavans.split('\n').pop().
+}
+console.log(getPadawanNames())
+
+
+function getLightsaberScores(){
+const reiting = fs.readFileSync('./data/scores.txt', 'utf-8');
+return reiting.split('\n')
+}
+console.log(getLightsaberScores())
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
+
