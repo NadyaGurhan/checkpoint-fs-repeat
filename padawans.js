7fb86e131d83e99e8f93c5b4a1343d15ee3a468f
Lusine1991 <M.L.R.777@yandex.ru>
2025-06-24 10:09:37 +0300

    test

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
 const fs = require('fs')
+function getPadawanNames(){
+  const pad = fs.readFileSync('./data/padawans.txt','utf-8');
+  let result = pad.split('\n')
+  let pads = result.filter((item) => item.length>0);
+  return result
+  // console.log(result)
+}
+getPadawanNames()
+
+function getLightsaberScores(){
+  const gets = fs.readFileSync('./data/scores.txt','utf-8');
+let paw = gets.split('\n')
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
-  writeStats,
+  // getStats,
+  // writeStats,
 };
+
