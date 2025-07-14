be29429fa0727777bae6bb6a12503e316b29f54e
giorgio0007 <giorgio0007@mail.ru>
2025-07-14 15:05:22 +0300

    Не успел(

--- padawans.js
+++ padawans.js
@@ -1,4 +1,43 @@
 const fs = require('fs')
+
+const { EOL } = require('os');
+
+const dataPadawans = fs.readFileSync('./data/padawans.txt', 'utf8')
+const arrayPadawans = dataPadawans.split(EOL)
+
+const dataScores = fs.readFileSync('./data/scores.txt', 'utf8')
+const arrayScores = dataScores.split(EOL).map(el => +el)
+
+
+function getPadawanNames(){
+  return arrayPadawans.filter((el)=> el !== '')
+}
+
+function getLightsaberScores(){
+  return arrayScores
+}
+
+function getStats(){
+  // const arrStats = []
+  // arrayScores.forEach((el,index)=>{
+  //   arrStats.push([...el, arrayPadawans[index]])
+  // })
+  // return arrStats
+}
+
+console.log(getStats());
+
+
+
+function writeStats(){}
+
+
+
+
+
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
