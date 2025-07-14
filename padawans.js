d97914dc72f71248e7c13c461a2f1189e7a01ef5
SardanaStr <sardana.straus@gmail.com>
2025-07-14 17:26:15 +0500

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
 const fs = require('fs')
+
+
+function getPadawanNames() {
+  let padawansList = fs.readFileSync('data/padawans.txt', 'utf8')
+  return padawansList.split('\n')
+}
+
+function getLightsaberScores() {
+  let lightsaberScores = fs.readFileSync('data/scores.txt', 'utf8')
+  return lightsaberScores.split('\n')
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
