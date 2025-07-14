9dfbaa370b2ec0f690d299d6021511ecf71dca54
Abdrey05 <abdrey371@gmail.com>
2025-07-14 15:10:31 +0300

    checkpoint-fs

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
 const fs = require('fs')
+const { EOL } = require('os')
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+function padawans () {
+  const content = fs.readFileSync('./data/adawanas.txt', 'utf-8')
+  return content.split()
+ 
+}
+console.log(padawans())
