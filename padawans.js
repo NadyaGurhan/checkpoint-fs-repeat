df6b5ffc2985fc89c272878c6fd94e578946fbac
Алан-Мамм <mammeev98@mail.ru>
2025-07-14 15:13:02 +0300

    чекпоинт

--- padawans.js
+++ padawans.js
@@ -1,7 +1,21 @@
 const fs = require('fs')
+const { EOL } = require('os')
+
+
+// console.log(content);
+
+
+
+
+function padawansName(){
+  const content = fs.readFileSync('./data./padawans.txt', 'utf-8');
+  return content.split()
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+ 
