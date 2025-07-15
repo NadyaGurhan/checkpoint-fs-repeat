771f3f8ef08e859f1c4346c54b85fb28e709a1f2
1babii1 <sabangulov.ilnaz@mail.ru>
2025-07-15 10:02:36 +0300

    My homework

--- padawans.js
+++ padawans.js
@@ -1,4 +1,31 @@
 const fs = require('fs')
+
+function getPadawanNames() { 
+  const padawans = fs.readFileSync("./data/padawans.txt", "utf-8")
+  return padawans.split("\n")
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync("./data/scores.txt", "utf-8").split("\n")
+  const result = scores.map(i => +i)
+  return result
+}
+
+function getStats() {
+  const scor = getLightsaberScores();
+  const pad = getPadawanNames();
+  const result = [];
+  for(let i = 0; i < scor.length; i++) {
+    result.push([pad[i],scor[i]])
+  }
+  return result
+}
+
+function writeStats() {
+//   let stat = getStats();
+//   stat 
+// }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
