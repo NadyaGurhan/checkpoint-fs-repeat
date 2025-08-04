c0c8b4bdb59f7d93a49e21278f6a583cba50d868
DmitryMankevic <mdsinedesign@gmail.com>
2025-08-04 15:23:31 +0300

    P1W1D1_Dmitry_Mankevic

--- padawans.js
+++ padawans.js
@@ -1,4 +1,13 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const filePadawan = fs.readFileSync('./data/padawans.txt', 'utf-8');
+  const names = filePadawan.split('\r\n');
+  console.log(typeof filePadawan);
+  console.log(typeof names);
+  return names;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
