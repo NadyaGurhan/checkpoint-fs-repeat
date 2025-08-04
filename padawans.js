897004e3c468dbb4471ae670bd2a765a87646179
ericoshet <maister.bk.ru>
2025-08-04 15:20:32 +0300

    CheckPoint-111

--- padawans.js
+++ padawans.js
@@ -1,7 +1,18 @@
 const fs = require('fs')
 module.exports = {
-  getPadawanNames,
-  getLightsaberScores,
+
+
+  getPadawanNames(content){
+  content = fs.readFileSync('./data/padawans.txt', 'utf-8')
+      return content;
+  } 
+  getLightsaberScores(scores) {
+  scores = fs.readFileSync('./data/scores.txt', 'utf-8')
+     return scores;
+  }
+
   getStats,
   writeStats,
 };
+
+
--- spec/padawans.spec.js
+++ spec/padawans.spec.js
@@ -8,14 +8,17 @@ const {
 } = require('../padawans');
 
 describe('Статистика о падаванах', () => {
+
   it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
     const names = getPadawanNames();
     expect(names).toEqual(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']);
   });
+
   it('getLightsaberScores возвращает оценки владения световым мечом из файла `data/scores.txt`', () => {
     const names = getLightsaberScores();
     expect(names).toEqual([99.9, 92, 87, 82]);
   });
+
   it('getStats возвращает таблицу соответствия падавана и оценки владения световым мечом', () => {
     const stats = getStats();
     expect(stats).toEqual([
@@ -25,6 +28,7 @@ describe('Статистика о падаванах', () => {
       ['Juhani', 82],
     ]);
   });
+
   it('writeStats сохраняет статистику в файл `data/stats.txt`', () => {
     const stats = getStats();
     writeStats(stats);
