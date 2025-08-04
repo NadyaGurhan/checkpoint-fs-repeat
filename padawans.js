99ea7559315c6f01aae349cdd74cd7cf64ca2821
Andrey Ivanov <Workemailaai@icloud.com>
2025-08-04 15:23:01 +0300

    not done

--- padawans.js
+++ padawans.js
@@ -1,7 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
+
+
+function getPadawanNames(){
+const padawanNames = fs.readFileSync('./data/padawans.txt', "utf-8");
+const namesArr = padawanNames.split('\r\n');
+return namesArr
+}
+
