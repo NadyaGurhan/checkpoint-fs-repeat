e3d406991fe71f44bccae5fda5df126a41a0d9bb
xakep2199 <xakep2199@gmail.com>
2025-08-04 15:42:42 +0300

    1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+const { toNamespacedPath } = require('path/win32');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
 };
+
+function getPadawanNames(){
+const names = fs.readFileSync ('data/padawans.txt', 'utf-8');
+return names.split();
+}
+
+function getLightsaberScores(){
+const stats  = fs.readFileSync ('data/scores.txt', 'utf-8');
+return stats.split();
+}
+
+function getStats () {
+  const names = getPadawanNames();
+  const stats = getLightsaberScores();
+  const allStats = names.map((a, b) => [a, allStats[b]]);
+  return allStats;
+}
