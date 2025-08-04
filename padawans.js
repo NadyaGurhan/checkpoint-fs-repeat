5bece3922c8d71360abed5a68de79d743f2637f2
NadezhdaLobacheva <nadyushland@gmail.com>
2025-08-04 15:40:42 +0300

    release0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,27 @@
 const fs = require('fs')
+
+const getPadawanNames = () => {
+  const files = fs.readFileSync ('./data/padawans.txt', 'utf8');
+  const result = files.split ('\n');
+  result.pop();
+  return result;
+}
+
+// function getLightsaberScores (){
+
+// }
+
+// function getStats (){
+
+// }
+
+// function writeStats (){
+
+// }
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+  // getLightsaberScores,
+  // getStats,
+  // writeStats,
 };
