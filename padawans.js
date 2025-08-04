f947f9058e15ef01b30e276259726bb0f5a5eace
xakep2199 <xakep2199@gmail.com>
2025-08-04 21:13:31 +0300

    3 из 4

--- padawans.js
+++ padawans.js
@@ -3,22 +3,26 @@ const { toNamespacedPath } = require('path/win32');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
-  getStats,
 };
 
 function getPadawanNames(){
-const names = fs.readFileSync ('data/padawans.txt', 'utf-8');
-return names.split();
+const names = fs.readFileSync ('./data/padawans.txt', 'utf-8');
+const names1 = names.split('\r\n')
+names1.pop()
+return names1
 }
+console.log (getPadawanNames());
 
 function getLightsaberScores(){
-const stats  = fs.readFileSync ('data/scores.txt', 'utf-8');
-return stats.split();
+const stats  = fs.readFileSync ('./data/scores.txt', 'utf-8');
+return stats.split('\r\n').map((a)=> Number(a));
 }
+console.log (getLightsaberScores());
 
 function getStats () {
   const names = getPadawanNames();
   const stats = getLightsaberScores();
-  const allStats = names.map((a, b) => [a, allStats[b]]);
+  const allStats = names.map((a, b) => [a, stats[b]]);
   return allStats;
 }
+console.log (getStats());
