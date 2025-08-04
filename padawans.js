28b4e17cfe122fdd59bfc7891cb7170a746a1535
Михаил Шахнаров <mihailsahnarov@MacBook-Pro-Mihail.local>
2025-08-04 15:19:24 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,7 +1,30 @@
 const fs = require('fs')
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const data = fs.readFileSync('data/padawans.txt', 'utf8');
+  return data.split(EOL).filter(name => name.trim() !== '');
+}
+
+function getLightsaberScores() {
+  const data = fs.readFileSync('data/scores.txt', 'utf8');
+  return data.split(EOL)
+    .filter(score => score.trim() !== '')
+    .map(score => parseFloat(score));
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  return names.map((name, index) => [name, scores[index]]);
+}
+
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
+
