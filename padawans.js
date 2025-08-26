6a002e75d386eb00a2221cad0a4756938695d901
Ada <plumsiren@mail.ru>
2025-08-26 10:02:52 +0300

    release 0

--- padawans.js
+++ padawans.js
@@ -1,7 +1,17 @@
-const fs = require('fs')
+const fs = require('fs');
+
+const padawan = fs.readFileSync('./data/padawans.txt', 'utf-8')
+
+function getPadawanNames(){
+   return padawan.split('\r\n').slice(0, 4)
+}
+getPadawanNames()
+
+
+
 module.exports = {
   getPadawanNames,
-  getLightsaberScores,
-  getStats,
-  writeStats,
+//  getLightsaberScores,
+//  getStats,
+//  writeStats,
 };
