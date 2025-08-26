ab804ee7fb4b58f45876f1d4df0db81279eb9fc5
Evgeny-Den <zdenisov99@yandex.ru>
2025-08-26 09:55:46 +0300

    Запятые в последнем тесте я не победил

--- padawans.js
+++ padawans.js
@@ -1,4 +1,33 @@
-const fs = require('fs')
+const fs = require('fs');
+const EOL = require('os').EOL;
+
+function getPadawanNames() {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split('\n');
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync('./data/scores.txt', 'utf-8')
+    .split('\n')
+    .map((el) => +el);
+}
+
+function getStats() {
+  const result = [];
+  let name = getPadawanNames();
+  let num = getLightsaberScores();
+
+  for (let i = 0; i < name.length; i++) {
+    result.push([name[i], num[i]]);
+  }
+  return result;
+}
+
+function writeStats(stats) {
+  const data = stats.map((stat) => `${stat[0]} ${stat[1]}`).join(EOL);
+  return fs.writeFileSync('./data/stats.txt', data);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
