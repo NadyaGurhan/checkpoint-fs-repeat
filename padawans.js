b267c54e74ab8ee457dd598b23d0941679c5dbd2
NadezhdaLobacheva <nadyushland@gmail.com>
2025-09-15 16:00:47 +0300

    checkpoint fs

--- padawans.js
+++ padawans.js
@@ -1,7 +1,24 @@
 const fs = require('fs')
+
+function getPadawanNames(){
+  const names = fs.readFileSync('./data/padawans.txt', 'utf-8').trim();
+  return names.split('\n')
+}
+
+function getLightsaberScores (){
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split('\n').map((el) => Number(el));
+  return scores
+}
+
+function getStats (){
+  
+}
+
+// дальше не успела сделать, были проблемы с зависимостями
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  //writeStats,
 };
