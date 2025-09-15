c264d72671367c5f545fad444454985c952888d4
R4sl4n <tqe774@gmail.com>
2025-09-15 15:23:03 +0300

    release 3 functions

--- padawans.js
+++ padawans.js
@@ -1,4 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
+
+function getPadawanNames() {
+  const content = fs.readFileSync('./data/padawans.txt', 'utf8').trim();
+  const lines = content.split('\n');
+  const names = lines.map((el) => el.trim().split(',')[0]);
+  return names;
+}
+
+function getLightsaberScores() {
+  const content = fs.readFileSync('./data/scores.txt', 'utf8').trim();
+  const lines = content.split('\n');
+  const scores = lines.map((el) => parseFloat(el.trim()));
+  return scores;
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const status = names.map((el, val)=>[el, scores[val]]);
+  return status
+}
+
+function writeStats(){
+  const datas = fs.writeFileSync('./data', 'utf8').trim();
+  
+}
+
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
