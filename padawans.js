15a589bd090fdf00e4b83ebf953094e96b3e5e40
NadyaGurhan <nadi.gavina98@mail.ru>
2025-09-15 15:25:42 +0300

    3 tests are ready

--- padawans.js
+++ padawans.js
@@ -1,4 +1,43 @@
-const fs = require('fs')
+const { log } = require('console');
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  return fs
+    .readFileSync('./data/padawans.txt', 'utf8')
+    .split(EOL)
+    .filter((i) => i !== '');
+}
+
+function getLightsaberScores() {
+  return fs
+    .readFileSync('./data/scores.txt', 'utf8')
+    .split(EOL)
+    .map((i) => +i)
+    .filter((i) => i !== '');
+}
+
+function getStats() {
+  const names = getPadawanNames();
+  const scores = getLightsaberScores();
+  const stats = [];
+  for (let i = 0; i < names.length; i++) {
+    stats.push([names[i], scores[i]]);
+  }
+  return stats;
+}
+
+function writeStats(arr) {
+  let string = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    const name = arr[i][0];
+    const score = arr[i][1];
+    string += `${name} ${score}`;
+  }
+  fs.writeFileSync('./data/stats.txt', string);
+}
+// return fs.writeFileSync('./data/stats.txt', arr.join(EOL));;
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
