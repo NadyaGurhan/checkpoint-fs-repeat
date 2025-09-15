4b7da36a4e90e7d3be37749a09e0a318e935f902
Dende98 <danilopalov@gmail.com>
2025-09-15 15:19:24 +0300

    cheak point1

--- padawans.js
+++ padawans.js
@@ -1,7 +1,25 @@
-const fs = require('fs')
+const { equal } = require('assert')
+const fs =require('fs')
+const { EOL } = require('os')
+
+function getPadawanNames(){
+  const data =fs.readFileSync('data/padawans.txt')
+  return data.split(EOL).filter(line=>line.trim() !=='')
+}
+function getLightsaberScores(){
+  const data =fs.writeFileSync('data/score.txt')
+  return data.split(EOL).filter(line => line.trim() !== '').map(Number);
+
+}
+function writeStats(stats) {
+  const content = stats.map(([name, score]) => `${name} ${score}`).join(EOL) + EOL;
+  fs.writeFileSync('data/stats.txt', content, 'utf8');
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
