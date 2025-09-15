87f36ff6cddc0104ebde8d4c9b6d0668e55e8859
Konsolev-Loger <ocuperusha@gmail.com>
2025-09-15 16:08:36 +0300

    Chekpoint 45 min + 50 min)

--- padawans.js
+++ padawans.js
@@ -1,7 +1,36 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
+
+function getPadawanNames() {
+  const arrNames = fs.readFileSync('./data/padawans.txt', 'utf8').split(EOL);
+  return arrNames;
+}
+
+function getLightsaberScores() {
+  const arrScores = fs.readFileSync('./data/scores.txt', 'utf8').split(EOL);
+  const newArr = [];
+  for (let i = 0; i < arrScores.length; i++) {
+    const num = Number(arrScores[i]);
+    newArr.push(num);
+  }
+  return newArr;
+}
+
+function getStats() {
+  const arrNames = fs.readFileSync('./data/padawans.txt', 'utf8').split(EOL);
+  const arrScores = fs.readFileSync('./data/scores.txt', 'utf8').split(EOL);
+  const newArr = [];
+  for (let i = 0; i < arrNames.length; i++) {
+    const name = arrNames[i];
+    const num = Number(arrScores[i]);
+    newArr.push([name, num]);
+  }
+  return newArr;
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
-  writeStats,
+  // writeStats,
 };
