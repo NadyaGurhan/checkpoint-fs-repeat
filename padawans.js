3bf36b074002c98463b6ace08a028c98e1221043
smska <smskkaa@gmail.com>
2025-09-15 15:20:19 +0300

    3/4

--- padawans.js
+++ padawans.js
@@ -1,7 +1,38 @@
-const fs = require('fs')
+const fs = require('fs');
+const { EOL } = require('os');
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
   getStats,
   writeStats,
 };
+
+
+function getPadawanNames()  {
+  return fs.readFileSync('./data/padawans.txt', 'utf-8').split(EOL)
+}
+
+function getLightsaberScores()  {
+  return fs.readFileSync('./data/scores.txt', 'utf-8')
+  .split(EOL)
+  .map((el) => Number(el))
+}
+
+function getStats() {
+  const padawans = getPadawanNames();
+  const scores = getLightsaberScores();
+  const merged = [];
+  const maxLength = Math.max(padawans.length, scores.length);
+  for (let i = 0; i < maxLength; i++) {
+    const res = []
+    if (i < padawans.length) res.push(padawans[i]);
+    if (i < scores.length) res.push(scores[i]);
+    merged.push(res)
+  }
+  return merged;
+}
+
+function writeStats() {
+  const data = JSON.stringify(getStats());
+  return fs.writeFileSync('./data/stats.txt', data, 'utf-8')
+}
