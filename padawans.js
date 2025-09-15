a57cea2b17dc54fed2692828ef37fc42b555b93e
ericoshet <maister.bk.ru>
2025-09-15 15:29:28 +0300

    fs - 111

--- padawans.js
+++ padawans.js
@@ -1,4 +1,32 @@
 const fs = require('fs')
+const { EOL } = require('os')
+
+function getPadawanNames(){
+  const list = fs.readFileSync(`data/padawans.txt`, 'utf-8').trim();
+  return list.split(EOL);
+}
+
+function getLightsaberScores() {
+  const scores = fs.readFileSync('./data/scores.txt', 'utf-8').split(EOL).map((el) => Number(el));
+  return scores;
+}
+
+function getStats() {
+  const list = getPadawanNames();
+  const scores = getLightsaberScores();
+  const arrStats = [];
+  for (let i = 0; i < list.length; i++) {
+    arrStats.push([list[i], scores[i]]);
+  }
+  return arrStats;
+}
+
+function writeStats() {
+  const arrStats = getStats();
+  const newData = arrStats.join('\n').split(',').join(' ');
+  return fs.writeFileSync('./data/stats.txt', newData);
+}
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
